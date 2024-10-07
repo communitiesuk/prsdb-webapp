5be03129f43713ca87b90f9ccf31cc0783459718
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-10-07 14:04:05 +0100

    PRSD-NONE: Adds integration test set-up

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/IntegrationTest.kt
@@ -0,0 +1,33 @@
+package uk.gov.communities.prsdb.webapp.integration
+
+import com.microsoft.playwright.junit.UsePlaywright
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.context.annotation.Import
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory
+import uk.gov.communities.prsdb.webapp.TestcontainersConfiguration
+import uk.gov.communities.prsdb.webapp.config.OneLoginConfig
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@UsePlaywright
+abstract class IntegrationTest {
+    @LocalServerPort
+    val port: Int = 0
+
+    @MockBean
+    lateinit var mockClientRegistrationRepository: ClientRegistrationRepository
+
+    @MockBean
+    lateinit var mockDefaultAuthorizationCodeTokenResponseClient: DefaultAuthorizationCodeTokenResponseClient
+
+    @MockBean
+    lateinit var jwtDecoderFactory: JwtDecoderFactory<ClientRegistration?>
+
+    @MockBean
+    lateinit var oneLoginConfig: OneLoginConfig
+}
