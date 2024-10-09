70db082a7a01c11aff5117cf410183e987ddc955
JasCon <jasmin.conterio@softwire.com>
2024-10-09 15:47:27 +0100

    PRSD-358: Get list of users from database for a given given local authority id

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
@@ -1,20 +1,28 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import uk.gov.communities.prsdb.webapp.constants.SERVICE_NAME
+import uk.gov.communities.prsdb.webapp.services.ManageLocalAuthorityUserService
 
 // @PreAuthorize("hasRole('LA_ADMIN')")
 @Controller
 @RequestMapping("/manage-users")
-class ManageLAUsersController {
+class ManageLocalAuthorityUsersController(
+    val manageLocalAuthorityUserService: ManageLocalAuthorityUserService,
+) {
     @GetMapping
     fun index(model: Model): String {
+        val users = manageLocalAuthorityUserService.getLocalAuthorityUsersForLocalAuthority(1)
+        val usersJson = Json.encodeToString(users)
         model.addAttribute("contentHeader", "Manage Local Authority Users")
         model.addAttribute("title", "Manage Local Authority Users")
         model.addAttribute("serviceName", SERVICE_NAME)
+        model.addAttribute("usersJson", usersJson)
         return "manageLAUsers"
     }
 }
--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/LocalAuthorityUserRepository.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/LocalAuthorityUserRepository.kt
@@ -3,4 +3,7 @@ package uk.gov.communities.prsdb.webapp.database.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.communities.prsdb.webapp.database.entity.LocalAuthorityUser
 
-interface LocalAuthorityUserRepository : JpaRepository<LocalAuthorityUser?, Long?>
+interface LocalAuthorityUserRepository : JpaRepository<LocalAuthorityUser?, Long?> {
+    @Suppress("ktlint:standard:function-naming")
+    fun findByLocalAuthority_Id(localAuthorityId: Int): List<LocalAuthorityUser>
+}
