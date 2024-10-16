6a656e35558fe8b485055b9677058357bef68c69
Alexander Read <alexander.read@softwire.com>
2024-10-16 09:46:18 +0100

    PRSD-364: Seperate template and service tests

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/viewmodels/EmailTemplateModelsTests.kt
@@ -0,0 +1,45 @@
+package uk.gov.communities.prsdb.webapp.viewmodels
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import uk.gov.communities.prsdb.webapp.viewmodel.EmailTemplateModel
+import uk.gov.communities.prsdb.webapp.viewmodel.TestEmail
+
+class EmailTemplateModelsTests {
+    companion object {
+        @JvmStatic
+        private fun templateList() =
+            listOf(
+                EmailTemplateTestData(TestEmail("test string"), "/emails/TestEmail.md"),
+            )
+    }
+
+    data class EmailTemplateTestData(
+        val model: EmailTemplateModel,
+        val markdownLocation: String,
+    ) {
+        override fun toString(): String = model.javaClass.simpleName
+    }
+
+    @ParameterizedTest(name = "{0} keys match markdown")
+    @MethodSource("templateList")
+    fun `EmailTemplateModels hashmaps have keys that match the parameters in their markdown templates`(testData: EmailTemplateTestData) {
+        // Arrange
+        var storedBody = javaClass.getResource(testData.markdownLocation)?.readText() ?: ""
+        var parameters = getParametersFromBody(storedBody)
+
+        // Act
+        var modelHashMap = testData.model.toHashMap()
+
+        // Assert
+        Assertions.assertEquals(parameters.size, modelHashMap.size)
+        for (parameter in parameters) {
+            modelHashMap.keys.single { key -> key == parameter }
+        }
+    }
+
+    private fun getParametersFromBody(body: String): List<String> {
+        val parameterRegex = Regex("\\(\\(.*\\)\\)")
+        return parameterRegex.findAll(body).map { result -> result.value.trim(')').trim('(') }.toList()
+    }
+}
