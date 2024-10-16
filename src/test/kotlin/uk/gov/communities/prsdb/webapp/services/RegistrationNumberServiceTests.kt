14915d606f5a8084581c931ce7886f0c6c9e37c1
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-10-16 16:35:04 +0100

    PRSD-353: Refactors and updates RegistrationNumberServiceTests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/services/RegistrationNumberServiceTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/services/RegistrationNumberServiceTests.kt
@@ -1,9 +1,12 @@
 package uk.gov.communities.prsdb.webapp.services
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.ArgumentCaptor.captor
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.mock
@@ -13,21 +16,57 @@ import org.mockito.Mockito.`when`
 import uk.gov.communities.prsdb.webapp.constants.MAX_REG_NUM
 import uk.gov.communities.prsdb.webapp.constants.MIN_REG_NUM
 import uk.gov.communities.prsdb.webapp.constants.enums.RegistrationNumberType
-import uk.gov.communities.prsdb.webapp.database.entity.Landlord
 import uk.gov.communities.prsdb.webapp.database.entity.RegistrationNumber
-import uk.gov.communities.prsdb.webapp.database.repository.LandlordRepository
 import uk.gov.communities.prsdb.webapp.database.repository.RegistrationNumberRepository
+import uk.gov.communities.prsdb.webapp.models.dataModels.RegistrationNumberDataModel
 
 class RegistrationNumberServiceTests {
     private lateinit var mockRegNumRepository: RegistrationNumberRepository
-    private lateinit var mockLandlordRepository: LandlordRepository
     private lateinit var regNumService: RegistrationNumberService
 
     @BeforeEach
     fun setup() {
         mockRegNumRepository = mock()
-        mockLandlordRepository = mock()
-        regNumService = RegistrationNumberService(mockRegNumRepository, mockLandlordRepository)
+        regNumService = RegistrationNumberService(mockRegNumRepository)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideParseableStringsAndRegNumDataModels() =
+            listOf(
+                // Expected format
+                Arguments.of("L-CCCC-CCCC", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM)),
+                Arguments.of("P-9999-9999", RegistrationNumberDataModel(RegistrationNumberType.PROPERTY, MAX_REG_NUM)),
+                Arguments.of("A-CCCC-CCCC", RegistrationNumberDataModel(RegistrationNumberType.AGENT, MIN_REG_NUM)),
+                // Unexpected segmentation
+                Arguments.of("L99999999", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM)),
+                Arguments.of("L-999-999-99", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM)),
+                // Unexpected segmenting symbol
+                Arguments.of("L 9999 9999", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM)),
+                // Unexpected case
+                Arguments.of("l-cccc-cccc", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM)),
+            )
+
+        @JvmStatic
+        fun provideNonParseableRegNumStrings() =
+            listOf(
+                // Invalid registration number type
+                "9-9999-9999",
+                // Invalid registration number characters
+                "L-1111-1111",
+                "L-LLLL-LLLL",
+                // Invalid registration number length
+                "L-9999-9999-9",
+                "L-9999",
+            )
+
+        @JvmStatic
+        fun provideRegNumsAndStrings() =
+            listOf(
+                Arguments.of(RegistrationNumber(RegistrationNumberType.LANDLORD, MIN_REG_NUM), "L-CCCC-CCCC"),
+                Arguments.of(RegistrationNumber(RegistrationNumberType.PROPERTY, MAX_REG_NUM), "P-9999-9999"),
+                Arguments.of(RegistrationNumber(RegistrationNumberType.AGENT, MAX_REG_NUM), "A-9999-9999"),
+            )
     }
 
     @Test
@@ -50,45 +89,27 @@ class RegistrationNumberServiceTests {
         verify(mockRegNumRepository, times(2)).existsByNumber(any(Long::class.java))
     }
 
-    @Test
-    fun `retrieveEntity retrieves a landlord given their registration number`() {
-        val decRegNums =
-            listOf(
-                MIN_REG_NUM,
-                MAX_REG_NUM,
-            )
-        val formattedRegNums = listOf("L-CCCC-CCCC", "L-9999-9999")
-        val landlord = Landlord()
-
-        for (i in decRegNums.indices) {
-            `when`(mockLandlordRepository.findByRegistrationNumber_Number(decRegNums[i])).thenReturn(landlord)
-
-            assertEquals(regNumService.retrieveEntity(formattedRegNums[i]), landlord)
-        }
+    @ParameterizedTest
+    @MethodSource("provideParseableStringsAndRegNumDataModels")
+    fun `stringToRegNum returns a corresponding registration number data model`(
+        parseableString: String,
+        expectedRegNum: RegistrationNumberDataModel,
+    ) {
+        assertEquals(regNumService.stringToRegNum(parseableString), expectedRegNum)
     }
 
-    @Test
-    fun `retrieveEntity returns null given a non-existent registration number`() {
-        val decRegNum = 1L
-        val formattedRegNum = "L-CCCC-CCCC"
-        val landlord = Landlord()
-
-        `when`(mockLandlordRepository.findByRegistrationNumber_Number(decRegNum)).thenReturn(landlord)
-
-        assertNull(regNumService.retrieveEntity(formattedRegNum))
+    @ParameterizedTest
+    @MethodSource("provideNonParseableRegNumStrings")
+    fun `stringToRegNum throws an illegal argument exception when given an invalid registration number`(nonParseableString: String) {
+        assertThrows<IllegalArgumentException> { regNumService.stringToRegNum(nonParseableString) }
     }
 
-    @Test
-    fun `formatRegNum returns a formatted registration number`() {
-        val regNums =
-            listOf(
-                RegistrationNumber(RegistrationNumberType.LANDLORD, MIN_REG_NUM),
-                RegistrationNumber(RegistrationNumberType.PROPERTY, MAX_REG_NUM),
-            )
-        val formattedRegNums = listOf("L-CCCC-CCCC", "P-9999-9999")
-
-        for (i in regNums.indices) {
-            assertEquals(regNumService.formatRegNum(regNums[i]), formattedRegNums[i])
-        }
+    @ParameterizedTest
+    @MethodSource("provideRegNumsAndStrings")
+    fun `regNumToString returns a correctly formatted registration number string`(
+        regNum: RegistrationNumber,
+        expectedString: String,
+    ) {
+        assertEquals(regNumService.regNumToString(regNum), expectedString)
     }
 }
