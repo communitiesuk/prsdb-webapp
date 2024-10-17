6c5e5ef88724c34ab9b69583d930231ea07b9962
JasCon <jasmin.conterio@softwire.com>
2024-10-17 11:33:52 +0100

    PRSD-358: First pass at the table

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
@@ -1,13 +1,12 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import uk.gov.communities.prsdb.webapp.constants.SERVICE_NAME
+import uk.gov.communities.prsdb.webapp.models.dataModels.LocalAuthorityUserDataModel
 import uk.gov.communities.prsdb.webapp.services.LocalAuthorityDataService
 import java.security.Principal
 
@@ -24,13 +23,22 @@ class ManageLocalAuthorityUsersController(
     ): String {
         val currentUserLocalAuthority = localAuthorityDataService.getLocalAuthorityForUser(principal.name)!!
 
-        val users = localAuthorityDataService.getLocalAuthorityUsersForLocalAuthority(currentUserLocalAuthority)
-        val usersJson = Json.encodeToString(users)
-        model.addAttribute("usersJson", usersJson)
+        val activeUsers = localAuthorityDataService.getLocalAuthorityUsersForLocalAuthority(currentUserLocalAuthority)
+        val pendingUsers =
+            listOf(
+                LocalAuthorityUserDataModel("Invited user 1", isManager = false, isPending = true),
+                LocalAuthorityUserDataModel("Invited user 2", isManager = false, isPending = true),
+            )
 
-        model.addAttribute("contentHeader", "Manage Local Authority Users")
+        val users = activeUsers + pendingUsers
+
+        model.addAttribute("contentHeader", "Manage ${currentUserLocalAuthority?.name}'s users")
         model.addAttribute("title", "Manage Local Authority Users")
         model.addAttribute("serviceName", SERVICE_NAME)
+        model.addAttribute("userList", users)
+        model.addAttribute("tableColumn1Heading", "Username")
+        model.addAttribute("tableColumn2Heading", "Access level")
+        model.addAttribute("tableColumn3Heading", "Account status")
 
         return "manageLAUsers"
     }
