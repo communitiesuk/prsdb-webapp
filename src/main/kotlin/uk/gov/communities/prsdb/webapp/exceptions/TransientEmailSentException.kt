c73280e14a04b5b9a789735916cca429b8838edc
Alexander Read <alexander.read@softwire.com>
2024-10-17 14:57:03 +0100

    PRSD-364: Stub calls to notify for local

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/exceptions/TransientEmailSentException.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/exceptions/TransientEmailSentException.kt
@@ -1,6 +1,6 @@
 package uk.gov.communities.prsdb.webapp.exceptions
 
-class TransientEmailSentException(
-    message: String,
-    cause: Throwable,
-) : PrsdbWebException(message, cause)
+class TransientEmailSentException : PrsdbWebException {
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(message: String) : super(message)
+}
