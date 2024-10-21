ac15e55f7ef89b79e6b18644b57d9f29d2667df3
JasCon <jasmin.conterio@softwire.com>
2024-10-21 15:20:02 +0100

    PRSD-358: Move page text to message.properties

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
@@ -1,5 +1,7 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.MessageSource
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -9,12 +11,14 @@ import uk.gov.communities.prsdb.webapp.constants.SERVICE_NAME
 import uk.gov.communities.prsdb.webapp.models.dataModels.LocalAuthorityUserDataModel
 import uk.gov.communities.prsdb.webapp.services.LocalAuthorityDataService
 import java.security.Principal
+import java.util.Locale
 
 @PreAuthorize("hasRole('LA_ADMIN')")
 @Controller
 @RequestMapping("/manage-users")
 class ManageLocalAuthorityUsersController(
     val localAuthorityDataService: LocalAuthorityDataService,
+    @Qualifier("messageSource") private val messageSource: MessageSource,
 ) {
     @GetMapping
     fun index(
@@ -33,11 +37,24 @@ class ManageLocalAuthorityUsersController(
 
         val users = activeUsers + pendingUsers
 
-        model.addAttribute("contentHeader", "Manage ${currentUserLocalAuthority?.name}'s users")
-        model.addAttribute("title", "Manage Local Authority Users")
+        model.addAttribute(
+            "contentHeader",
+            messageSource.getMessage("manageLAUsers.contentHeader.part1", null, Locale("en")) +
+                " " + currentUserLocalAuthority.name +
+                messageSource.getMessage("manageLAUsers.contentHeader.part2", null, Locale("en")),
+        )
+        model.addAttribute("title", messageSource.getMessage("manageLAUsers.title", null, Locale("en")))
         model.addAttribute("serviceName", SERVICE_NAME)
         model.addAttribute("userList", users)
-        model.addAttribute("tableColumnHeadings", listOf("Username", "Access level", "Account status", ""))
+        model.addAttribute(
+            "tableColumnHeadings",
+            listOf(
+                messageSource.getMessage("manageLAUsers.table.column1Heading", null, Locale("en")),
+                messageSource.getMessage("manageLAUsers.table.column2Heading", null, Locale("en")),
+                messageSource.getMessage("manageLAUsers.table.column3Heading", null, Locale("en")),
+                "",
+            ),
+        )
 
         return "manageLAUsers"
     }
