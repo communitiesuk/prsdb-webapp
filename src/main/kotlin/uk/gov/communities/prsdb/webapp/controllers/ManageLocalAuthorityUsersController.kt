acc01c66db436d47898a5a024b72ece23c626ca7
JasCon <jasmin.conterio@softwire.com>
2024-10-22 15:47:47 +0100

    PRSD-358: Back end pagination now working

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
@@ -9,12 +9,14 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import uk.gov.communities.prsdb.webapp.constants.SERVICE_NAME
 import uk.gov.communities.prsdb.webapp.database.entity.LocalAuthority
 import uk.gov.communities.prsdb.webapp.models.dataModels.LocalAuthorityUserDataModel
 import uk.gov.communities.prsdb.webapp.services.LocalAuthorityDataService
 import java.security.Principal
 import java.util.Locale
+import kotlin.math.ceil
 
 @PreAuthorize("hasRole('LA_ADMIN')")
 @Controller
@@ -23,17 +25,57 @@ class ManageLocalAuthorityUsersController(
     val localAuthorityDataService: LocalAuthorityDataService,
     @Qualifier("messageSource") private val messageSource: MessageSource,
 ) {
-    val maxUsersDisplayed = 2
+    val maxUsersDisplayed = 3
 
     @GetMapping
     fun index(
         model: Model,
         principal: Principal,
+        @RequestParam(required = false) page: String?,
     ): String {
         val currentUserLocalAuthority = localAuthorityDataService.getLocalAuthorityForUser(principal.name)!!
+        var activeUsers = listOf<LocalAuthorityUserDataModel>()
+        var pendingUsers = listOf<LocalAuthorityUserDataModel>()
 
-        val activeUsers = getActiveUsersPaginated(currentUserLocalAuthority, 1, maxUsersDisplayed)
-        val pendingUsers = getPendingUsersPaginated(currentUserLocalAuthority, 1, maxUsersDisplayed)
+        val nActiveUsers = localAuthorityDataService.countActiveLocalAuthorityUsersForLocalAuthority(currentUserLocalAuthority)
+        val nPendingUsers = localAuthorityDataService.countPendingLocalAuthorityUsersForLocalAuthority(currentUserLocalAuthority)
+        val totalUsers = nActiveUsers + nPendingUsers
+        val totalPages = ceil((totalUsers.toDouble() / maxUsersDisplayed.toDouble())).toInt()
+        val shouldPaginate = totalPages > 1
+
+        val currentPageNumber = getCurrentPage(page)
+
+        if (shouldPaginate) {
+            val firstDisplayedUserTotalIndex = (currentPageNumber - 1) * maxUsersDisplayed
+            if (firstDisplayedUserTotalIndex < nActiveUsers) {
+                activeUsers = getActiveUsersPaginated(currentUserLocalAuthority, currentPageNumber, maxUsersDisplayed)
+            }
+
+            if (activeUsers.size < maxUsersDisplayed) {
+                if (activeUsers.isNotEmpty()) {
+                    val nPendingUsersOnMixedPage = maxUsersDisplayed - activeUsers.size
+                    pendingUsers = getPendingUsersPaginated(currentUserLocalAuthority, 1, nPendingUsersOnMixedPage, 0)
+                } else {
+                    val nPagesWithActiveUsers = ceil(nActiveUsers.toDouble() / maxUsersDisplayed.toDouble()).toInt()
+                    val pendingUserPage = currentPageNumber - nPagesWithActiveUsers
+
+                    val nActiveUsersOnMixedPage = (nActiveUsers % maxUsersDisplayed).toInt()
+                    val nPendingUsersOnMixedPage = maxUsersDisplayed - nActiveUsersOnMixedPage
+
+                    pendingUsers =
+                        getPendingUsersPaginated(
+                            currentUserLocalAuthority,
+                            pendingUserPage,
+                            maxUsersDisplayed,
+                            nPendingUsersOnMixedPage,
+                        )
+                }
+            }
+        } else {
+            activeUsers = getActiveUsersPaginated(currentUserLocalAuthority, 1, maxUsersDisplayed)
+            pendingUsers = getPendingUsersPaginated(currentUserLocalAuthority, 1, maxUsersDisplayed, 0)
+        }
+
         val users = activeUsers + pendingUsers
 
         model.addAttribute(
@@ -54,16 +96,13 @@ class ManageLocalAuthorityUsersController(
                 "",
             ),
         )
+        model.addAttribute("shouldPaginate", shouldPaginate)
+        model.addAttribute("totalPages", totalPages)
+        model.addAttribute("currentPage", currentPageNumber)
+        model.addAttribute("isLastPage", currentPageNumber == totalPages)
 
         return "manageLAUsers"
     }
-
-    private fun shouldPaginate(localAuthority: LocalAuthority): Boolean =
-        (
-            localAuthorityDataService.countActiveLocalAuthorityUsersForLocalAuthority(localAuthority) +
-                localAuthorityDataService.countPendingLocalAuthorityUsersForLocalAuthority(localAuthority)
-                > maxUsersDisplayed
-        )
 
     private fun getActiveUsersPaginated(
         localAuthority: LocalAuthority,
@@ -78,8 +117,28 @@ class ManageLocalAuthorityUsersController(
         localAuthority: LocalAuthority,
         page: Int,
         nUsers: Int,
+        initialOffset: Int,
     ): List<LocalAuthorityUserDataModel> {
-        val pageRequest = PageRequest.of(page - 1, nUsers, Sort.by(Sort.Direction.ASC, "invitedEmail"))
+        val pageRequest = MyPageRequest(page - 1, nUsers, Sort.by(Sort.Direction.ASC, "invitedEmail"), initialOffset)
         return localAuthorityDataService.getLocalAuthorityPendingUsersForLocalAuthority(localAuthority, pageRequest)
     }
+
+    private fun getCurrentPage(pageString: String?): Int {
+        if (pageString == null) return 1
+
+        try {
+            return pageString.toInt()
+        } catch (e: NumberFormatException) {
+            return 1
+        }
+    }
+}
+
+class MyPageRequest(
+    pageNumber: Int,
+    pageSize: Int,
+    sort: Sort,
+    private val initialOffset: Int,
+) : PageRequest(pageNumber, pageSize, sort) {
+    override fun getOffset(): Long = (this.pageNumber * this.pageSize + initialOffset).toLong()
 }
