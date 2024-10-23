0ce78f1151aa69cfcf72b60a058e0b3c923dd463
Alexander Read <Alexander.read@softwire.com>
2024-10-23 14:38:38 +0100

    PRSD-404: Reflection in validator

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/ConfirmedEmailDataModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/ConfirmedEmailDataModel.kt
@@ -2,10 +2,10 @@ package uk.gov.communities.prsdb.webapp.models.dataModels
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
-import uk.gov.communities.prsdb.webapp.validation.constraints.ValidConfirmedEmail
+import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingProperties
 
-@ValidConfirmedEmail
+@HasMatchingProperties("confirmEmail must match email")
 class ConfirmedEmailDataModel(
     @field:NotBlank @field:Email val email: String,
-    @field:NotBlank @field:Email val confirmEmail: String,
+    @property:HasMatchingProperties.Matches("email") @field:NotBlank @field:Email val confirmEmail: String,
 )
---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/validation/constraints/HasMatchingProperties.kt
@@ -0,0 +1,21 @@
+package uk.gov.communities.prsdb.webapp.validation.constraints
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import uk.gov.communities.prsdb.webapp.validation.validators.MatchingPropertiesValidator
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [MatchingPropertiesValidator::class])
+annotation class HasMatchingProperties(
+    val message: String = "Property values must match",
+    val groups: Array<KClass<Any>> = [],
+    val payload: Array<KClass<Payload>> = [],
+) {
+    @Target(AnnotationTarget.PROPERTY)
+    @Retention(AnnotationRetention.RUNTIME)
+    annotation class Matches(
+        val propertyName: String,
+    )
+}
