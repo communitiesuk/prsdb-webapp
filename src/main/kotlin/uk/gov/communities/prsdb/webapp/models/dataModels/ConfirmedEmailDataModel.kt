a70ac850662cfdb2557deb43445c7f973aa58888
Alexander Read <Alexander.read@softwire.com>
2024-10-24 11:11:58 +0100

    PRSD-404: Limit match validator to Strings

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/ConfirmedEmailDataModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/ConfirmedEmailDataModel.kt
@@ -2,10 +2,10 @@ package uk.gov.communities.prsdb.webapp.models.dataModels
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
-import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingProperties
+import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingStringProperties
 
-@HasMatchingProperties("confirmEmail must match email")
+@HasMatchingStringProperties("confirmEmail must match email")
 class ConfirmedEmailDataModel(
     @field:NotBlank @field:Email val email: String,
-    @property:HasMatchingProperties.Matches("email") @field:NotBlank @field:Email val confirmEmail: String,
+    @property:HasMatchingStringProperties.Matches("email") @field:NotBlank @field:Email val confirmEmail: String,
 )
