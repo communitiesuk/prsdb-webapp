b1b3fcd5fe7834cb22d9bf9dea90d6cbdd97611c
Alexander Read <Alexander.read@softwire.com>
2024-10-24 11:11:58 +0100

    PRSD-404: Limit match validator to Strings

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/validation/validators/MatchingPropertiesValidator.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/validation/validators/MatchingPropertiesValidator.kt
@@ -2,13 +2,15 @@ package uk.gov.communities.prsdb.webapp.validation.validators
 
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
-import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingProperties
-import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingProperties.Matches
+import jakarta.validation.ValidationException
+import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingStringProperties
+import uk.gov.communities.prsdb.webapp.validation.constraints.HasMatchingStringProperties.Matches
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberProperties
 
-class MatchingPropertiesValidator : ConstraintValidator<HasMatchingProperties, Any> {
+class MatchingPropertiesValidator : ConstraintValidator<HasMatchingStringProperties, Any> {
     override fun isValid(
         value: Any?,
         context: ConstraintValidatorContext?,
@@ -20,12 +22,36 @@ class MatchingPropertiesValidator : ConstraintValidator<HasMatchingProperties, A
                 val annotation = property.findAnnotation<Matches>()!!
                 val matchingProperty = value::class.memberProperties.single { it.name == annotation.propertyName }
 
-                if (matchingProperty.call(value) != property.call(value)) {
+                val stringValue = property.call(value) as? String
+                val matchingStringValue = matchingProperty.call(value) as? String
+
+                if (stringValue == null || matchingStringValue == null) {
+                    throw ValidationException(
+                        exceptionMessageForProperties(value, property, matchingProperty),
+                    )
+                }
+                if (stringValue.trim() != matchingStringValue.trim()) {
                     return false
                 }
             }
         }
 
         return true
+    }
+
+    companion object {
+        fun exceptionMessageForProperties(
+            value: Any,
+            prop1: KProperty1<out Any, *>,
+            prop2: KProperty1<out Any, *>,
+        ): String =
+            "On annotated class ${value::class.simpleName}, " +
+                "property ${prop1.name} was type: ${prop1.returnType} and " +
+                "property ${prop2.name} was type: ${prop1.returnType}. \n" +
+                MESSAGE_CONSTANT
+
+        const val MESSAGE_CONSTANT =
+            "HasMatchingStringProperties can only compare matching String properties on a data model. " +
+                "If you need to have other matching property types, create a new Annotation and Validator."
     }
 }
