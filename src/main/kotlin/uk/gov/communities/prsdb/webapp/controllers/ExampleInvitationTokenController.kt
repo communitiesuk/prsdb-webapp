79f3fa17b615c5b4050b2ba2fafcf33bd7304758
Alexander Read <Alexander.read@softwire.com>
2024-10-28 10:15:54 +0000

    PRSD-404: Use CharSequence return type for controller method

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ExampleInvitationTokenController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ExampleInvitationTokenController.kt
@@ -3,7 +3,6 @@ package uk.gov.communities.prsdb.webapp.controllers
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.servlet.ModelAndView
 import uk.gov.communities.prsdb.webapp.constants.SERVICE_NAME
 import uk.gov.communities.prsdb.webapp.services.LocalAuthorityInvitationService
 
@@ -18,15 +17,15 @@ class ExampleInvitationTokenController(
     fun acceptInvitation(
         model: Model,
         token: String,
-    ): ModelAndView {
+    ): CharSequence {
         val authority = invitationService.getAuthorityForToken(token)
         model.addAttribute("contentHeader", "The local authority issuing that token was: ${authority.name}")
         model.addAttribute("title", "Magic link")
         model.addAttribute("serviceName", SERVICE_NAME)
 
-        // TODO PRSD-405: This is using a ModelAndView instead of returning a string to handle an error that otherwise occurs in
+        // TODO PRSD-405: This is using a CharSequence instead of returning a String to handle an error that otherwise occurs in
         // the LocalAuthorityInvitationService method that creates the invitation url using MvcUriComponentsBuilder.fromMethodName
-        // see https://stackoverflow.com/questions/28580281/using-mvcuricomponentsbuilderfrommethodcall-with-string-as-the-return-type for details
-        return ModelAndView("index", "index", model)
+        // see https://github.com/spring-projects/spring-hateoas/issues/155 for details
+        return "index"
     }
 }
