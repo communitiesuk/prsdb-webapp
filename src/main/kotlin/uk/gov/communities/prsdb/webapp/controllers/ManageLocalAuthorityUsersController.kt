5b94738803d5d164e82d2a0fee4bfe33c77f9207
Rowan Hill <rowan.hill@softwire.com>
2024-11-07 17:34:31 +0000

    PRSD-407: Delete LA users (via confirm screen) (#46)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/ManageLocalAuthorityUsersController.kt
@@ -71,6 +71,7 @@ class ManageLocalAuthorityUsersController(
         val localAuthorityUser =
             localAuthorityDataService.getLocalAuthorityUserIfAuthorizedLA(localAuthorityUserId, localAuthorityId)
 
+        model.addAttribute("backLinkPath", "../manage-users")
         model.addAttribute("localAuthorityUser", localAuthorityUser)
         model.addAttribute(
             "options",
@@ -103,6 +104,47 @@ class ManageLocalAuthorityUsersController(
 
         localAuthorityDataService.updateUserAccessLevel(localAuthorityUserAccessLevel, localAuthorityUserId)
         return "redirect:/local-authority/{localAuthorityId}/manage-users"
+    }
+
+    @GetMapping("/delete-user/{localAuthorityUserId}")
+    fun confirmDeleteUser(
+        @PathVariable localAuthorityId: Int,
+        @PathVariable localAuthorityUserId: Long,
+        model: Model,
+        principal: Principal,
+    ): String {
+        localAuthorityDataService.getLocalAuthorityIfAuthorizedUser(localAuthorityId, principal.name)
+        val user = localAuthorityDataService.getLocalAuthorityUserIfAuthorizedLA(localAuthorityUserId, localAuthorityId)
+        model.addAttribute("user", user)
+        model.addAttribute("backLinkPath", "../edit-user/$localAuthorityUserId")
+        return "deleteLAUser"
+    }
+
+    @PostMapping("/delete-user/{localAuthorityUserId}")
+    fun deleteUser(
+        @PathVariable localAuthorityId: Int,
+        @PathVariable localAuthorityUserId: Long,
+        principal: Principal,
+        redirectAttributes: RedirectAttributes,
+    ): String {
+        localAuthorityDataService.getLocalAuthorityIfAuthorizedUser(localAuthorityId, principal.name)
+        val user = localAuthorityDataService.getLocalAuthorityUserIfAuthorizedLA(localAuthorityUserId, localAuthorityId)
+
+        localAuthorityDataService.deleteUser(localAuthorityUserId)
+
+        redirectAttributes.addFlashAttribute("deletedUserName", user.userName)
+        return "redirect:../delete-user/success"
+    }
+
+    @GetMapping("/delete-user/success")
+    fun deleteUserSuccess(
+        @PathVariable localAuthorityId: Int,
+        model: Model,
+        principal: Principal,
+    ): String {
+        val authority = localAuthorityDataService.getLocalAuthorityIfAuthorizedUser(localAuthorityId, principal.name)
+        model.addAttribute("localAuthority", authority)
+        return "deleteLAUserSuccess"
     }
 
     @GetMapping("/invite-new-user")
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/EditLAUserTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/EditLAUserTests.kt
@@ -1,6 +1,7 @@
 package uk.gov.communities.prsdb.webapp.integration
 
 import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import uk.gov.communities.prsdb.webapp.integration.pageobjects.pages.EditLaUserPage
@@ -18,7 +19,7 @@ class EditLAUserTests : IntegrationTest() {
         // Editing the user shows Arthur Dent's page
         var editUserPage = row.editUser()
         assertThat(editUserPage.userName).containsText("Arthur Dent")
-        assertThat(editUserPage.email).containsText("Arthur Dent") // TODO: fix when LA users have email addresses
+        assertThat(editUserPage.email).containsText("Arthur Dent") // TODO PRSD-405: fix when LA users have email addresses
         assertEquals(EditLaUserPage.AccessLevelSelection.BASIC, editUserPage.accessLevel())
 
         // Update the user's access level to admin
@@ -33,5 +34,28 @@ class EditLAUserTests : IntegrationTest() {
         // The user's page also now defaults to admin
         editUserPage = manageUsersPage.table.row(0).editUser()
         assertEquals(EditLaUserPage.AccessLevelSelection.ADMIN, editUserPage.accessLevel())
+    }
+
+    @Test
+    fun `a user can be deleted`() {
+        // Edit Arthur Dent
+        var manageUsersPage = navigator.goToManageLaUsers(1)
+        var row = manageUsersPage.table.row(0)
+        assertTrue(row.username().contains("Arthur Dent"))
+        var editUserPage = row.editUser()
+
+        // Delete the user
+        val confirmDeletePage = editUserPage.deleteUser()
+        confirmDeletePage.assertUserNameVisible("Arthur Dent")
+        confirmDeletePage.assertEmailVisible("Arthur Dent") // TODO PRSD-405: fix when LA users have email addresses
+        val successPage = confirmDeletePage.deleteUser()
+
+        // The success page confirms the user is deleted
+        successPage.confirmationBanner.assertHasMessage("You've removed Arthur Dent's account from Betelgeuse")
+        manageUsersPage = successPage.returnToManageUsers()
+
+        // The user is no longer in the table
+        row = manageUsersPage.table.row(0)
+        assertFalse(row.username().contains("Arthur Dent"))
     }
 }
