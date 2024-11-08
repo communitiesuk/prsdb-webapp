8e1716776469fd253a2543fc3aacdab1b9d6a620
Kiran Randhawa Kukar <kiran.randhawakukar@softwire.com>
2024-11-08 17:47:40 +0000

    feat/PRSD-369: fix RegisterLandlordControllerTests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/RegisterLandlordControllerTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/RegisterLandlordControllerTests.kt
@@ -1,12 +1,15 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.get
 import org.springframework.web.context.WebApplicationContext
 import uk.gov.communities.prsdb.webapp.forms.journeys.LandlordRegistrationJourney
+import uk.gov.communities.prsdb.webapp.forms.steps.LandlordRegistrationStepId
 
 @WebMvcTest(RegisterLandlordController::class)
 class RegisterLandlordControllerTests(
@@ -14,6 +17,11 @@ class RegisterLandlordControllerTests(
 ) : ControllerTest(webContext) {
     @MockBean
     lateinit var mockLandlordRegistrationJourney: LandlordRegistrationJourney
+
+    @BeforeEach
+    fun steup() {
+        whenever(mockLandlordRegistrationJourney.initialStepId).thenReturn(LandlordRegistrationStepId.Email)
+    }
 
     @Test
     fun `RegisterLandlordController returns 200 for unauthenticated user`() {
