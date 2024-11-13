0d9d99e394f617f9e7bfd5a3cbaf4a6c350dd4d2
Travis Woodward <Travis.Woodward@softwire.com>
2024-11-13 07:34:43 +0000

    PRSD-422: Adds objectToStringKeyedMap tests

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/JourneyData.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/JourneyData.kt
@@ -6,7 +6,7 @@ typealias PageData = MutableMap<String, Any?>
 
 fun objectToStringKeyedMap(obj: Any?): JourneyData? {
     if (obj == null) return null
-    val initialMap: MutableMap<*, *> = obj as MutableMap<*, *>
+    val initialMap: MutableMap<*, *> = obj as? MutableMap<*, *> ?: return null
     val result: JourneyData = mutableMapOf()
     for ((key, value) in initialMap) {
         val stringKey = key as? String ?: return null
---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/forms/PageTests.kt
@@ -0,0 +1,4 @@
+package uk.gov.communities.prsdb.webapp.forms
+
+class PageTests {
+}
