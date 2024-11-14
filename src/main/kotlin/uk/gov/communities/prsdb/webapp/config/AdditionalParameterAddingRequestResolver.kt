771839e2de333b661e29c16f484b5d9fb1b32833
Alexander Read <alexander.read@communities.gov.uk>
2024-11-14 11:40:30 +0000

    PRSD-546: Minor fixes

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/config/AdditionalParameterAddingRequestResolver.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/AdditionalParameterAddingRequestResolver.kt
@@ -7,11 +7,11 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequest
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest
 
 class AdditionalParameterAddingRequestResolver(
-    repo: ClientRegistrationRepository,
+    clientRegistrationRepository: ClientRegistrationRepository,
     private val additionalKey: String,
     private val additionalValue: Any,
 ) : OAuth2AuthorizationRequestResolver {
-    private val innerResolver = DefaultOAuth2AuthorizationRequestResolver(repo, "/oauth2/authorization")
+    private val innerResolver = DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization")
 
     override fun resolve(request: HttpServletRequest?): OAuth2AuthorizationRequest? {
         val authRequest = innerResolver.resolve(request) ?: return null
@@ -24,7 +24,7 @@ class AdditionalParameterAddingRequestResolver(
         clientRegistrationId: String?,
     ): OAuth2AuthorizationRequest? {
         val authRequest = innerResolver.resolve(request, clientRegistrationId) ?: return null
-        val additionalParams = LinkedHashMap(authRequest.additionalParameters).apply { put("vtr", "Cl.Cm.P2") }
+        val additionalParams = LinkedHashMap(authRequest.additionalParameters).apply { put(additionalKey, additionalValue) }
         return OAuth2AuthorizationRequest.from(authRequest).additionalParameters(additionalParams).build()
     }
 }
