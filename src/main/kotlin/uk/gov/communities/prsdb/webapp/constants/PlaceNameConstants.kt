f318d30a8d12bc874b9b347109fe08290e3303b6
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-11-18 14:56:36 +0000

    PRSD-560: Loads in place names from csv files

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/PlaceNameConstants.kt
@@ -0,0 +1,25 @@
+package uk.gov.communities.prsdb.webapp.constants
+
+import org.springframework.util.ResourceUtils
+import java.io.InputStream
+
+fun readPlaceNamesCsv(fileName: String): List<String> {
+    val inputStream: InputStream = ResourceUtils.getFile(fileName).inputStream()
+    val reader = inputStream.bufferedReader()
+    reader.readLine()
+
+    val placeNames = mutableListOf<String>()
+    reader.forEachLine { line ->
+        val placeName = line.split(",")[1].replace("\"", "")
+        placeNames.add(placeName)
+    }
+    return placeNames
+}
+
+// Files taken from https://www.gov.wales/bydtermcymru/international-place-names
+val PLACE_NAMES =
+    (
+        readPlaceNamesCsv("classpath:data/place_names/country_names.csv") +
+            readPlaceNamesCsv("classpath:data/place_names/crown_dependency_names.csv") +
+            readPlaceNamesCsv("classpath:data/place_names/overseas_territory_names.csv")
+    ).sorted()
