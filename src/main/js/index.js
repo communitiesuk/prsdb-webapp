d1bb7fa32fe0bd212015d6ad7d1d5abb3d6aefa0
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-11-18 14:48:41 +0000

    PRSD-560: Adds progressive enhancement to select component

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,3 +1,16 @@
 import {initAll} from 'govuk-frontend'
+import {enhanceSelectElement} from 'accessible-autocomplete'
 
 initAll()
+
+window.onload = addAccessibleAutocompleteToSelectElements
+
+function addAccessibleAutocompleteToSelectElements() {
+    document.querySelectorAll('.govuk-select')
+        .forEach((selectElement) => enhanceSelectElement({
+            selectElement: selectElement,
+            showAllValues: true,
+            inputClasses: 'govuk-label',
+            menuClasses: 'govuk-label',
+        }))
+}
