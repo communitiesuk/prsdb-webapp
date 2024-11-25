5a6a7b00c4da95129e7cdf28f0d99ba97ea59636
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-11-25 09:34:41 +0000

    PRSD-NONE: Allows the navigator to skip to form pages

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageobjects/pages/Navigator.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageobjects/pages/Navigator.kt
@@ -14,66 +14,51 @@ class Navigator(
     private val page: Page,
     private val port: Int,
 ) {
+    fun goToManageLaUsers(authorityId: Int): ManageLaUsersPage {
+        navigate("local-authority/$authorityId/manage-users")?.url()
+        return BasePage.createValidPage(page, ManageLaUsersPage::class)
+    }
+
     fun goToInviteNewLaUser(authorityId: Int): InviteNewLaUserPage {
         navigate("local-authority/$authorityId/invite-new-user")
-        return BasePage.createValid(page, InviteNewLaUserPage::class)
-    }
-
-    fun goToManageLaUsers(authorityId: Int): ManageLaUsersPage {
-        navigate("local-authority/$authorityId/manage-users")
-        return BasePage.createValid(page, ManageLaUsersPage::class)
-    }
-
-    fun goToEditLaUser(
-        authorityId: Int,
-        userId: Int,
-    ): EditLaUserPage {
-        navigate("local-authority/$authorityId/edit-user/$userId")
-        return BasePage.createValid(page, EditLaUserPage::class)
+        return BasePage.createValidPage(page, InviteNewLaUserPage::class)
     }
 
     fun goToLandlordRegistrationNameFormPage(): NameFormPageLandlordRegistration {
         navigate("register-as-a-landlord/name")
-        return BasePage.createValid(page, NameFormPageLandlordRegistration::class)
+        return BasePage.createValidPage(page, NameFormPageLandlordRegistration::class)
     }
 
     fun goToLandlordRegistrationEmailFormPage(): EmailFormPageLandlordRegistration {
-        navigate("register-as-a-landlord/email")
-        return BasePage.createValid(page, EmailFormPageLandlordRegistration::class)
+        val nameFormPage = goToLandlordRegistrationNameFormPage()
+        nameFormPage.nameInput.fill("Arthur Dent")
+        val emailFormPage = nameFormPage.submitFormAndAssertNextPage()
+        return emailFormPage
     }
 
     fun goToLandlordRegistrationPhoneNumberFormPage(): PhoneNumberFormPageLandlordRegistration {
-        navigate("register-as-a-landlord/phone-number")
-        return BasePage.createValid(page, PhoneNumberFormPageLandlordRegistration::class)
+        val emailFormPage = goToLandlordRegistrationEmailFormPage()
+        emailFormPage.emailInput.fill("test@example.com")
+        val phoneNumberPage = emailFormPage.submitFormAndAssertNextPage()
+        return phoneNumberPage
     }
 
     fun goToLaUserRegistrationLandingPage(): LandingPageLaUserRegistration {
         navigate("register-local-authority-user/landing-page")
-        return BasePage.createValid(page, LandingPageLaUserRegistration::class)
+        return BasePage.createValidPage(page, LandingPageLaUserRegistration::class)
     }
 
     fun goToLaUserRegistrationNameFormPage(): NameFormPageLaUserRegistration {
         val landingPage = goToLaUserRegistrationLandingPage()
-        landingPage.submit()
-        return BasePage.createValid(page, NameFormPageLaUserRegistration::class)
+        val namePage = landingPage.clickBeginAndAssertNextPage()
+        return namePage
     }
 
-    fun goToLaUserRegistrationEmailFormPage(): EmailFormPageLaUserRegistration = completeLaUserRegistrationNameStep()
-
-    fun skipToLaUserRegistrationNameFormPage(): LandingPageLaUserRegistration {
-        navigate("register-local-authority-user/name")
-        return BasePage.createValid(page, LandingPageLaUserRegistration::class)
-    }
-
-    fun skipToLaUserRegistrationEmailFormPage(): LandingPageLaUserRegistration {
-        navigate("register-local-authority-user/email")
-        return BasePage.createValid(page, LandingPageLaUserRegistration::class)
-    }
-
-    private fun completeLaUserRegistrationNameStep(): EmailFormPageLaUserRegistration {
+    fun goToLaUserRegistrationEmailFormPage(): EmailFormPageLaUserRegistration {
         val namePage = goToLaUserRegistrationNameFormPage()
-        namePage.fillInput("Test user")
-        return BasePage.createValid(namePage.submit(), EmailFormPageLaUserRegistration::class)
+        namePage.nameInput.fill("Test user")
+        val emailPage = namePage.submitFormAndAssertNextPage()
+        return emailPage
     }
 
     fun navigate(path: String): Response? = page.navigate("http://localhost:$port/$path")
