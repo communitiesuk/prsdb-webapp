5e496d9d8d08cfc71a9837461fc1b4a217a95332
Alexander Read <alexander.read@communities.gov.uk>
2024-11-29 11:43:49 +0000

    PRSD-367: Cache verified identity

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/OneLoginIdentityService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/OneLoginIdentityService.kt
@@ -1,30 +1,62 @@
 package uk.gov.communities.prsdb.webapp.services
 
+import jakarta.servlet.http.HttpSession
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.JwtDecoderFactory
 import org.springframework.stereotype.Service
 import uk.gov.communities.prsdb.webapp.constants.OneLoginClaimKeys
 import uk.gov.communities.prsdb.webapp.models.dataModels.VerifiedCredentialModel
 
+private const val VERIFIED_IDENTITY_CACHE_KEY = "verified-identity-cache"
+
 @Service
 class OneLoginIdentityService(
     private val decoderFactory: JwtDecoderFactory<Unit>,
+    private val session: HttpSession,
 ) {
     fun getVerifiedIdentityData(user: OidcUser): MutableMap<String, Any?>? {
+        val cachedVerifiedIdentity = retrieveCachedVerifiedIdentity()
+        if (cachedVerifiedIdentity != null) {
+            return cachedVerifiedIdentity
+        }
+
         val idClaimString = user.claims[OneLoginClaimKeys.CORE_IDENTITY] as? String
         if (idClaimString != null) {
-            val decoder = decoderFactory.createDecoder(Unit)
-            val idClaimJwt = decoder.decode(idClaimString)
-
-            val verifiableCredentialMap = idClaimJwt.claims["vc"] as? Map<*, *>
-            val verifiableCredential = VerifiedCredentialModel.fromUnknownMap(verifiableCredentialMap)
-
-            return mutableMapOf(
-                "name" to verifiableCredential.credentialSubject.getCurrentName(),
-                "birthDate" to verifiableCredential.credentialSubject.birthDate,
-            )
+            val idClaimJwt = decodeCoreIdentityJwt(idClaimString)
+            val verifiedIdentity = extractVerifiedIdentity(idClaimJwt)
+            cacheVerifiedIdentity(verifiedIdentity)
+            return verifiedIdentity
         }
 
         return null
+    }
+
+    private fun cacheVerifiedIdentity(verifiedIdentity: MutableMap<String, Any?>) {
+        session.setAttribute(VERIFIED_IDENTITY_CACHE_KEY, verifiedIdentity)
+    }
+
+    private fun extractVerifiedIdentity(idClaimJwt: Jwt): MutableMap<String, Any?> {
+        val verifiableCredentialMap = idClaimJwt.claims["vc"] as? Map<*, *>
+        val verifiableCredential = VerifiedCredentialModel.fromUnknownMap(verifiableCredentialMap)
+
+        return mutableMapOf(
+            "name" to verifiableCredential.credentialSubject.getCurrentName(),
+            "birthDate" to verifiableCredential.credentialSubject.birthDate,
+        )
+    }
+
+    private fun decodeCoreIdentityJwt(idClaimString: String): Jwt {
+        val decoder = decoderFactory.createDecoder(Unit)
+        return decoder.decode(idClaimString)
+    }
+
+    private fun retrieveCachedVerifiedIdentity(): MutableMap<String, Any?>? {
+        val cached = session.getAttribute(VERIFIED_IDENTITY_CACHE_KEY) as? Map<*, *>
+        return cached
+            ?.map { (key, value) ->
+                if (key !is String) return null
+                key to value
+            }?.toMap(mutableMapOf())
     }
 }
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/services/OneLoginIdentityServiceTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/services/OneLoginIdentityServiceTests.kt
@@ -1,9 +1,11 @@
 package uk.gov.communities.prsdb.webapp.services
 
+import jakarta.servlet.http.HttpSession
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.times
 import org.mockito.kotlin.whenever
@@ -21,6 +23,7 @@ class OneLoginIdentityServiceTests {
     private lateinit var decoderFactory: JwtDecoderFactory<Unit>
     private lateinit var user: OidcUser
     private lateinit var jwt: Jwt
+    private lateinit var session: HttpSession
     private val mockIdentityJwt = "mockIdentityJwt"
 
     @BeforeEach
@@ -29,7 +32,10 @@ class OneLoginIdentityServiceTests {
         decoder = mock()
         user = mock()
         jwt = mock()
-        identityService = OneLoginIdentityService(decoderFactory)
+        session = mock()
+        identityService = OneLoginIdentityService(decoderFactory, session)
+
+        whenever(session.getAttribute(anyString())).thenReturn(null)
 
         whenever(decoderFactory.createDecoder(Unit)).thenReturn(decoder)
 
@@ -76,6 +82,23 @@ class OneLoginIdentityServiceTests {
 
         // Act & Assert
         assertThrows<VerifiedCredentialParsingException> { identityService.getVerifiedIdentityData(user) }
+    }
+
+    @Test
+    fun `getVerifiedIdentityData returns cached identity if it is present`() {
+        // Arrange
+        // Arrange
+        whenever(user.claims).thenReturn(mapOf(OneLoginClaimKeys.CORE_IDENTITY to mockIdentityJwt))
+        whenever(jwt.claims).thenReturn(mapOf("vc" to buildVcMap("name", LocalDate.now())))
+
+        val verifiedCredentialMap = mapOf("testKey" to "testValue")
+        whenever(session.getAttribute(anyString())).thenReturn(verifiedCredentialMap)
+
+        // Act
+        val verifiedIdentityData = identityService.getVerifiedIdentityData(user)
+
+        // Assert
+        assertEquals(verifiedCredentialMap, verifiedIdentityData)
     }
 
     private fun buildVcMap(
