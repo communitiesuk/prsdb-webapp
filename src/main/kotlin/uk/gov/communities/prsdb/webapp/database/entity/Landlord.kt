070c3303356436d69f1a07c089b56cddb1bcb566
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-12-06 12:33:58 +0000

    PRSD-641: Adds LandlordService.createLandlordAndReturnRegistrationNumber

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/Landlord.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/Landlord.kt
@@ -12,7 +12,7 @@ import jakarta.persistence.OneToOne
 import java.util.Date
 
 @Entity
-class Landlord : ModifiableAuditableEntity() {
+class Landlord() : ModifiableAuditableEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private val id: Long? = null
@@ -61,4 +61,25 @@ class Landlord : ModifiableAuditableEntity() {
     @Column(nullable = false)
     var isActive: Boolean? = null
         private set
+
+    constructor(
+        baseUser: OneLoginUser,
+        name: String,
+        email: String,
+        phoneNumber: String,
+        address: Address,
+        registrationNumber: RegistrationNumber,
+        internationalAddress: String?,
+        dateOfBirth: Date?,
+    ) : this() {
+        this.baseUser = baseUser
+        this.name = name
+        this.email = email
+        this.phoneNumber = phoneNumber
+        this.address = address
+        this.registrationNumber = registrationNumber
+        this.isActive = true
+        if (internationalAddress != null) this.internationalAddress = internationalAddress
+        if (dateOfBirth != null) this.dateOfBirth = dateOfBirth
+    }
 }
--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordService.kt
@@ -4,16 +4,51 @@ import org.springframework.stereotype.Service
 import uk.gov.communities.prsdb.webapp.constants.enums.RegistrationNumberType
 import uk.gov.communities.prsdb.webapp.database.entity.Landlord
 import uk.gov.communities.prsdb.webapp.database.repository.LandlordRepository
+import uk.gov.communities.prsdb.webapp.database.repository.OneLoginUserRepository
+import uk.gov.communities.prsdb.webapp.models.dataModels.AddressDataModel
 import uk.gov.communities.prsdb.webapp.models.dataModels.RegistrationNumberDataModel
+import java.util.Date
 
 @Service
 class LandlordService(
     val landlordRepository: LandlordRepository,
+    val oneLoginUserRepository: OneLoginUserRepository,
+    val addressService: AddressService,
+    val registrationNumberService: RegistrationNumberService,
 ) {
     fun retrieveLandlordByRegNum(regNum: RegistrationNumberDataModel): Landlord? {
         if (regNum.type != RegistrationNumberType.LANDLORD) {
             throw IllegalArgumentException("Invalid registration number type")
         }
         return landlordRepository.findByRegistrationNumber_Number(regNum.number)
+    }
+
+    fun createLandlordAndReturnRegistrationNumber(
+        baseUserId: String,
+        name: String,
+        email: String,
+        phoneNumber: String,
+        addressDataModel: AddressDataModel,
+        internationalAddress: String? = null,
+        dateOfBirth: Date? = null,
+    ): String {
+        val baseUser = oneLoginUserRepository.getReferenceById(baseUserId)
+        val address = addressService.findOrCreateAddress(addressDataModel)
+        val registrationNumber = registrationNumberService.createRegistrationNumber(RegistrationNumberType.LANDLORD)
+
+        landlordRepository.save(
+            Landlord(
+                baseUser,
+                name,
+                email,
+                phoneNumber,
+                address,
+                registrationNumber,
+                internationalAddress,
+                dateOfBirth,
+            ),
+        )
+
+        return registrationNumber.toString()
     }
 }
