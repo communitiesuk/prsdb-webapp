1aa4dc79453a3e9bc47442b75b33c226846c9abb
Alexander Read <alexander.read@communities.gov.uk>
2024-12-09 10:53:36 +0000

    PRSD-496: Add trigram search to example endpoint

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/LandlordRepository.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/LandlordRepository.kt
@@ -1,10 +1,24 @@
 package uk.gov.communities.prsdb.webapp.database.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import uk.gov.communities.prsdb.webapp.database.entity.Landlord
 
 interface LandlordRepository : JpaRepository<Landlord?, Long?> {
     // The underscore tells JPA to access fields relating to the referenced table
     @Suppress("ktlint:standard:function-naming")
     fun findByRegistrationNumber_Number(registrationNumber: Long): Landlord?
+
+    @Query(
+        "SELECT l.* " +
+            "FROM landlord l  " +
+            "WHERE (l.phone_number || ' ' || l.email || ' ' || l.name) %> :searchQuery " +
+            "ORDER BY (l.phone_number || ' ' || l.email || ' ' || l.name) <->> :searchQuery LIMIT :limit",
+        nativeQuery = true,
+    )
+    fun searchMatching(
+        @Param("searchQuery")searchQuery: String,
+        @Param("limit")limit: Int,
+    ): List<Landlord>
 }
