1e6ea6bba6a083f10818bb7a523f32bfa07795c0
Isobel Ibironke <isobel.ibironke@softwire.com>
2024-12-12 10:04:24 +0000

    PRSD-642: Updates ConfirmationPageLandlordRegistration

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/landlordRegistrationJourneyPages/ConfirmationPageLandlordRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/landlordRegistrationJourneyPages/ConfirmationPageLandlordRegistration.kt
@@ -4,12 +4,14 @@ import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.constants.REGISTER_LANDLORD_JOURNEY_URL
 import uk.gov.communities.prsdb.webapp.controllers.RegisterLandlordController.Companion.CONFIRMATION_PAGE_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BaseComponent
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BaseComponent.Companion.getChildComponent
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 
 class ConfirmationPageLandlordRegistration(
     page: Page,
 ) : BasePage(page, "$REGISTER_LANDLORD_JOURNEY_URL/$CONFIRMATION_PAGE_PATH_SEGMENT") {
-    val confirmationBanner = BaseComponent.getConfirmationPageBanner(page)
+    private val confirmationBanner = BaseComponent.getConfirmationPageBanner(page)
+    val registrationNumberText: String = getChildComponent(confirmationBanner, "strong").innerText()
 
     fun clickGoToDashboard() = clickButton(BaseComponent.getButton(page, "Go to Dashboard"))
 }
