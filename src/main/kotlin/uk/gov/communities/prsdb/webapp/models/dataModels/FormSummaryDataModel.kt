9306051f607ee1699f600a65f10a60c9377fd257
Alexander Read <alexander.read@communities.gov.uk>
2024-12-18 10:54:45 +0000

    PRSD-495: Display list items on separate lines

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/FormSummaryDataModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/FormSummaryDataModel.kt
@@ -4,4 +4,6 @@ data class FormSummaryDataModel(
     val fieldHeading: String,
     val fieldValue: Any?,
     val changeUrl: String?,
-)
+) {
+    fun isListSummaryItem(): Boolean = fieldValue is List<*>
+}
