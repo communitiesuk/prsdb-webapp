98091fc4bf4568e6297287941e69f5bb27b441bb
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-01-09 11:51:43 +0000

    PRSD-597: Removes unnecessary insertion from data-mockuser-not-landlord.sql

--- src/test/resources/data-mockuser-not-landlord.sql
+++ src/test/resources/data-mockuser-not-landlord.sql
@@ -1,5 +1,2 @@
 INSERT INTO one_login_user (id, name, email, created_date, last_modified_date)
 VALUES ('urn:fdc:gov.uk:2022:UVWXY', 'Mock User', 'test@example.com', '10/14/24', '10/14/24');
-
-INSERT INTO landlord_user (subject_identifier, phone_number, date_of_birth, created_date, last_modified_date)
-VALUES ('urn:fdc:gov.uk:2022:UVWXY', '01406946277', '06/16/84', '10/14/24', '10/14/24');
