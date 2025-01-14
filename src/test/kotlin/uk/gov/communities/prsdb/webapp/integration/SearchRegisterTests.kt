df6a818cdb23fcfba42b43998162064798ef314e
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-01-14 11:35:01 +0000

    PRSD-649: Tests SearchLandlordRegisterPage

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/SearchRegisterTests.kt
@@ -0,0 +1,63 @@
+package uk.gov.communities.prsdb.webapp.integration
+
+import com.microsoft.playwright.Page
+import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.opentest4j.AssertionFailedError
+import org.springframework.test.context.jdbc.Sql
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.ErrorPage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.SearchLandlordRegisterPage.Companion.ADDRESS_COL_INDEX
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.SearchLandlordRegisterPage.Companion.CONTACT_INFO_COL_INDEX
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.SearchLandlordRegisterPage.Companion.LANDLORD_COL_INDEX
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage.Companion.assertPageIs
+import kotlin.test.assertContains
+
+@Sql("/data-local.sql")
+class SearchRegisterTests : IntegrationTest() {
+    @Test
+    fun `results table does not show before search has been requested`() {
+        val searchLandlordRegisterPage = navigator.goToSearchLandlordRegister()
+
+        val exception = assertThrows<AssertionFailedError> { searchLandlordRegisterPage.getResultTable() }
+        assertContains(exception.message!!, "Expected 1 instance of Locator@.govuk-table, found 0")
+    }
+
+    @Test
+    fun `results table shows after search has been requested`() {
+        val searchLandlordRegisterPage = navigator.goToSearchLandlordRegister()
+        searchLandlordRegisterPage.searchBar.search("L-CKSQ-3SX9")
+        val resultTable = searchLandlordRegisterPage.getResultTable()
+
+        assertThat(resultTable.getHeaderCell(LANDLORD_COL_INDEX)).containsText("Landlord")
+        assertThat(resultTable.getCell(0, LANDLORD_COL_INDEX)).containsText("Alexander Smith\nL-CKSQ-3SX9")
+
+        assertThat(resultTable.getHeaderCell(ADDRESS_COL_INDEX)).containsText("Contact address")
+        assertThat(resultTable.getCell(0, ADDRESS_COL_INDEX)).containsText("1 Fictional Road")
+
+        assertThat(resultTable.getHeaderCell(CONTACT_INFO_COL_INDEX)).containsText("Contact information")
+        assertThat(resultTable.getCell(0, CONTACT_INFO_COL_INDEX)).containsText("7111111111\nalex.surname@example.com")
+    }
+
+    @Test
+    fun `fuzzy search functionality works`() {
+        val searchLandlordRegisterPage = navigator.goToSearchLandlordRegister()
+        searchLandlordRegisterPage.searchBar.search("Alex")
+        val resultTable = searchLandlordRegisterPage.getResultTable()
+
+        assertThat(resultTable.getCell(0, LANDLORD_COL_INDEX)).containsText("Alexander Smith")
+        assertThat(resultTable.getCell(1, LANDLORD_COL_INDEX)).containsText("Alexandra Davies")
+        assertThat(resultTable.getCell(2, LANDLORD_COL_INDEX)).containsText("Evan Alexandrescu")
+    }
+
+    @Test
+    fun `landlord links goes to landlord details page`(page: Page) {
+        val searchLandlordRegisterPage = navigator.goToSearchLandlordRegister()
+        searchLandlordRegisterPage.searchBar.search("L-CKSQ-3SX9")
+        searchLandlordRegisterPage.getLandlordLink(rowIndex = 0).click()
+
+        // TODO PRSD-656: Replace with landlord details page assertion
+        assertPageIs(page, ErrorPage::class)
+        assertContains(page.url(), "/landlord-details/1")
+    }
+}
