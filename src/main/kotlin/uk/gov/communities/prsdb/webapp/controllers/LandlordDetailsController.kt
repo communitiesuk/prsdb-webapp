d489ca80fb93b647c6185103766157d6562b9608
Alexander Read <alexander.read@communities.gov.uk>
2025-01-14 16:33:15 +0000

    Revert "PRSD-701: Use summary list row view model"
    This reverts commit 40c84cc2461ec41e092b8dc91825e868366789dd.

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LandlordDetailsController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LandlordDetailsController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import uk.gov.communities.prsdb.webapp.exceptions.PrsdbWebException
-import uk.gov.communities.prsdb.webapp.models.viewModels.UnlabelledSummaryListRowViewModel
+import uk.gov.communities.prsdb.webapp.helpers.converters.MessageKeyConverter
 import uk.gov.communities.prsdb.webapp.services.AddressDataService
 import uk.gov.communities.prsdb.webapp.services.LandlordService
 import java.security.Principal
@@ -33,30 +33,19 @@ class LandlordDetailsController(
 
         // Add personal details to model
         // TODO PRSD-747 to pass Id verification status to model with verified label and message key
-        model.addAttribute("name", UnlabelledSummaryListRowViewModel(landlord.name, "landlord-details/update/name"))
-        model.addAttribute("registrationDate", UnlabelledSummaryListRowViewModel(registeredDate, null))
-        model.addAttribute("dateOfBirth", UnlabelledSummaryListRowViewModel(landlord.dateOfBirth, "landlord-details/update/date-of-birth"))
-        model.addAttribute("email", UnlabelledSummaryListRowViewModel(landlord.email, "landlord-details/update/email"))
-        model.addAttribute("phoneNumber", UnlabelledSummaryListRowViewModel(landlord.phoneNumber, "landlord-details/update/phone-number"))
-        model.addAttribute(
-            "isUKResident",
-            UnlabelledSummaryListRowViewModel(landlord.internationalAddress == null, "landlord-details/update/uk-resident"),
-        )
+        model.addAttribute("name", landlord.name)
+        model.addAttribute("registrationDate", registeredDate)
+        model.addAttribute("dateOfBirth", landlord.dateOfBirth)
+        model.addAttribute("email", landlord.email)
+        model.addAttribute("phoneNumber", landlord.phoneNumber)
+        model.addAttribute("isUKResident", MessageKeyConverter.convert(landlord.internationalAddress == null))
         // TODO PRSD-742 will make country of residence available and should be displayed alongside the international address
-        model.addAttribute(
-            "internationalAddress",
-            UnlabelledSummaryListRowViewModel(landlord.internationalAddress, "landlord-details/update/international-address"),
-        )
-        model.addAttribute(
-            "ukAddress",
-            UnlabelledSummaryListRowViewModel(landlord.address.singleLineAddress, "landlord-details/update/address"),
-        )
+        model.addAttribute("internationalAddress", landlord.internationalAddress)
+        model.addAttribute("ukAddress", landlord.address.singleLineAddress)
         // TODO PRSD-670: Replace with link to dashboard
         model.addAttribute("backUrl", "/")
 
         // TODO PRSD-746 - add user consent information to this page once it is captured.
-        model.addAttribute("legalChanges", UnlabelledSummaryListRowViewModel("TODO-746", null))
-        model.addAttribute("research", UnlabelledSummaryListRowViewModel("TODO-746", null))
 
         // Add properties to model
         // TODO PRSD-702 add properties to model
