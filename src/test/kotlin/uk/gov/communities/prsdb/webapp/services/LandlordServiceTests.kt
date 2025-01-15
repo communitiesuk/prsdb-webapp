33ceb33bb08c8a4fdf67698269603e1e931afb84
JasCon <jasmin.conterio@softwire.com>
2025-01-15 11:24:11 +0000

    PRSD-652: Add new service test

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordServiceTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordServiceTests.kt
@@ -219,4 +219,38 @@ class LandlordServiceTests {
 
         assertEquals(expectedSearchResults, searchResults.content)
     }
+
+    @Test
+    fun `searchForLandlords returns the request page of LandlordSearchResultDataModels`() {
+        val searchQuery = "query"
+        val maxEntriesOnPage = 25
+
+        val landlordsFromRepository = mutableListOf<Landlord>()
+        for (i in 1..40) {
+            landlordsFromRepository.add(createLandlord())
+        }
+
+        val pageRequest1 = PageRequest.of(0, maxEntriesOnPage)
+        val pageRequest2 = PageRequest.of(1, maxEntriesOnPage)
+
+        whenever(mockLandlordRepository.searchMatching(searchQuery, pageRequest1))
+            .thenReturn(PageImpl(landlordsFromRepository.subList(0, maxEntriesOnPage)))
+        whenever(mockLandlordRepository.searchMatching(searchQuery, pageRequest2))
+            .thenReturn(PageImpl(landlordsFromRepository.subList(maxEntriesOnPage, 40)))
+
+        val expectedSearchResultsPage1 =
+            landlordsFromRepository
+                .subList(0, maxEntriesOnPage)
+                .map { LandlordSearchResultDataModel.fromLandlord(it) }
+        val expectedSearchResultsPage2 =
+            landlordsFromRepository
+                .subList(maxEntriesOnPage, 40)
+                .map { LandlordSearchResultDataModel.fromLandlord(it) }
+
+        val searchResults1 = landlordService.searchForLandlords(searchQuery, 0, maxEntriesOnPage)
+        val searchResults2 = landlordService.searchForLandlords(searchQuery, 1, maxEntriesOnPage)
+
+        assertEquals(expectedSearchResultsPage1, searchResults1.content)
+        assertEquals(expectedSearchResultsPage2, searchResults2.content)
+    }
 }
