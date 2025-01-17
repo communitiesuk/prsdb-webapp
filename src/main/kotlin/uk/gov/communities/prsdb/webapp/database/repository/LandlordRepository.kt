c0e09c8d114178d863f6d8abffc3bf6ff263fa3f
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-01-17 13:59:38 +0000

    PRSD-651: Creates Landlord search functionality with optional LA filter

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/LandlordRepository.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/LandlordRepository.kt
@@ -18,11 +18,44 @@ interface LandlordRepository : JpaRepository<Landlord?, Long?> {
         "SELECT l.* " +
             "FROM landlord l  " +
             "WHERE (l.phone_number || ' ' || l.email || ' ' || l.name) %> :searchQuery " +
+            LA_FILTER +
             "ORDER BY (l.phone_number || ' ' || l.email || ' ' || l.name) <->> :searchQuery",
         nativeQuery = true,
     )
     fun searchMatching(
-        @Param("searchQuery")searchQuery: String,
+        @Param("searchQuery") searchQuery: String,
+        @Param("laUserBaseId") laUserBaseId: String? = null,
         pageable: Pageable,
     ): Page<Landlord>
+
+    @Query(
+        "SELECT l.* " +
+            "FROM landlord l JOIN registration_number r on l.registration_number_id = r.id " +
+            "WHERE r.number = :searchLRN " +
+            LA_FILTER,
+        nativeQuery = true,
+    )
+    fun searchMatchingLRN(
+        @Param("searchLRN") searchLRN: Long,
+        @Param("laUserBaseId") laUserBaseId: String? = null,
+        pageable: Pageable,
+    ): Page<Landlord>
+
+    companion object {
+        // Determines if the landlord has an active property ownership in the LA user's LA
+        private const val LA_FILTER =
+            """
+             AND (EXISTS (SELECT po.id 
+                          FROM property_ownership po 
+                          JOIN property p ON po.property_id = p.id 
+                          JOIN address a ON p.address_id = a.id
+                          JOIN local_authority la ON a.local_authority_id = la.id
+                          JOIN local_authority_user lau ON la.id = lau.local_authority_id
+                          JOIN one_login_user ol ON lau.subject_identifier = ol.id
+                          WHERE l.id = po.primary_landlord_id 
+                          AND po.is_active 
+                          AND ol.id = :laUserBaseId)
+                  OR :laUserBaseId IS NULL) 
+            """
+    }
 }
