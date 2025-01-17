a009473f6aec5556c83dff297805fff92b6e8a74
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-01-17 11:15:31 +0000

    PRSD-651: Changes RegistrationNumberDataModel.parseOrNull to parseTypeOrNull

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/RegistrationNumberDataModelTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/RegistrationNumberDataModelTests.kt
@@ -1,8 +1,8 @@
 package uk.gov.communities.prsdb.webapp.models.dataModels
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Named
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -17,78 +17,86 @@ class RegistrationNumberDataModelTests {
         @JvmStatic
         fun provideParseableStringsAndRegNums() =
             listOf(
-                // Expected format
-                Arguments.of("L-CCCC-CCCC", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM)),
-                Arguments.of("P-9999-9999", RegistrationNumberDataModel(RegistrationNumberType.PROPERTY, MAX_REG_NUM)),
-                Arguments.of("A-CCCC-CCCC", RegistrationNumberDataModel(RegistrationNumberType.AGENT, MIN_REG_NUM)),
-                // Unexpected segmentation
-                Arguments.of("L99999999", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM)),
-                Arguments.of("L-999-999-99", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM)),
-                // Unexpected segmenting symbol
-                Arguments.of("L 9999 9999", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM)),
-                // Unexpected case
-                Arguments.of("l-cccc-cccc", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM)),
+                Arguments.of(
+                    Named.of("expected format - LRN", "L-CCCC-CCCC"),
+                    RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM),
+                ),
+                Arguments.of(
+                    Named.of("expected format - PRN", "P-9999-9999"),
+                    RegistrationNumberDataModel(RegistrationNumberType.PROPERTY, MAX_REG_NUM),
+                ),
+                Arguments.of(
+                    Named.of("expected format - ARN", "A-CCCC-CCCC"),
+                    RegistrationNumberDataModel(RegistrationNumberType.AGENT, MIN_REG_NUM),
+                ),
+                Arguments.of(
+                    Named.of("unexpected segmentation - none", "L99999999"),
+                    RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM),
+                ),
+                Arguments.of(
+                    Named.of("unexpected segmentation - too much", "L-999-999-99"),
+                    RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM),
+                ),
+                Arguments.of(
+                    Named.of("unexpected segmenting symbol", "L 9999 9999"),
+                    RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MAX_REG_NUM),
+                ),
+                Arguments.of(
+                    Named.of("unexpected case", "l-cccc-cccc"),
+                    RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM),
+                ),
             )
 
         @JvmStatic
         fun provideNonParseableRegNumStrings() =
             listOf(
-                // Invalid registration number type
-                "9-9999-9999",
-                // Invalid registration number characters
-                "L-1111-1111",
-                "L-LLLL-LLLL",
-                // Invalid registration number length
-                "L-9999-9999-9",
-                "L-9999",
+                Named.of("invalid registration number type", "9-9999-9999"),
+                Named.of("invalid registration number characters - number not in charset", "L-1111-1111"),
+                Named.of("invalid registration number characters - letter not in charset", "L-LLLL-LLLL"),
+                Named.of("invalid registration number length - too long", "L-9999-9999-9"),
+                Named.of("invalid registration number length - too short", "L-9999"),
             )
 
         @JvmStatic
         fun provideRegNumsAndStrings() =
             listOf(
-                Arguments.of(RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM), "L-CCCC-CCCC"),
-                Arguments.of(RegistrationNumberDataModel(RegistrationNumberType.PROPERTY, MAX_REG_NUM), "P-9999-9999"),
-                Arguments.of(RegistrationNumberDataModel(RegistrationNumberType.AGENT, MAX_REG_NUM), "A-9999-9999"),
+                Arguments.of(
+                    Named.of("LRN", RegistrationNumberDataModel(RegistrationNumberType.LANDLORD, MIN_REG_NUM)),
+                    "L-CCCC-CCCC",
+                ),
+                Arguments.of(
+                    Named.of("PRN", RegistrationNumberDataModel(RegistrationNumberType.PROPERTY, MAX_REG_NUM)),
+                    "P-9999-9999",
+                ),
+                Arguments.of(
+                    Named.of("ARN", RegistrationNumberDataModel(RegistrationNumberType.AGENT, MAX_REG_NUM)),
+                    "A-9999-9999",
+                ),
             )
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}")
     @MethodSource("provideParseableStringsAndRegNums")
-    fun `parse returns a corresponding registration number data model`(
+    fun `parseTypeOrNull returns a corresponding registration number data model when given an`(
         parseableString: String,
         expectedRegNum: RegistrationNumberDataModel,
     ) {
-        assertEquals(RegistrationNumberDataModel.parse(parseableString), expectedRegNum)
+        assertEquals(RegistrationNumberDataModel.parseTypeOrNull(parseableString, expectedRegNum.type), expectedRegNum)
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}")
     @MethodSource("provideNonParseableRegNumStrings")
-    fun `parse throws an illegal argument exception when given an invalid registration number`(nonParseableString: String) {
-        assertThrows<IllegalArgumentException> { RegistrationNumberDataModel.parse(nonParseableString) }
+    fun `parseTypeOrNull returns null when given an`(nonParseableString: String) {
+        val anyType = RegistrationNumberType.LANDLORD
+
+        assertNull(RegistrationNumberDataModel.parseTypeOrNull(nonParseableString, anyType))
     }
 
-    @ParameterizedTest
-    @MethodSource("provideRegNumsAndStrings")
-    fun `toString returns a correctly formatted registration number string`(
-        regNum: RegistrationNumberDataModel,
-        expectedString: String,
-    ) {
-        assertEquals(regNum.toString(), expectedString)
-    }
+    @Test
+    fun `parseTypeOrNull returns null when the given registration number is not of the given type`() {
+        val landlordRegNumString = "L-CCCC-CCCC"
 
-    @ParameterizedTest
-    @MethodSource("provideParseableStringsAndRegNums")
-    fun `parseOrNull returns a corresponding registration number data model`(
-        parseableString: String,
-        expectedRegNum: RegistrationNumberDataModel,
-    ) {
-        assertEquals(RegistrationNumberDataModel.parseOrNull(parseableString), expectedRegNum)
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideNonParseableRegNumStrings")
-    fun `parseOrNull returns null when given an invalid registration number`(nonParseableString: String) {
-        assertNull(RegistrationNumberDataModel.parseOrNull(nonParseableString))
+        assertNull(RegistrationNumberDataModel.parseTypeOrNull(landlordRegNumString, RegistrationNumberType.PROPERTY))
     }
 
     @Test
@@ -99,5 +107,14 @@ class RegistrationNumberDataModelTests {
         val registrationNumberDataModel = RegistrationNumberDataModel.fromRegistrationNumber(registrationNumber)
 
         assertEquals(expectedRegNumDataModel, registrationNumberDataModel)
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideRegNumsAndStrings")
+    fun `toString returns a correctly formatted registration number string when given a`(
+        regNum: RegistrationNumberDataModel,
+        expectedString: String,
+    ) {
+        assertEquals(regNum.toString(), expectedString)
     }
 }
