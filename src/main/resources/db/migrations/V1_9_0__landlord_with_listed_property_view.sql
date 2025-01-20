8d5b1da33a128d3b0a0ae75bbc7e93471c2c9cf9
JasCon <jasmin.conterio@softwire.com>
2025-01-20 16:51:40 +0000

    PRSD-654: Fix query to include landlords with no property ownerships

--- src/main/resources/db/migrations/V1_9_0__landlord_with_listed_property_view.sql
+++ src/main/resources/db/migrations/V1_9_0__landlord_with_listed_property_view.sql
@@ -1,4 +1,5 @@
 CREATE VIEW landlord_with_listed_property_count AS
-SELECT po.primary_landlord_id as landlord_id, COUNT(po.primary_landlord_id) as count
+SELECT l.id as landlord_id, COUNT(po.primary_landlord_id) as count
     FROM property_ownership po
-    GROUP BY po.primary_landlord_id
+    RIGHT JOIN landlord l ON po.primary_landlord_id = l.id
+    GROUP BY l.id
