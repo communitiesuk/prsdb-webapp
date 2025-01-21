8097379dc5fdf0b6a7059b705a2fddba059181b9
JasCon <jasmin.conterio@softwire.com>
2025-01-21 15:49:21 +0000

    PRSD-654: Tweaks before review

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/LandlordWithListedPropertyCount.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/LandlordWithListedPropertyCount.kt
@@ -1,8 +1,6 @@
 package uk.gov.communities.prsdb.webapp.database.entity
 
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
@@ -10,7 +8,6 @@ import jakarta.persistence.OneToOne
 @Entity(name = "landlord_with_listed_property_count")
 class LandlordWithListedPropertyCount(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val landlordId: Long = 0,
 ) {
     @OneToOne(optional = false)
