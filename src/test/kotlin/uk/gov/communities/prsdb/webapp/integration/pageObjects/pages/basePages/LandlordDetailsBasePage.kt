5aa09b3e412c2129379af5a755b5dea665f41d54
Kiran Randhawa Kukar <113037474+Kiran-R-K@users.noreply.github.com>
2025-01-27 13:36:55 +0000

    Feat/prsd 658 local authority landlord record view integration tests (#153)

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/basePages/LandlordDetailsBasePage.kt
@@ -0,0 +1,19 @@
+package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages
+
+import com.microsoft.playwright.Page
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Table
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Tabs
+
+abstract class LandlordDetailsBasePage(
+    page: Page,
+    urlSegment: String,
+) : BasePage(page, urlSegment) {
+    val tabs = Tabs(page, 2)
+    val table = Table(page)
+
+    fun getActiveTabPanelId() = tabs.getActiveTabPanelId()
+
+    fun goToRegisteredProperties() {
+        tabs.goToTab("Registered properties")
+    }
+}
