d52e44c7016aef7f79d364119ac50affc4bd8ca2
JasCon <jasmin.conterio@softwire.com>
2025-01-27 09:59:05 +0000

    PRSD-654: Update the view query

--- src/main/resources/db/migrations/V1_11_0__landlord_with_listed_property_view.sql
+++ src/main/resources/db/migrations/V1_11_0__landlord_with_listed_property_view.sql
@@ -1,5 +1,5 @@
 CREATE VIEW landlord_with_listed_property_count AS
-SELECT l.id as landlord_id, COUNT (*) FILTER (WHERE po.is_active = true) AS listed_property_count
+SELECT l.id as landlord_id, COUNT (po.primary_landlord_id) AS listed_property_count
     FROM property_ownership po
-    RIGHT JOIN landlord l ON po.primary_landlord_id = l.id
+    RIGHT JOIN landlord l ON po.primary_landlord_id = l.id AND po.is_active = true
     GROUP BY l.id
