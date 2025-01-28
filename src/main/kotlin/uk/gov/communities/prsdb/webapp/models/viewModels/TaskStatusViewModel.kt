90579a2f75bee0d65dcbca6e4cbfbe1175df8da9
Alexander Read <alexander.read@communities.gov.uk>
2025-01-28 11:43:00 +0000

    PRSD-600: Make boolean values on TaskStatusViewModel easier to use

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/TaskStatusViewModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/TaskStatusViewModel.kt
@@ -4,16 +4,18 @@ import uk.gov.communities.prsdb.webapp.constants.enums.TaskStatus
 
 data class TaskStatusViewModel(
     val textKey: String,
-    val tag: Boolean = false,
     val tagClass: String? = null,
-    val cannotStart: Boolean = false,
+    val isCannotStart: Boolean = false,
 ) {
+    val isTag
+        get() = tagClass != null
+
     companion object {
         fun fromStatus(status: TaskStatus): TaskStatusViewModel {
             when (status) {
-                TaskStatus.CANNOT_START_YET -> return TaskStatusViewModel("taskList.status.cannotStartYet", cannotStart = true)
-                TaskStatus.NOT_YET_STARTED -> return TaskStatusViewModel("taskList.status.notYetStarted", true, "govuk-tag--blue")
-                TaskStatus.IN_PROGRESS -> return TaskStatusViewModel("taskList.status.inProgress", true, "govuk-tag--light-blue")
+                TaskStatus.CANNOT_START_YET -> return TaskStatusViewModel("taskList.status.cannotStartYet", isCannotStart = true)
+                TaskStatus.NOT_YET_STARTED -> return TaskStatusViewModel("taskList.status.notYetStarted", tagClass = "govuk-tag--blue")
+                TaskStatus.IN_PROGRESS -> return TaskStatusViewModel("taskList.status.inProgress", tagClass = "govuk-tag--light-blue")
                 TaskStatus.COMPLETED -> return TaskStatusViewModel("taskList.status.completed")
             }
         }
