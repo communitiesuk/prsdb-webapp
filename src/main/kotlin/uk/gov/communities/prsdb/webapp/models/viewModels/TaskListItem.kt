237cb05f220a6e92adfd9c6e9dfeef74f3fd49a5
Alexander Read <alexander.read@communities.gov.uk>
2025-01-28 09:14:01 +0000

    PRSD-600: Task list stores name key not name

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/TaskListItem.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/TaskListItem.kt
@@ -1,7 +1,7 @@
 package uk.gov.communities.prsdb.webapp.models.viewModels
 
 data class TaskListItem(
-    val name: String,
+    val nameKey: String,
     val status: TaskStatusViewModel,
     val url: String? = null,
 )
