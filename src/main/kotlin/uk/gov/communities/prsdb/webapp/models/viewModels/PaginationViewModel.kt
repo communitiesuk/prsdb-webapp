1514da611ce10b0b63bcd35d8cefc76f5610daac
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-01-30 16:02:34 +0000

    PRSD-NONE: Review refactors

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/PaginationViewModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/PaginationViewModel.kt
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServletRequest
 import uk.gov.communities.prsdb.webapp.helpers.URIQueryBuilder
 
 data class PaginationViewModel(
-    val page: Int,
+    val currentPage: Int,
     val totalPages: Int,
     private val httpServletRequest: HttpServletRequest,
 ) {
@@ -15,7 +15,7 @@ data class PaginationViewModel(
             .build()
             .toUriString()
 
-    fun getPreviousPageLink() = getPageLink(page - 1)
+    fun getPreviousPageLink() = getPageLink(currentPage - 1)
 
-    fun getNextPageLink() = getPageLink(page + 1)
+    fun getNextPageLink() = getPageLink(currentPage + 1)
 }
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/PaginationViewModelTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/PaginationViewModelTests.kt
@@ -17,7 +17,7 @@ class PaginationViewModelTests {
     @Test
     fun `getPageLink returns a link to the given page number (page insert)`() {
         mockHTTPServletRequest.queryString = "name=value"
-        val paginationViewModel = PaginationViewModel(page = 1, totalPages = 10, mockHTTPServletRequest)
+        val paginationViewModel = PaginationViewModel(currentPage = 1, totalPages = 10, mockHTTPServletRequest)
         val expectedPageLink = "${mockHTTPServletRequest.requestURI}?name=value&page=5"
 
         val pageLink = paginationViewModel.getPageLink(5)
@@ -28,7 +28,7 @@ class PaginationViewModelTests {
     @Test
     fun `getPageLink returns a link to the given page number (page update)`() {
         mockHTTPServletRequest.queryString = "name=value&page=2"
-        val paginationViewModel = PaginationViewModel(page = 2, totalPages = 10, mockHTTPServletRequest)
+        val paginationViewModel = PaginationViewModel(currentPage = 2, totalPages = 10, mockHTTPServletRequest)
         val expectedPageLink = "${mockHTTPServletRequest.requestURI}?name=value&page=5"
 
         val pageLink = paginationViewModel.getPageLink(5)
@@ -39,7 +39,7 @@ class PaginationViewModelTests {
     @Test
     fun `getPreviousPageLink returns a link to the previous page`() {
         mockHTTPServletRequest.queryString = "name=value&page=2"
-        val paginationViewModel = PaginationViewModel(page = 2, totalPages = 10, mockHTTPServletRequest)
+        val paginationViewModel = PaginationViewModel(currentPage = 2, totalPages = 10, mockHTTPServletRequest)
         val expectedPageLink = "${mockHTTPServletRequest.requestURI}?name=value&page=1"
 
         val pageLink = paginationViewModel.getPreviousPageLink()
@@ -50,7 +50,7 @@ class PaginationViewModelTests {
     @Test
     fun `getNextPageLink returns a link to the next page`() {
         mockHTTPServletRequest.queryString = "name=value&page=2"
-        val paginationViewModel = PaginationViewModel(page = 2, totalPages = 10, mockHTTPServletRequest)
+        val paginationViewModel = PaginationViewModel(currentPage = 2, totalPages = 10, mockHTTPServletRequest)
         val expectedPageLink = "${mockHTTPServletRequest.requestURI}?name=value&page=3"
 
         val pageLink = paginationViewModel.getNextPageLink()
