054beda5be497bec143eaa245695cbd2a5b9e44c
Jasmin Conterio <jasmin.conterio@communities.gov.uk>
2025-02-03 15:09:45 +0000

    Feat/prsd 719 property details (#170)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/PropertyDetailsController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/PropertyDetailsController.kt
@@ -1,11 +1,14 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.server.ResponseStatusException
+import uk.gov.communities.prsdb.webapp.models.viewModels.PropertyDetailsViewModel
 import uk.gov.communities.prsdb.webapp.services.PropertyOwnershipService
 
 @Controller
@@ -19,7 +22,16 @@ class PropertyDetailsController(
         @PathVariable propertyOwnershipId: Long,
         model: Model,
     ): String {
-        val propertyOwnership = propertyOwnershipService.retrievePropertyOwnership(propertyOwnershipId)
+        val propertyOwnership =
+            propertyOwnershipService.retrievePropertyOwnershipById(propertyOwnershipId)
+                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Property ownership $propertyOwnershipId not found")
+        if (!propertyOwnership.isActive) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Property ownership $propertyOwnershipId is inactive")
+        }
+
+        val propertyDetails = PropertyDetailsViewModel(propertyOwnership)
+
+        model.addAttribute("propertyDetails", propertyDetails)
 
         return "propertyDetailsView"
     }
