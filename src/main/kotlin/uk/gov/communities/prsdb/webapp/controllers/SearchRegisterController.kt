9ed10a3649608ea1a4779646d8853e0f531c98ab
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-02-03 18:16:36 +0000

    PRSD-651: Implements LA filter for landlord search

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/SearchRegisterController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/SearchRegisterController.kt
@@ -8,8 +8,11 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import uk.gov.communities.prsdb.webapp.helpers.URIQueryBuilder
+import uk.gov.communities.prsdb.webapp.models.dataModels.LandlordSearchResultDataModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.searchModels.LandlordSearchRequestModel
 import uk.gov.communities.prsdb.webapp.models.viewModels.PaginationViewModel
-import uk.gov.communities.prsdb.webapp.models.wrapperModels.SearchWrapperModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.searchModels.LandlordSearchViewModel
 import uk.gov.communities.prsdb.webapp.services.LandlordService
 import java.security.Principal
 
@@ -22,36 +25,50 @@ class SearchRegisterController(
     @GetMapping("/landlord")
     fun searchForLandlords(
         model: Model,
-        @RequestParam(required = false) query: String?,
-        @RequestParam(value = "page", required = false) @Min(1) page: Int = 1,
         principal: Principal,
         httpServletRequest: HttpServletRequest,
+        searchRequest: LandlordSearchRequestModel,
+        @RequestParam(value = "page", required = false) @Min(1) page: Int = 1,
     ): String {
-        var totalPages = 0
-
-        if (!query.isNullOrBlank()) {
-            val pagedLandlordList =
-                landlordService.searchForLandlords(query, principal.name, currentPageNumber = page - 1)
-
-            if (pagedLandlordList.totalPages != 0 && pagedLandlordList.totalPages < page) {
-                return "redirect:/search/landlord?query=$query"
-            }
-
-            totalPages = pagedLandlordList.totalPages
-
-            model.addAttribute("searchResults", pagedLandlordList.content)
+        if (searchRequest.query?.isBlank() == true) {
+            return "redirect:landlord"
         }
 
+        var searchResults: List<LandlordSearchResultDataModel>? = null
+        var totalPages = 0
+
+        if (searchRequest.query != null) {
+            val pagedLandlordList =
+                landlordService.searchForLandlords(
+                    searchRequest.query!!,
+                    principal.name,
+                    searchRequest.useLAFilter,
+                    currentPageNumber = page - 1,
+                )
+
+            if (pagedLandlordList.totalPages != 0 && pagedLandlordList.totalPages < page) {
+                return "redirect:${
+                    URIQueryBuilder.fromHTTPServletRequest(httpServletRequest).removeParam("page").build().toUriString()
+                }"
+            }
+
+            searchResults = pagedLandlordList.content
+            totalPages = pagedLandlordList.totalPages
+        }
+
+        model.addAttribute("searchResults", searchResults)
         model.addAttribute("paginationViewModel", PaginationViewModel(page, totalPages, httpServletRequest))
 
-        model.addAttribute("searchWrapperModel", SearchWrapperModel())
+        model.addAttribute("searchRequest", searchRequest)
+        model.addAttribute("searchViewModel", LandlordSearchViewModel(searchRequest, httpServletRequest))
+
         model.addAttribute("baseLandlordDetailsURL", "/landlord-details")
         // TODO PRSD-659: add LA property search base URL to model
         model.addAttribute("propertySearchURL", "property")
         // TODO PRSD-647: Set backURL to LA landing page
         model.addAttribute("backURL", "")
 
-        return if (query?.isBlank() == true) "redirect:landlord" else "searchLandlord"
+        return "searchLandlord"
     }
 
     // TODO PRSD-659: implement property search endpoint
