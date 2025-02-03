a7809df21d1cc3d81a4288715ccf2374b80e6a9b
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-02-03 18:11:43 +0000

    PRSD-651: Creates SearchViewModel (determines selected options and links)

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/searchModels/SearchViewModel.kt
@@ -0,0 +1,77 @@
+package uk.gov.communities.prsdb.webapp.models.viewModels.searchModels
+
+import jakarta.servlet.http.HttpServletRequest
+import uk.gov.communities.prsdb.webapp.helpers.URIQueryBuilder
+import uk.gov.communities.prsdb.webapp.models.requestModels.searchModels.SearchRequestModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.CheckboxViewModel
+import kotlin.reflect.full.memberProperties
+
+abstract class SearchViewModel(
+    val filters: List<FilterViewModel>,
+    searchRequestModel: SearchRequestModel,
+    httpServletRequest: HttpServletRequest,
+) {
+    val clearLink =
+        URIQueryBuilder
+            .fromHTTPServletRequest(httpServletRequest)
+            .removeParams(filters.map { it.category })
+            .build()
+            .toUriString()
+
+    val showFilterLink =
+        URIQueryBuilder
+            .fromHTTPServletRequest(httpServletRequest)
+            .updateParam("showFilter", !searchRequestModel.showFilter)
+            .build()
+            .toUriString()
+
+    init {
+        filters.forEach { it.initializeSelectedOptions(searchRequestModel, httpServletRequest) }
+    }
+}
+
+class FilterViewModel(
+    val headingMsgKey: String,
+    val category: String,
+    val options: List<CheckboxViewModel<Any>>,
+) {
+    lateinit var selectedOptions: List<SelectedFilterOptionViewModel>
+
+    fun initializeSelectedOptions(
+        searchRequestModel: SearchRequestModel,
+        httpServletRequest: HttpServletRequest,
+    ) {
+        val selectedOptionRequest =
+            searchRequestModel::class
+                .memberProperties
+                .single { it.name == category }
+                .getter
+                .call(searchRequestModel)
+
+        selectedOptions =
+            options
+                .filter {
+                    if (selectedOptionRequest is Collection<*>) {
+                        selectedOptionRequest.contains(it.value)
+                    } else {
+                        selectedOptionRequest?.equals(it.value) == true
+                    }
+                }.map { SelectedFilterOptionViewModel(category, it, httpServletRequest) }
+    }
+}
+
+class SelectedFilterOptionViewModel(
+    category: String,
+    selectedOption: CheckboxViewModel<Any>,
+    httpServletRequest: HttpServletRequest,
+) {
+    val labelMsgOrVal = selectedOption.labelMsgKey ?: selectedOption.valueStr
+
+    val removeLink =
+        URIQueryBuilder
+            .fromHTTPServletRequest(httpServletRequest)
+            .removeParamValue(category, selectedOption.value.toString())
+            .removeParam("page")
+            .build()
+            .toUriString()
+}
