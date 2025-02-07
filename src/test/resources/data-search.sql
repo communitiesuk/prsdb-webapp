578c1b5195f3a7f7062dd164e500f1533ec38239
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-02-07 12:17:16 +0000

    PRSD-NONE: Updates seed data to use fake email addresses

--- src/test/resources/data-search.sql
+++ src/test/resources/data-search.sql
@@ -1,9 +1,9 @@
 INSERT INTO one_login_user (id, name, email, created_date, last_modified_date)
-VALUES ('urn:fdc:gov.uk:2022:ABCDE', 'Bob T Builder', 'bobthebuilder@gmail.com', '09/13/24', '09/13/24'),
-       ('urn:fdc:gov.uk:2022:FGHIJ', 'Anne Other', 'Anne.Other@hotmail.com', '09/13/24', '09/13/24'),
-       ('urn:fdc:gov.uk:2022:KLMNO', 'Ford Prefect', 'Ford.Prefect@hotmail.com', '10/07/24', '10/07/24'),
+VALUES ('urn:fdc:gov.uk:2022:ABCDE', 'Bob T Builder', 'bobthebuilder@test.com', '09/13/24', '09/13/24'),
+       ('urn:fdc:gov.uk:2022:FGHIJ', 'Anne Other', 'Anne.Other@example.com', '09/13/24', '09/13/24'),
+       ('urn:fdc:gov.uk:2022:KLMNO', 'Ford Prefect', 'Ford.Prefect@test.com', '10/07/24', '10/07/24'),
        ('urn:fdc:gov.uk:2022:UVWXY', 'Mock User', 'test@example.com', '10/14/24', '10/14/24'),
-       ('urn:fdc:gov.uk:2022:PQRST', 'Arthur Dent', 'Arthur.Dent@hotmail.com', '10/09/24', '10/09/24'),
+       ('urn:fdc:gov.uk:2022:PQRST', 'Arthur Dent', 'Arthur.Dent@example.com', '10/09/24', '10/09/24'),
        ('urn:fdc:gov.uk:2022:07lXHJeQwE0k5PZO7w_PQF425vT8T7e63MrvyPYNSoI', 'Jasmin Conterio',
         'jasmin.conterio@softwire.com', '10/07/24', '10/07/24'),
        ('urn:fdc:gov.uk:2022:mwfvbb5GgiDh0acjz9EDDQ7zwskWZzUSnWfavL70f6s', 'Isobel Ibironke',
@@ -43,7 +43,7 @@ VALUES ('urn:fdc:gov.uk:2022:ABCDE', 'Bob T Builder', 'bobthebuilder@gmail.com',
 
 INSERT INTO local_authority_user (subject_identifier, is_manager, local_authority_id, created_date, last_modified_date,
                                   name, email)
-VALUES ('urn:fdc:gov.uk:2022:UVWXY', true, 1, '10/14/24', '10/14/24', 'Mock User', 'test@la.com');
+VALUES ('urn:fdc:gov.uk:2022:UVWXY', true, 1, '10/14/24', '10/14/24', 'Mock User', 'test@example.com');
 
 INSERT INTO registration_number (id, created_date, number, type)
 VALUES (1, '09/13/24', 2001001001, 1),
@@ -158,13 +158,13 @@ VALUES (1, '09/13/24', '09/13/24', 1, 1, '09/13/2000', true, 07111111111, 'urn:f
        (2, '09/13/24', '09/13/24', 2, 2, '08/13/2001', true, 07111111111, 'urn:fdc:gov.uk:2022:ABCDE',
         'Alexandra Davies', 'alexandra.q.davies@example.com'),
        (3, '09/13/24', '09/13/24', 3, 3, '07/13/1997', true, 07111111111, 'urn:fdc:gov.uk:2022:PQRST',
-        'Evan Alexandrescu', 'unrelatedemail@completelydifferentdomain.com'),
+        'Evan Alexandrescu', 'unrelatedemail@test.com'),
        (4, '09/13/24', '09/13/24', 4, 4, '06/13/1989', true, 07111111111,
         'urn:fdc:gov.uk:2022:07lXHJeQwE0k5PZO7w_PQF425vT8T7e63MrvyPYNSoI', 'Tobias Evans PRSDB',
-        'tobyevans@importantco.com'),
+        'tobyevans@test.com'),
        (5, '09/13/24', '09/13/24', 5, 5, '05/13/1950', true, 07111111111,
         'urn:fdc:gov.uk:2022:mwfvbb5GgiDh0acjz9EDDQ7zwskWZzUSnWfavL70f6s', 'Margaret Mary Smith PRSDB',
-        'mm.smith@importantco.com'),
+        'mm.smith@test.com'),
        (6, '12/19/24', '12/19/24', 7, 5, '06/13/1989', true, 07111111111,
         'urn:fdc:gov.uk:2022:mGHDySEVfCsvfvc6lVWf6Qt9Dv0ZxPQWKoEzcjnBlUo', 'PRSDB Landlord',
         'Team-PRSDB+landlord@softwire.com'),
