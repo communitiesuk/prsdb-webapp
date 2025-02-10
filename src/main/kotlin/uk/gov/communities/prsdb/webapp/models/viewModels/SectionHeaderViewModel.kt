7acae3d5f55bf2ec1aa7e06adf5186bc42f22425
JasCon <jasmin.conterio@softwire.com>
2025-02-10 14:58:23 +0000

    PRSD-858: Add section header to lookup address page (with hardcoded values)

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/SectionHeaderViewModel.kt
@@ -0,0 +1,7 @@
+package uk.gov.communities.prsdb.webapp.models.viewModels
+
+class SectionHeaderViewModel(
+    val sectionNameKey: String,
+    val sectionNumber: Int,
+    val totalSections: Int,
+)
