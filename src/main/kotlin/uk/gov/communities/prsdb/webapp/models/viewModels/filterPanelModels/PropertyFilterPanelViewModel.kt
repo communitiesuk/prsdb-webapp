76ac67c20f45cb734c847d4d11dcd28586c3cbce
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-02-13 17:20:19 +0000

    PRSD-663: Adds license filter to PropertySearchRequestModel and PropertyFilterPanelViewModel

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/filterPanelModels/PropertyFilterPanelViewModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/filterPanelModels/PropertyFilterPanelViewModel.kt
@@ -1,6 +1,7 @@
 package uk.gov.communities.prsdb.webapp.models.viewModels.filterPanelModels
 
 import jakarta.servlet.http.HttpServletRequest
+import uk.gov.communities.prsdb.webapp.constants.enums.LicensingType
 import uk.gov.communities.prsdb.webapp.models.requestModels.searchModels.PropertySearchRequestModel
 import uk.gov.communities.prsdb.webapp.models.viewModels.CheckboxViewModel
 
@@ -14,6 +15,29 @@ class PropertyFilterPanelViewModel(
                     headingMsgKey = "propertySearch.filter.la.heading",
                     searchRequestProperty = "restrictToLA",
                     options = listOf(CheckboxViewModel(value = true, labelMsgKey = "propertySearch.filter.la.label")),
+                ),
+                FilterViewModel(
+                    headingMsgKey = "propertySearch.filter.licence.heading",
+                    searchRequestProperty = "restrictToLicenses",
+                    options =
+                        listOf(
+                            CheckboxViewModel(
+                                value = LicensingType.SELECTIVE_LICENCE,
+                                labelMsgKey = "propertySearch.filter.licence.selective.label",
+                            ),
+                            CheckboxViewModel(
+                                value = LicensingType.HMO_MANDATORY_LICENCE,
+                                labelMsgKey = "propertySearch.filter.licence.hmoMandatory.label",
+                            ),
+                            CheckboxViewModel(
+                                value = LicensingType.HMO_ADDITIONAL_LICENCE,
+                                labelMsgKey = "propertySearch.filter.licence.hmoAdditional.label",
+                            ),
+                            CheckboxViewModel(
+                                value = LicensingType.NO_LICENSING,
+                                labelMsgKey = "propertySearch.filter.licence.not.label",
+                            ),
+                        ),
                 ),
             ),
         searchRequestModel = searchRequestModel,
