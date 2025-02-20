046bf82f31f42ab7ca47098fe0075047e573b9c0
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-02-20 16:20:54 +0000

    PRSD-835: Makes NavigationLink model a view model

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/NavigationLinkDataModel.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.communities.prsdb.webapp.models.dataModels
-
-data class NavigationLinkDataModel(
-    val href: String,
-    val messageProperty: String,
-    val isActive: Boolean,
-)
