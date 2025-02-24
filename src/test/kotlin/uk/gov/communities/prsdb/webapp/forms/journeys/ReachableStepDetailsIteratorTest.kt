4708ad11ba14cda3bff7580080f962e09c918f7c
Alexander Read <alexander.read@communities.gov.uk>
2025-02-24 13:51:25 +0000

    PRSD-901: Add Failing tests

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/ReachableStepDetailsIteratorTest.kt
@@ -0,0 +1,395 @@
+package uk.gov.communities.prsdb.webapp.forms.journeys
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.mock
+import uk.gov.communities.prsdb.webapp.forms.steps.Step
+import uk.gov.communities.prsdb.webapp.forms.steps.StepId
+import kotlin.test.assertEquals
+
+class ReachableStepDetailsIteratorTest {
+    @Nested
+    inner class HasNextTests {
+        @Test
+        fun `hasNext returns false if the journey is completed and the current step is the last step`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEnd(TestStepModel("1"))
+                    .build()
+
+            // Act && assert
+            assertFalse(testIterator.hasNext())
+        }
+
+        @Test
+        fun `hasNext returns false if the current step hasn't been completed`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEndWithoutJourneyData(TestStepModel("1", isSatisfied = true))
+                    .addStepToEndWithoutJourneyData(TestStepModel("2", isSatisfied = true))
+                    .build()
+
+            // Act && assert
+            assertFalse(testIterator.hasNext())
+        }
+
+        @Test
+        fun `hasNext returns false if the current step has been completed with invalid data`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = false))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .build()
+
+            // Act && assert
+            assertFalse(testIterator.hasNext())
+        }
+
+        @Test
+        fun `hasNext returns true if the current step has been completed with valid data and isn't the last step in the journey`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .build()
+
+            // Act && assert
+            assertTrue(testIterator.hasNext())
+        }
+
+        @Test
+        fun `hasNext returns false if it hasn't been initialised and there is no initial step`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .withMissingFirstStep()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .withMissingFirstStep()
+                    .build()
+
+            // Act && assert
+            assertFalse(testIterator.hasNext())
+        }
+
+        @Test
+        fun `hasNext returns false if it hasn't been initialised and there are multiple initial steps`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .build()
+
+            // Act && assert
+            assertFalse(testIterator.hasNext())
+        }
+
+        @Test
+        fun `hasNext returns true if it hasn't been initialised and there is a valid initial step`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .build()
+
+            // Act && assert
+            assertTrue(testIterator.hasNext())
+        }
+    }
+
+    @Nested
+    inner class NextTests {
+        @Test
+        fun `next returns the initial step if it hasn't been initialised`() {
+            // Arrange
+            val firstUrlSegment = "test step id"
+            val testIterator =
+                TestIteratorBuilder()
+                    .addStepToEnd(TestStepModel(firstUrlSegment, isSatisfied = true))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .build()
+
+            // Act && assert
+            assertEquals(TestStepId(firstUrlSegment), testIterator.next().step.id)
+        }
+
+        @Test
+        fun `next returns the the current step's next action if it has initialised`() {
+            // Arrange
+            val subsequentUrlSegment = "test step id"
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .addStepToEnd(TestStepModel(subsequentUrlSegment, isSatisfied = false))
+                    .build()
+
+            // Act
+            val nextStepDetails = testIterator.next()
+
+            // Assert
+            assertEquals(TestStepId(subsequentUrlSegment), nextStepDetails.step.id)
+        }
+
+        @Test
+        fun `next adds the current step's data to the filtered journey data of the next step`() {
+            // Arrange
+            val currentStepModel = TestStepModel("1", isSatisfied = true)
+            val builder =
+                TestIteratorBuilder()
+                    .onStep(1)
+                    .addStepToEnd(currentStepModel)
+                    .addStepToEnd(TestStepModel("2", isSatisfied = true))
+            val pageDataForStep = builder.getDataForStep(currentStepModel.urlPathSegment)
+
+            val testIterator = builder.build()
+
+            // Act
+            val nextStepDetails = testIterator.next()
+
+            // Assert
+            assertEquals(pageDataForStep, nextStepDetails.filteredJourneyData[currentStepModel.urlPathSegment])
+        }
+
+        @Test
+        fun `next does not add the next step's data to the filtered journey data of the next step`() {
+            // Arrange
+            val stepModel = TestStepModel("1", isSatisfied = true)
+            val builder = TestIteratorBuilder().addStepToEnd(stepModel)
+
+            val testIterator = builder.build()
+
+            // Act
+            val nextStepDetails = testIterator.next()
+
+            // Assert
+            assertNull(nextStepDetails.filteredJourneyData[stepModel.urlPathSegment])
+        }
+
+        @Test
+        fun `next does not change any of the filtered journey data except the for the current steps data`() {
+            // Arrange
+            val builder =
+                TestIteratorBuilder()
+                    .onStep(4)
+                    .addStepToEnd(TestStepModel("1", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("3", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("4", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("5", isSatisfied = true))
+                    .addStepToEnd(TestStepModel("6", isSatisfied = true))
+
+            val testIterator = builder.build()
+
+            // Act
+            val currentStepDetails = testIterator.next()
+            val nextStepDetails = testIterator.next()
+
+            // Assert
+            val previousFilteredJourneyData = currentStepDetails.filteredJourneyData
+            for (key in previousFilteredJourneyData.keys) {
+                if (key != nextStepDetails.step.id.urlPathSegment) {
+                    assertEquals(previousFilteredJourneyData[key], nextStepDetails.filteredJourneyData[key])
+                }
+            }
+        }
+
+        @Test
+        fun `next throws an exception if the current step does not have valid data`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEnd(TestStepModel("1", isSatisfied = false))
+                    .addStepToEnd(TestStepModel("2", isSatisfied = false))
+                    .build()
+
+            // Act && assert
+            assertThrows<NoSuchElementException> { testIterator.next() }
+        }
+
+        @Test
+        fun `next throws an exception if the current step does not have a next action`() {
+            // Arrange
+            val testIterator =
+                TestIteratorBuilder()
+                    .initialised()
+                    .addStepToEnd(TestStepModel("1"))
+                    .build()
+
+            // Act && assert
+            assertThrows<NoSuchElementException> { testIterator.next() }
+        }
+    }
+
+    @Nested
+    inner class ImmutableJourneyDataTests {
+        @Test
+        fun `Changing the initialising journey data after the iterator is created does not change the iterator`() {
+            // Arrange
+            val previousModel = TestStepModel("1")
+            val builder =
+                TestIteratorBuilder()
+                    .onStep(2)
+                    .addStepToEnd(previousModel)
+                    .addStepToEnd(TestStepModel("2"))
+                    .addStepToEnd(TestStepModel("3"))
+
+            val testIterator = builder.build()
+
+            // Act
+            builder.clearJourneyData()
+            val testStepDetails = testIterator.next()
+
+            // Assert
+            assertTrue(testStepDetails.filteredJourneyData.containsKey(previousModel.urlPathSegment))
+        }
+
+        @Test
+        fun `Changing the current step's filtered journey data does not affect the next step's journey data`() {
+            // Arrange
+            val previousModel = TestStepModel("1")
+            val testIterator =
+                TestIteratorBuilder()
+                    .onStep(1)
+                    .addStepToEnd(previousModel)
+                    .addStepToEnd(TestStepModel("2"))
+                    .addStepToEnd(TestStepModel("3"))
+                    .build()
+            val currentStepDetails = testIterator.next()
+
+            // Act
+            currentStepDetails.filteredJourneyData.clear()
+            val nextStepDetails = testIterator.next()
+
+            // Assert
+            assertTrue(nextStepDetails.filteredJourneyData.containsKey(previousModel.urlPathSegment))
+        }
+
+        @Test
+        fun `If a nextStep function updates the journey data, that does not affect the next step's journey data`() {
+            // Arrange
+            val previousModel = TestStepModel("1")
+            val testIterator =
+                TestIteratorBuilder()
+                    .onStep(2)
+                    .addStepToEnd(previousModel)
+                    .addStepToEnd(TestStepModel("2", customNextActionAddition = { journeyData -> journeyData.clear() }))
+                    .addStepToEnd(TestStepModel("3"))
+                    .build()
+
+            // Act
+            val nextStepDetails = testIterator.next()
+
+            // Assert
+            assertTrue(nextStepDetails.filteredJourneyData.containsKey(previousModel.urlPathSegment))
+        }
+    }
+}
+
+data class TestStepId(
+    override val urlPathSegment: String,
+) : StepId
+
+data class TestStepModel(
+    val urlPathSegment: String,
+    val isSatisfied: Boolean = true,
+    val customNextActionAddition: ((JourneyData) -> Unit)? = null,
+)
+
+class TestIteratorBuilder {
+    private var initialised = false
+    private val steps: MutableList<TestStepModel> = mutableListOf()
+    private val journeyData: JourneyData = mutableMapOf()
+    private var missingFirstStep: Boolean = false
+    private var onStep: Int? = null
+
+    fun initialised(): TestIteratorBuilder {
+        initialised = true
+        return this
+    }
+
+    fun onStep(step: Int): TestIteratorBuilder {
+        onStep = step
+        return this
+    }
+
+    fun withMissingFirstStep(): TestIteratorBuilder {
+        missingFirstStep = true
+        return this
+    }
+
+    fun addStepToEnd(stepModel: TestStepModel): TestIteratorBuilder {
+        journeyData[stepModel.urlPathSegment] = mapOf("urlPathSegment" to stepModel.urlPathSegment, "isSatisfied" to stepModel.isSatisfied)
+        return addStepToEndWithoutJourneyData(stepModel)
+    }
+
+    fun addStepToEndWithoutJourneyData(stepModel: TestStepModel): TestIteratorBuilder {
+        steps.add(stepModel)
+        return this
+    }
+
+    fun build(): ReachableStepDetailsIterator<TestStepId> {
+        val linkedSteps =
+            steps.zipWithNext {
+                    stepModel,
+                    nextStepModel,
+                ->
+                testStep(stepModel.urlPathSegment, nextStepModel.urlPathSegment, stepModel.isSatisfied, stepModel.customNextActionAddition)
+            } + testStep(steps.last().urlPathSegment, isSatisfied = steps.last().isSatisfied)
+
+        val firstStepId =
+            if (missingFirstStep) {
+                // A concatenation of all strings in a list cannot be contained by the list
+                TestStepId(steps.joinToString { it.urlPathSegment })
+            } else {
+                TestStepId(steps.first().urlPathSegment)
+            }
+
+        val iterator = ReachableStepDetailsIterator(journeyData, linkedSteps, firstStepId, mock())
+
+        val currentOnStep = onStep
+        if (currentOnStep != null) {
+            repeat(currentOnStep) { iterator.next() }
+        } else if (initialised) {
+            iterator.next()
+        }
+
+        return iterator
+    }
+
+    private fun testStep(
+        urlPathSegment: String,
+        nextSegment: String? = null,
+        isSatisfied: Boolean = true,
+        customNextActionAddition: ((JourneyData) -> Unit)? = null,
+    ) = Step(
+        TestStepId(urlPathSegment),
+        mock(),
+        handleSubmitAndRedirect = null,
+        isSatisfied = { _, _ -> isSatisfied },
+        nextAction = { journeyData, _ ->
+            customNextActionAddition?.invoke(journeyData)
+            Pair(nextSegment?.let { TestStepId(it) }, null)
+        },
+    )
+
+    fun getDataForStep(urlPathSegment: String): Any? = journeyData[urlPathSegment]
+
+    fun clearJourneyData() = journeyData.clear()
+}
