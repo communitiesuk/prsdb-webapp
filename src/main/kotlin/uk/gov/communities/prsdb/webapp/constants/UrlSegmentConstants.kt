f3908396decce0e226c3a451546b305db243f879
Jasmin Conterio <jasmin.conterio@communities.gov.uk>
2025-02-26 15:54:35 +0000

    PRSD-NONE: move all path segments to UrlSegmentConstants.kt (#241)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/UrlSegmentConstants.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/UrlSegmentConstants.kt
@@ -2,4 +2,12 @@ package uk.gov.communities.prsdb.webapp.constants
 
 const val UPDATE_PATH_SEGMENT = "update"
 const val DETAILS_PATH_SEGMENT = "details"
-const val LANDLORD_DETAILS_SEGMENT = "landlord-details"
+const val LANDLORD_DETAILS_PATH_SEGMENT = "landlord-details"
+const val LOCAL_AUTHORITY_PATH_SEGMENT = "local-authority"
+const val LANDLORD_PATH_SEGMENT = "landlord"
+const val PROPERTY_PATH_SEGMENT = "property"
+const val DASHBOARD_PATH_SEGMENT = "dashboard"
+const val SEARCH_PATH_SEGMENT = "search"
+
+// TODO PRSD-670: Set to landlord dashboard
+val LANDLORD_DASHBOARD_URL = "/$LANDLORD_PATH_SEGMENT/$DASHBOARD_PATH_SEGMENT"
