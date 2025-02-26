2b6ade05cf05fe9b0aae59758950073f49abfcb5
JasCon <jasmin.conterio@softwire.com>
2025-02-26 11:02:25 +0000

    PRSD-916: Update dashboard styling

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,7 +2,7 @@ import {initAll as initGDS} from 'govuk-frontend';
 import {initSelectAutocomplete} from "./autocomplete";
 import $ from 'jquery'
 import {initAll as initMoJDS} from '@ministryofjustice/frontend'
-import '../resources/css/prsdClasses.scss'
+import '../resources/css/custom.scss'
 
 initGDS()
 
