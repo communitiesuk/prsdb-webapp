40bbb129ca630dc3632ac79b7054b45ffb343f4e
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-03-05 11:56:20 +0000

    PRSD-NONE: Bases originalDataKey on journeyPathSegment

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/Journey.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/Journey.kt
@@ -16,7 +16,7 @@ import java.security.Principal
 import java.util.Optional
 
 abstract class Journey<T : StepId>(
-    protected val journeyType: JourneyType,
+    private val journeyType: JourneyType,
     protected val journeyPathSegment: String,
     val initialStepId: T,
     protected val validator: Validator,
