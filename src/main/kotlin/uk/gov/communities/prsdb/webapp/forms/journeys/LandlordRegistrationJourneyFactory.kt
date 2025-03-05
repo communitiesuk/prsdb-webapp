7a04a472dfe2236405c5ee094cdc534970e82d2c
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-03-05 12:03:03 +0000

    PRSD-NONE: Creates LandlordRegistrationJourneyFactory

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/LandlordRegistrationJourneyFactory.kt
@@ -0,0 +1,33 @@
+package uk.gov.communities.prsdb.webapp.forms.journeys
+
+import org.springframework.stereotype.Component
+import org.springframework.validation.Validator
+import uk.gov.communities.prsdb.webapp.models.viewModels.emailModels.LandlordRegistrationConfirmationEmail
+import uk.gov.communities.prsdb.webapp.services.AbsoluteUrlProvider
+import uk.gov.communities.prsdb.webapp.services.AddressDataService
+import uk.gov.communities.prsdb.webapp.services.AddressLookupService
+import uk.gov.communities.prsdb.webapp.services.EmailNotificationService
+import uk.gov.communities.prsdb.webapp.services.JourneyDataService
+import uk.gov.communities.prsdb.webapp.services.LandlordService
+
+@Component
+class LandlordRegistrationJourneyFactory(
+    private val validator: Validator,
+    private val journeyDataService: JourneyDataService,
+    private val addressLookupService: AddressLookupService,
+    private val addressDataService: AddressDataService,
+    private val landlordService: LandlordService,
+    private val absoluteUrlProvider: AbsoluteUrlProvider,
+    private val emailNotificationService: EmailNotificationService<LandlordRegistrationConfirmationEmail>,
+) {
+    fun create(): LandlordRegistrationJourney =
+        LandlordRegistrationJourney(
+            validator,
+            journeyDataService,
+            addressLookupService,
+            addressDataService,
+            landlordService,
+            absoluteUrlProvider,
+            emailNotificationService,
+        )
+}
