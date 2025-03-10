5afe9f1a1fa9db09ac0b54fbcc68db2e067ee7f6
Kiran Randhawa Kukar <kiran.randhawakukar@softwire.com>
2025-03-10 14:25:10 +0000

    feat/PRSD-896 add number of people not less than number of households validation

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/pages/PropertyRegistrationNumberOfPeoplePage.kt
@@ -0,0 +1,42 @@
+package uk.gov.communities.prsdb.webapp.forms.pages
+
+import org.springframework.validation.BindingResult
+import org.springframework.validation.Validator
+import org.springframework.web.servlet.ModelAndView
+import uk.gov.communities.prsdb.webapp.forms.JourneyData
+import uk.gov.communities.prsdb.webapp.forms.PageData
+import uk.gov.communities.prsdb.webapp.helpers.PropertyRegistrationJourneyDataHelper
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
+import uk.gov.communities.prsdb.webapp.services.JourneyDataService
+import kotlin.reflect.KClass
+
+class PropertyRegistrationNumberOfPeoplePage(
+    formModel: KClass<out FormModel>,
+    templateName: String,
+    content: Map<String, Any>,
+    shouldDisplaySectionHeader: Boolean = false,
+    private val journeyDataService: JourneyDataService,
+) : AbstractPage(formModel, templateName, content, shouldDisplaySectionHeader) {
+    override fun enrichModel(
+        modelAndView: ModelAndView,
+        journeyData: JourneyData?,
+    ) {}
+
+    override fun bindDataToFormModel(
+        validator: Validator,
+        formData: PageData?,
+    ): BindingResult {
+        val newFormData = formData?.toMutableMap()
+        if (newFormData != null) {
+            val numberOfHouseholds = getNumberOfHouseholds()
+            newFormData["numberOfHouseholds"] = numberOfHouseholds
+        }
+        return super.bindDataToFormModel(validator, newFormData)
+    }
+
+    private fun getNumberOfHouseholds(): String {
+        val journeyData = journeyDataService.getJourneyDataFromSession()
+        val numberOfHouseholds = PropertyRegistrationJourneyDataHelper.getNumberOfHouseholds(journeyData)
+        return numberOfHouseholds.toString()
+    }
+}
