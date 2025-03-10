75947b34cabb6550960f8ceb5b65076fa845f76a
Kiran Randhawa Kukar <kiran.randhawakukar@softwire.com>
2025-03-10 14:25:10 +0000

    feat/PRSD-896 add number of people not less than number of households validation

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/NumberOfPeopleFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/NumberOfPeopleFormModel.kt
@@ -1,13 +1,14 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
 import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
+import uk.gov.communities.prsdb.webapp.validation.DelegatedPropertyConstraintValidator
 import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
 import uk.gov.communities.prsdb.webapp.validation.NotBlankConstraintValidator
-import uk.gov.communities.prsdb.webapp.validation.PositiveOrZeroIntegerValidator
+import uk.gov.communities.prsdb.webapp.validation.PositiveIntegerValidator
 import uk.gov.communities.prsdb.webapp.validation.ValidatedBy
 
 @IsValidPrioritised
-class NumberOfPeopleFormModel : FormModel {
+class NumberOfPeopleFormModel(
     @ValidatedBy(
         constraints = [
             ConstraintDescriptor(
@@ -16,9 +17,17 @@ class NumberOfPeopleFormModel : FormModel {
             ),
             ConstraintDescriptor(
                 messageKey = "forms.numberOfPeople.input.error.invalidFormat",
-                validatorType = PositiveOrZeroIntegerValidator::class,
+                validatorType = PositiveIntegerValidator::class,
+            ),
+            ConstraintDescriptor(
+                messageKey = "forms.numberOfPeople.input.error.invalidNumber",
+                validatorType = DelegatedPropertyConstraintValidator::class,
+                targetMethod = "isNotLessThanNumberOfHouseholds",
             ),
         ],
     )
-    var numberOfPeople: String = ""
+    var numberOfPeople: String = "",
+    var numberOfHouseholds: String = "",
+) : FormModel {
+    fun isNotLessThanNumberOfHouseholds(): Boolean = numberOfPeople.toInt() >= numberOfHouseholds.toInt()
 }
