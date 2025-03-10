1d44e01db3c23038fae7cf5758b99385e496cc4d
Jasmin Conterio <jasmin.conterio@communities.gov.uk>
2025-03-10 09:32:14 +0000

    PRSD-670: Replace link to external content (#261)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/ExternalLinks.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/ExternalLinks.kt
@@ -2,4 +2,4 @@ package uk.gov.communities.prsdb.webapp.constants
 
 const val RENTERS_RIGHTS_BILL_URL =
     "https://www.gov.uk/government/publications/" +
-        "guide-to-the-renters-rights-bill/82ffc7fb-64b0-4af5-a72e-c24701a5f12a#private-rented-sector-database"
+        "guide-to-the-renters-rights-bill/guide-to-the-renters-rights-bill#private-rented-sector-database"
