c65c7497d21d49fa86fa30aef979bc09a97c8446
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-03-10 16:46:56 +0000

    PRSD-NONE: Creates JourneyDataServiceFactory

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/factories/JourneyDataServiceFactory.kt
@@ -0,0 +1,21 @@
+package uk.gov.communities.prsdb.webapp.services.factories
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpSession
+import org.springframework.stereotype.Service
+import org.springframework.web.context.annotation.RequestScope
+import uk.gov.communities.prsdb.webapp.database.repository.FormContextRepository
+import uk.gov.communities.prsdb.webapp.database.repository.OneLoginUserRepository
+import uk.gov.communities.prsdb.webapp.services.JourneyDataService
+
+@Service
+@RequestScope
+class JourneyDataServiceFactory(
+    private val session: HttpSession,
+    private val formContextRepository: FormContextRepository,
+    private val oneLoginUserRepository: OneLoginUserRepository,
+    private val objectMapper: ObjectMapper,
+) {
+    fun create(journeyDataKey: String) =
+        JourneyDataService(session, formContextRepository, oneLoginUserRepository, objectMapper, journeyDataKey)
+}
