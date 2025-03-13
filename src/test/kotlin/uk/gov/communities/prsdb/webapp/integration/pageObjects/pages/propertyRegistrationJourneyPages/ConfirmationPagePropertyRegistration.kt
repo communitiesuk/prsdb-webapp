9312c0f332c5e4e69234a4238818571c4d9ae7fd
JasCon <jasmin.conterio@softwire.com>
2025-03-13 12:17:40 +0000

    PRSD-NONE: Use common CONFIRMATION_PATH_SEGMENT for all journeys

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/ConfirmationPagePropertyRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/ConfirmationPagePropertyRegistration.kt
@@ -1,15 +1,15 @@
 package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyRegistrationJourneyPages
 
 import com.microsoft.playwright.Page
+import uk.gov.communities.prsdb.webapp.constants.CONFIRMATION_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.REGISTER_PROPERTY_JOURNEY_URL
-import uk.gov.communities.prsdb.webapp.controllers.RegisterPropertyController.Companion.CONFIRMATION_PAGE_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Button
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Table
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 
 class ConfirmationPagePropertyRegistration(
     page: Page,
-) : BasePage(page, "$REGISTER_PROPERTY_JOURNEY_URL/$CONFIRMATION_PAGE_PATH_SEGMENT") {
+) : BasePage(page, "$REGISTER_PROPERTY_JOURNEY_URL/$CONFIRMATION_PATH_SEGMENT") {
     private val detailTable = Table(page)
     val registrationNumberText: String = detailTable.getCell(0, 1).innerText()
     val goToDashboardButton = Button.byText(page, "Go to Dashboard")
