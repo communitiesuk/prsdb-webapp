b6d0bb68f37d56b1601e868d88f89015b73693a6
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-03-14 14:03:26 +0000

    PRSD-NONE: Creates PageWithContentProviders (#272)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/pages/PageWithContentProvider.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/pages/PageWithContentProvider.kt
@@ -5,17 +5,17 @@ import uk.gov.communities.prsdb.webapp.forms.JourneyData
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
 import kotlin.reflect.KClass
 
-class PageWithSingleLineAddress(
+class PageWithContentProvider(
     formModel: KClass<out FormModel>,
     templateName: String,
     content: Map<String, Any>,
     shouldDisplaySectionHeader: Boolean = false,
-    val addressProvider: () -> String,
+    private val contentProvider: () -> Map<String, Any>,
 ) : AbstractPage(formModel, templateName, content, shouldDisplaySectionHeader) {
     override fun enrichModel(
         modelAndView: ModelAndView,
         filteredJourneyData: JourneyData?,
     ) {
-        modelAndView.addObject("singleLineAddress", addressProvider())
+        contentProvider().forEach { modelAndView.addObject(it.key, it.value) }
     }
 }
