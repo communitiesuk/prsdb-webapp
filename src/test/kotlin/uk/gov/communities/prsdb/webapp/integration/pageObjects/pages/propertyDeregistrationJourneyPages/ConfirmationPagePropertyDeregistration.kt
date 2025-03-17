e1a0826f1b1d99aa3cad5be7d42314c2ecef7048
JasCon <jasmin.conterio@softwire.com>
2025-03-17 12:04:53 +0000

    PRSD-698: Add journey test

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyDeregistrationJourneyPages/ConfirmationPagePropertyDeregistration.kt
@@ -0,0 +1,16 @@
+package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyDeregistrationJourneyPages
+
+import com.microsoft.playwright.Page
+import uk.gov.communities.prsdb.webapp.constants.CONFIRMATION_PATH_SEGMENT
+import uk.gov.communities.prsdb.webapp.constants.DEREGISTER_PROPERTY_JOURNEY_URL
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Button
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.ConfirmationBanner
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+
+class ConfirmationPagePropertyDeregistration(
+    page: Page,
+    urlArguments: Map<String, String>,
+) : BasePage(page, "$DEREGISTER_PROPERTY_JOURNEY_URL/${urlArguments["propertyOwnershipId"]}/$CONFIRMATION_PATH_SEGMENT") {
+    val confirmationBanner = ConfirmationBanner(page)
+    val goToDashboardButton = Button.byText(page, "Go to Dashboard")
+}
