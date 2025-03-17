52c0128b23d4ad6d020f9cc6dd660c1a53b61670
Kiran Randhawa Kukar <kiran.randhawakukar@softwire.com>
2025-03-17 15:52:05 +0000

    feat/PRSD-896 added coherence valisation to to PropertyRegistrationService before saving property

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyDetailsUpdateJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyDetailsUpdateJourneyTests.kt
@@ -101,6 +101,31 @@ class PropertyDetailsUpdateJourneyTests : IntegrationTest() {
         )
     }
 
+    @Test
+    fun `When number of households is updated to less than number of people the user is redirected to number of people page`(page: Page) {
+        // Update details page
+        var propertyDetailsUpdatePage = navigator.goToPropertyDetailsUpdatePage(propertyOwnershipId)
+        assertThat(propertyDetailsUpdatePage.heading).containsText("1, Example Road, EG")
+
+        val newNumberOfHouseholds = 3
+        val numberOfPeopleUpdatePage = updateNumberOfHouseholdsAndReturnNumberOfPeople(propertyDetailsUpdatePage, newNumberOfHouseholds)
+
+        val newNumberOfPeople = 4
+        propertyDetailsUpdatePage = updateNumberOfPeopleAndReturn(page, numberOfPeopleUpdatePage, newNumberOfPeople)
+
+        // Submit changes TODO PRSD-355 add proper submit button and declaration page
+        propertyDetailsUpdatePage.submitButton.clickAndWait()
+        propertyDetailsUpdatePage = assertPageIs(page, PropertyDetailsUpdatePage::class, urlArguments)
+
+        // Check changes have occurred
+        assertThat(propertyDetailsUpdatePage.propertyDetailsSummaryList.numberOfHouseholdsRow.value).containsText(
+            newNumberOfHouseholds.toString(),
+        )
+        assertThat(propertyDetailsUpdatePage.propertyDetailsSummaryList.numberOfPeopleRow.value).containsText(
+            newNumberOfPeople.toString(),
+        )
+    }
+
     private fun updateOwnershipTypeAndReturn(
         detailsPage: PropertyDetailsUpdatePage,
         newOwnershipType: OwnershipType,
@@ -111,7 +136,7 @@ class PropertyDetailsUpdateJourneyTests : IntegrationTest() {
         val updateOwnershipTypePage = assertPageIs(page, OwnershipTypeFormPagePropertyDetailsUpdate::class, urlArguments)
         updateOwnershipTypePage.submitOwnershipType(newOwnershipType)
 
-        return assertPageIs(page, PropertyDetailsUpdatePage::class, urlArguments)
+        return assertPageIsPropertyDetailsUpdatePage(page)
     }
 
     private fun updateNumberOfHouseholdsAndReturn(
@@ -124,7 +149,20 @@ class PropertyDetailsUpdateJourneyTests : IntegrationTest() {
         val updateNumberOfHouseholdsPage = assertPageIs(page, NumberOfHouseholdsFormPagePropertyDetailsUpdate::class, urlArguments)
         updateNumberOfHouseholdsPage.submitNumberOfHouseholds(newNumberOfHouseholds)
 
-        return assertPageIs(page, PropertyDetailsUpdatePage::class, urlArguments)
+        return assertPageIsPropertyDetailsUpdatePage(page)
+    }
+
+    private fun updateNumberOfHouseholdsAndReturnNumberOfPeople(
+        detailsPage: PropertyDetailsUpdatePage,
+        newNumberOfHouseholds: Int,
+    ): NumberOfPeopleFormPagePropertyDetailsUpdate {
+        val page = detailsPage.page
+        detailsPage.propertyDetailsSummaryList.numberOfHouseholdsRow.clickActionLinkAndWait()
+
+        val updateNumberOfHouseholdsPage = assertPageIs(page, NumberOfHouseholdsFormPagePropertyDetailsUpdate::class, urlArguments)
+        updateNumberOfHouseholdsPage.submitNumberOfHouseholds(newNumberOfHouseholds)
+
+        return assertPageIs(page, NumberOfPeopleFormPagePropertyDetailsUpdate::class, urlArguments)
     }
 
     private fun updateNumberOfPeopleAndReturn(
@@ -135,8 +173,19 @@ class PropertyDetailsUpdateJourneyTests : IntegrationTest() {
         detailsPage.propertyDetailsSummaryList.numberOfPeopleRow.clickActionLinkAndWait()
 
         val updateNumberOfPeoplePage = assertPageIs(page, NumberOfPeopleFormPagePropertyDetailsUpdate::class, urlArguments)
-        updateNumberOfPeoplePage.submitNumOfPeople(newNumberOfPeople)
 
-        return assertPageIs(page, PropertyDetailsUpdatePage::class, urlArguments)
+        return updateNumberOfPeopleAndReturn(page, updateNumberOfPeoplePage, newNumberOfPeople)
     }
+
+    private fun updateNumberOfPeopleAndReturn(
+        page: Page,
+        numberOfPeopleUpdatePage: NumberOfPeopleFormPagePropertyDetailsUpdate,
+        newNumberOfPeople: Int,
+    ): PropertyDetailsUpdatePage {
+        numberOfPeopleUpdatePage.submitNumOfPeople(newNumberOfPeople)
+
+        return assertPageIsPropertyDetailsUpdatePage(page)
+    }
+
+    private fun assertPageIsPropertyDetailsUpdatePage(page: Page) = assertPageIs(page, PropertyDetailsUpdatePage::class, urlArguments)
 }
