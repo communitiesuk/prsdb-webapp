3884bcd71213fbe054a9fd85bec8352dea40a18b
Alexander Read <alexander.read@communities.gov.uk>
2025-03-24 16:42:31 +0000

    PRSD-999: Store token in session and cookie to verify uploader

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/examples/ExampleFileUploadController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/examples/ExampleFileUploadController.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,7 +15,8 @@ import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.server.ResponseStatusException
 
 @Controller
-@RequestMapping("example/file-upload")
+// This free segment allows the example controller to simulate multiple journeys in parallel
+@RequestMapping("example/file-upload/{freeSegment}")
 class ExampleFileUploadController(
     private val fileUploader: FileUploader,
     private val tokenService: FileUploadTokenService,
@@ -22,10 +24,10 @@ class ExampleFileUploadController(
     @GetMapping
     fun getFileUploadForm(
         response: HttpServletResponse,
-        model: Model,
+        @PathVariable("freeSegment") freeSegment: String,
     ): String {
-        val token = tokenService.issueToken()
-        val tokenCookie = createFileUploadTokenCookie(token)
+        val token = tokenService.issueTokenFor(freeSegment)
+        val tokenCookie = createFileUploadTokenCookie(token, freeSegment)
         response.addCookie(tokenCookie)
         return "example/fileUpload"
     }
@@ -33,10 +35,11 @@ class ExampleFileUploadController(
     @PostMapping
     fun uploadFile(
         @RequestParam("uploaded-file") file: MultipartFile,
-        @CookieValue(value = "token-cookie") token: String,
+        @CookieValue(value = COOKIE_NAME) token: String,
         model: Model,
+        @PathVariable("freeSegment") freeSegment: String,
     ): String {
-        if (!tokenService.checkToken(token)) {
+        if (!tokenService.checkTokenIsFor(token, freeSegment)) {
             throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid upload token")
         }
 
@@ -50,22 +53,28 @@ class ExampleFileUploadController(
                 "uploadReturnValue" to uploadOutcome,
                 "size" to file.size,
                 "contentType" to file.contentType,
+                "cookie-value" to token,
             ),
         )
         return "example/fileUpload"
     }
 
-    private fun createFileUploadTokenCookie(token: String): Cookie {
-        // This cookie name needs to uniquely identify the context in which this upload should occur, e.g. the exact
-        // property and compliance file being uploaded, so that parallel journeys in the same session can be supported
-        // OR it can have a generic name, and the path attribute can be used to differentiate between them.
-        val tokenCookie = Cookie("example-file-upload-token", token)
+    private fun createFileUploadTokenCookie(
+        token: String,
+        segment: String,
+    ): Cookie {
+        // This cookie needs to be used in a way such that parallel journeys in the same session can be supported
+        // for example it could use the path attribute can be used to differentiate between them alongside validating
+        // that the token is being used correctly by storing some additional information with the token in the session.
+        val tokenCookie = Cookie(COOKIE_NAME, token)
         tokenCookie.isHttpOnly = true
-        tokenCookie.path = "/example/file-upload"
+        tokenCookie.path = "/example/file-upload/$segment"
         tokenCookie.secure = true
 
-        // In non-example code, this max age property should be set thoughtfully - and potentially be configurable
-        tokenCookie.maxAge = 60 * 60 * 2
         return tokenCookie
+    }
+
+    companion object {
+        const val COOKIE_NAME = "example-file-upload-token"
     }
 }
--- src/main/kotlin/uk/gov/communities/prsdb/webapp/examples/FileUploadTokenService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/examples/FileUploadTokenService.kt
@@ -5,18 +5,22 @@ import org.springframework.stereotype.Service
 import java.util.UUID
 
 // TODO: PRSD-945/PRSD-395 Move this to service folder and manage multiple tokens
+// It may be that these session attributes should/will need store additional data, e.g. property ownership id
 @Service
 class FileUploadTokenService(
     private val session: HttpSession,
 ) {
-    fun issueToken(): String {
+    fun issueTokenFor(value: Any): String {
         val token = UUID.randomUUID().toString()
-        session.setAttribute(token, true)
+        session.setAttribute(token, value)
         return token
     }
 
-    fun checkToken(token: String): Boolean {
-        if (session.getAttribute(token) == true) {
+    fun checkTokenIsFor(
+        token: String,
+        value: Any,
+    ): Boolean {
+        if (session.getAttribute(token) == value) {
             session.removeAttribute(token)
             return true
         } else {
