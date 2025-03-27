2e0a1852e17221162ad5fb7498ee81a212d95457
Jasmin Conterio <jasmin.conterio@communities.gov.uk>
2025-03-27 11:23:11 +0000

    Feat/prsd 703 landlord deregistration submit are you sure (#302)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/factories/LandlordDeregistrationJourneyFactory.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/factories/LandlordDeregistrationJourneyFactory.kt
@@ -4,16 +4,22 @@ import org.springframework.stereotype.Component
 import org.springframework.validation.Validator
 import uk.gov.communities.prsdb.webapp.constants.DEREGISTER_LANDLORD_JOURNEY_URL
 import uk.gov.communities.prsdb.webapp.forms.journeys.LandlordDeregistrationJourney
+import uk.gov.communities.prsdb.webapp.services.LandlordDeregistrationService
+import uk.gov.communities.prsdb.webapp.services.SecurityContextService
 import uk.gov.communities.prsdb.webapp.services.factories.JourneyDataServiceFactory
 
 @Component
 class LandlordDeregistrationJourneyFactory(
     private val validator: Validator,
     private val journeyDataServiceFactory: JourneyDataServiceFactory,
+    private val landlordDeregistrationService: LandlordDeregistrationService,
+    private val securityContextService: SecurityContextService,
 ) {
     fun create() =
         LandlordDeregistrationJourney(
             validator,
             journeyDataServiceFactory.create(DEREGISTER_LANDLORD_JOURNEY_URL),
+            landlordDeregistrationService,
+            securityContextService,
         )
 }
