3ab95c6a146c81028c74b9caf60f9a7cf0a5eca2
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-03-31 11:26:04 +0100

    PRSD-941: More JourneyWithTaskList Generalisation  (#308)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/tasks/JourneySection.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/tasks/JourneySection.kt
@@ -6,6 +6,7 @@ import uk.gov.communities.prsdb.webapp.forms.steps.StepId
 class JourneySection<T : StepId>(
     val tasks: List<JourneyTask<T>>,
     val headingKey: String? = null,
+    val sectionId: String? = null,
 ) {
     fun isStepInSection(stepId: T): Boolean = stepId in tasks.flatMap { task -> task.steps.map { step -> step.id } }
 
@@ -13,8 +14,13 @@ class JourneySection<T : StepId>(
         fun <T : StepId> withOneTask(
             task: JourneyTask<T>,
             headingKey: String? = null,
-        ) = JourneySection(listOf(task), headingKey)
+            sectionId: String? = null,
+        ) = JourneySection(listOf(task), headingKey, sectionId)
 
-        fun <T : StepId> withOneStep(step: Step<T>) = JourneySection(listOf(JourneyTask.withOneStep(step)))
+        fun <T : StepId> withOneStep(
+            step: Step<T>,
+            headingKey: String? = null,
+            sectionId: String? = null,
+        ) = JourneySection(listOf(JourneyTask.withOneStep(step)), headingKey, sectionId)
     }
 }
