a9b9f697681687a41a802fa185cfbb19512cbc1e
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-03-31 17:18:23 +0100

    PRSD-941: Adds ProvideComplianceControllerTests for task list endpoint

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/ProvideComplianceControllerTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/ProvideComplianceControllerTests.kt
@@ -1,24 +1,35 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.get
 import org.springframework.web.context.WebApplicationContext
+import uk.gov.communities.prsdb.webapp.constants.TASK_LIST_PATH_SEGMENT
+import uk.gov.communities.prsdb.webapp.forms.journeys.ComplianceProvisionJourney
+import uk.gov.communities.prsdb.webapp.forms.journeys.factories.ComplianceProvisionJourneyFactory
 import uk.gov.communities.prsdb.webapp.services.PropertyOwnershipService
 
-@WebMvcTest(controllers = [ProvideComplianceController::class])
+@WebMvcTest(ProvideComplianceController::class)
 class ProvideComplianceControllerTests(
     @Autowired val webContext: WebApplicationContext,
 ) : ControllerTest(webContext) {
-    @MockBean
+    @MockitoBean
     private lateinit var propertyOwnershipService: PropertyOwnershipService
+
+    @MockitoBean
+    private lateinit var complianceProvisionJourneyFactory: ComplianceProvisionJourneyFactory
+
+    @Mock
+    private lateinit var complianceProvisionJourney: ComplianceProvisionJourney
 
     private val validPropertyOwnershipId = 1L
     private val validProvideComplianceUrl = ProvideComplianceController.getProvideCompliancePath(validPropertyOwnershipId)
@@ -32,34 +43,79 @@ class ProvideComplianceControllerTests(
         whenever(propertyOwnershipService.getIsPrimaryLandlord(eq(invalidPropertyOwnershipId), any())).thenReturn(false)
     }
 
-    @Test
-    fun `index returns a redirect for unauthenticated user`() {
-        mvc.get(validProvideComplianceUrl).andExpect {
-            status { is3xxRedirection() }
+    @Nested
+    inner class Index {
+        @Test
+        fun `index returns a redirect for unauthenticated user`() {
+            mvc.get(validProvideComplianceUrl).andExpect {
+                status { is3xxRedirection() }
+            }
+        }
+
+        @Test
+        @WithMockUser
+        fun `index returns 403 for an unauthorised user`() {
+            mvc.get(validProvideComplianceUrl).andExpect {
+                status { isForbidden() }
+            }
+        }
+
+        @Test
+        @WithMockUser(roles = ["LANDLORD"])
+        fun `index returns 404 for a landlord user that doesn't own the property`() {
+            mvc.get(invalidProvideComplianceUrl).andExpect {
+                status { isNotFound() }
+            }
+        }
+
+        @Test
+        @WithMockUser(roles = ["LANDLORD"])
+        fun `index returns 200 for a landlord user that does own the property`() {
+            mvc.get(validProvideComplianceUrl).andExpect {
+                status { isOk() }
+            }
         }
     }
 
-    @Test
-    @WithMockUser
-    fun `index returns 400 for an unauthorised user`() {
-        mvc.get(validProvideComplianceUrl).andExpect {
-            status { isForbidden() }
-        }
-    }
+    @Nested
+    inner class GetTaskList {
+        private val validProvideComplianceTaskListUrl = "$validProvideComplianceUrl/$TASK_LIST_PATH_SEGMENT"
+        private val invalidProvideComplianceTaskListUrl = "$invalidProvideComplianceUrl/$TASK_LIST_PATH_SEGMENT"
 
-    @Test
-    @WithMockUser(roles = ["LANDLORD"])
-    fun `index returns 404 for a landlord user that doesn't own the property`() {
-        mvc.get(invalidProvideComplianceUrl).andExpect {
-            status { isNotFound() }
+        @BeforeEach
+        fun setUp() {
+            whenever(complianceProvisionJourneyFactory.create(validPropertyOwnershipId)).thenReturn(complianceProvisionJourney)
         }
-    }
 
-    @Test
-    @WithMockUser(roles = ["LANDLORD"])
-    fun `index returns 200 for a landlord user that does own the property`() {
-        mvc.get(validProvideComplianceUrl).andExpect {
-            status { isOk() }
+        @Test
+        fun `getTaskList returns a redirect for unauthenticated user`() {
+            mvc.get(validProvideComplianceTaskListUrl).andExpect {
+                status { is3xxRedirection() }
+            }
+        }
+
+        @Test
+        @WithMockUser
+        fun `getTaskList returns 403 for an unauthorised user`() {
+            mvc.get(validProvideComplianceTaskListUrl).andExpect {
+                status { isForbidden() }
+            }
+        }
+
+        @Test
+        @WithMockUser(roles = ["LANDLORD"])
+        fun `getTaskList returns 404 for a landlord user that doesn't own the property`() {
+            mvc.get(invalidProvideComplianceTaskListUrl).andExpect {
+                status { isNotFound() }
+            }
+        }
+
+        @Test
+        @WithMockUser(roles = ["LANDLORD"])
+        fun `getTaskList returns 200 for a landlord user that does own the property`() {
+            mvc.get(validProvideComplianceTaskListUrl).andExpect {
+                status { isOk() }
+            }
         }
     }
 }
