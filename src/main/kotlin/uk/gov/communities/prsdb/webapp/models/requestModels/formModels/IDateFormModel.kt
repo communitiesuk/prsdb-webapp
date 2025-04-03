6c3c2deb46a7a64088255cf354238c633ee883a0
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-04-03 14:47:07 +0100

    PRSD-943: Creates IDateFormModel; makes DateOfBirthFormModel implement it

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/IDateFormModel.kt
@@ -0,0 +1,40 @@
+package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
+
+import uk.gov.communities.prsdb.webapp.validation.DateValidator
+
+interface IDateFormModel : FormModel {
+    var day: String
+
+    var month: String
+
+    var year: String
+
+    fun notAllBlank(): Boolean = !(DateValidator().isAllBlank(day, month, year))
+
+    fun notDayAndMonthBlank(): Boolean = !(DateValidator().isBothBlank(day, month))
+
+    fun notDayAndYearBlank(): Boolean = !(DateValidator().isBothBlank(day, year))
+
+    fun notMonthAndYearBlank(): Boolean = !(DateValidator().isBothBlank(month, year))
+
+    fun notAllInvalid(): Boolean = isValidDay() || isValidMonth() || isValidYear()
+
+    fun notDayAndMonthInvalid(): Boolean = isValidDay() || isValidMonth()
+
+    fun notDayAndYearInvalid(): Boolean = isValidDay() || isValidYear()
+
+    fun notMonthAndYearInvalid(): Boolean = isValidMonth() || isValidYear()
+
+    fun isValidDay(): Boolean = DateValidator().isAnyBlank(day, month, year) || DateValidator().isValidDay(day)
+
+    fun isValidMonth(): Boolean = DateValidator().isAnyBlank(day, month, year) || DateValidator().isValidMonth(month)
+
+    fun isValidYear(): Boolean = DateValidator().isAnyBlank(day, month, year) || DateValidator().isValidYear(year)
+
+    fun isDayOrMonthOrYearInvalid(): Boolean = DateValidator().isDayOrMonthOrYearNotValid(day, month, year)
+
+    fun isValidDate(): Boolean {
+        if (isDayOrMonthOrYearInvalid()) return true
+        return DateValidator().isValidDate(day, month, year)
+    }
+}
