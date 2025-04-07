92f9c73c167cf82c7aa15ba19faa7b95db26ce31
Travis Woodward <travis.woodward@softwire.com>
2025-04-07 12:05:14 +0100

    PRSD-1053: Revert change to registration number id

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -75,6 +75,6 @@ VALUES (1, 1, true, 1, 1, '2024-10-15 00:00:00+00', null);
 SELECT setval(pg_get_serial_sequence('property', 'id'), (SELECT MAX(id) FROM property));
 
 INSERT INTO property_ownership (id, is_active, occupancy_type, ownership_type, current_num_households, current_num_tenants, registration_number_id, primary_landlord_id, property_id, created_date, last_modified_date)
-VALUES (1, true, 0, 1, 1, 2, 9, 1, 1, '2024-10-15 00:00:00+00', null);
+VALUES (1, true, 0, 1, 1, 2, 13, 1, 1, '2024-10-15 00:00:00+00', null);
 
 SELECT setval(pg_get_serial_sequence('property_ownership', 'id'), (SELECT MAX(id) FROM property_ownership));
