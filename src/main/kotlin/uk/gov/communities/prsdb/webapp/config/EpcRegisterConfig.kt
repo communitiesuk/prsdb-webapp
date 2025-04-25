3eeaeb95d6ecd1d2472788876e75dd0e25031260
Alex Read <alexander.read@communities.gov.uk>
2025-04-25 09:14:26 +0100

    Feat/prsd 1065 integrate with epc register (#370)

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/EpcRegisterConfig.kt
@@ -0,0 +1,35 @@
+package uk.gov.communities.prsdb.webapp.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.web.client.RestClient
+
+@Configuration
+class EpcRegisterConfig(
+    private val authorizedClientManager: OAuth2AuthorizedClientManager,
+) {
+    @Value("\${epc.base-url}")
+    lateinit var baseUrl: String
+
+    @Bean("epc-client")
+    fun epcWebClient(): RestClient =
+        RestClient
+            .builder()
+            .requestInterceptor { request, body, execution ->
+                val authRequest =
+                    OAuth2AuthorizeRequest
+                        .withClientRegistrationId("epc-register")
+                        .principal("epc-register")
+                        .build()
+
+                val authorizedClient = authorizedClientManager.authorize(authRequest)
+                val token = authorizedClient?.accessToken?.tokenValue ?: ""
+
+                request.headers.setBearerAuth(token)
+                execution.execute(request, body)
+            }.baseUrl(baseUrl)
+            .build()
+}
