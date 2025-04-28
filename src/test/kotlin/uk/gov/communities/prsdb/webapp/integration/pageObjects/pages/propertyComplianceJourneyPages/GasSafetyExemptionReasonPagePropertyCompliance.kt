75ac72f2876b238e02365472fb0fb08053dc0c1b
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-04-28 11:56:09 +0100

    PRSD-958: Creates base ExemptionReasonFormPage

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/GasSafetyExemptionReasonPagePropertyCompliance.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/GasSafetyExemptionReasonPagePropertyCompliance.kt
@@ -4,28 +4,13 @@ import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.constants.enums.GasSafetyExemptionReason
 import uk.gov.communities.prsdb.webapp.controllers.PropertyComplianceController
 import uk.gov.communities.prsdb.webapp.forms.steps.PropertyComplianceStepId
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Form
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Radios
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.ExemptionReasonFormPage
 
 class GasSafetyExemptionReasonPagePropertyCompliance(
     page: Page,
     urlArguments: Map<String, String>,
-) : BasePage(
+) : ExemptionReasonFormPage<GasSafetyExemptionReason>(
         page,
         PropertyComplianceController.getPropertyCompliancePath(urlArguments["propertyOwnershipId"]!!.toLong()) +
             "/${PropertyComplianceStepId.GasSafetyExemptionReason.urlPathSegment}",
-    ) {
-    val form = ExemptionReasonForm(page)
-
-    fun submitExemptionReason(exemptionReason: GasSafetyExemptionReason) {
-        form.exemptionReasonRadios.selectValue(exemptionReason)
-        form.submit()
-    }
-
-    class ExemptionReasonForm(
-        page: Page,
-    ) : Form(page) {
-        val exemptionReasonRadios = Radios(locator)
-    }
-}
+    )
