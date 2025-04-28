eafa2ccaaaf6b4635d4dc13a80b141d2546e1afd
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-04-28 15:21:51 +0100

    PRSD-954: Review mark-ups

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/EicrFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/EicrFormModel.kt
@@ -1,19 +1,8 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
-import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
-import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
-import uk.gov.communities.prsdb.webapp.validation.NotNullConstraintValidator
-import uk.gov.communities.prsdb.webapp.validation.ValidatedBy
+import jakarta.validation.constraints.NotNull
 
-@IsValidPrioritised
 class EicrFormModel : FormModel {
-    @ValidatedBy(
-        constraints = [
-            ConstraintDescriptor(
-                messageKey = "forms.eicr.error.missing",
-                validatorType = NotNullConstraintValidator::class,
-            ),
-        ],
-    )
+    @NotNull(message = "forms.eicr.error.missing")
     var hasCert: Boolean? = null
 }
