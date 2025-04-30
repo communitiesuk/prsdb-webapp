92dfaa33b4c0cf4d87578e05ac3b49175d7450a9
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-04-30 11:32:05 +0100

    PRSD-961: Property Compliance - EICR Outdated (#383)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/ExternalLinks.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/ExternalLinks.kt
@@ -5,3 +5,7 @@ const val RENTERS_RIGHTS_BILL_URL =
         "guide-to-the-renters-rights-bill/guide-to-the-renters-rights-bill#private-rented-sector-database"
 
 const val GAS_SAFE_REGISTER = "https://www.gassaferegister.co.uk/find-an-engineer-or-check-the-register"
+
+const val RCP_ELECTRICAL_INFO_URL = "https://www.electricalsafetyroundtable.co.uk/safety-guidance/competent-registered-electrician.aspx"
+
+const val RCP_ELECTRICAL_REGISTER_URL = "https://electricalcompetentperson.co.uk"
