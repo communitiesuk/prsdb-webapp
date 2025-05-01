7d6f3a1e4fa0686c082cb0fd17ab85a1db8981ca
Alex Read <alexander.read@communities.gov.uk>
2025-05-01 15:14:11 +0100

    PRSD-1099: Split change for properties POC (#384)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/steps/UpdatePropertyDetailsStepId.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/steps/UpdatePropertyDetailsStepId.kt
@@ -1,17 +1,30 @@
 package uk.gov.communities.prsdb.webapp.forms.steps
 
-import uk.gov.communities.prsdb.webapp.constants.DETAILS_PATH_SEGMENT
-
 enum class UpdatePropertyDetailsStepId(
     override val urlPathSegment: String,
-) : StepId {
-    UpdateOwnershipType("ownership-type"),
-    UpdateOccupancy("occupancy"),
-    UpdateNumberOfHouseholds("number-of-households"),
-    UpdateNumberOfPeople("number-of-people"),
-    UpdateLicensingType("licensing-type"),
-    UpdateSelectiveLicence("selective-licence"),
-    UpdateHmoMandatoryLicence("hmo-mandatory-licence"),
-    UpdateHmoAdditionalLicence("hmo-additional-licence"),
-    UpdateDetails(DETAILS_PATH_SEGMENT),
+    override val groupIdentifier: UpdateGroupIdentifier,
+) : GroupedStepId<UpdateGroupIdentifier> {
+    UpdateOwnershipType("ownership-type", UpdateGroupIdentifier.Ownership),
+    UpdateOccupancy("occupancy", UpdateGroupIdentifier.Occupancy),
+    UpdateNumberOfHouseholds("number-of-households", UpdateGroupIdentifier.Occupancy),
+    UpdateNumberOfPeople("number-of-people", UpdateGroupIdentifier.Occupancy),
+    CheckYourOccupancyAnswers("check-occupancy-answers", UpdateGroupIdentifier.Occupancy),
+    UpdateLicensingType("licensing-type", UpdateGroupIdentifier.Licensing),
+    UpdateSelectiveLicence("selective-licence", UpdateGroupIdentifier.Licensing),
+    UpdateHmoMandatoryLicence("hmo-mandatory-licence", UpdateGroupIdentifier.Licensing),
+    UpdateHmoAdditionalLicence("hmo-additional-licence", UpdateGroupIdentifier.Licensing),
+    CheckYourLicensingAnswers("check-licensing-answers", UpdateGroupIdentifier.Licensing),
+    ;
+
+    companion object {
+        fun fromPathSegment(segment: String): UpdatePropertyDetailsStepId? = entries.find { it.urlPathSegment == segment }
+    }
+}
+
+enum class UpdateGroupIdentifier(
+    val identifierString: String,
+) {
+    Ownership("-OWNERSHIP"),
+    Occupancy("-OCCUPANCY"),
+    Licensing("-LICENSING"),
 }
