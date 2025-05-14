787e4522ec4b47dca5979a251d3ad9d0dae6a411
JasCon <jasmin.conterio@softwire.com>
2025-05-14 10:46:56 +0100

    PRSD-1134: Simplify EpcFormModel

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/EpcFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/EpcFormModel.kt
@@ -1,20 +1,9 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
+import jakarta.validation.constraints.NotNull
 import uk.gov.communities.prsdb.webapp.constants.enums.HasEpc
-import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
-import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
-import uk.gov.communities.prsdb.webapp.validation.NotNullConstraintValidator
-import uk.gov.communities.prsdb.webapp.validation.ValidatedBy
 
-@IsValidPrioritised
 class EpcFormModel : FormModel {
-    @ValidatedBy(
-        constraints = [
-            ConstraintDescriptor(
-                messageKey = "forms.epc.error.missing",
-                validatorType = NotNullConstraintValidator::class,
-            ),
-        ],
-    )
+    @NotNull(message = "forms.epc.error.missing")
     var hasCert: HasEpc? = null
 }
