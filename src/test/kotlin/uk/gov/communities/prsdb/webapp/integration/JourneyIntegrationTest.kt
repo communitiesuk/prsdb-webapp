46fac266526f1ca8a2e21d385127a311cbeb73d8
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-05-15 17:37:13 +0100

    PRSD-623: Creates framework for configuring seed data

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/JourneyIntegrationTest.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/JourneyIntegrationTest.kt
@@ -3,13 +3,11 @@ package uk.gov.communities.prsdb.webapp.integration
 import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.communities.prsdb.webapp.testHelpers.IntegrationTestHelper
 
 class JourneyIntegrationTest : IntegrationTest() {
     @AfterEach
     fun resetDatabase(
         @Autowired flyway: Flyway,
-    ) {
-        flyway.clean()
-        flyway.migrate()
-    }
+    ) = IntegrationTestHelper.resetDatabase(flyway)
 }
