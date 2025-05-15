9ea1521d6881c34d8a61dac02cb5e1a8f968b624
JasCon <jasmin.conterio@softwire.com>
2025-05-15 09:56:16 +0100

    PRSD-1135: Update tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/EpcExemptionConfirmationPagePropertyCompliance.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/EpcExemptionConfirmationPagePropertyCompliance.kt
@@ -1,16 +1,15 @@
 package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyComplianceJourneyPages
 
 import com.microsoft.playwright.Page
-import uk.gov.communities.prsdb.webapp.constants.enums.EpcExemptionReason
 import uk.gov.communities.prsdb.webapp.controllers.PropertyComplianceController
 import uk.gov.communities.prsdb.webapp.forms.steps.PropertyComplianceStepId
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.ExemptionReasonFormPage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 
-class EpcExemptionReasonPagePropertyCompliance(
+class EpcExemptionConfirmationPagePropertyCompliance(
     page: Page,
     urlArguments: Map<String, String>,
-) : ExemptionReasonFormPage<EpcExemptionReason>(
+) : BasePage(
         page,
         PropertyComplianceController.getPropertyCompliancePath(urlArguments["propertyOwnershipId"]!!.toLong()) +
-            "/${PropertyComplianceStepId.EpcExemptionReason.urlPathSegment}",
+            "/${PropertyComplianceStepId.EpcExemptionConfirmation.urlPathSegment}",
     )
