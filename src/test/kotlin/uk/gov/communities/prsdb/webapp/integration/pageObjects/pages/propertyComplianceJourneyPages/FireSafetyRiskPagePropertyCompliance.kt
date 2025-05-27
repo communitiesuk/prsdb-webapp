99990cfc2b550d28e3a90efa742ad26c9b0c664e
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-05-27 10:57:35 +0100

    feat/PRSD-1151 fire safety risk page tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/FireSafetyRiskPagePropertyCompliance.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/FireSafetyRiskPagePropertyCompliance.kt
@@ -3,6 +3,8 @@ package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyCo
 import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.controllers.PropertyComplianceController
 import uk.gov.communities.prsdb.webapp.forms.steps.PropertyComplianceStepId
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Form
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Heading
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 
 class FireSafetyRiskPagePropertyCompliance(
@@ -12,4 +14,8 @@ class FireSafetyRiskPagePropertyCompliance(
         page,
         PropertyComplianceController.getPropertyCompliancePath(urlArguments["propertyOwnershipId"]!!.toLong()) +
             "/${PropertyComplianceStepId.FireSafetyRisk.urlPathSegment}",
-    )
+    ) {
+    val heading = Heading(page.locator("h1.govuk-heading-l"))
+
+    val form = Form(page)
+}
