ae05fbbc83b1db922354be9969e1347e1a2c4277
Alexander Read <alexander.read@communities.gov.uk>
2025-06-03 11:15:37 +0100

    PRSD-805: Override back links from property search

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/searchResultModels/PropertySearchResultViewModelTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/searchResultModels/PropertySearchResultViewModelTests.kt
@@ -1,6 +1,9 @@
 package uk.gov.communities.prsdb.webapp.models.viewModels.searchResultModels
 
 import org.junit.jupiter.api.Test
+import uk.gov.communities.prsdb.webapp.constants.LOCAL_AUTHORITY_PATH_SEGMENT
+import uk.gov.communities.prsdb.webapp.constants.PROPERTY_DETAILS_SEGMENT
+import uk.gov.communities.prsdb.webapp.controllers.LandlordDetailsController
 import uk.gov.communities.prsdb.webapp.models.dataModels.RegistrationNumberDataModel
 import uk.gov.communities.prsdb.webapp.testHelpers.mockObjects.MockLandlordData
 import kotlin.test.assertEquals
@@ -25,7 +28,9 @@ class PropertySearchResultViewModelTests {
                     PropertySearchResultLandlordViewModel(
                         id = propertyOwnership.primaryLandlord.id,
                         name = propertyOwnership.primaryLandlord.name,
+                        recordLink = "${LandlordDetailsController.LANDLORD_DETAILS_ROUTE}/${propertyOwnership.primaryLandlord.id}",
                     ),
+                recordLink = "/$LOCAL_AUTHORITY_PATH_SEGMENT/$PROPERTY_DETAILS_SEGMENT/${propertyOwnership.id}",
             )
 
         val propertySearchResultViewModel = PropertySearchResultViewModel.fromPropertyOwnership(propertyOwnership)
