84df55a5407cf21f2b7e6a5f4ad87ea13e9d346b
Alexander Read <alexander.read@communities.gov.uk>
2025-06-03 12:06:50 +0100

    PRSD-805: Prefer val over var

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/searchResultModels/LandlordSearchResultViewModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/searchResultModels/LandlordSearchResultViewModel.kt
@@ -12,8 +12,8 @@ data class LandlordSearchResultViewModel(
     val contactAddress: String,
     val email: String,
     val phoneNumber: String,
-    var listedPropertyCount: Int = 0,
-    var recordLink: String? = null,
+    val listedPropertyCount: Int = 0,
+    val recordLink: String? = null,
 ) {
     companion object {
         fun fromLandlordWithListedPropertyCount(
