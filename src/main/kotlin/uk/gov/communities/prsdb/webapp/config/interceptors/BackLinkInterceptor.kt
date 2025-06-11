4709a4818d154232a266ea664bdc57652bcc918d
Alex Read <alexander.read@communities.gov.uk>
2025-06-11 16:50:17 +0100

    Feat/prsd 805 apply custom back links to resume journey (#479)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/config/interceptors/BackLinkInterceptor.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/interceptors/BackLinkInterceptor.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.view.RedirectView
 import org.springframework.web.util.UriComponentsBuilder
 import uk.gov.communities.prsdb.webapp.constants.BACK_URL_ATTR_NAME
 import uk.gov.communities.prsdb.webapp.constants.WITH_BACK_URL_PARAMETER_NAME
@@ -19,6 +20,15 @@ class BackLinkInterceptor(
         modelAndView: ModelAndView?,
     ) {
         val backUrlKey = getBackUrlParameter(request)
+
+        if (modelAndView?.view is RedirectView ||
+            modelAndView?.viewName?.startsWith("redirect:") == true ||
+            modelAndView?.viewName?.startsWith("forward:") == true
+        ) {
+            modelAndView.modelMap.addAttribute(WITH_BACK_URL_PARAMETER_NAME, backUrlKey)
+            return
+        }
+
         if (backUrlKey != null) {
             val backUrl = retrieveBackUrl(backUrlKey)
             if (backUrl != null) {
