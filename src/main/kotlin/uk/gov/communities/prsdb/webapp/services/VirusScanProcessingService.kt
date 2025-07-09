50f3140b518dbeff2d72ecb03721e8803bfe958f
Alex Read <alexander.read@communities.gov.uk>
2025-07-09 16:27:00 +0100

    Feat/prsd 1283 update user compliance certificate files (#550)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/VirusScanProcessingService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/VirusScanProcessingService.kt
@@ -1,21 +1,30 @@
 package uk.gov.communities.prsdb.webapp.services
 
 import org.springframework.stereotype.Service
+import uk.gov.communities.prsdb.webapp.database.entity.PropertyOwnership
+import uk.gov.communities.prsdb.webapp.database.repository.PropertyComplianceRepository
+import uk.gov.communities.prsdb.webapp.database.repository.PropertyOwnershipRepository
 import uk.gov.communities.prsdb.webapp.exceptions.PrsdbWebException
 import uk.gov.communities.prsdb.webapp.models.dataModels.PropertyFileNameInfo
+import uk.gov.communities.prsdb.webapp.models.dataModels.PropertyFileNameInfo.FileCategory
 import uk.gov.communities.prsdb.webapp.models.dataModels.ScanResult
 
 @Service
 class VirusScanProcessingService(
+    private val propertyOwnershipRepository: PropertyOwnershipRepository,
+    private val complianceRepository: PropertyComplianceRepository,
     private val dequarantiner: FileDequarantiner,
 ) {
     fun processScan(
         fileNameInfo: PropertyFileNameInfo,
         scanResultStatus: ScanResult,
     ) {
+        val ownership = getOwnershipForFileInfo(fileNameInfo)
         when (scanResultStatus) {
             ScanResult.NoThreats -> {
-                if (!dequarantiner.dequarantine(fileNameInfo.toString())) {
+                if (dequarantiner.dequarantine(fileNameInfo.toString())) {
+                    addFileToComplianceRecordIfPresent(fileNameInfo, ownership)
+                } else {
                     throw PrsdbWebException("Failed to dequarantine file: $fileNameInfo")
                 }
             }
@@ -23,6 +32,25 @@ class VirusScanProcessingService(
             ScanResult.Unsupported -> TODO("PRSD-1284")
             ScanResult.AccessDenied -> TODO("PRSD-1284")
             ScanResult.Failed -> TODO("PRSD-1284")
+        }
+    }
+
+    private fun getOwnershipForFileInfo(fileNameInfo: PropertyFileNameInfo) =
+        propertyOwnershipRepository
+            .findByIdAndIsActiveTrue(fileNameInfo.propertyOwnershipId)
+            ?: throw PrsdbWebException("No ownership found for $fileNameInfo")
+
+    private fun addFileToComplianceRecordIfPresent(
+        fileNameInfo: PropertyFileNameInfo,
+        ownership: PropertyOwnership,
+    ) {
+        val complianceRecord = complianceRepository.findByPropertyOwnership_Id(ownership.id)
+        if (complianceRecord != null) {
+            when (fileNameInfo.fileCategory) {
+                FileCategory.Eirc -> complianceRecord.eicrS3Key = fileNameInfo.toString()
+                FileCategory.GasSafetyCert -> complianceRecord.gasSafetyCertS3Key = fileNameInfo.toString()
+            }
+            complianceRepository.save(complianceRecord)
         }
     }
 }
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/services/VirusScanProcessingServiceTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/services/VirusScanProcessingServiceTests.kt
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
+import org.mockito.ArgumentMatchers.anyLong
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import uk.gov.communities.prsdb.webapp.database.entity.PropertyCompliance
+import uk.gov.communities.prsdb.webapp.database.entity.PropertyOwnership
+import uk.gov.communities.prsdb.webapp.database.repository.PropertyComplianceRepository
+import uk.gov.communities.prsdb.webapp.database.repository.PropertyOwnershipRepository
 import uk.gov.communities.prsdb.webapp.exceptions.PrsdbWebException
 import uk.gov.communities.prsdb.webapp.models.dataModels.PropertyFileNameInfo
 import uk.gov.communities.prsdb.webapp.models.dataModels.PropertyFileNameInfo.FileCategory
@@ -17,41 +23,87 @@ class VirusScanProcessingServiceTests {
     private lateinit var virusScanProcessingService: VirusScanProcessingService
     private lateinit var dequarantiner: FileDequarantiner
 
+    private lateinit var propertyOwnershipRepository: PropertyOwnershipRepository
+    private lateinit var complianceRepository: PropertyComplianceRepository
+
     @BeforeEach
     fun setup() {
         dequarantiner = mock()
-        virusScanProcessingService = VirusScanProcessingService(dequarantiner)
+        propertyOwnershipRepository = mock()
+        complianceRepository = mock()
+        virusScanProcessingService = VirusScanProcessingService(propertyOwnershipRepository, complianceRepository, dequarantiner)
+
+        whenever(propertyOwnershipRepository.findByIdAndIsActiveTrue(anyLong())).thenAnswer { invocation ->
+            val ownership = mock<PropertyOwnership>()
+            whenever(ownership.id).thenReturn(invocation.getArgument(0))
+            ownership
+        }
     }
 
     @Test
-    fun `processScan calls the dequarantiner when no threats are found`() {
+    fun `when no threats are found, processScan calls the dequarantiner`() {
+        // Arrange
         val fileNameInfo = PropertyFileNameInfo(5L, FileCategory.GasSafetyCert, "txt")
         val scanResultStatus = ScanResult.NoThreats
 
         whenever(dequarantiner.dequarantine(fileNameInfo.toString())).thenReturn(true)
 
+        // Act
         virusScanProcessingService.processScan(fileNameInfo, scanResultStatus)
 
+        // Assert
         verify(dequarantiner).dequarantine(fileNameInfo.toString())
     }
 
+    @EnumSource(FileCategory::class)
+    @ParameterizedTest
+    fun `when the dequarantiner succeeds, processScan adds the certificate to the compliance record if present`(category: FileCategory) {
+        // Arrange
+        val fileNameInfo = PropertyFileNameInfo(5L, category, "jpg")
+        val scanResultStatus = ScanResult.NoThreats
+
+        whenever(dequarantiner.dequarantine(fileNameInfo.toString())).thenReturn(true)
+        whenever(complianceRepository.findByPropertyOwnership_Id(fileNameInfo.propertyOwnershipId)).thenReturn(PropertyCompliance())
+
+        // Act
+        virusScanProcessingService.processScan(fileNameInfo, scanResultStatus)
+
+        // Assert
+        val captor = argumentCaptor<PropertyCompliance>()
+        verify(complianceRepository).save(captor.capture())
+        assert(getKeyFromRecordForCategory(captor.firstValue, category) == fileNameInfo.toString())
+    }
+
+    private fun getKeyFromRecordForCategory(
+        complianceRecord: PropertyCompliance,
+        category: FileCategory,
+    ): String? =
+        when (category) {
+            FileCategory.Eirc -> complianceRecord.eicrS3Key
+            FileCategory.GasSafetyCert -> complianceRecord.gasSafetyCertS3Key
+        }
+
     @Test
     fun `if the dequarantiner fails the processScan throws an exception`() {
+        // Arrange
         val fileNameInfo = PropertyFileNameInfo(5L, FileCategory.GasSafetyCert, "txt")
         val scanResultStatus = ScanResult.NoThreats
 
         whenever(dequarantiner.dequarantine(fileNameInfo.toString())).thenReturn(false)
 
+        // Act & Assert
         assertThrows<PrsdbWebException> { virusScanProcessingService.processScan(fileNameInfo, scanResultStatus) }
     }
 
     @EnumSource(ScanResult::class)
     @ParameterizedTest
     fun `processScan throws an error for each scan result other than NoThreats`(scanResultStatus: ScanResult) {
+        // Ignore NoThreats case since it is already tested
         if (scanResultStatus == ScanResult.NoThreats) {
             return
         }
 
+        // Arrange
         val fileNameInfo = PropertyFileNameInfo(5L, FileCategory.GasSafetyCert, "txt")
         val scanResultStatus = scanResultStatus
 
