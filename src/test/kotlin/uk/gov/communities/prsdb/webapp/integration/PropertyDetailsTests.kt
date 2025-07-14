524fe0756f6aa62d6d039440412185356ac47a73
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-07-14 15:34:34 +0100

    feat/PRSD-1033 add front end tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyDetailsTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyDetailsTests.kt
@@ -3,6 +3,7 @@ package uk.gov.communities.prsdb.webapp.integration
 import com.microsoft.playwright.Page
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import uk.gov.communities.prsdb.webapp.constants.COMPLIANCE_INFO_FRAGMENT
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BaseComponent.Companion.assertThat
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.LandlordDashboardPage
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.LandlordDetailsPage
@@ -30,6 +31,14 @@ class PropertyDetailsTests : SinglePageTestWithSeedData("data-local.sql") {
             detailsPage.tabs.goToLandlordDetails()
 
             assertEquals(detailsPage.tabs.activeTabPanelId, "landlord-details")
+        }
+
+        @Test
+        fun `loading the landlord details page and clicking compliance information tab shows the compliance information tab`(page: Page) {
+            val detailsPage = navigator.goToPropertyDetailsLandlordView(1)
+            detailsPage.tabs.goToComplianceInformation()
+
+            assertEquals(detailsPage.tabs.activeTabPanelId, COMPLIANCE_INFO_FRAGMENT)
         }
 
         @Test
@@ -80,6 +89,69 @@ class PropertyDetailsTests : SinglePageTestWithSeedData("data-local.sql") {
                 AreYouSureFormPagePropertyDeregistration::class,
                 mapOf("propertyOwnershipId" to propertyOwnershipId.toString()),
             )
+        }
+
+        @Nested
+        inner class NotificationBanner {
+            @Test
+            fun `is visible and includes correct messages when all certs are missing`(page: Page) {
+                val propertyOwnershipId = 8
+                val detailsPage = navigator.goToPropertyDetailsLandlordView(propertyOwnershipId.toLong())
+
+                assertThat(detailsPage.notificationBanner).isVisible()
+                assertThat(detailsPage.notificationBanner.title).containsText("Important")
+                assertThat(
+                    detailsPage.notificationBanner.content,
+                ).containsText(
+                    "This property is missing a gas safety certificate. Upload a certificate as soon as possible.\n" +
+                        "This property is missing a Electrical Installation Condition Report (EICR)." +
+                        " Upload an EICR as soon as possible.\n" +
+                        "This property is missing an energy performance certificate (EPC). Add a new certificate as soon as possible.",
+                )
+            }
+
+            @Test
+            fun `is visible and includes correct messages when all certs are expired`(page: Page) {
+                val propertyOwnershipId = 9
+                val detailsPage = navigator.goToPropertyDetailsLandlordView(propertyOwnershipId.toLong())
+
+                assertThat(detailsPage.notificationBanner).isVisible()
+                assertThat(detailsPage.notificationBanner.title).containsText("Important")
+                assertThat(
+                    detailsPage.notificationBanner.content,
+                ).containsText(
+                    "The gas safety certificate for this property has expired. Upload a new certificate as soon as possible.\n" +
+                        "The Electrical Installation Condition Report (EICR) for this property has expired. " +
+                        "Upload a new EICR as soon as possible.\n" +
+                        "The energy performance certificate (EPC) for this property has expired. " +
+                        "Add a new certificate as soon as possible.",
+                )
+            }
+
+            @Test
+            fun `is visible and includes correct message when epc has a low rating and mees exemption is missing`(page: Page) {
+                val propertyOwnershipId = 10
+                val detailsPage = navigator.goToPropertyDetailsLandlordView(propertyOwnershipId.toLong())
+
+                assertThat(detailsPage.notificationBanner).isVisible()
+                assertThat(detailsPage.notificationBanner.title).containsText("Important")
+                assertThat(
+                    detailsPage.notificationBanner.content,
+                ).containsText(
+                    "This property’s energy performance certificate (EPC) is below E. " +
+                        "You must add a new certificate or add a MEES exemption.",
+                )
+            }
+
+            @Test
+            fun `is not visible when all certs are compliant`(page: Page) {
+                val propertyOwnershipId = 11
+                val detailsPage = navigator.goToPropertyDetailsLandlordView(propertyOwnershipId.toLong())
+
+                assertThat(detailsPage.notificationBanner).isHidden()
+            }
+
+            // TODO PRSD-1297 add tests for update links in notification messages
         }
     }
 
