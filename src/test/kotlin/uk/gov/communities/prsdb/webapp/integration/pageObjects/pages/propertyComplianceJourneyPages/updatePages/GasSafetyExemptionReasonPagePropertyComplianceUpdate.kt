e2f0d160e696034f846bed2c7c2d2620bd9633cd
JasCon <jasmin.conterio@softwire.com>
2025-07-16 13:14:27 +0100

    PRSD-1244: Add disabled gas safety exemption tests

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyComplianceJourneyPages/updatePages/GasSafetyExemptionReasonPagePropertyComplianceUpdate.kt
@@ -0,0 +1,16 @@
+package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyComplianceJourneyPages.updatePages
+
+import com.microsoft.playwright.Page
+import uk.gov.communities.prsdb.webapp.constants.enums.GasSafetyExemptionReason
+import uk.gov.communities.prsdb.webapp.controllers.PropertyComplianceController
+import uk.gov.communities.prsdb.webapp.forms.steps.PropertyComplianceStepId
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.ExemptionReasonFormPage
+
+class GasSafetyExemptionReasonPagePropertyComplianceUpdate(
+    page: Page,
+    urlArguments: Map<String, String>,
+) : ExemptionReasonFormPage<GasSafetyExemptionReason>(
+        page,
+        PropertyComplianceController.getUpdatePropertyCompliancePath(urlArguments["propertyOwnershipId"]!!.toLong()) +
+            "/${PropertyComplianceStepId.GasSafetyExemptionReason.urlPathSegment}",
+    )
