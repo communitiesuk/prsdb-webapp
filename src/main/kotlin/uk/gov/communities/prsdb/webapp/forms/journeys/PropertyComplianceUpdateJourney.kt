e0f1e8eb71dfd87660abf94d36ad279981b63cec
Alexander Read <alexander.read@communities.gov.uk>
2025-07-17 10:21:13 +0100

    PRSD-1244: Add extension tests

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/PropertyComplianceUpdateJourney.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/journeys/PropertyComplianceUpdateJourney.kt
@@ -14,7 +14,7 @@ import uk.gov.communities.prsdb.webapp.forms.steps.StepId
 import uk.gov.communities.prsdb.webapp.forms.steps.factories.PropertyComplianceSharedStepFactory
 import uk.gov.communities.prsdb.webapp.forms.tasks.JourneySection
 import uk.gov.communities.prsdb.webapp.forms.tasks.JourneyTask
-import uk.gov.communities.prsdb.webapp.helpers.extensions.journeyExtensions.PropertyComplianceJourneyDataExtensions.Companion.getIsAddingNewGasSafetyCertificate
+import uk.gov.communities.prsdb.webapp.helpers.extensions.journeyExtensions.PropertyComplianceJourneyDataExtensions.Companion.getHasNewGasSafetyCertificate
 import uk.gov.communities.prsdb.webapp.models.dataModels.updateModels.PropertyComplianceUpdateModel
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.GasSafeEngineerNumFormModel
@@ -215,7 +215,7 @@ class PropertyComplianceUpdateJourney(
                                 "title" to "propertyCompliance.title",
                                 "fieldSetHeading" to "forms.update.gasSafetyType.fieldSetHeading",
                                 "fieldSetHint" to "forms.gasSafety.fieldSetHint",
-                                "radioVariableName" to UpdateGasSafetyCertificateFormModel::isUploadingNewCertificate.name,
+                                "radioVariableName" to UpdateGasSafetyCertificateFormModel::hasNewCertificate.name,
                                 "radioOptions" to
                                     listOf(
                                         RadiosButtonViewModel(
@@ -233,7 +233,7 @@ class PropertyComplianceUpdateJourney(
                             ),
                     ),
                 nextAction = { journeyData, _ ->
-                    if (journeyData.getIsAddingNewGasSafetyCertificate()) {
+                    if (journeyData.getHasNewGasSafetyCertificate()) {
                         Pair(PropertyComplianceStepId.GasSafetyIssueDate, null)
                     } else {
                         Pair(PropertyComplianceStepId.GasSafetyExemptionReason, null)
