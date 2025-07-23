22e2a4aec2b87124753270f0f06b79792fe6f9ab
JasCon <jasmin.conterio@softwire.com>
2025-07-23 09:35:10 +0100

    PRSD-1312: Update TODO

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/UpdateGasSafetyCertificateFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/UpdateGasSafetyCertificateFormModel.kt
@@ -4,7 +4,7 @@ import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
 
 @IsValidPrioritised
 class UpdateGasSafetyCertificateFormModel : FormModel {
-    // TODO PRSD-1312: Make this reachable, maybe with help from Alex
+    // TODO PRSD-1245: Add ths validation back in when originalJourneyData is being loaded correctly
 //    @NotNull(message = "forms.update.gasSafetyType.error.missing")
     var hasNewCertificate: Boolean? = null
 }
