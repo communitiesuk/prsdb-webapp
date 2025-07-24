5eea6a9a696d89271c75f4d73022e0b36e346257
JasCon <jasmin.conterio@softwire.com>
2025-07-24 16:52:26 +0100

    PRSD-1312: Remove IsValidPrioritised annotation

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/UpdateEicrFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/UpdateEicrFormModel.kt
@@ -1,8 +1,5 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
-import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
-
-@IsValidPrioritised
 class UpdateEicrFormModel : FormModel {
     // TODO PRSD-1245 or PRSD-1246: Add ths validation back in when originalJourneyData is being loaded correctly
 //    @NotNull(message = "forms.update.eicr.error.missing")
