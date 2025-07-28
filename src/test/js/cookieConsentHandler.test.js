b0bcd80ee385188f4b23d45d2f5b46f6b2050535
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-07-28 17:47:59 +0100

    PRSD-1167: Tears down global jsdom between tests

--- src/test/js/cookieConsentHandler.test.js
+++ src/test/js/cookieConsentHandler.test.js
@@ -1,9 +1,9 @@
 import jsdom from 'global-jsdom';
-import { describe, test, beforeEach } from 'node:test';
+import { describe, test } from 'node:test';
 import assert from 'node:assert/strict';
 import { addCookieConsentHandler } from '#main-javascript/cookieConsentHandler.js';
 
-function setup(onCookiesPage = false) {
+function setupAndReturnTeardown(onCookiesPage = false) {
     const html = `
         <div class="govuk-cookie-banner" hidden>
             <div id="cookie-banner-message">
@@ -18,84 +18,95 @@ function setup(onCookiesPage = false) {
         </div>
     `;
 
-    jsdom(html, {
+    return jsdom(html, {
         url: onCookiesPage ? 'https://example.com/cookies' : 'https://example.com'
     });
 }
 
 describe('Cookie Consent Handler', () => {
-    beforeEach(() => { setup() });
-
     test('Displays cookie banner if not on cookies page and consent cookie is not set', () => {
+        const teardown = setupAndReturnTeardown();
         const cookieBanner = document.querySelector('.govuk-cookie-banner');
 
         addCookieConsentHandler();
 
         assert.strictEqual(cookieBanner.hidden, false);
+
+        teardown();
     });
 
     test('Does not display cookie banner if consent cookie is set', () => {
+        const teardown = setupAndReturnTeardown();
         document.cookie = 'cookie_consent=true';
         const cookieBanner = document.querySelector('.govuk-cookie-banner');
 
         addCookieConsentHandler();
 
         assert.strictEqual(cookieBanner.hidden, true);
+
+        teardown();
     });
 
     test('Does not display cookie banner on cookies page', () => {
         const onCookiesPage = true;
-        setup(onCookiesPage);
-
+        const teardown = setupAndReturnTeardown(onCookiesPage);
         const cookieBanner = document.querySelector('.govuk-cookie-banner');
 
         addCookieConsentHandler();
 
         assert.strictEqual(cookieBanner.hidden, true);
+
+        teardown();
     });
 
     test('Makes accept cookies button create consent cookie and display confirmation message when clicked', () => {
+        const teardown = setupAndReturnTeardown();
         const cookieMessage = document.getElementById('cookie-banner-message');
         const acceptButton = document.getElementById('accept-cookies-button');
         const confirmationMessage = document.getElementById('cookie-banner-confirmation-message');
         const acceptedText = document.getElementById('cookies-accepted-text');
 
         addCookieConsentHandler();
-
         acceptButton.click();
 
         assert.strictEqual(document.cookie.includes('cookie_consent=true'), true);
         assert.strictEqual(cookieMessage.hidden, true);
         assert.strictEqual(confirmationMessage.hidden, false);
         assert.strictEqual(acceptedText.hidden, false);
+
+        teardown();
     });
 
     test('Makes reject cookies button create no-consent cookie and display rejection message when clicked', () => {
+        const teardown = setupAndReturnTeardown();
         const cookieMessage = document.getElementById('cookie-banner-message');
         const rejectButton = document.getElementById('reject-cookies-button');
         const rejectionMessage = document.getElementById('cookie-banner-confirmation-message');
         const rejectedText = document.getElementById('cookies-rejected-text');
 
         addCookieConsentHandler();
-
         rejectButton.click();
 
         assert.strictEqual(document.cookie.includes('cookie_consent=false'), true);
         assert.strictEqual(cookieMessage.hidden, true);
         assert.strictEqual(rejectionMessage.hidden, false);
         assert.strictEqual(rejectedText.hidden, false);
+
+        teardown();
     });
 
     test('Makes hide confirmation button hide cookie banner when clicked', () => {
+        const teardown = setupAndReturnTeardown();
         const cookieBanner = document.querySelector('.govuk-cookie-banner');
         const acceptButton = document.getElementById('accept-cookies-button');
         const hideButton = document.getElementById('hide-cookies-confirmation-button');
 
         addCookieConsentHandler();
-
         acceptButton.click();
         hideButton.click();
 
         assert.strictEqual(cookieBanner.hidden, true);
+
+        teardown();
     });
 });
