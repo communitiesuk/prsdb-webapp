7e11dcf5945cd9fa9a72e8ae8e1b45bd2838b5d1
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-08-05 16:06:24 +0100

    PRSD-1357: update test

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/laUserRegistrationJourneyPages/ConfirmationPageLaUserRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/laUserRegistrationJourneyPages/ConfirmationPageLaUserRegistration.kt
@@ -4,12 +4,13 @@ import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.constants.CONFIRMATION_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.RegisterLAUserController
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Button
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Heading
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 
 class ConfirmationPageLaUserRegistration(
     page: Page,
 ) : BasePage(page, "${RegisterLAUserController.LA_USER_REGISTRATION_ROUTE}/$CONFIRMATION_PATH_SEGMENT") {
-    val bannerHeading = page.locator(".govuk-panel__title")
-    val bodyHeading = page.locator(".govuk-heading-xl")
+    val bannerHeading = Heading(page.locator(".govuk-panel__title"))
+    val bodyHeading = Heading(page.locator(".govuk-heading-xl"))
     val returnToDashboardButton = Button.byText(page, "Go to dashboard")
 }
