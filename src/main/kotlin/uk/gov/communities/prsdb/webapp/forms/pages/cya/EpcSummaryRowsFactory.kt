d7c6c1b9fc4baf57ee10d5f5766080b734e74aed
JasCon <jasmin.conterio@softwire.com>
2025-08-07 09:58:10 +0100

    PRSD-1392: Make EPC link open in new tab

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/pages/cya/EpcSummaryRowsFactory.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/forms/pages/cya/EpcSummaryRowsFactory.kt
@@ -59,6 +59,7 @@ class EpcSummaryRowsFactory(
             fieldValue,
             epcStartingStep.urlPathSegment,
             valueUrl,
+            valueUrlOpensNewTab = true,
         )
     }
 
