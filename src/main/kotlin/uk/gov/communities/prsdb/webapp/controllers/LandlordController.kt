b1c6c9dcd38f43725f818d65b8982831b5ae17ba
Bill Haigh <bill.haigh@softwire.com>
2025-08-08 17:14:58 +0100

    updated landlord controller and model to provide a route, updated both html pages so that it displays

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LandlordController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LandlordController.kt
@@ -25,6 +25,7 @@ import uk.gov.communities.prsdb.webapp.controllers.LandlordController.Companion.
 import uk.gov.communities.prsdb.webapp.exceptions.PrsdbWebException
 import uk.gov.communities.prsdb.webapp.models.dataModels.RegistrationNumberDataModel
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.DeleteIncompletePropertyRegistrationAreYouSureFormModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.formModels.BetaFeedbackModel
 import uk.gov.communities.prsdb.webapp.models.viewModels.formModels.RadiosButtonViewModel
 import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.IncompleteComplianceViewModelBuilder
 import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.IncompletePropertyViewModelBuilder
@@ -181,7 +182,10 @@ class LandlordController(
     }
 
     @GetMapping("/${BETA_BANNER_FEEDBACK_PATH_SEGMENT}")
-    fun betaBannerFeedback(): String = "betaBannerFeedback"
+    fun betaBannerFeedback(model: Model): String {
+        model.addAttribute("betaFeedbackModel", BetaFeedbackModel())
+        return "betaBannerFeedback"
+    }
 
     fun populateDeleteIncompletePropertyRegistrationModel(
         model: Model,
