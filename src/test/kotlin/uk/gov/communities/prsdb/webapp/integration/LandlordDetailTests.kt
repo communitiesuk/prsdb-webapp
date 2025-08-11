5be1f41c684bf08521a6db0c8a0eef826b010425
Kiran Randhawa Kukar <113037474+Kiran-R-K@users.noreply.github.com>
2025-08-11 09:32:16 +0100

    PRSD-1355: update webapp content local authorities to local councils (#636)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDetailTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDetailTests.kt
@@ -30,7 +30,7 @@ class LandlordDetailTests : SinglePageTestWithSeedData("data-local.sql") {
 
             assertEquals(detailsPage.tabs.activeTabPanelId, "registered-properties")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(0)).containsText("Property address")
-            assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(1)).containsText("Local authority")
+            assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(1)).containsText("Local council")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(2)).containsText("Property licence")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(3)).containsText("Tenanted")
         }
@@ -73,7 +73,7 @@ class LandlordDetailTests : SinglePageTestWithSeedData("data-local.sql") {
             assertEquals(detailsPage.tabs.activeTabPanelId, "registered-properties")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(0)).containsText("Property address")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(1)).containsText("Registration number")
-            assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(2)).containsText("Local authority")
+            assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(2)).containsText("Local council")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(3)).containsText("Licensing type")
             assertThat(detailsPage.registeredPropertiesTable.headerRow.getCell(4)).containsText("Tenanted")
         }
