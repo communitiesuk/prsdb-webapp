b2d00c0bc314d40182be056c43c3466094e8ca3c
JasCon <jasmin.conterio@softwire.com>
2025-08-11 17:00:18 +0100

    PRSD-1183: delete domain (works on integration) and non-domain (works in tests and on local) versions...

--- src/main/js/cookieConsentHandler.js
+++ src/main/js/cookieConsentHandler.js
@@ -32,6 +32,7 @@ function updateGaConsent(isGranted = false) {
 
 function deleteCookie(name) {
     document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=communities.gov.uk;';
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 }
 
 class CookieBanner {
