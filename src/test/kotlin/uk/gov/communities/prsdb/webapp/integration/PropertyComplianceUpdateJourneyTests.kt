3fe00ac6b820e7829d125b4451a1f439025ba5ea
Alexander Read <alexander.read@communities.gov.uk>
2025-08-11 12:29:41 +0100

    PRSD-976: Update tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyComplianceUpdateJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyComplianceUpdateJourneyTests.kt
@@ -128,7 +128,7 @@ class PropertyComplianceUpdateJourneyTests : JourneyTestWithSeedData("data-local
         val cyaPage = assertPageIs(page, GasSafetyCheckYourAnswersPropertyComplianceUpdate::class, urlArguments)
 
         // Gas Safety Check Your Answers page
-        assertThat(cyaPage.form.summaryList.gasSafetyRow.value).containsText("Download certificate")
+        assertThat(cyaPage.form.summaryList.gasSafetyRow.value).containsText("Pending virus scan")
         assertThat(cyaPage.form.summaryList.issueDateRow.value).containsText(dateFormat.format(currentDate))
         assertThat(cyaPage.form.summaryList.engineerRow.value).containsText("1234567")
         cyaPage.form.submit()
@@ -254,7 +254,7 @@ class PropertyComplianceUpdateJourneyTests : JourneyTestWithSeedData("data-local
 
         // EICR Check Your Answers page
         val cyaPage = assertPageIs(page, EicrCheckYourAnswersPagePropertyComplianceUpdate::class, urlArguments)
-        assertThat(cyaPage.form.summaryList.eicrRow.value).containsText("Download EICR")
+        assertThat(cyaPage.form.summaryList.eicrRow.value).containsText("Pending virus scan")
         assertThat(cyaPage.form.summaryList.issueDateRow.value).containsText(dateFormat.format(currentDate))
         cyaPage.form.submit()
 
