b10a1a7eee1317dc85cf6be1dfcb9e5e0cc2636e
JasCon <jasmin.conterio@softwire.com>
2025-08-12 13:21:48 +0100

    PRSD-1183: Pass google tag id into layout.html

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
@@ -1,5 +1,6 @@
 package uk.gov.communities.prsdb.webapp.controllers.controllerAdvice
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
 import uk.gov.communities.prsdb.webapp.PrsdbControllerAdvice
@@ -7,8 +8,12 @@ import uk.gov.communities.prsdb.webapp.controllers.CookiesController.Companion.C
 
 @PrsdbControllerAdvice
 class GlobalModelAttributes {
+    @Value("\${google-analytics.measurement-id}")
+    private lateinit var gaMeasurementId: String
+
     @ModelAttribute
     fun addGlobalModelAttributes(model: Model) {
         model.addAttribute("cookiesUrl", COOKIES_ROUTE)
+        model.addAttribute("googleAnalyticsMeasurementId", gaMeasurementId)
     }
 }
