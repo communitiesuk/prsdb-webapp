39b9fef2982a61c40edde64f8b085b7eb954f41c
Bill Haigh <bill.haigh@softwire.com>
2025-08-12 14:49:35 +0100

    moved back button and made some changes to controllers

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
@@ -4,9 +4,6 @@ import jakarta.servlet.http.HttpServletRequest
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
 import uk.gov.communities.prsdb.webapp.PrsdbControllerAdvice
-import uk.gov.communities.prsdb.webapp.constants.BETA_BANNER_PATH_SEGMENT
-import uk.gov.communities.prsdb.webapp.constants.LANDLORD_PATH_SEGMENT
-import uk.gov.communities.prsdb.webapp.constants.LOCAL_AUTHORITY_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.CookiesController.Companion.COOKIES_ROUTE
 
 @PrsdbControllerAdvice
@@ -17,17 +14,12 @@ class GlobalModelAttributes {
     }
 
     @ModelAttribute("feedbackBannerUrl")
-    fun addFeedbackBannerUrl(
-        model: Model,
-        request: HttpServletRequest,
-    ) {
+    fun addFeedbackBannerUrl(request: HttpServletRequest): String {
         val path = request.requestURI
-        val url =
-            when {
-                path.startsWith("/$LOCAL_AUTHORITY_PATH_SEGMENT") -> "/$LOCAL_AUTHORITY_PATH_SEGMENT/$BETA_BANNER_PATH_SEGMENT"
-                path.startsWith("/$LANDLORD_PATH_SEGMENT") -> "/$LANDLORD_PATH_SEGMENT/$BETA_BANNER_PATH_SEGMENT"
-                else -> "/$LANDLORD_PATH_SEGMENT/$BETA_BANNER_PATH_SEGMENT"
-            }
-        model.addAttribute("feedbackBannerUrl", url)
+        return when {
+            path.startsWith("/local-authority") -> "/local-authority/feedback"
+            path.startsWith("/landlord") -> "/landlord/feedback"
+            else -> "/landlord/beta-banner"
+        }
     }
 }
