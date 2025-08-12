a93baee4b3f9271ccc98b8c6d05f1cd53c04d2cd
Bill Haigh <bill.haigh@softwire.com>
2025-08-12 10:25:10 +0100

    added route handling through GlobalModelAttributes into betaBanner. Updated BetaFeedbackController

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
@@ -3,6 +3,8 @@ package uk.gov.communities.prsdb.webapp.controllers.controllerAdvice
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
 import uk.gov.communities.prsdb.webapp.PrsdbControllerAdvice
+import uk.gov.communities.prsdb.webapp.constants.ROLE_LA_ADMIN
+import uk.gov.communities.prsdb.webapp.constants.ROLE_LA_USER
 import uk.gov.communities.prsdb.webapp.controllers.CookiesController.Companion.COOKIES_ROUTE
 
 @PrsdbControllerAdvice
@@ -10,5 +12,21 @@ class GlobalModelAttributes {
     @ModelAttribute
     fun addGlobalModelAttributes(model: Model) {
         model.addAttribute("cookiesUrl", COOKIES_ROUTE)
+    }
+
+    @ModelAttribute("feedbackUrl")
+    fun addFeedbackUrl(
+        model: Model,
+        auth: org.springframework.security.core.Authentication?,
+    ) {
+        val url =
+            if (auth != null && auth.isAuthenticated &&
+                auth.authorities.any { it.authority == ROLE_LA_USER || it.authority == ROLE_LA_ADMIN }
+            ) {
+                "/local-authority/feedback"
+            } else {
+                "/landlord/feedback"
+            }
+        model.addAttribute("feedbackUrl", url)
     }
 }
