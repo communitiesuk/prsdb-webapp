9dcbe8f2510f4b44384bd977ea9a0b05421cea88
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-08-15 10:06:38 +0100

    PRSD-1422: Uses pure html in form

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/CookiesController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/CookiesController.kt
@@ -8,7 +8,6 @@ import uk.gov.communities.prsdb.webapp.constants.BACK_URL_ATTR_NAME
 import uk.gov.communities.prsdb.webapp.constants.COOKIES_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.DASHBOARD_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.CookiesController.Companion.COOKIES_ROUTE
-import uk.gov.communities.prsdb.webapp.models.viewModels.formModels.RadiosButtonViewModel
 
 @PrsdbController
 @RequestMapping(COOKIES_ROUTE)
@@ -16,22 +15,6 @@ class CookiesController {
     @GetMapping
     fun getCookiesPage(model: Model): String {
         model.addAttribute(BACK_URL_ATTR_NAME, DASHBOARD_PATH_SEGMENT)
-
-        // We use an anonymous form model object as JavaScript is used to handle form submission
-        model.addAttribute(
-            "consentWrapper",
-            object {
-                var consent: Boolean? = null
-            },
-        )
-        model.addAttribute(
-            "radioOptions",
-            listOf(
-                RadiosButtonViewModel(value = true, valueStr = "yes", labelMsgKey = "forms.radios.option.yes.label"),
-                RadiosButtonViewModel(value = false, valueStr = "no", labelMsgKey = "forms.radios.option.no.label"),
-            ),
-        )
-
         return "cookies"
     }
 
