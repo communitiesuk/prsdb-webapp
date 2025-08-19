fa886b26b832fa41c0b76a794ea476adc4e54d57
Bill Haigh <bill.haigh@softwire.com>
2025-08-19 17:20:28 +0100

    PRSD-1439: Added validation to model

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/formModels/BetaFeedbackModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/formModels/BetaFeedbackModel.kt
@@ -1,6 +1,25 @@
 package uk.gov.communities.prsdb.webapp.models.viewModels.formModels
+import uk.gov.communities.prsdb.webapp.constants.BETA_FEEDBACK_MAX_LENGTH
+import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
+import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
+import uk.gov.communities.prsdb.webapp.validation.LengthConstraintValidator
+import uk.gov.communities.prsdb.webapp.validation.NotBlankConstraintValidator
+import uk.gov.communities.prsdb.webapp.validation.ValidatedBy
 
-class BetaFeedbackModel(
-    var feedback: String? = null,
-    var email: String? = null,
-)
+@IsValidPrioritised
+class BetaFeedbackModel {
+    @ValidatedBy(
+        constraints = [
+            ConstraintDescriptor(
+                messageKey = "forms.betaFeedback.error.missing",
+                validatorType = NotBlankConstraintValidator::class,
+            ),
+            ConstraintDescriptor(
+                messageKey = "forms.betaFeedback.error.tooLong",
+                validatorType = LengthConstraintValidator::class,
+                validatorArgs = arrayOf("0", BETA_FEEDBACK_MAX_LENGTH.toString()),
+            ),
+        ],
+    )
+    var feedback: String = ""
+}
