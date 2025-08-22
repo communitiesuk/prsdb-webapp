09fd133afc3273d453f67014280987a44a2e9b0a
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-08-22 16:31:31 +0100

    PRSD-1455: Adds footer links

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
@@ -3,9 +3,16 @@ package uk.gov.communities.prsdb.webapp.controllers.controllerAdvice
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
-import uk.gov.communities.prsdb.webapp.PrsdbControllerAdvice
+import uk.gov.communities.prsdb.webapp.annotations.PrsdbControllerAdvice
 import uk.gov.communities.prsdb.webapp.config.interceptors.BackLinkInterceptor.Companion.overrideBackLinkForUrl
+import uk.gov.communities.prsdb.webapp.constants.CROWN_COPYRIGHT_URL
+import uk.gov.communities.prsdb.webapp.constants.GOV_LICENCE_URL
+import uk.gov.communities.prsdb.webapp.constants.MHCLG_URL
+import uk.gov.communities.prsdb.webapp.constants.PRSD_EMAIL
+import uk.gov.communities.prsdb.webapp.constants.RENTERS_RIGHTS_BILL_URL
 import uk.gov.communities.prsdb.webapp.controllers.CookiesController.Companion.COOKIES_ROUTE
+import uk.gov.communities.prsdb.webapp.controllers.LandlordPrivacyNoticeController.Companion.LANDLORD_PRIVACY_NOTICE_ROUTE
+import uk.gov.communities.prsdb.webapp.controllers.LocalAuthorityPrivacyNoticeController.Companion.LOCAL_AUTHORITY_PRIVACY_NOTICE_ROUTE
 import uk.gov.communities.prsdb.webapp.services.BackUrlStorageService
 
 @PrsdbControllerAdvice
@@ -23,10 +30,20 @@ class GlobalModelAttributes(
 
     @ModelAttribute
     fun addGlobalModelAttributes(model: Model) {
+        // Cookie banner attributes
         model.addAttribute("cookiesUrl", COOKIES_ROUTE.overrideBackLinkForUrl(backUrlStorageService.storeCurrentUrlReturningKey()))
         model.addAttribute("googleAnalyticsMeasurementId", gaMeasurementId)
         model.addAttribute("googleAnalyticsCookieDomain", gaCookieDomain)
         model.addAttribute("plausibleDomainId", plausibleDomainId)
+
+        // Footer attributes
+        model.addAttribute("prsdbEmail", PRSD_EMAIL)
+        model.addAttribute("landlordPrivacyUrl", LANDLORD_PRIVACY_NOTICE_ROUTE)
+        model.addAttribute("localCouncilPrivacyUrl", LOCAL_AUTHORITY_PRIVACY_NOTICE_ROUTE)
+        model.addAttribute("rentersRightsBillUrl", RENTERS_RIGHTS_BILL_URL)
+        model.addAttribute("mhclgUrl", MHCLG_URL)
+        model.addAttribute("licenceUrl", GOV_LICENCE_URL)
+        model.addAttribute("copyrightUrl", CROWN_COPYRIGHT_URL)
     }
 
     @ModelAttribute("feedbackBannerUrl")
