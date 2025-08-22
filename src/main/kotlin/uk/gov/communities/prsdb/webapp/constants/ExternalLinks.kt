af10ac86416d52c4674aa94b619675c2fc2189d3
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-08-22 18:11:08 +0100

    PRSD-1455: Adds footer links (#694)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/ExternalLinks.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/ExternalLinks.kt
@@ -56,6 +56,8 @@ const val ONE_LOGIN_INFO_URL = "https://www.gov.uk/using-your-gov-uk-one-login"
 const val ONE_LOGIN_INFO_URL_POVING_YOUR_IDENTITY =
     "https://www.gov.uk/using-your-gov-uk-one-login/proving-your-identity"
 
+const val RENTERS_RIGHTS_BILL_URL = "https://bills.parliament.uk/bills/3764"
+
 const val RENTERS_RIGHTS_BILL_GUIDE_URL =
     "https://www.gov.uk/government/publications/guide-to-the-renters-rights-bill" +
         "/guide-to-the-renters-rights-bill"
@@ -66,9 +68,17 @@ const val RENTERS_RIGHTS_BILL_PRSD =
 
 const val TENANCY_TYPES_GUIDE_URL = "https://www.gov.uk/private-renting-tenancy-agreements/tenancy-types"
 
+const val MHCLG_URL = "https://www.gov.uk/government/organisations/ministry-of-housing-communities-local-government"
+
 const val COMPLAINTS_PROCEDURE_URL =
     "https://www.gov.uk/government/organisations/ministry-of-housing-communities-local-government/about/complaints-procedure"
 
 const val INFORMATION_COMMISSIONERS_OFFICE_URL = "https://ico.org.uk/"
 
 const val FEEDBACK_FORM_URL = "https://forms.cloud.microsoft/e/q1mupnMCMY"
+
+const val GOV_LICENCE_URL = "https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+
+const val CROWN_COPYRIGHT_URL =
+    "https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information" +
+        "/uk-government-licensing-framework/crown-copyright/"
