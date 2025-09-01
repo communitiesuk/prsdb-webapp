97f84cfc2f0470941384a6a5431af3aed3a9429e
Bill Haigh <bill.haigh@softwire.com>
2025-09-01 15:34:45 +0100

    PRSD-1513: incomplete property records now deleted as part of landlord deregistration

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordDeregistrationService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordDeregistrationService.kt
@@ -4,7 +4,9 @@ import jakarta.servlet.http.HttpSession
 import jakarta.transaction.Transactional
 import uk.gov.communities.prsdb.webapp.annotations.PrsdbWebService
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_HAD_ACTIVE_PROPERTIES
+import uk.gov.communities.prsdb.webapp.constants.enums.JourneyType
 import uk.gov.communities.prsdb.webapp.database.entity.PropertyOwnership
+import uk.gov.communities.prsdb.webapp.database.repository.FormContextRepository
 import uk.gov.communities.prsdb.webapp.database.repository.LandlordRepository
 import uk.gov.communities.prsdb.webapp.database.repository.OneLoginUserRepository
 
@@ -14,10 +16,21 @@ class LandlordDeregistrationService(
     private val oneLoginUserRepository: OneLoginUserRepository,
     private val propertyOwnershipService: PropertyOwnershipService,
     private val propertyDeregistrationService: PropertyDeregistrationService,
+    private val formContextService: FormContextService,
+    private val formContextRepository: FormContextRepository,
     private val session: HttpSession,
 ) {
     @Transactional
     fun deregisterLandlordAndTheirProperties(baseUserId: String): List<PropertyOwnership> {
+        val incompletePropertyRegistrations =
+            formContextRepository.findAllByUser_IdAndJourneyType(
+                baseUserId,
+                JourneyType.PROPERTY_REGISTRATION,
+            )
+        if (incompletePropertyRegistrations.isNotEmpty()) {
+            formContextService.deleteFormContexts(incompletePropertyRegistrations)
+        }
+
         val registeredProperties = propertyOwnershipService.retrieveAllPropertiesForLandlord(baseUserId)
         if (registeredProperties.isNotEmpty()) {
             propertyDeregistrationService.deregisterProperties(registeredProperties)
