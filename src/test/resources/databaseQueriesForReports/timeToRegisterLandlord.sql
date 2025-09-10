3b7965728e17bbaf8bdd5ba0ef30f63d3990e696
JasCon <jasmin.conterio@softwire.com>
2025-09-10 16:27:13 +0100

    PRSD-1521: Script for time to register landlord

---
+++ src/test/resources/databaseQueriesForReports/timeToRegisterLandlord.sql
@@ -0,0 +1,18 @@
+-- We count the start of landlord registration as the time when the landlord claims a passcode (assumed to be when their passcode was last updated)
+SELECT
+    percentile_cont(0.50) WITHIN GROUP (ORDER BY time_to_register_landlord) AS p50_time_to_register_landlord,
+    percentile_cont(0.95) WITHIN GROUP (ORDER BY time_to_register_landlord) AS p95_time_to_register_landlord,
+    percentile_cont(0.99) WITHIN GROUP (ORDER BY time_to_register_landlord) AS p99_time_to_register_landlord
+FROM (
+     SELECT
+         p.last_modified_date - l.created_date AS time_to_register_landlord
+     FROM
+         landlord l
+             JOIN (
+             SELECT
+                 subject_identifier,
+                 last_modified_date
+             FROM
+                 passcode
+         ) p ON p.subject_identifier = p.subject_identifier
+ ) t
