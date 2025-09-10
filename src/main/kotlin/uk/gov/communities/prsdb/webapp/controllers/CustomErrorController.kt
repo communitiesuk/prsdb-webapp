644bd0be5e99edbba3b294cc8e22432288645e72
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-09-10 09:47:26 +0100

    PRSD-1540: make error path segments constants

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/CustomErrorController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/CustomErrorController.kt
@@ -10,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping
 import uk.gov.communities.prsdb.webapp.annotations.PrsdbController
 import uk.gov.communities.prsdb.webapp.constants.CYA_ERROR_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.ERROR_PATH_SEGMENT
+import uk.gov.communities.prsdb.webapp.constants.FILE_TOO_LARGE_PATH_SEGMENT
 
 @PrsdbController
-@RequestMapping("error")
+@RequestMapping(ERROR_PATH_SEGMENT)
 class CustomErrorController : ErrorController {
-    @GetMapping("file-too-large")
+    @GetMapping(FILE_TOO_LARGE_PATH_SEGMENT)
     fun fileTooLargeErrorPage() = "error/fileTooLarge"
 
     // We are sending CYA page 'data has changed' errors to this endpoint
