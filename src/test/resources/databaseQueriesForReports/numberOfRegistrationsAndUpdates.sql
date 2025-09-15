728204b4af40566c77d2de9773f63b51b7ae3b38
JasCon <jasmin.conterio@softwire.com>
2025-09-15 16:10:01 +0100

    PRSD-1521: Update queries for the most recent reporting period to take a :reference_date and add a lower date limit

--- src/test/resources/databaseQueriesForReports/numberOfRegistrationsAndUpdates.sql
+++ src/test/resources/databaseQueriesForReports/numberOfRegistrationsAndUpdates.sql
@@ -1,25 +1,27 @@
+-- A :reference_date of CURRENT_DATE will count records between just before midnight last night and 14 days before that.
+-- A :reference_date of make_date(2025, 09, 15) will count records from 1st September (2025-09-01 00:00:00.000000) - 14th September inclusive (2025-09-015 00:00:00.000000 is NOT included).
 -- Total current registrations, and those in the last 2 weeks
 -- Landlord registrations / updates
 SELECT
     COUNT(*) AS total_landlords,
-    COUNT(*) FILTER (WHERE created_date >= NOW() - INTERVAL '14 DAYS') AS new_landlords_last_2_weeks,
-    COUNT(*) FILTER (WHERE last_modified_date >= NOW() - INTERVAL '14 DAYS') AS updated_landlords_last_2_weeks
+    COUNT(*) FILTER (WHERE created_date >= :reference_date - INTERVAL '14 DAYS' AND created_date < :reference_date) AS new_landlords_last_2_weeks,
+    COUNT(*) FILTER (WHERE last_modified_date >= :reference_date - INTERVAL '14 DAYS' AND last_modified_date < :reference_date) AS updated_landlords_last_2_weeks
 FROM
     landlord;
 
 -- Property registrations / updates
 SELECT
     COUNT(*) AS total_property_ownerships,
-    COUNT(*) FILTER (WHERE created_date >= NOW() - INTERVAL '14 DAYS') AS new_property_ownerships_last_2_weeks,
-    COUNT(*) FILTER (WHERE last_modified_date >= NOW() - INTERVAL '14 DAYS') AS updated_property_ownerships_last_2_weeks
+    COUNT(*) FILTER (WHERE created_date >= :reference_date - INTERVAL '14 DAYS' AND created_date < :reference_date) AS new_property_ownerships_last_2_weeks,
+    COUNT(*) FILTER (WHERE last_modified_date >= :reference_date - INTERVAL '14 DAYS' AND last_modified_date < :reference_date) AS updated_property_ownerships_last_2_weeks
 FROM
     property_ownership;
 
 -- Compliances added / updated
 SELECT
     COUNT(*) AS total_property_compliances,
-    COUNT(*) FILTER (WHERE created_date >= NOW() - INTERVAL '14 DAYS') AS new_property_compliances_last_2_weeks,
-    COUNT(*) FILTER (WHERE last_modified_date >= NOW() - INTERVAL '14 DAYS') AS updated_property_compliances_last_2_weeks
+    COUNT(*) FILTER (WHERE created_date >= :reference_date - INTERVAL '14 DAYS' AND created_date < :reference_date) AS new_property_compliances_last_2_weeks,
+    COUNT(*) FILTER (WHERE last_modified_date >= :reference_date - INTERVAL '14 DAYS' AND last_modified_date < :reference_date) AS updated_property_compliances_last_2_weeks
 FROM
     property_compliance;
 
@@ -27,8 +29,8 @@ FROM
 SELECT
     is_manager AS is_admin,
     COUNT(*) AS total_lc_users,
-    COUNT(*) FILTER (WHERE created_date >= NOW() - INTERVAL '14 DAYS') AS new_lc_users_last_2_weeks,
-    COUNT(*) FILTER (WHERE last_modified_date >= NOW() - INTERVAL '14 DAYS') AS updated_lc_users_last_2_weeks
+    COUNT(*) FILTER (WHERE created_date >= :reference_date - INTERVAL '14 DAYS' AND created_date < :reference_date) AS new_lc_users_last_2_weeks,
+    COUNT(*) FILTER (WHERE last_modified_date >= :reference_date - INTERVAL '14 DAYS' AND last_modified_date < :reference_date) AS updated_lc_users_last_2_weeks
 FROM
     local_authority_user
 WHERE local_authority_id != 2
@@ -36,6 +38,6 @@ GROUP BY is_manager;
 
 -- Passcodes generated
 SELECT
-    COUNT(*) FILTER (WHERE created_date >= NOW() - INTERVAL '14 DAYS') AS new_passcodes_last_2_weeks
+    COUNT(*) FILTER (WHERE created_date >= :reference_date - INTERVAL '14 DAYS' AND created_date < :reference_date) AS new_passcodes_last_2_weeks
 FROM
     passcode;
--- src/test/resources/databaseQueriesForReports/registrationsPerLocalCouncil.sql
+++ src/test/resources/databaseQueriesForReports/registrationsPerLocalCouncil.sql
@@ -1,9 +1,12 @@
+-- A :reference_date of CURRENT_DATE will count records between just before midnight last night and 14 days before that.
+-- A :reference_date of make_date(2025, 09, 15) will count records from 1st September (2025-09-01 00:00:00.000000) - 14th September inclusive (2025-09-015 00:00:00.000000 is NOT included).
+
 -- Landlord (not usually associated with a local council but we can use claimed passcodes)
 SELECT
     la.name AS local_council_name,
     p.local_authority_id AS local_council_id,
     COUNT(*) AS total_claimed_passcodes,
-    COUNT(*) FILTER (WHERE p.last_modified_date >= NOW() - INTERVAL '14 DAYS') AS newly_claimed_passcodes_last_2_weeks
+    COUNT(*) FILTER (WHERE p.last_modified_date >= :reference_date - INTERVAL '14 DAYS' AND p.last_modified_date < :reference_date) AS newly_claimed_passcodes_last_2_weeks
 FROM
     passcode p
     JOIN local_authority la ON p.local_authority_id = la.id
@@ -17,7 +20,7 @@ ORDER BY la.name;
 SELECT
     la.name AS local_council_name,
     COUNT(*) AS total_property_ownerships,
-    COUNT(*) FILTER (WHERE ownerships.po_created_date >= NOW() - INTERVAL '14 DAYS') AS new_property_ownerships_last_2_weeks
+    COUNT(*) FILTER (WHERE ownerships.po_created_date >= :reference_date - INTERVAL '14 DAYS' AND ownerships.po_created_date < :reference_date) AS new_property_ownerships_last_2_weeks
 FROM(
         SELECT
             a.local_authority_id AS local_council_id,
@@ -39,7 +42,7 @@ SELECT
     la.name AS local_council_name,
     lau.local_authority_id AS local_council_id,
     COUNT(*) AS total_lc_users,
-    COUNT(*) FILTER (WHERE lau.created_date >= NOW() - INTERVAL '14 DAYS') AS new_lc_users_last_2_weeks
+    COUNT(*) FILTER (WHERE lau.created_date >= :reference_date - INTERVAL '14 DAYS' AND lau.created_date < :reference_date) AS new_lc_users_last_2_weeks
 FROM
     local_authority_user lau
         JOIN local_authority la ON lau.local_authority_id = la.id
