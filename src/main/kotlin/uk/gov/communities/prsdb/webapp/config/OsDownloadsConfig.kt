be30481bd503809f7295f5fb6921af8dd7930845
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-09-23 15:25:28 +0100

    PRSD-1021: Creates OsDownloadsConfig

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/OsDownloadsConfig.kt
@@ -0,0 +1,21 @@
+package uk.gov.communities.prsdb.webapp.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import uk.gov.communities.prsdb.webapp.clients.OsDownloadsClient
+import java.net.http.HttpClient
+
+@Configuration
+class OsDownloadsConfig {
+    @Value("\${os.downloads.base-url}")
+    lateinit var baseURL: String
+
+    @Value("\${os.api-key}")
+    lateinit var apiKey: String
+
+    val httpClient: HttpClient = HttpClient.newHttpClient()
+
+    @Bean
+    fun osDownloadsClient() = OsDownloadsClient(httpClient, baseURL, apiKey)
+}
