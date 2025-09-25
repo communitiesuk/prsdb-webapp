83d3f1267f855c7bc647ca7bce2b448a1ff9a6f1
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-09-25 10:00:31 +0100

    PRSD-1021: Applies process only annotations

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/examples/ExampleOsDownloadsController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/examples/ExampleOsDownloadsController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
-import uk.gov.communities.prsdb.webapp.annotations.PrsdbController
+import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbController
 import uk.gov.communities.prsdb.webapp.clients.OsDownloadsClient
 import java.util.zip.ZipInputStream
 
