80618c8037e83402833060e4463432db5ca4f7f3
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2025-10-30 11:44:26 +0000

    PRSD-1604: Creates example application runner for scheduled tasks

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/application/ExampleScheduledTaskApplicationRunner.kt
@@ -0,0 +1,25 @@
+package uk.gov.communities.prsdb.webapp.application
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import kotlin.system.exitProcess
+
+@Component
+@Profile("web-server-deactivated & example-scheduled-task")
+class ExampleScheduledTaskApplicationRunner(
+    private val context: ApplicationContext,
+) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        println("Executing example scheduled task")
+
+        val code =
+            SpringApplication.exit(context, { 0 }).also {
+                println("Scheduled task executed. Application will exit now.")
+            }
+        exitProcess(code)
+    }
+}
