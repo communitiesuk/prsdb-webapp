8bab26a479e9b1525411d410e8aea22ddeb9ac0a
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2025-11-03 16:19:14 +0000

    PRSD-1604: Remove accidental qualification from class names

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/application/DefaultScheduledTaskApplicationRunner.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/application/DefaultScheduledTaskApplicationRunner.kt
@@ -1,6 +1,8 @@
 package uk.gov.communities.prsdb.webapp.application
 
+import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Profile
 import org.springframework.core.Ordered
@@ -14,11 +16,11 @@ import kotlin.system.exitProcess
 class DefaultScheduledTaskApplicationRunner(
     private val context: ApplicationContext,
 ) : ApplicationRunner {
-    override fun run(args: org.springframework.boot.ApplicationArguments?) {
+    override fun run(args: ApplicationArguments?) {
         println("The application was not configured for this scheduled task. Application will exit now.")
 
         val code =
-            org.springframework.boot.SpringApplication.exit(context, { 0 })
+            SpringApplication.exit(context, { 0 })
         exitProcess(code)
     }
 }
