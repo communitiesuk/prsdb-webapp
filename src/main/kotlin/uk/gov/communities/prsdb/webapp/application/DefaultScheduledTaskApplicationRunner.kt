f98898bac1c244d39f95f7eb7d370a80203950bb
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2025-11-03 12:36:11 +0000

    PRSD-1604: Adds default scheduled task handler

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/application/DefaultScheduledTaskApplicationRunner.kt
@@ -0,0 +1,24 @@
+package uk.gov.communities.prsdb.webapp.application
+
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Profile
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import kotlin.system.exitProcess
+
+@Component
+@Profile("web-server-deactivated")
+@Order(Ordered.LOWEST_PRECEDENCE)
+class DefaultScheduledTaskApplicationRunner(
+    private val context: ApplicationContext,
+) : ApplicationRunner {
+    override fun run(args: org.springframework.boot.ApplicationArguments?) {
+        println("The application was not configured for this scheduled task. Application will exit now.")
+
+        val code =
+            org.springframework.boot.SpringApplication.exit(context, { 0 })
+        exitProcess(code)
+    }
+}
