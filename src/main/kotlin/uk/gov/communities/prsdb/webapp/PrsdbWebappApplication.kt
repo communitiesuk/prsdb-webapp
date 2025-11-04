31758f4e5caf807c8353788220e0cf45896084f0
JasCon <jasmin.conterio@softwire.com>
2025-11-04 11:26:35 +0000

    PRSD-1647: Remove the EnableAspectJAutoProxy annotation (still seems to work)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/PrsdbWebappApplication.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/PrsdbWebappApplication.kt
@@ -2,10 +2,8 @@ package uk.gov.communities.prsdb.webapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication(scanBasePackages = ["uk.gov.communities.prsdb.webapp", "org.ff4j.aop"])
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 class PrsdbWebappApplication
 
 fun main(args: Array<String>) {
