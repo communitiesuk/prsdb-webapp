b2cb22dd62a37a029b7bd315e29959c010058d12
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-11-04 18:29:17 +0000

    PRSD-1655: add security headers

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/controllerAdvice/GlobalModelAttributes.kt
@@ -7,10 +7,13 @@ import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbControlle
 import uk.gov.communities.prsdb.webapp.config.interceptors.BackLinkInterceptor.Companion.overrideBackLinkForUrl
 import uk.gov.communities.prsdb.webapp.constants.CONFIRM_SIGN_OUT_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.CROWN_COPYRIGHT_URL
+import uk.gov.communities.prsdb.webapp.constants.GOOGLE_TAG_MANAGER
 import uk.gov.communities.prsdb.webapp.constants.GOV_LICENCE_URL
 import uk.gov.communities.prsdb.webapp.constants.MHCLG_URL
+import uk.gov.communities.prsdb.webapp.constants.PLAUSIBLE_SCRIPT_DOWNLOADS
 import uk.gov.communities.prsdb.webapp.constants.PRIVACY_NOTICE_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.PRSD_EMAIL
+import uk.gov.communities.prsdb.webapp.constants.REGION_1_GOOGLE_ANALYTICS
 import uk.gov.communities.prsdb.webapp.constants.RENTERS_RIGHTS_BILL_URL
 import uk.gov.communities.prsdb.webapp.controllers.BetaFeedbackController.Companion.FEEDBACK_URL
 import uk.gov.communities.prsdb.webapp.controllers.CookiesController.Companion.COOKIES_ROUTE
@@ -36,6 +39,9 @@ class GlobalModelAttributes(
         model.addAttribute("googleAnalyticsMeasurementId", gaMeasurementId)
         model.addAttribute("googleAnalyticsCookieDomain", gaCookieDomain)
         model.addAttribute("plausibleDomainId", plausibleDomainId)
+        model.addAttribute("plausibleUrl", PLAUSIBLE_SCRIPT_DOWNLOADS)
+        model.addAttribute("googleTagManagerUrl", GOOGLE_TAG_MANAGER)
+        model.addAttribute("googleAnalyticsUrl", REGION_1_GOOGLE_ANALYTICS)
 
         // Feedback banner attributes
         model.addAttribute("feedbackBannerUrl", FEEDBACK_URL)
