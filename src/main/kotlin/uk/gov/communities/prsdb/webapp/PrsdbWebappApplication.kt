6c41dfdfdfbd7be8c67bc5945cfdfaa62b886820
JasCon <jasmin.conterio@softwire.com>
2025-11-10 11:38:28 +0000

    PRSD-1647: Use the spring-boot-starter ff4j package instead of choosing libraries

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/PrsdbWebappApplication.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/PrsdbWebappApplication.kt
@@ -3,7 +3,7 @@ package uk.gov.communities.prsdb.webapp
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["uk.gov.communities.prsdb.webapp", "org.ff4j.aop"])
+@SpringBootApplication
 class PrsdbWebappApplication
 
 fun main(args: Array<String>) {
