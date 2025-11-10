985eaecb826451c3f0ea6c9aa0be7cb27d9a7529
JasCon <jasmin.conterio@softwire.com>
2025-11-10 11:43:03 +0000

    PRSD-1647: Remove the web only condition from PrsdbFlip

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/annotations/webAnnotations/PrsdbFlip.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/annotations/webAnnotations/PrsdbFlip.kt
@@ -1,11 +1,9 @@
 package uk.gov.communities.prsdb.webapp.annotations.webAnnotations
 
 import org.ff4j.aop.Flip
-import org.springframework.context.annotation.Conditional
 import org.springframework.core.annotation.AliasFor
 
 @Flip(name = "")
-@Conditional(WebServerOnly::class)
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
 annotation class PrsdbFlip(
