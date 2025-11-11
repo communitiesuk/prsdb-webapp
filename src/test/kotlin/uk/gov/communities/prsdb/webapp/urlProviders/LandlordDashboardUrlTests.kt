41f384c926bfa249a5f2dd138a04e5596be5569d
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-11-11 11:59:51 +0000

    PRSD-NONE: Delete unused property ownership fields (#850)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/urlProviders/LandlordDashboardUrlTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/urlProviders/LandlordDashboardUrlTests.kt
@@ -221,7 +221,6 @@ class LandlordDashboardUrlTests(
                 anyOrNull(),
                 anyOrNull(),
                 anyOrNull(),
-                anyOrNull(),
             ),
         ).thenReturn(propertyOwnership)
 
