71aeeabd4824b5d4f857151cb70dd20a57ad906d
Isobel Ibironke <isobel.ibironke@softwire.com>
2025-11-11 12:36:04 +0000

    PRSD-1474: Tests orphanRemovals

--- src/test/resources/data-mockuser-landlord-with-properties.sql
+++ src/test/resources/data-mockuser-landlord-with-properties.sql
@@ -1,6 +1,9 @@
 INSERT INTO one_login_user (id, created_date)
 VALUES ('urn:fdc:gov.uk:2022:UVWXY', '10/14/24');
 
+INSERT INTO passcode (passcode, local_authority_id, subject_identifier)
+VALUES ('PRS23', 2, 'urn:fdc:gov.uk:2022:UVWXY');
+
 INSERT INTO registration_number (id, created_date, number, type)
 VALUES (1, '09/13/24', 2001001001, 1),
        (2, '3/26/25', 1001001001, 0),
@@ -21,13 +24,25 @@ VALUES (1, '09/13/24', '09/13/24', 1, 1, '09/13/2000', true, 07111111111, 'urn:f
         'Alexander Smith', 'alex.surname@example.com', 'England or Wales', false, true);
 SELECT setval(pg_get_serial_sequence('landlord', 'id'), (SELECT MAX(id) FROM landlord));
 
-INSERT INTO property_ownership (id, is_active, ownership_type, current_num_households, current_num_tenants, registration_number_id, primary_landlord_id, address_id, created_date, property_build_type)
-VALUES (1, true, 1, 1, 2, 2, 1, 2, '3/26/25', 1),
-       (2, true, 1, 1, 2, 3, 1, 3, '3/26/25', 1),
-       (3, true, 1, 1, 2, 4, 1, 4, '3/26/25', 1);
+INSERT INTO form_context (id, journey_type, context, subject_identifier)
+VALUES (1, 7, '{}','urn:fdc:gov.uk:2022:UVWXY');
+SELECT setval(pg_get_serial_sequence('form_context', 'id'), (SELECT MAX(id) FROM form_context));
+
+INSERT INTO license (id, license_type, license_number)
+VALUES (1, 1, 'LIC123456');
+SELECT setval(pg_get_serial_sequence('license', 'id'), (SELECT MAX(id) FROM license));
+
+INSERT INTO property_ownership (id, is_active, ownership_type, current_num_households, current_num_tenants, registration_number_id, primary_landlord_id, address_id, property_build_type, license_id, incomplete_compliance_form_id)
+VALUES (1, true, 1, 1, 2, 2, 1, 2, 1, 1, 1),
+       (2, true, 1, 1, 2, 3, 1, 3, 1, null, null),
+       (3, true, 1, 1, 2, 4, 1, 4, 1, null, null);
 
 INSERT INTO file_upload (id, created_date, status, object_key, e_tag, version_id, extension)
 VALUES (1, '09/13/24', 1, 'file-key-123', 'e-tag-123', 'version-id-123', 'pdf');
 
 INSERT INTO certificate_upload (id, created_date, file_upload_id, property_ownership_id, category)
 VALUES (1, '09/13/24', 1, 1, 1);
+
+INSERT INTO property_compliance (id, property_ownership_id, gas_safety_upload_id, gas_safety_cert_issue_date, gas_safety_cert_engineer_num)
+VALUES (1, 1,1, current_date, '1234567');
+
