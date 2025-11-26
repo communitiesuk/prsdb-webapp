bef82712ed75e18a09c3843c139e9e2862f4e839
Alexander Read <alexander.read@communities.gov.uk>
2025-11-26 11:15:57 +0000

    PRSD-1550: CYA journeys work forwards

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/shared/CheckYourAnswersJourneyState.kt
@@ -0,0 +1,29 @@
+package uk.gov.communities.prsdb.webapp.journeys.shared
+
+import uk.gov.communities.prsdb.webapp.journeys.Destination
+import uk.gov.communities.prsdb.webapp.journeys.JourneyState
+import uk.gov.communities.prsdb.webapp.journeys.JourneyStep
+import uk.gov.communities.prsdb.webapp.journeys.builders.JourneyBuilder
+import uk.gov.communities.prsdb.webapp.journeys.builders.StepLikeInitialiser
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.CheckAnswersFormModel
+
+interface CheckYourAnswersJourneyState : JourneyState {
+    val cyaStep: JourneyStep.RequestableStep<Complete, CheckAnswersFormModel, *>
+    val baseJourneyId: String
+    val cyaChildJourneyId: String?
+    val isCheckingAnswers: Boolean
+}
+
+fun <T : CheckYourAnswersJourneyState> JourneyBuilder<T>.checkYourAnswersJourney() {
+    configureTagged("checkable") {
+        modifyNextDestination { originalDestinationProvider ->
+            if (journey.isCheckingAnswers) {
+                { Destination.VisitableStep(journey.cyaStep, journey.baseJourneyId) }
+            } else {
+                originalDestinationProvider
+            }
+        }
+    }
+}
+
+fun StepLikeInitialiser<*>.checkable() = taggedWith("checkable")
