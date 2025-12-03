02035f622a48a58e5d20283ca857a8d4786ab7c3
Bill Haigh <bill.haigh@softwire.com>
2025-12-03 15:01:53 +0000

    PDJB-99: NumberOfBedroomsFormModel added

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/NumberOfBedroomsFormModel.kt
@@ -0,0 +1,19 @@
+package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
+
+import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
+import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
+import uk.gov.communities.prsdb.webapp.validation.PositiveIntegerValidator
+import uk.gov.communities.prsdb.webapp.validation.ValidatedBy
+
+@IsValidPrioritised
+class NumberOfBedroomsFormModel {
+    @ValidatedBy(
+        constraints = [
+            ConstraintDescriptor(
+                messageKey = "forms.numberOfBedrooms.input.error",
+                validatorType = PositiveIntegerValidator::class,
+            ),
+        ],
+    )
+    var numberOfBedrooms: String = ""
+}
