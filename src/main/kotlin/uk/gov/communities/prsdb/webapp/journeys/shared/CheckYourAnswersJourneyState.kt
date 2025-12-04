d63704103737a7586976ddca9f4fd3b6172c40ed
Alexander Read <alexander.read@communities.gov.uk>
2025-12-04 14:11:01 +0000

    PRSD-1550: Separate concerns correctly

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/shared/CheckYourAnswersJourneyState.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/shared/CheckYourAnswersJourneyState.kt
@@ -2,21 +2,23 @@ package uk.gov.communities.prsdb.webapp.journeys.shared
 
 import uk.gov.communities.prsdb.webapp.journeys.Destination
 import uk.gov.communities.prsdb.webapp.journeys.JourneyState
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStateDelegateProvider
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStep
 import uk.gov.communities.prsdb.webapp.journeys.builders.ConfigurableElement
 import uk.gov.communities.prsdb.webapp.journeys.builders.JourneyBuilder
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.CheckAnswersFormModel
 
-interface CheckYourAnswersPartialJourneyState {
+interface CheckYourAnswersJourneyState : JourneyState {
     val cyaStep: JourneyStep.RequestableStep<Complete, CheckAnswersFormModel, *>
-    val baseJourneyId: String
     val cyaChildJourneyId: String?
+
+    val baseJourneyId: String
+        get() = journeyMetadata.baseJourneyId ?: journeyId
+
     val isCheckingAnswers: Boolean
+        get() = journeyMetadata.childJourneyName != null
 
     companion object {
-        fun <T> JourneyBuilder<T>.checkYourAnswersJourney() where T : JourneyState, T : CheckYourAnswersPartialJourneyState {
+        fun <T> JourneyBuilder<T>.checkYourAnswersJourney() where T : JourneyState, T : CheckYourAnswersJourneyState {
             configureTagged(CHECKABLE) {
                 if (journey.isCheckingAnswers) {
                     modifyNextDestination {
@@ -31,18 +33,4 @@ interface CheckYourAnswersPartialJourneyState {
 
         private const val CHECKABLE = "checkable"
     }
-}
-
-class CheckYourAnswersPartialJourneyStateProxy(
-    delegateProvider: JourneyStateDelegateProvider,
-    private val journeyStateService: JourneyStateService,
-    override val cyaStep: JourneyStep.RequestableStep<Complete, CheckAnswersFormModel, *>,
-) : CheckYourAnswersPartialJourneyState {
-    override var cyaChildJourneyId: String? by delegateProvider.mutableDelegate("cyaChildJourneyId")
-
-    override val baseJourneyId: String
-        get() = journeyStateService.journeyMetadata.baseJourneyId ?: journeyStateService.journeyId
-
-    override val isCheckingAnswers: Boolean
-        get() = journeyStateService.journeyMetadata.childJourneyName != null
 }
