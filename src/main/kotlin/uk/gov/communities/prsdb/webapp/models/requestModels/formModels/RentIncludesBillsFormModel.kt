0d1421f70c23679099b4b5a725a1d66b3c8ed173
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-12-08 10:09:20 +0000

    PDJB-101: Update form model

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/RentIncludesBillsFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/RentIncludesBillsFormModel.kt
@@ -1,9 +1,7 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
 import jakarta.validation.constraints.NotNull
-import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
 
-@IsValidPrioritised
 class RentIncludesBillsFormModel : FormModel {
     @NotNull(message = "forms.rentIncludesBills.radios.error.missing")
     var rentIncludesBills: Boolean? = null
