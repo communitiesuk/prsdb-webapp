04bde2f278b215cbdd9ebd843f6ad79ff14f9c57
Alexander Read <alexander.read@communities.gov.uk>
2025-12-08 10:43:20 +0000

    PRSD-1550: Separate builder interfaces

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/PropertyStateSessionBuilder.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/PropertyStateSessionBuilder.kt
@@ -2,12 +2,54 @@ package uk.gov.communities.prsdb.webapp.testHelpers.builders
 
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
+import org.mockito.Mockito.mock
 import uk.gov.communities.prsdb.webapp.constants.enums.LicensingType
+import uk.gov.communities.prsdb.webapp.constants.enums.OwnershipType
+import uk.gov.communities.prsdb.webapp.constants.enums.PropertyType
 import uk.gov.communities.prsdb.webapp.models.dataModels.AddressDataModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.CheckAnswersFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OwnershipTypeFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.PropertyTypeFormModel
+import uk.gov.communities.prsdb.webapp.services.LocalCouncilService
 
-class PropertyStateSessionBuilder : JourneyStateSessionBuilder<PropertyStateSessionBuilder>() {
+class PropertyStateSessionBuilder(
+    override val mockLocalCouncilService: LocalCouncilService = mock(),
+) : JourneyStateSessionBuilder<PropertyStateSessionBuilder>(),
+    AddressStateBuilder<PropertyStateSessionBuilder>,
+    LicensingStateBuilder<PropertyStateSessionBuilder>,
+    OccupancyStateBuilder<PropertyStateSessionBuilder> {
     fun withIsAddressAlreadyRegistered(isRegistered: Boolean): PropertyStateSessionBuilder {
         additionalDataMap["isAddressAlreadyRegistered"] = Json.Default.encodeToString(serializer(), isRegistered)
+        return this
+    }
+
+    fun withCheckedAnswers(): PropertyStateSessionBuilder {
+        val checkAnswersFormModel = CheckAnswersFormModel()
+        withSubmittedValue("check-answers", checkAnswersFormModel)
+        return this
+    }
+
+    fun withPropertyType(
+        type: PropertyType = PropertyType.DETACHED_HOUSE,
+        customType: String = "type",
+    ): PropertyStateSessionBuilder {
+        val propertyTypeFormModel =
+            PropertyTypeFormModel().apply {
+                propertyType = type
+                if (type == PropertyType.OTHER) {
+                    customPropertyType = customType
+                }
+            }
+        withSubmittedValue("property-type", propertyTypeFormModel)
+        return this
+    }
+
+    fun withOwnershipType(ownershipType: OwnershipType = OwnershipType.FREEHOLD): PropertyStateSessionBuilder {
+        val ownershipTypeFormModel =
+            OwnershipTypeFormModel().apply {
+                this.ownershipType = ownershipType
+            }
+        withSubmittedValue("ownership-type", ownershipTypeFormModel)
         return this
     }
 
