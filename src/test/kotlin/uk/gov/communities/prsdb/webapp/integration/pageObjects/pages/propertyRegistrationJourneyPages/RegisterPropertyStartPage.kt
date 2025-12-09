6b4943b38b974bb3a74922d41681a7cd1f280124
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2025-12-09 10:42:08 +0000

    PDJB-175: Updated tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/RegisterPropertyStartPage.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/RegisterPropertyStartPage.kt
@@ -2,13 +2,14 @@ package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyRe
 
 import com.microsoft.playwright.Locator
 import com.microsoft.playwright.Page
-import uk.gov.communities.prsdb.webapp.controllers.NewRegisterPropertyController
+import uk.gov.communities.prsdb.webapp.constants.REGISTER_PROPERTY_JOURNEY_URL
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Button
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 
 class RegisterPropertyStartPage(
     page: Page,
-) : BasePage(page, NewRegisterPropertyController.PROPERTY_REGISTRATION_ROUTE) {
+    // TODO PDJB-104 update urlSegment to full property registration journey route
+) : BasePage(page, REGISTER_PROPERTY_JOURNEY_URL) {
     val heading: Locator? = page.locator(".govuk-heading-l")
     val startButton = Button.byText(page, "Start now")
 }
