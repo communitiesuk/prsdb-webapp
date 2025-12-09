6a56ab329d1d70488dbd208dcc5a97e6d3d17705
Bill Haigh <bill.haigh@softwire.com>
2025-12-09 14:08:18 +0000

    PDJB-108: added enums

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/enums/BillsIncluded.kt
@@ -0,0 +1,14 @@
+package uk.gov.communities.prsdb.webapp.constants.enums
+
+enum class BillsIncluded {
+    GAS,
+    ELECTRICITY,
+    WATER,
+    COUNCIL_TAX,
+    CONTENTS_INSURANCE,
+    BROADBAND,
+    TV_LICENCE,
+    CABLE_OR_SATELLITE_TV,
+    GARDENING,
+    CLEANER_FOR_COMMUNAL_AREAS,
+}
