bbd9ef51161a41805e7d936e7854fe2b2e8df92d
JasCon <jasmin.conterio@softwire.com>
2025-12-10 15:41:07 +0000

    PRSD-1030: Send email (hardcoded addresses)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/application/IncompletePropertiesReminderTaskApplicationRunner.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/application/IncompletePropertiesReminderTaskApplicationRunner.kt
@@ -7,6 +7,9 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Profile
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
+import uk.gov.communities.prsdb.webapp.models.viewModels.emailModels.IncompletePropertyReminderEmail
+import uk.gov.communities.prsdb.webapp.services.AbsoluteUrlProvider
+import uk.gov.communities.prsdb.webapp.services.EmailNotificationService
 import kotlin.system.exitProcess
 
 @Component
@@ -14,16 +17,35 @@ import kotlin.system.exitProcess
 @Order(1)
 class IncompletePropertiesReminderTaskApplicationRunner(
     private val context: ApplicationContext,
+    private val emailSender: EmailNotificationService<IncompletePropertyReminderEmail>,
+    private val absoluteUrlProvider: AbsoluteUrlProvider,
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         println("Executing incomplete properties reminder scheduled task")
 
-        // TODO - PRDS-1030 - implement the actual task logic here
+        // TODO - PRDS-1030 - retrieve incomplete properties older than 21 days
+
+        sendEmailToLandlord("jasmin.conterio@softwire.com", "HARDCODED ADDRESS")
 
         val code =
             SpringApplication.exit(context, { 0 }).also {
                 println("Scheduled task executed. Application will exit now.")
             }
         exitProcess(code)
+    }
+
+    private fun sendEmailToLandlord(
+        emailAddress: String,
+        propertyAddress: String,
+    ) {
+        val prsdUrl = absoluteUrlProvider.buildLandlordDashboardUri().toString()
+
+        emailSender.sendEmail(
+            emailAddress,
+            IncompletePropertyReminderEmail(
+                singleLineAddress = propertyAddress,
+                prsdUrl = prsdUrl,
+            ),
+        )
     }
 }
