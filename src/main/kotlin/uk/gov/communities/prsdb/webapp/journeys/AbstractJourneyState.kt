b8454561afcbb5a6658d8b8cf5292b9470a2ea53
Alex Read <alexander.read@communities.gov.uk>
2025-12-10 11:46:22 +0000

    PRSD-1550: Colocate decodeOrNull with use (#904)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/AbstractJourneyState.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/AbstractJourneyState.kt
@@ -1,8 +1,5 @@
 package uk.gov.communities.prsdb.webapp.journeys
 
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.json.Json
 import uk.gov.communities.prsdb.webapp.forms.PageData
 import uk.gov.communities.prsdb.webapp.forms.objectToStringKeyedMap
 
@@ -41,19 +38,5 @@ abstract class AbstractJourneyState(
 
         journeyStateService.initialiseChildJourney(newJourneyId, childJourneyName)
         return newJourneyId
-    }
-
-    companion object {
-        fun <T> decodeFromStringOrNull(
-            deserializer: KSerializer<T>,
-            json: String,
-        ): T? =
-            try {
-                Json.decodeFromString(deserializer, json)
-            } catch (_: IllegalArgumentException) {
-                null
-            } catch (_: SerializationException) {
-                null
-            }
     }
 }
