f56ba797b20559a836376aab85dd7c75fe62c653
Alexander Read <alexander.read@communities.gov.uk>
2025-12-15 12:08:38 +0000

    PRSD-1550: Journey Framework Components are web components

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/annotations/webAnnotations/JourneyFrameworkComponent.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/annotations/webAnnotations/JourneyFrameworkComponent.kt
@@ -2,13 +2,11 @@ package uk.gov.communities.prsdb.webapp.annotations.webAnnotations
 
 import org.springframework.context.annotation.Scope
 import org.springframework.core.annotation.AliasFor
-import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 
 @Scope("prototype")
-@Service
+@PrsdbWebComponent
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 annotation class JourneyFrameworkComponent(
-    @get:AliasFor(annotation = Component::class) val value: String = "",
+    @get:AliasFor(annotation = PrsdbWebComponent::class) val value: String = "",
 )
