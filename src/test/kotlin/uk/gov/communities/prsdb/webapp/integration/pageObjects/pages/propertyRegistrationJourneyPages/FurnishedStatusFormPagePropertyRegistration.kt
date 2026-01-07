78610ad8b5de314ab20972328fa7771dd1f1db2e
Bill Haigh <bill.haigh@softwire.com>
2026-01-07 15:50:43 +0000

    PDJB-103: created base page

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/FurnishedStatusFormPagePropertyRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/FurnishedStatusFormPagePropertyRegistration.kt
@@ -1,29 +1,13 @@
 package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyRegistrationJourneyPages
 
 import com.microsoft.playwright.Page
-import uk.gov.communities.prsdb.webapp.constants.enums.FurnishedStatus
 import uk.gov.communities.prsdb.webapp.controllers.NewRegisterPropertyController
 import uk.gov.communities.prsdb.webapp.forms.steps.RegisterPropertyStepId
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.FormWithSectionHeader
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Radios
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.FurnishedStatusFormBasePage
 
 class FurnishedStatusFormPagePropertyRegistration(
     page: Page,
-) : BasePage(
+) : FurnishedStatusFormBasePage(
         page,
         "${NewRegisterPropertyController.PROPERTY_REGISTRATION_ROUTE}/${RegisterPropertyStepId.FurnishedStatus.urlPathSegment}",
-    ) {
-    val form = FurnishedForm(page)
-
-    fun submitFurnishedStatus(furnishedStatus: FurnishedStatus) {
-        form.furnishedRadios.selectValue(furnishedStatus)
-        form.submit()
-    }
-
-    class FurnishedForm(
-        page: Page,
-    ) : FormWithSectionHeader(page) {
-        val furnishedRadios = Radios(page)
-    }
-}
+    )
