64f0c8ef86906da058159ac31f0eea40956e3ddf
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2026-01-07 14:55:05 +0000

    PDJB-102: Update currency validation

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/RentAmountFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/RentAmountFormModel.kt
@@ -1,6 +1,7 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
 import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
+import uk.gov.communities.prsdb.webapp.validation.DelegatedPropertyConstraintValidator
 import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
 import uk.gov.communities.prsdb.webapp.validation.PositiveBigDecimalValidator
 import uk.gov.communities.prsdb.webapp.validation.ValidatedBy
@@ -13,11 +14,14 @@ class RentAmountFormModel : FormModel {
                 messageKey = "forms.rentAmount.error",
                 validatorType = PositiveBigDecimalValidator::class,
             ),
+            ConstraintDescriptor(
+                messageKey = "forms.rentAmount.error",
+                validatorType = DelegatedPropertyConstraintValidator::class,
+                targetMethod = "isNotMoreThanTwoDecimalPlaces",
+            ),
         ],
     )
     var rentAmount: String = ""
 
-    // TODO PDJB-102 rentAmount on PropertyOwnership entity is of type BigDecimal with up to 2 decimal places
-//      We'll need a validator - maybe custom? - to validate this is a positive number that is either an integer or has up to 2 decimal places
-//      current validator is making the string a BigDecimal and checking it's positive but not checking the decimal places
+    fun isNotMoreThanTwoDecimalPlaces() = rentAmount.toBigDecimal().scale() <= 2
 }
