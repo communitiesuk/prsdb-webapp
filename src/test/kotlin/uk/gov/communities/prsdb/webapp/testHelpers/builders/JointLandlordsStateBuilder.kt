744ee6c6e6cce2d160709754f8dad9e704763fde
Bill Haigh <bill.haigh@softwire.com>
2026-01-20 15:05:27 +0000

    PDJB-112: created JointLandlordsStateBuilder

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/JointLandlordsStateBuilder.kt
@@ -0,0 +1,42 @@
+package uk.gov.communities.prsdb.webapp.testHelpers.builders
+
+import uk.gov.communities.prsdb.webapp.forms.steps.RegisterPropertyStepId
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.HasJointLandlordsFormModel
+
+interface JointLandlordsStateBuilder<SelfType : JointLandlordsStateBuilder<SelfType>> {
+    val submittedValueMap: MutableMap<String, FormModel>
+
+    fun withSubmittedValue(
+        key: String,
+        value: FormModel,
+    ): SelfType
+
+    fun self(): SelfType
+
+    fun withNoJointLandlords(): SelfType = withHasJointLandlordsSetToFalse()
+
+    fun withHasJointLandlordsSetToFalse(): SelfType {
+        val hasJointLandlordsFormModel =
+            HasJointLandlordsFormModel().apply {
+                hasJointLandlords = false
+            }
+        withSubmittedValue("hasJointLandlords", hasJointLandlordsFormModel)
+        return self()
+    }
+
+    fun withHasJointLandlords(hasJointLandlords: Boolean): SelfType {
+        val hasJointLandlordsFormModel =
+            HasJointLandlordsFormModel().apply {
+                this.hasJointLandlords = hasJointLandlords
+            }
+        withSubmittedValue(RegisterPropertyStepId.Occupancy.urlPathSegment, hasJointLandlordsFormModel)
+        return self()
+    }
+
+    fun withJointLandlords(): SelfType {
+        withHasJointLandlords(true)
+        @Suppress("UNCHECKED_CAST")
+        return self()
+    }
+}
