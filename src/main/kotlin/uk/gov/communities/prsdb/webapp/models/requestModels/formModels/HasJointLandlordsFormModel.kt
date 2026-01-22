acb53e6b18e03ab08c46b24d4791d6840ce9c9f7
Bill Haigh <bill.haigh@softwire.com>
2026-01-22 15:54:38 +0000

    PDJB-112: missed a bit

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/HasJointLandlordsFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/HasJointLandlordsFormModel.kt
@@ -5,6 +5,6 @@ import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
 
 @IsValidPrioritised
 class HasJointLandlordsFormModel : FormModel {
-    @NotNull(message = "forms.jointLandlords.hasJointLandlords.error.missing")
+    @NotNull(message = "jointLandlords.hasJointLandlords.error.missing")
     var hasJointLandlords: Boolean? = null
 }
