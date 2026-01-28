3a2bd3b826ce75c3545ca03a4c312e96988d6c38
Isobel Ibironke <isobel.ibironke@softwire.com>
2026-01-28 09:32:19 +0000

    PRSD-1745: Makes VerifiedIdentityDataModel serializable

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/VerifiedIdentityDataModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/VerifiedIdentityDataModel.kt
@@ -1,17 +1,14 @@
 package uk.gov.communities.prsdb.webapp.models.dataModels
 
+import kotlinx.serialization.Serializable
+import uk.gov.communities.prsdb.webapp.helpers.LocalDateAsStringSerializer
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
 import java.time.LocalDate
 
 // TODO PRSD-1745: This won't need to be a form model after migrating to the new journey framework
+@Serializable
 data class VerifiedIdentityDataModel(
     var name: String? = null,
+    @Serializable(with = LocalDateAsStringSerializer::class)
     var birthDate: LocalDate? = null,
-) : FormModel {
-    fun toMap() = mapOf("name" to name, "birthDate" to birthDate)
-
-    companion object {
-        fun fromMap(data: Map<*, *>): VerifiedIdentityDataModel =
-            VerifiedIdentityDataModel(data["name"] as String?, data["birthDate"] as LocalDate?)
-    }
-}
+) : FormModel
