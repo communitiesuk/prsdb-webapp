d24920ddb7185793c0f4fa8d1c1cc0d313ca2d01
Isobel Ibironke <isobel.ibironke@softwire.com>
2026-01-30 09:16:31 +0000

    PRSD-1745: Makes VerifiedIdentityDataModel serializable (#972)

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/helpers/LocalDateSerializer.kt
@@ -0,0 +1,22 @@
+package uk.gov.communities.prsdb.webapp.helpers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDate
+
+class LocalDateSerializer : KSerializer<LocalDate> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(this::class.qualifiedName!!, PrimitiveKind.STRING)
+
+    override fun serialize(
+        encoder: Encoder,
+        value: LocalDate,
+    ) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate = LocalDate.parse(decoder.decodeString())
+}
--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/VerifiedIdentityDataModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/VerifiedIdentityDataModel.kt
@@ -1,17 +1,16 @@
 package uk.gov.communities.prsdb.webapp.models.dataModels
 
+import kotlinx.serialization.Serializable
+import uk.gov.communities.prsdb.webapp.helpers.LocalDateSerializer
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
 import java.time.LocalDate
+import java.io.Serializable as JavaSerializable
 
 // TODO PRSD-1745: This won't need to be a form model after migrating to the new journey framework
+@Serializable
 data class VerifiedIdentityDataModel(
     var name: String? = null,
+    @Serializable(with = LocalDateSerializer::class)
     var birthDate: LocalDate? = null,
-) : FormModel {
-    fun toMap() = mapOf("name" to name, "birthDate" to birthDate)
-
-    companion object {
-        fun fromMap(data: Map<*, *>): VerifiedIdentityDataModel =
-            VerifiedIdentityDataModel(data["name"] as String?, data["birthDate"] as LocalDate?)
-    }
-}
+) : FormModel,
+    JavaSerializable // Required for @Serializable to be compatible with Spring's DefaultSerializer
