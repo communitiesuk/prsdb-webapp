c66d58e9b7723aec4d64d7a2e87e439ceb59bc37
Jasmin Conterio <jasmin.conterio@communities.gov.uk>
2026-02-03 10:17:05 +0000

    Chore/pdjb 467 migrate property compliance part 1 (#980)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/FeatureFlagNames.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/FeatureFlagNames.kt
@@ -22,6 +22,8 @@ const val FAILOVER_TEST_ENDPOINTS = "failover-test-endpoints"
 
 const val MIGRATE_PROPERTY_REGISTRATION = "migrate-property-registration"
 
+const val MIGRATE_PROPERTY_COMPLIANCE = "migrate-property-compliance"
+
 val featureFlagNames =
     listOf(
         EXAMPLE_FEATURE_FLAG_ONE,
@@ -30,4 +32,5 @@ val featureFlagNames =
         EXAMPLE_FEATURE_FLAG_FOUR,
         FAILOVER_TEST_ENDPOINTS,
         MIGRATE_PROPERTY_REGISTRATION,
+        MIGRATE_PROPERTY_COMPLIANCE,
     )
