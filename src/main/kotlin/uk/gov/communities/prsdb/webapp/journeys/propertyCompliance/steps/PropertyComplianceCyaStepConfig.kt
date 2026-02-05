0da2b4055d08102da3f9efa7a20a804b68b20907
JasCon <jasmin.conterio@softwire.com>
2026-02-05 08:05:31 +0000

    PDJB-467: Start CYA (just enough to show a page)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyCompliance/steps/PropertyComplianceCyaStepConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyCompliance/steps/PropertyComplianceCyaStepConfig.kt
@@ -4,13 +4,27 @@ import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFramewo
 import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.PropertyComplianceJourneyState
 import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStep
 import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStepConfig
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.CheckAnswersFormModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryListRowViewModel
 
 @JourneyFrameworkComponent
 class PropertyComplianceCyaStepConfig : AbstractCheckYourAnswersStepConfig<PropertyComplianceJourneyState>() {
+    override fun chooseTemplate(state: PropertyComplianceJourneyState) = "forms/propertyComplianceCheckAnswersForm"
+
     override fun getStepSpecificContent(state: PropertyComplianceJourneyState) =
         mapOf(
-            "summaryName" to "forms.checkComplianceAnswers.summaryName",
+            "propertyAddress" to "HARDCODED ADDRESS",
+            "gasSafetyData" to getGasSafetyData(state),
+            "eicrData" to getEicrData(state),
+            "epcData" to getEpcData(state),
+            "submittedFilteredJourneyData" to CheckAnswersFormModel.serializeJourneyData(state.getSubmittedStepData()),
         )
+
+    fun getGasSafetyData(state: PropertyComplianceJourneyState) = emptyList<SummaryListRowViewModel>()
+
+    fun getEicrData(state: PropertyComplianceJourneyState) = emptyList<SummaryListRowViewModel>()
+
+    fun getEpcData(state: PropertyComplianceJourneyState) = emptyList<SummaryListRowViewModel>()
 }
 
 @JourneyFrameworkComponent
