6842eaa51989993448bda1c18b66a1365043cfc2
JasCon <jasmin.conterio@softwire.com>
2026-02-12 17:32:23 +0000

    PDJB-467: WIP reuse one has exemption mode enum

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyCompliance/ExemptionMode.kt
@@ -0,0 +1,7 @@
+package uk.gov.communities.prsdb.webapp.journeys.propertyCompliance
+
+// TODO: migrate uses of EicrExemptionMode, GasSafetyExemptionMode and MeesExemptionCheckMode to this enum and remove those enums
+enum class ExemptionMode {
+    HAS_EXEMPTION,
+    NO_EXEMPTION,
+}
