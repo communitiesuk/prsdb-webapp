657f4ffbd2be5081a280d6122d4fd59a3dad0420
Bill Haigh <Bill.Haigh@softwire.com>
2026-02-12 11:46:14 +0000

    PDJB-119: removed unused functions from service

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/JointLandlordInvitationService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/JointLandlordInvitationService.kt
@@ -1,25 +1,15 @@
 package uk.gov.communities.prsdb.webapp.services
 
-import jakarta.servlet.http.HttpSession
-import kotlinx.datetime.Clock
-import kotlinx.datetime.toKotlinInstant
-import org.springframework.http.HttpStatus
-import org.springframework.web.server.ResponseStatusException
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
-import uk.gov.communities.prsdb.webapp.constants.JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS
-import uk.gov.communities.prsdb.webapp.constants.JOINT_LANDLORD_INVITATION_TOKEN
 import uk.gov.communities.prsdb.webapp.database.entity.JointLandlordInvitation
 import uk.gov.communities.prsdb.webapp.database.entity.PropertyOwnership
 import uk.gov.communities.prsdb.webapp.database.repository.JointLandlordInvitationRepository
-import uk.gov.communities.prsdb.webapp.exceptions.TokenNotFoundException
 import uk.gov.communities.prsdb.webapp.models.viewModels.emailModels.JointLandlordInvitationEmail
 import java.util.UUID
-import kotlin.time.Duration.Companion.hours
 
 @PrsdbWebService
 class JointLandlordInvitationService(
     val invitationRepository: JointLandlordInvitationRepository,
-    private val session: HttpSession,
     private val emailNotificationService: EmailNotificationService<JointLandlordInvitationEmail>,
     private val absoluteUrlProvider: AbsoluteUrlProvider,
 ) {
@@ -45,78 +35,12 @@ class JointLandlordInvitationService(
         }
     }
 
-    fun createInvitationToken(
+    private fun createInvitationToken(
         email: String,
         propertyOwnership: PropertyOwnership,
     ): String {
         val token = UUID.randomUUID()
         invitationRepository.save(JointLandlordInvitation(token, email, propertyOwnership))
         return token.toString()
-    }
-
-    fun getPropertyOwnershipForToken(token: String): PropertyOwnership = getInvitationFromToken(token).registeredOwnership
-
-    fun getEmailAddressForToken(token: String): String = getInvitationFromToken(token).invitedEmail
-
-    fun getInvitationFromToken(token: String): JointLandlordInvitation {
-        val tokenUuid = UUID.fromString(token)
-        val invitation =
-            invitationRepository.findByToken(tokenUuid) ?: throw TokenNotFoundException("Invitation token not found in database")
-
-        return invitation
-    }
-
-    fun deleteInvitation(invitation: JointLandlordInvitation) {
-        invitationRepository.delete(invitation)
-    }
-
-    fun deleteInvitation(invitationId: Long) {
-        invitationRepository.deleteById(invitationId)
-    }
-
-    fun tokenIsValid(token: String): Boolean {
-        try {
-            val invitation = getInvitationFromToken(token)
-            return !getInvitationHasExpired(invitation)
-        } catch (e: TokenNotFoundException) {
-            return false
-        }
-    }
-
-    fun getInvitationOrNull(token: String): JointLandlordInvitation? =
-        try {
-            getInvitationFromToken(token)
-        } catch (e: TokenNotFoundException) {
-            null
-        }
-
-    fun storeTokenInSession(token: String) {
-        session.setAttribute(JOINT_LANDLORD_INVITATION_TOKEN, token)
-    }
-
-    fun getTokenFromSession(): String? = session.getAttribute(JOINT_LANDLORD_INVITATION_TOKEN) as String?
-
-    fun clearTokenFromSession() {
-        session.removeAttribute(JOINT_LANDLORD_INVITATION_TOKEN)
-    }
-
-    fun getInvitationByIdOrNull(id: Long): JointLandlordInvitation? = invitationRepository.findById(id).orElse(null)
-
-    fun throwErrorIfInvitationExists(invitation: JointLandlordInvitation) {
-        if (invitationRepository.existsById(invitation.id)) {
-            throw ResponseStatusException(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                "Invitation with id ${invitation.id} is still in the joint_landlord_invitation table",
-            )
-        }
-    }
-
-    fun getInvitationHasExpired(invitation: JointLandlordInvitation): Boolean {
-        val expiresAtInstant =
-            invitation.createdDate
-                .toKotlinInstant()
-                .plus(JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS.hours)
-
-        return Clock.System.now() > expiresAtInstant
     }
 }
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/services/JointLandlordInvitationServiceTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/services/JointLandlordInvitationServiceTests.kt
@@ -1,16 +1,8 @@
 package uk.gov.communities.prsdb.webapp.services
 
-import jakarta.servlet.http.HttpSession
-import kotlinx.datetime.Clock
-import kotlinx.datetime.toJavaInstant
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertNull
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentCaptor.captor
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
@@ -18,285 +10,28 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.whenever
-import org.springframework.http.HttpStatus
-import org.springframework.mock.web.MockHttpSession
-import org.springframework.web.server.ResponseStatusException
-import uk.gov.communities.prsdb.webapp.constants.JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS
-import uk.gov.communities.prsdb.webapp.database.entity.JointLandlordInvitation
 import uk.gov.communities.prsdb.webapp.database.repository.JointLandlordInvitationRepository
-import uk.gov.communities.prsdb.webapp.exceptions.TokenNotFoundException
 import uk.gov.communities.prsdb.webapp.models.viewModels.emailModels.JointLandlordInvitationEmail
-import uk.gov.communities.prsdb.webapp.testHelpers.mockObjects.MockJointLandlordData
 import uk.gov.communities.prsdb.webapp.testHelpers.mockObjects.MockLandlordData
 import java.net.URI
-import java.util.Optional
-import java.util.UUID
-import kotlin.time.Duration.Companion.hours
-import kotlin.time.Duration.Companion.minutes
 
 class JointLandlordInvitationServiceTests {
     private lateinit var mockJointLandlordInvitationRepository: JointLandlordInvitationRepository
     private lateinit var mockEmailNotificationService: EmailNotificationService<JointLandlordInvitationEmail>
     private lateinit var mockAbsoluteUrlProvider: AbsoluteUrlProvider
     private lateinit var invitationService: JointLandlordInvitationService
-    private lateinit var session: HttpSession
 
     @BeforeEach
     fun setup() {
         mockJointLandlordInvitationRepository = mock()
         mockEmailNotificationService = mock()
         mockAbsoluteUrlProvider = mock()
-        session = MockHttpSession()
         invitationService =
             JointLandlordInvitationService(
                 mockJointLandlordInvitationRepository,
-                session,
                 mockEmailNotificationService,
                 mockAbsoluteUrlProvider,
             )
-    }
-
-    @Test
-    fun `createInvitationToken saves the created invite token for the given property ownership`() {
-        val propertyOwnership = MockLandlordData.createPropertyOwnership()
-
-        val token = invitationService.createInvitationToken("email@example.com", propertyOwnership)
-
-        val inviteCaptor = captor<JointLandlordInvitation>()
-        verify(mockJointLandlordInvitationRepository).save(inviteCaptor.capture())
-
-        assertEquals(propertyOwnership, inviteCaptor.value.registeredOwnership)
-        assertEquals(token, inviteCaptor.value.token.toString())
-        assertEquals("email@example.com", inviteCaptor.value.invitedEmail)
-    }
-
-    @Test
-    fun `getPropertyOwnershipForToken returns the property ownership the token was created with`() {
-        val testUuid = UUID.randomUUID()
-        val testPropertyOwnership = MockLandlordData.createPropertyOwnership(id = 789)
-
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid))
-            .thenReturn(MockJointLandlordData.createJointLandlordInvitation(propertyOwnership = testPropertyOwnership, token = testUuid))
-
-        val propertyOwnership = invitationService.getPropertyOwnershipForToken(testUuid.toString())
-
-        assertEquals(propertyOwnership, testPropertyOwnership)
-    }
-
-    @Test
-    fun `getEmailAddressForToken returns the email the invitation was sent to`() {
-        val testUuid = UUID.randomUUID()
-        val testEmail = "test@example.com"
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid))
-            .thenReturn(MockJointLandlordData.createJointLandlordInvitation(email = testEmail, token = testUuid))
-
-        val email = invitationService.getEmailAddressForToken(testUuid.toString())
-
-        assertEquals(email, testEmail)
-    }
-
-    @Test
-    fun `getInvitationFromToken returns an invitation if the token is in the database`() {
-        val testUuid = UUID.randomUUID()
-        val testInvitation = MockJointLandlordData.createJointLandlordInvitation(token = testUuid)
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid))
-            .thenReturn(testInvitation)
-
-        val invitation = invitationService.getInvitationFromToken(testUuid.toString())
-
-        assertEquals(invitation, testInvitation)
-    }
-
-    @Test
-    fun `getInvitationFromToken throws an exception if the token is not in the database`() {
-        val testUuid = UUID.randomUUID()
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid)).thenReturn(null)
-
-        val thrown = assertThrows(TokenNotFoundException::class.java) { invitationService.getInvitationFromToken(testUuid.toString()) }
-        assertEquals("Invitation token not found in database", thrown.message)
-    }
-
-    @Test
-    fun `getInvitationOrNull returns an invitation if the token is in the database`() {
-        val testUuid = UUID.randomUUID()
-        val testInvitation = MockJointLandlordData.createJointLandlordInvitation(token = testUuid)
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid))
-            .thenReturn(testInvitation)
-
-        val invitation = invitationService.getInvitationOrNull(testUuid.toString())
-
-        assertEquals(invitation, testInvitation)
-    }
-
-    @Test
-    fun `getInvitationOrNull returns null if the token is not in the database`() {
-        val testUuid = UUID.randomUUID()
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid)).thenReturn(null)
-
-        val invitation = invitationService.getInvitationOrNull(testUuid.toString())
-        assertNull(invitation)
-    }
-
-    @Test
-    fun `getInvitationHasExpired returns true if the invitation has expired`() {
-        val testUuid = UUID.randomUUID()
-        val createdDate =
-            Clock.System
-                .now()
-                .minus(JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS.hours)
-                .minus(1.minutes)
-                .toJavaInstant()
-
-        val invitation = MockJointLandlordData.createJointLandlordInvitation(token = testUuid, createdDate = createdDate)
-
-        assertTrue(invitationService.getInvitationHasExpired(invitation))
-    }
-
-    @Test
-    fun `getInvitationHasExpired returns false if the invitation has not expired`() {
-        val testUuid = UUID.randomUUID()
-        val createdDate =
-            Clock.System
-                .now()
-                .minus(JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS.hours)
-                .plus(30.minutes)
-                .toJavaInstant()
-
-        val invitation = MockJointLandlordData.createJointLandlordInvitation(token = testUuid, createdDate = createdDate)
-
-        assertFalse(invitationService.getInvitationHasExpired(invitation))
-    }
-
-    @Test
-    fun `tokenIsValid returns true if the token is in the database and has not expired`() {
-        val testUuid = UUID.randomUUID()
-        val createdDate =
-            Clock.System
-                .now()
-                .minus(JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS.hours)
-                .plus(30.minutes)
-                .toJavaInstant()
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid))
-            .thenReturn(MockJointLandlordData.createJointLandlordInvitation(token = testUuid, createdDate = createdDate))
-
-        assertTrue(invitationService.tokenIsValid(testUuid.toString()))
-    }
-
-    @Test
-    fun `tokenIsValid returns false if the token is in the database but has expired`() {
-        val testUuid = UUID.randomUUID()
-        val createdDate =
-            Clock.System
-                .now()
-                .minus(JOINT_LANDLORD_INVITATION_LIFETIME_IN_HOURS.hours)
-                .minus(1.minutes)
-                .toJavaInstant()
-
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid))
-            .thenReturn(MockJointLandlordData.createJointLandlordInvitation(token = testUuid, createdDate = createdDate))
-
-        assertFalse(invitationService.tokenIsValid(testUuid.toString()))
-    }
-
-    @Test
-    fun `tokenIsValid returns false if the token is not in the database`() {
-        val testUuid = UUID.randomUUID()
-        whenever(mockJointLandlordInvitationRepository.findByToken(testUuid)).thenReturn(null)
-
-        assertFalse(invitationService.tokenIsValid(testUuid.toString()))
-    }
-
-    @Test
-    fun `getInvitationByIdOrNull returns an invitation if the id is in the database`() {
-        val testId = 123.toLong()
-        val invitationFromDatabase = MockJointLandlordData.createJointLandlordInvitation(id = testId)
-
-        whenever(mockJointLandlordInvitationRepository.findById(testId))
-            .thenReturn(Optional.of(invitationFromDatabase) as Optional<JointLandlordInvitation?>)
-
-        assertEquals(invitationFromDatabase, invitationService.getInvitationByIdOrNull(testId))
-    }
-
-    @Test
-    fun `getInvitationByIdOrNull returns null if the id is not in the database`() {
-        val testId = 123.toLong()
-
-        assertNull(invitationService.getInvitationByIdOrNull(testId))
-    }
-
-    @Test
-    fun `deleteInvitation by entity deletes the invitation from the repository`() {
-        val invitation = MockJointLandlordData.createJointLandlordInvitation()
-
-        invitationService.deleteInvitation(invitation)
-
-        verify(mockJointLandlordInvitationRepository).delete(invitation)
-    }
-
-    @Test
-    fun `deleteInvitation by id deletes the invitation from the repository`() {
-        val invitationId = 123L
-
-        invitationService.deleteInvitation(invitationId)
-
-        verify(mockJointLandlordInvitationRepository).deleteById(invitationId)
-    }
-
-    @Test
-    fun `throwErrorIfInvitationExists does not throw error if invitation doesn't exist`() {
-        val invitation = MockJointLandlordData.createJointLandlordInvitation()
-        whenever(mockJointLandlordInvitationRepository.existsById(invitation.id)).thenReturn(false)
-
-        org.junit.jupiter.api.assertDoesNotThrow {
-            invitationService.throwErrorIfInvitationExists(invitation)
-        }
-    }
-
-    @Test
-    fun `throwErrorIfInvitationExists throws INTERNAL SERVER ERROR if invitation still exists`() {
-        val invitation = MockJointLandlordData.createJointLandlordInvitation()
-        whenever(mockJointLandlordInvitationRepository.existsById(invitation.id)).thenReturn(true)
-
-        val errorThrown =
-            org.junit.jupiter.api.assertThrows<ResponseStatusException> {
-                invitationService.throwErrorIfInvitationExists(invitation)
-            }
-        kotlin.test.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, errorThrown.statusCode)
-    }
-
-    @Test
-    fun `storeTokenInSession stores the token in the session`() {
-        val testToken = "test-token-123"
-
-        invitationService.storeTokenInSession(testToken)
-
-        assertEquals(testToken, session.getAttribute("joint-landlord-invitation-token"))
-    }
-
-    @Test
-    fun `getTokenFromSession returns the token from the session`() {
-        val testToken = "test-token-456"
-        session.setAttribute("joint-landlord-invitation-token", testToken)
-
-        val token = invitationService.getTokenFromSession()
-
-        assertEquals(testToken, token)
-    }
-
-    @Test
-    fun `getTokenFromSession returns null if no token in session`() {
-        val token = invitationService.getTokenFromSession()
-
-        assertNull(token)
-    }
-
-    @Test
-    fun `clearTokenFromSession clears the token from the session`() {
-        session.setAttribute("joint-landlord-invitation-token", "test-token")
-
-        invitationService.clearTokenFromSession()
-
-        assertNull(session.getAttribute("joint-landlord-invitation-token"))
     }
 
     @Test
