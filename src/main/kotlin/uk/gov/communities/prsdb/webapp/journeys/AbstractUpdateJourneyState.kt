7bbb6c741ada9d04ee4d842da94f6f342f9902f8
Kiran Randhawa Kukar <Kiran.RandhawaKukar@softwire.com>
2026-02-16 16:19:42 +0000

    PDJB-545 refactor

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/AbstractUpdateJourneyState.kt
@@ -0,0 +1,38 @@
+package uk.gov.communities.prsdb.webapp.journeys
+
+import java.security.Principal
+
+abstract class AbstractUpdateJourneyState(
+    private val journeyStateService: JourneyStateService,
+    private val delegateProvider: JourneyStateDelegateProvider,
+    private val updateJourneyType: String,
+) : AbstractJourneyState(journeyStateService) {
+    var isStateInitialized: Boolean by delegateProvider.requiredDelegate("isStateInitialized", false)
+
+    override fun generateJourneyId(seed: Any?): String {
+        val ownershipUserPair: Pair<Long, Principal>? = convertSeedToOwnershipUserPairOrNull(seed)
+
+        return super.generateJourneyId(
+            ownershipUserPair?.let {
+                generateSeedForPropertyOwnershipAndUser(it.first, it.second, updateJourneyType)
+            },
+        )
+    }
+
+    private fun convertSeedToOwnershipUserPairOrNull(seed: Any?): Pair<Long, Principal>? =
+        (seed as? Pair<*, *>)?.let {
+            (it.first as? Long)?.let { ownershipId ->
+                (it.second as? Principal)?.let { user ->
+                    Pair(ownershipId, user)
+                }
+            }
+        }
+
+    companion object {
+        fun generateSeedForPropertyOwnershipAndUser(
+            ownershipId: Long,
+            user: Principal,
+            updateJourneyType: String,
+        ): String = "Update $updateJourneyType for property $ownershipId by user ${user.name}"
+    }
+}
