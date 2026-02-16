38231585ee0d046ebaf433c8b985c0887c1d9ec5
Isobel Ibironke <isobel.ibironke@softwire.com>
2026-02-16 10:03:10 +0000

    PDJB-239: Creates application runner for NftDataSeeder

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/application/NftDataSeedingTaskApplicationRunner.kt
@@ -0,0 +1,22 @@
+package uk.gov.communities.prsdb.webapp.application
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ApplicationContext
+import uk.gov.communities.prsdb.webapp.annotations.taskAnnotations.PrsdbTask
+import uk.gov.communities.prsdb.webapp.services.NftDataSeeder
+import kotlin.system.exitProcess
+
+@PrsdbTask("nft-data-seeder")
+class NftDataSeedingTaskApplicationRunner(
+    private val context: ApplicationContext,
+    private val nftDataSeeder: NftDataSeeder,
+) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        println("Executing NFT data seeding task")
+        nftDataSeeder.seedDatabase()
+        println("NFT data seeding task executed successfully. Application will exit now.")
+        exitProcess(SpringApplication.exit(context, { 0 }))
+    }
+}
