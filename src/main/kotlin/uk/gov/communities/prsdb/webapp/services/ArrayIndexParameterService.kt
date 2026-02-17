d24ad188eb6a22a045a38559dc4d9489249372df
Alexander Read <alexander.read@communities.gov.uk>
2026-02-17 14:01:19 +0000

    PRSD-433: Use correct service type

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/ArrayIndexParameterService.kt
@@ -0,0 +1,19 @@
+package uk.gov.communities.prsdb.webapp.services
+
+import jakarta.servlet.ServletRequest
+import org.springframework.web.context.annotation.RequestScope
+import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
+
+@PrsdbWebService
+@RequestScope
+class ArrayIndexParameterService(
+    request: ServletRequest,
+) : UrlParameterService<Int>(request) {
+    override fun getParameterOrNull(): Int? = getParameterOrNull(PARAMETER_NAME)?.toIntOrNull()
+
+    override fun getParameterPair(index: Int): Pair<String, String> = PARAMETER_NAME to index.toString()
+
+    companion object {
+        private const val PARAMETER_NAME = "index"
+    }
+}
--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/UrlParameterService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/UrlParameterService.kt
@@ -1,8 +1,6 @@
 package uk.gov.communities.prsdb.webapp.services
 
 import jakarta.servlet.ServletRequest
-import org.springframework.stereotype.Service
-import org.springframework.web.context.annotation.RequestScope
 
 abstract class UrlParameterService<T>(
     val request: ServletRequest,
@@ -12,18 +10,4 @@ abstract class UrlParameterService<T>(
     abstract fun getParameterOrNull(): T?
 
     abstract fun getParameterPair(index: T): Pair<String, String>
-}
-
-@Service
-@RequestScope
-class ArrayIndexParameterService(
-    request: ServletRequest,
-) : UrlParameterService<Int>(request) {
-    override fun getParameterOrNull(): Int? = getParameterOrNull(PARAMETER_NAME)?.toIntOrNull()
-
-    override fun getParameterPair(index: Int): Pair<String, String> = PARAMETER_NAME to index.toString()
-
-    companion object {
-        private const val PARAMETER_NAME = "index"
-    }
 }
