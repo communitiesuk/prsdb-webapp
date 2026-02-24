78f9ad36a73252051cb7022ac2273425764a98c7
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2026-02-24 12:21:22 +0000

    Restore required self-import for annotation

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/UpdateLandlordPhoneNumberController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/UpdateLandlordPhoneNumberController.kt
@@ -14,6 +14,7 @@ import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbControlle
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_DETAILS_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.MIGRATE_LANDLORD_PHONE_NUMBER_UPDATE
+import uk.gov.communities.prsdb.webapp.controllers.UpdateLandlordPhoneNumberController.Companion.UPDATE_PHONE_NUMBER_ROUTE
 import uk.gov.communities.prsdb.webapp.forms.PageData
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
 import uk.gov.communities.prsdb.webapp.journeys.NoSuchJourneyException
