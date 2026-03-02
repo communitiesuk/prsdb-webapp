e792e14f2e83cdaa3835f9da28fa4ff87c93a01a
Tom Hanmer <43523985+TomHanmer29@users.noreply.github.com>
2026-03-02 13:30:23 +0000

    PDJB-389: save custom property type (#1035)

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -231,15 +231,17 @@ INSERT INTO property_ownership (id, is_active, ownership_type, current_num_house
                                 primary_landlord_id, address_id, created_date, last_modified_date, incomplete_compliance_form_id,
                                 property_build_type,
                                 num_bedrooms, bills_included_list, custom_bills_included, furnished_status, rent_frequency,
-                                custom_rent_frequency, rent_amount)
+                                custom_rent_frequency, rent_amount, custom_property_type)
 VALUES (1, true, 1, 1, 2, 21, 1, 1, '2024-10-15 00:00:00+00', null, 2, 1,
-        1, null, null, 2, 1, null, 123.12),
+        1, null, null, 2, 1, null, 123.12, null),
        (2, true, 0, 0, 0, 22, 1, 2, '2025-01-15 00:00:00+00', null, 3, 1,
-        null, null, null, null, null, null, null),
+        null, null, null, null, null, null, null, null),
        (3, true, 0, 0, 0, 23, 1, 3, '2025-01-15 00:00:00+00', null, 4, 1,
-        null, null, null, null, null, null, null),
+        null, null, null, null, null, null, null, null),
        (4, true, 0, 0, 0, 24, 1, 4, '2025-01-15 00:00:00+00', null, 5, 1,
-        null, null, null, null, null, null, null)
+        null, null, null, null, null, null, null, null),
+       (5, true, 0, 0, 0, 25, 1, 5, '2026-02-27 00:00:00+00', null, 6, 4,
+        null, null, null, null, null, null, null, 'End terrace')
 ON CONFLICT DO NOTHING;
 
 SELECT setval(pg_get_serial_sequence('property_ownership', 'id'), (SELECT MAX(id) FROM property_ownership));
