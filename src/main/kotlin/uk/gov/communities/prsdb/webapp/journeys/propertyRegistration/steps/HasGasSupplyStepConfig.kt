f0e5dc1f37f5318c6f7a80aa7fd8f1b3d46822c0
JasCon <jasmin.conterio@softwire.com>
2026-03-04 15:47:52 +0000

    PDJB-628: Add StepConfig tests

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/steps/HasGasSupplyStepConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/steps/HasGasSupplyStepConfig.kt
@@ -4,25 +4,32 @@ import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFramewo
 import uk.gov.communities.prsdb.webapp.journeys.AbstractRequestableStepConfig
 import uk.gov.communities.prsdb.webapp.journeys.JourneyState
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStep.RequestableStep
-import uk.gov.communities.prsdb.webapp.journeys.shared.Complete
+import uk.gov.communities.prsdb.webapp.journeys.shared.YesOrNo
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.GasSupplyFormModel
 import uk.gov.communities.prsdb.webapp.models.viewModels.formModels.RadiosViewModel
 
 @JourneyFrameworkComponent
-class HasGasSupplyStepConfig : AbstractRequestableStepConfig<Complete, GasSupplyFormModel, JourneyState>() {
+class HasGasSupplyStepConfig : AbstractRequestableStepConfig<YesOrNo, GasSupplyFormModel, JourneyState>() {
     override val formModelClass = GasSupplyFormModel::class
 
     override fun getStepSpecificContent(state: JourneyState) = mapOf("radioOptions" to RadiosViewModel.yesOrNoRadios())
 
     override fun chooseTemplate(state: JourneyState) = "forms/gasSupplyForm"
 
-    override fun mode(state: JourneyState) = getFormModelFromStateOrNull(state)?.let { Complete.COMPLETE }
+    override fun mode(state: JourneyState) =
+        getFormModelFromStateOrNull(state)?.let {
+            when (it.hasGasSupply) {
+                true -> YesOrNo.YES
+                false -> YesOrNo.NO
+                null -> null
+            }
+        }
 }
 
 @JourneyFrameworkComponent
 final class HasGasSupplyStep(
     stepConfig: HasGasSupplyStepConfig,
-) : RequestableStep<Complete, GasSupplyFormModel, JourneyState>(stepConfig) {
+) : RequestableStep<YesOrNo, GasSupplyFormModel, JourneyState>(stepConfig) {
     companion object {
         const val ROUTE_SEGMENT = "has-gas-supply"
     }
