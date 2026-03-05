c5e55db95c81d1c4c827ced0c99bc5addc0d8f50
JasCon <jasmin.conterio@softwire.com>
2026-03-05 12:23:10 +0000

    PDJB-629: Update integration test page objects

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/components/FormWithTwoSubmitButtons.kt
@@ -0,0 +1,11 @@
+package uk.gov.communities.prsdb.webapp.integration.pageObjects.components
+
+import com.microsoft.playwright.Page
+
+open class FormWithTwoSubmitButtons(
+    page: Page,
+) : PostForm(page) {
+    fun submitPrimaryButton(primaryButtonValue: String = "saveAndContinue") = submitSelectedButton(primaryButtonValue)
+
+    fun submitSecondaryButton(secondaryButtonValue: String = "provideThisLater") = submitSelectedButton(secondaryButtonValue)
+}
