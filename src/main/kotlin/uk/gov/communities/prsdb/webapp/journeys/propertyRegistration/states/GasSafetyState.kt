bf87635137a588f7214b2fa6dbd3183d7af4af9c
JasCon <jasmin.conterio@softwire.com>
2026-03-10 13:48:45 +0000

    PDJB-631: Implement GasCertIssueDateStep

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/states/GasSafetyState.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/states/GasSafetyState.kt
@@ -1,5 +1,8 @@
 package uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.states
 
+import kotlinx.datetime.yearsUntil
+import uk.gov.communities.prsdb.webapp.constants.GAS_SAFETY_CERT_VALIDITY_YEARS
+import uk.gov.communities.prsdb.webapp.helpers.DateTimeHelper
 import uk.gov.communities.prsdb.webapp.journeys.JourneyState
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckGasCertUploadsStep
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckGasSafetyAnswersStep
@@ -14,6 +17,18 @@ import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.Uploa
 
 interface GasSafetyState : JourneyState {
     val isOccupied: Boolean?
+
+    fun getGasSafetyCertificateIssueDateIfReachable() =
+        gasCertIssueDateStep.formModelIfReachableOrNull?.let { date ->
+            DateTimeHelper.parseDateOrNull(date.day, date.month, date.year)
+        }
+
+    fun getGasSafetyCertificateIsOutdated(): Boolean? =
+        getGasSafetyCertificateIssueDateIfReachable()?.let { issueDate ->
+            val today = DateTimeHelper().getCurrentDateInUK()
+            issueDate.yearsUntil(today) >= GAS_SAFETY_CERT_VALIDITY_YEARS
+        }
+
     val hasGasSupplyStep: HasGasSupplyStep
     val hasGasCertStep: HasGasCertStep
     val gasCertIssueDateStep: GasCertIssueDateStep
