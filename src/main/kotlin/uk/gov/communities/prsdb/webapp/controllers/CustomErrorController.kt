d9102b66aa3add5c4f2f5e5e0e8715437971637a
Tom Hanmer <tom.hanmer@softwire.com>
2026-03-23 16:49:29 +0000

    PDJB-697: readd cyaDataHasChangedException

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/CustomErrorController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/CustomErrorController.kt
@@ -8,6 +8,7 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbController
+import uk.gov.communities.prsdb.webapp.constants.CYA_ERROR_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.ERROR_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.FILE_TOO_LARGE_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.UPDATE_CONFLICT_ERROR_PATH_SEGMENT
@@ -17,6 +18,11 @@ import uk.gov.communities.prsdb.webapp.constants.UPDATE_CONFLICT_ERROR_PATH_SEGM
 class CustomErrorController : ErrorController {
     @GetMapping(FILE_TOO_LARGE_PATH_SEGMENT)
     fun fileTooLargeErrorPage() = "error/fileTooLarge"
+
+    // We are sending CYA page 'data has changed' errors to this endpoint
+    // so that we can track how often this error is occurring
+    @GetMapping("/$CYA_ERROR_PATH_SEGMENT")
+    fun cyaErrorPage(): String = "error/500"
 
     // We are sending conflict on update errors to this endpoint
     // so that we can track how often this error is occurring
@@ -38,6 +44,7 @@ class CustomErrorController : ErrorController {
     }
 
     companion object {
+        const val CYA_ERROR_ROUTE = "/$ERROR_PATH_SEGMENT/$CYA_ERROR_PATH_SEGMENT"
         const val FILE_TOO_LARGE_ERROR_ROUTE = "/$ERROR_PATH_SEGMENT/$FILE_TOO_LARGE_PATH_SEGMENT"
         const val UPDATE_CONFLICT_ERROR_ROUTE = "/$ERROR_PATH_SEGMENT/$UPDATE_CONFLICT_ERROR_PATH_SEGMENT"
     }
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/CustomErrorControllerTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/CustomErrorControllerTests.kt
@@ -14,6 +14,28 @@ class CustomErrorControllerTests(
     @Autowired val webContext: WebApplicationContext,
 ) : ControllerTest(webContext) {
     @Nested
+    inner class CyaErrorPageTests {
+        @Test
+        fun `returns 200 for unauthenticated users`() {
+            mvc
+                .get(CustomErrorController.CYA_ERROR_ROUTE)
+                .andExpect {
+                    status { isOk() }
+                }
+        }
+
+        @Test
+        @WithMockUser
+        fun `returns 200 for authenticated users`() {
+            mvc
+                .get(CustomErrorController.CYA_ERROR_ROUTE)
+                .andExpect {
+                    status { isOk() }
+                }
+        }
+    }
+
+    @Nested
     inner class FileTooLargeErrorPageTests {
         @Test
         fun `returns 200 for unauthenticated users`() {
