da749ddd6583cac8fb86bf125d4c7ca639811463
Tom Hanmer <tom.hanmer@softwire.com>
2026-03-23 17:23:08 +0000

    PDJB-697: remove compliance todos and form context

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/ComplianceStatusDataModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/dataModels/ComplianceStatusDataModel.kt
@@ -1,38 +1,8 @@
 package uk.gov.communities.prsdb.webapp.models.dataModels
 
-import kotlinx.datetime.toJavaLocalDate
-import kotlinx.serialization.json.Json
-import uk.gov.communities.prsdb.webapp.constants.EICR_VALIDITY_YEARS
-import uk.gov.communities.prsdb.webapp.constants.GAS_SAFETY_CERT_VALIDITY_YEARS
 import uk.gov.communities.prsdb.webapp.constants.enums.ComplianceCertStatus
-import uk.gov.communities.prsdb.webapp.constants.enums.NonStepJourneyDataKey
 import uk.gov.communities.prsdb.webapp.database.entity.PropertyCompliance
 import uk.gov.communities.prsdb.webapp.database.entity.PropertyOwnership
-import uk.gov.communities.prsdb.webapp.helpers.DateTimeHelper
-import uk.gov.communities.prsdb.webapp.helpers.JourneyDataHelper
-import uk.gov.communities.prsdb.webapp.journeys.JourneyData
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.CheckMatchedEpcStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EicrExemptionConfirmationStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EicrExemptionMissingStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EicrIssueDateStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EicrOutdatedStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EicrUploadConfirmationStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EpcExemptionConfirmationStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EpcExpiredStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EpcExpiryCheckStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EpcMissingStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.EpcNotFoundStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.GasSafetyExemptionConfirmationStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.GasSafetyExemptionMissingStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.GasSafetyIssueDateStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.GasSafetyOutdatedStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.GasSafetyUploadConfirmationStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.LowEnergyRatingStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyCompliance.steps.MeesExemptionConfirmationStep
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.CheckMatchedEpcFormModel
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.EpcExpiryCheckFormModel
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.TodayOrPastDateFormModel
-import java.time.LocalDate
 
 data class ComplianceStatusDataModel(
     val propertyOwnershipId: Long,
@@ -52,24 +22,17 @@ data class ComplianceStatusDataModel(
     private val certStatuses = listOf(gasSafetyStatus, eicrStatus, epcStatus)
 
     companion object {
-        // TODO PDJB-639 - add a version of this for the new journey framework (SavedJourneyState) if required
-        fun fromIncompleteComplianceForm(propertyOwnership: PropertyOwnership): ComplianceStatusDataModel {
-            val incompleteComplianceForm =
-                propertyOwnership.incompleteComplianceForm?.toJourneyData()
-                    ?: throw IllegalArgumentException(
-                        "No incomplete compliance form found for PropertyOwnership with ID ${propertyOwnership.id}",
-                    )
-
-            return ComplianceStatusDataModel(
+        // TODO PDJB-80 - Update this to use real state instead of NOT_STARTED
+        fun fromPropertyOwnershipWithoutCompliance(propertyOwnership: PropertyOwnership): ComplianceStatusDataModel =
+            ComplianceStatusDataModel(
                 propertyOwnershipId = propertyOwnership.id,
                 singleLineAddress = propertyOwnership.address.singleLineAddress,
                 registrationNumber = RegistrationNumberDataModel.fromRegistrationNumber(propertyOwnership.registrationNumber).toString(),
-                gasSafetyStatus = incompleteComplianceForm.getGasSafetyStatus(),
-                eicrStatus = incompleteComplianceForm.getEicrStatus(),
-                epcStatus = incompleteComplianceForm.getEpcStatus(),
+                gasSafetyStatus = ComplianceCertStatus.NOT_STARTED,
+                eicrStatus = ComplianceCertStatus.NOT_STARTED,
+                epcStatus = ComplianceCertStatus.NOT_STARTED,
                 isComplete = false,
             )
-        }
 
         fun fromPropertyCompliance(propertyCompliance: PropertyCompliance): ComplianceStatusDataModel =
             ComplianceStatusDataModel(
@@ -85,116 +48,6 @@ data class ComplianceStatusDataModel(
                 epcStatus = propertyCompliance.epcStatus,
                 isComplete = true,
             )
-
-        private fun JourneyData.getGasSafetyStatus(): ComplianceCertStatus =
-            if (hasCompletedStep(GasSafetyExemptionMissingStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.NOT_ADDED
-            } else if (getIsGasSafetyCertOutdated() == true || hasCompletedStep(GasSafetyOutdatedStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.EXPIRED
-            } else if (
-                hasCompletedStep(GasSafetyUploadConfirmationStep.ROUTE_SEGMENT) ||
-                hasCompletedStep(GasSafetyExemptionConfirmationStep.ROUTE_SEGMENT)
-            ) {
-                ComplianceCertStatus.ADDED
-            } else {
-                ComplianceCertStatus.NOT_STARTED
-            }
-
-        private fun JourneyData.getEicrStatus(): ComplianceCertStatus =
-            if (hasCompletedStep(EicrExemptionMissingStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.NOT_ADDED
-            } else if (getIsEicrOutdated() == true || hasCompletedStep(EicrOutdatedStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.EXPIRED
-            } else if (
-                hasCompletedStep(EicrUploadConfirmationStep.ROUTE_SEGMENT) ||
-                hasCompletedStep(EicrExemptionConfirmationStep.ROUTE_SEGMENT)
-            ) {
-                ComplianceCertStatus.ADDED
-            } else {
-                ComplianceCertStatus.NOT_STARTED
-            }
-
-        private fun JourneyData.getEpcStatus(): ComplianceCertStatus =
-            if (hasCompletedStep(EpcMissingStep.ROUTE_SEGMENT) || hasCompletedStep(EpcNotFoundStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.NOT_ADDED
-            } else if (hasCompletedStep(EpcExpiredStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.EXPIRED
-            } else if (getHasCompletedEpcAdded() || hasCompletedStep(EpcExemptionConfirmationStep.ROUTE_SEGMENT)) {
-                ComplianceCertStatus.ADDED
-            } else {
-                ComplianceCertStatus.NOT_STARTED
-            }
-
-        private fun JourneyData.hasCompletedStep(routeSegment: String): Boolean = JourneyDataHelper.getPageData(this, routeSegment) != null
-
-        private fun JourneyData.getIsGasSafetyCertOutdated(): Boolean? =
-            getIssueDate(GasSafetyIssueDateStep.ROUTE_SEGMENT)?.let { issueDate ->
-                val expiryCutoff =
-                    DateTimeHelper().getCurrentDateInUK().toJavaLocalDate().minusYears(
-                        GAS_SAFETY_CERT_VALIDITY_YEARS.toLong(),
-                    )
-                !issueDate.isAfter(expiryCutoff)
-            }
-
-        private fun JourneyData.getIsEicrOutdated(): Boolean? =
-            getIssueDate(EicrIssueDateStep.ROUTE_SEGMENT)?.let { issueDate ->
-                val expiryCutoff = DateTimeHelper().getCurrentDateInUK().toJavaLocalDate().minusYears(EICR_VALIDITY_YEARS.toLong())
-                issueDate.isBefore(expiryCutoff)
-            }
-
-        private fun JourneyData.getIssueDate(routeSegment: String): LocalDate? {
-            val day = JourneyDataHelper.getFieldStringValue(this, routeSegment, TodayOrPastDateFormModel::day.name)?.toIntOrNull()
-            val month = JourneyDataHelper.getFieldStringValue(this, routeSegment, TodayOrPastDateFormModel::month.name)?.toIntOrNull()
-            val year = JourneyDataHelper.getFieldStringValue(this, routeSegment, TodayOrPastDateFormModel::year.name)?.toIntOrNull()
-            if (day == null || month == null || year == null) return null
-
-            return runCatching { LocalDate.of(year, month, day) }.getOrNull()
-        }
-
-        private fun JourneyData.getHasCompletedEpcAdded(): Boolean {
-            if (hasCompletedStep(LowEnergyRatingStep.ROUTE_SEGMENT) || hasCompletedStep(MeesExemptionConfirmationStep.ROUTE_SEGMENT)) {
-                return true
-            }
-
-            val acceptedEpc = getAcceptedEpc() ?: return false
-            if (!acceptedEpc.isEnergyRatingEOrBetter()) return false
-            if (!acceptedEpc.isPastExpiryDate()) return true
-
-            return JourneyDataHelper.getFieldBooleanValue(
-                this,
-                EpcExpiryCheckStep.ROUTE_SEGMENT,
-                EpcExpiryCheckFormModel::tenancyStartedBeforeExpiry.name,
-            ) == true
-        }
-
-        private fun JourneyData.getAcceptedEpc(): EpcDataModel? {
-            val acceptedAutoMatched =
-                JourneyDataHelper.getFieldBooleanValue(
-                    this,
-                    CheckMatchedEpcStep.AUTOMATCHED_ROUTE_SEGMENT,
-                    CheckMatchedEpcFormModel::matchedEpcIsCorrect.name,
-                ) == true
-            if (acceptedAutoMatched) {
-                return getEpcData(NonStepJourneyDataKey.AutoMatchedEpc.key)
-            }
-
-            val acceptedSearched =
-                JourneyDataHelper.getFieldBooleanValue(
-                    this,
-                    CheckMatchedEpcStep.ROUTE_SEGMENT,
-                    CheckMatchedEpcFormModel::matchedEpcIsCorrect.name,
-                ) == true
-            if (acceptedSearched) {
-                return getEpcData(NonStepJourneyDataKey.LookedUpEpc.key)
-            }
-
-            return null
-        }
-
-        private fun JourneyData.getEpcData(key: String): EpcDataModel? {
-            val serializedEpc = this[key]?.toString() ?: return null
-            return runCatching { Json.decodeFromString<EpcDataModel?>(serializedEpc) }.getOrNull()
-        }
 
         private val PropertyCompliance.gasSafetyStatus: ComplianceCertStatus
             get() =
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordDeregistrationServiceTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/services/LandlordDeregistrationServiceTests.kt
@@ -13,11 +13,8 @@ import org.mockito.kotlin.whenever
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_HAD_ACTIVE_PROPERTIES
 import uk.gov.communities.prsdb.webapp.constants.ROLE_LANDLORD
 import uk.gov.communities.prsdb.webapp.constants.ROLE_LOCAL_COUNCIL_USER
-import uk.gov.communities.prsdb.webapp.constants.enums.JourneyType
-import uk.gov.communities.prsdb.webapp.database.repository.FormContextRepository
 import uk.gov.communities.prsdb.webapp.database.repository.LandlordRepository
 import uk.gov.communities.prsdb.webapp.database.repository.OneLoginUserRepository
-import uk.gov.communities.prsdb.webapp.testHelpers.mockObjects.MockLandlordData
 
 @ExtendWith(MockitoExtension::class)
 class LandlordDeregistrationServiceTests {
@@ -33,54 +30,27 @@ class LandlordDeregistrationServiceTests {
     @Mock
     private lateinit var mockHttpSession: HttpSession
 
-    @Mock
-    private lateinit var mockFormContextService: FormContextService
-
-    @Mock
-    private lateinit var mockFormContextRepository: FormContextRepository
-
     @InjectMocks
     private lateinit var landlordDeregistrationService: LandlordDeregistrationService
 
     @Test
     fun `deregisterLandlord deletes the user from the landlord table, and the one-login table if they are not a different type of user`() {
         val baseUserId = "one-login-user"
-        whenever(
-            mockFormContextRepository.findAllByUser_IdAndJourneyType(baseUserId, JourneyType.PROPERTY_REGISTRATION),
-        ).thenReturn(emptyList())
         whenever(mockUserRolesService.getAllRolesForSubjectId(baseUserId)).thenReturn(listOf(ROLE_LANDLORD))
 
         landlordDeregistrationService.deregisterLandlord(baseUserId)
 
-        verify(mockFormContextRepository).findAllByUser_IdAndJourneyType(baseUserId, JourneyType.PROPERTY_REGISTRATION)
         verify(mockOneLoginUserRepository).deleteById(baseUserId)
     }
 
     @Test
     fun `deregisterLandlord deletes the user from the landlord table, but not the one-login table if they are a different type of user`() {
         val baseUserId = "one-login-user"
-        whenever(
-            mockFormContextRepository.findAllByUser_IdAndJourneyType(baseUserId, JourneyType.PROPERTY_REGISTRATION),
-        ).thenReturn(emptyList())
         whenever(mockUserRolesService.getAllRolesForSubjectId(baseUserId)).thenReturn(listOf(ROLE_LANDLORD, ROLE_LOCAL_COUNCIL_USER))
 
         landlordDeregistrationService.deregisterLandlord(baseUserId)
 
-        verify(mockFormContextRepository).findAllByUser_IdAndJourneyType(baseUserId, JourneyType.PROPERTY_REGISTRATION)
         verify(mockOneLoginUserRepository, never()).deleteById(baseUserId)
-    }
-
-    @Test
-    fun `deregisterLandlord deletes incomplete property registrations if present`() {
-        val baseUserId = "one-login-user"
-        val incompleteList = listOf(MockLandlordData.createPropertyRegistrationFormContext())
-        whenever(
-            mockFormContextRepository.findAllByUser_IdAndJourneyType(baseUserId, JourneyType.PROPERTY_REGISTRATION),
-        ).thenReturn(incompleteList)
-
-        landlordDeregistrationService.deregisterLandlord(baseUserId)
-
-        verify(mockFormContextService).deleteFormContexts(incompleteList)
     }
 
     @Test
