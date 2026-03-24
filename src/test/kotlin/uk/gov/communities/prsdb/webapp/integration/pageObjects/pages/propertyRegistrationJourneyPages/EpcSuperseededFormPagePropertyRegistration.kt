3576d3ddcf377459c11e618c94909ddebefb892d
JasCon <jasmin.conterio@softwire.com>
2026-03-24 17:01:57 +0000

    PDJB-725: Start updating journey tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/EpcSuperseededFormPagePropertyRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/EpcSuperseededFormPagePropertyRegistration.kt
@@ -4,7 +4,9 @@ import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.controllers.RegisterPropertyController
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Form
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Heading
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Radios
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckMatchedEpcMode
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcSupersededStep
 
 // TODO PDJB-664: Implement EPC Superseded page object
@@ -12,5 +14,31 @@ class EpcSuperseededFormPagePropertyRegistration(
     page: Page,
 ) : BasePage(page, "${RegisterPropertyController.PROPERTY_REGISTRATION_ROUTE}/${EpcSupersededStep.ROUTE_SEGMENT}") {
     val heading = Heading(page.locator("h1"))
-    val form = Form(page)
+    val form = EpcSuperseededForm(page)
+
+    fun submitEpcCompliant() {
+        form.checkMatchedEpcModeRadios.selectValue(CheckMatchedEpcMode.EPC_COMPLIANT)
+        form.submit()
+    }
+
+    fun submitEpcIncorrect() {
+        form.checkMatchedEpcModeRadios.selectValue(CheckMatchedEpcMode.EPC_INCORRECT)
+        form.submit()
+    }
+
+    fun submitEpcOlderThan10Years() {
+        form.checkMatchedEpcModeRadios.selectValue(CheckMatchedEpcMode.EPC_OLDER_THAN_10_YEARS)
+        form.submit()
+    }
+
+    fun submitEpcLowEnergyRating() {
+        form.checkMatchedEpcModeRadios.selectValue(CheckMatchedEpcMode.EPC_LOW_ENERGY_RATING)
+        form.submit()
+    }
+
+    class EpcSuperseededForm(
+        page: Page,
+    ) : Form(page) {
+        val checkMatchedEpcModeRadios = Radios(locator)
+    }
 }
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/EpcStateBuilder.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/EpcStateBuilder.kt
@@ -1,23 +1,11 @@
 package uk.gov.communities.prsdb.webapp.testHelpers.builders
 
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckAutomatchedEpcStep
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckEpcAnswersStep
+import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckMatchedEpcMode
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.CheckMatchedEpcStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcExemptionStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcExpiredStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcExpiryCheckStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcMissingStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcNotFoundStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcSearchStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcSupersededStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.HasEpcStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.HasMeesExemptionStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.IsEpcRequiredStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.LowEnergyRatingStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.MeesExemptionStep
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.ProvideEpcLaterStep
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.FormModel
 import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.NoInputFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.TemporaryCheckMatchedEpcFormModel
 
 interface EpcStateBuilder<SelfType : EpcStateBuilder<SelfType>> {
     fun withSubmittedValue(
@@ -29,21 +17,10 @@ interface EpcStateBuilder<SelfType : EpcStateBuilder<SelfType>> {
 
     // TODO PDJB-656: Update to use actual logic
     fun withNoEpc(): SelfType {
-        withSubmittedValue(HasEpcStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(CheckAutomatchedEpcStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcSearchStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(CheckMatchedEpcStep.SEARCHED_ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcSupersededStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcNotFoundStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcExpiryCheckStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(HasMeesExemptionStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(MeesExemptionStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(LowEnergyRatingStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcExpiredStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(IsEpcRequiredStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcExemptionStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(EpcMissingStep.ROUTE_SEGMENT, NoInputFormModel())
-        withSubmittedValue(ProvideEpcLaterStep.ROUTE_SEGMENT, NoInputFormModel())
+        withSubmittedValue(
+            CheckMatchedEpcStep.MATCHED_ROUTE_SEGMENT,
+            TemporaryCheckMatchedEpcFormModel().apply { checkMatchedEpcMode = CheckMatchedEpcMode.EPC_COMPLIANT.name },
+        )
         withSubmittedValue(CheckEpcAnswersStep.ROUTE_SEGMENT, NoInputFormModel())
         return self()
     }
