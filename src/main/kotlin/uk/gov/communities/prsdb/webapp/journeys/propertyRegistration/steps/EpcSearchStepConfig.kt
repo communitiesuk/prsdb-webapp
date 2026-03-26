79a13934476d8879d82165a9167b6614f0d4ed0e
JasCon <jasmin.conterio@softwire.com>
2026-03-26 15:51:31 +0000

    PDJB-662: Implement EpcSearchStepConfig.kt

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/steps/EpcSearchStepConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/steps/EpcSearchStepConfig.kt
@@ -1,51 +1,46 @@
 package uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps
 
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
+import uk.gov.communities.prsdb.webapp.constants.FIND_EPC_URL
 import uk.gov.communities.prsdb.webapp.journeys.AbstractRequestableStepConfig
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStep.RequestableStep
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.states.EpcState
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcSearchMode.CURRENT_EPC_FOUND
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcSearchMode.NOT_FOUND
-import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcSearchMode.SUPERSEDED_EPC_FOUND
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.EpcSearchFormModel
-import uk.gov.communities.prsdb.webapp.models.viewModels.formModels.RadiosButtonViewModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.EpcLookupFormModel
+import uk.gov.communities.prsdb.webapp.services.EpcLookupService
 
-// TODO PDJB-662: Implement this step, this radios version is a placeholder
 @JourneyFrameworkComponent
-class EpcSearchStepConfig : AbstractRequestableStepConfig<EpcSearchMode, EpcSearchFormModel, EpcState>() {
-    override val formModelClass = EpcSearchFormModel::class
+class EpcSearchStepConfig(
+    private val epcLookupService: EpcLookupService,
+) : AbstractRequestableStepConfig<EpcSearchMode, EpcLookupFormModel, EpcState>() {
+    override val formModelClass = EpcLookupFormModel::class
 
     override fun getStepSpecificContent(state: EpcState) =
         mapOf(
-            "fieldSetHeading" to "forms.epcSearch.fieldSetHeading",
-            "fieldName" to "epcSearchMode",
-            "radioOptions" to
-                listOf(
-                    RadiosButtonViewModel(
-                        value = CURRENT_EPC_FOUND.name,
-                        labelMsgKey = "forms.epcSearch.mode.currentEpcFound",
-                    ),
-                    RadiosButtonViewModel(
-                        value = SUPERSEDED_EPC_FOUND.name,
-                        labelMsgKey = "forms.epcSearch.mode.supersededEpcFound",
-                    ),
-                    RadiosButtonViewModel(
-                        value = NOT_FOUND.name,
-                        labelMsgKey = "forms.epcSearch.mode.notFound",
-                    ),
-                ),
+            "fieldSetHeading" to "propertyCompliance.epcTask.epcSearch.fieldSetHeading",
+            "findEpcUrl" to FIND_EPC_URL,
         )
 
-    override fun chooseTemplate(state: EpcState) = "forms/todoWithRadios"
+    override fun chooseTemplate(state: EpcState) = "forms/epcSearchForm"
 
-    override fun mode(state: EpcState): EpcSearchMode? =
-        getFormModelFromStateOrNull(state)?.epcSearchMode?.let { EpcSearchMode.valueOf(it) }
+    override fun mode(state: EpcState): EpcSearchMode? {
+        val epc = state.epcRetrievedByCertificateNumber
+        return when {
+            epc == null -> EpcSearchMode.NOT_FOUND
+            epc.isLatestCertificateForThisProperty() -> EpcSearchMode.CURRENT_EPC_FOUND
+            else -> EpcSearchMode.SUPERSEDED_EPC_FOUND
+        }
+    }
+
+    override fun afterStepDataIsAdded(state: EpcState) {
+        val formModel = getFormModelFromState(state)
+        state.epcRetrievedByCertificateNumber = epcLookupService.getEpcByCertificateNumber(formModel.certificateNumber)
+    }
 }
 
 @JourneyFrameworkComponent
 final class EpcSearchStep(
     stepConfig: EpcSearchStepConfig,
-) : RequestableStep<EpcSearchMode, EpcSearchFormModel, EpcState>(stepConfig) {
+) : RequestableStep<EpcSearchMode, EpcLookupFormModel, EpcState>(stepConfig) {
     companion object {
         const val ROUTE_SEGMENT = "search-for-epc"
     }
