3fe55d21b34aa98ad54a6f2802f71efebb007e2c
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2026-03-26 16:43:00 +0000

    PDJB-607: Replace One Login with Internal Access for Local Council OAuth

--- src/test/resources/data-no-local-council-admin-users-or-invitations-user-is-system-operator.sql
+++ src/test/resources/data-no-local-council-admin-users-or-invitations-user-is-system-operator.sql
@@ -1,6 +1,7 @@
 INSERT INTO prsdb_user (id, created_date)
 VALUES ('urn:fdc:gov.uk:2022:UVWXY', '10/14/24'),
-       ('urn:fdc:gov.uk:2022:A', '10/14/24');
+       ('urn:fdc:gov.uk:2022:A', '10/14/24'),
+       ('ia-mock-user-12345', '10/14/24');
 
 INSERT INTO local_council_user (subject_identifier, is_manager, local_council_id, created_date, last_modified_date,
                                   name, email, has_accepted_privacy_notice)
