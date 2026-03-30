1210ae5aebe9c4b50b7c03159ffbf866465a69b7
Tom Hanmer <43523985+TomHanmer29@users.noreply.github.com>
2026-03-30 09:41:40 +0100

    PDJB-350: remove example feature flags (#1153)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/FeatureFlagReleaseNames.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/constants/FeatureFlagReleaseNames.kt
@@ -8,12 +8,5 @@ package uk.gov.communities.prsdb.webapp.constants
 // When loading the feature configuration, the application will check that all feature releases names
 // defined in application.yml are included in this list.
 
-// TODO PRSD-1683 - delete these example feature flags when no longer needed
-const val RELEASE_1_0 = "release-1-0"
-const val RELEASE_WITH_STRATEGY = "release-with-strategy"
-
 val featureFlagReleaseNames =
-    listOf(
-        RELEASE_1_0,
-        RELEASE_WITH_STRATEGY,
-    )
+    listOf<String>()
