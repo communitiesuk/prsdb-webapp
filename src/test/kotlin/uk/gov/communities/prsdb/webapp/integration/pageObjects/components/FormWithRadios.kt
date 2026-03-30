93ebe19f5b2631bf28e09dc56f27124d608109c3
JasCon <jasmin.conterio@softwire.com>
2026-03-30 12:15:27 +0100

    PDJB-662: remove unused FormWithRadios

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/components/FormWithRadios.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.communities.prsdb.webapp.integration.pageObjects.components
-
-import com.microsoft.playwright.Page
-
-class FormWithRadios(
-    page: Page,
-) : Form(page) {
-    val radios = Radios(locator)
-}
