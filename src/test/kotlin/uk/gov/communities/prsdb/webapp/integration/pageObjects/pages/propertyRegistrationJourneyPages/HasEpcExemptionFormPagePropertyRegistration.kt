17b447ba7c0ead754f389c78ca0e0ecce610518d
Tom Hanmer <tom.hanmer@softwire.com>
2026-04-01 11:11:45 +0100

    PDJB-657: add is property exempt page

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/HasEpcExemptionFormPagePropertyRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/HasEpcExemptionFormPagePropertyRegistration.kt
@@ -2,15 +2,16 @@ package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyRe
 
 import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.controllers.RegisterPropertyController
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Button
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Form
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Heading
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.EpcExemptionStep
 
-// TODO PDJB-658: Implement Has EPC Exemption page object
 class HasEpcExemptionFormPagePropertyRegistration(
     page: Page,
 ) : BasePage(page, "${RegisterPropertyController.PROPERTY_REGISTRATION_ROUTE}/${EpcExemptionStep.ROUTE_SEGMENT}") {
     val heading = Heading(page.locator("h1"))
     val form = Form(page)
+    val saveAndContinueButton = Button.byText(page, "Save and continue")
 }
