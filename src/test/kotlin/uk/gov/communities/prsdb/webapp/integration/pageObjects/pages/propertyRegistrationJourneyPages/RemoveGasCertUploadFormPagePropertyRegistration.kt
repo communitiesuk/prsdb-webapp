5654b63b44fc34ecd9e3fa45272fcb454e682056
Alex Read <alexander.read@communities.gov.uk>
2026-04-03 10:50:08 +0100

    Feat/pdjb 636 remove file uploads (#1182)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/RemoveGasCertUploadFormPagePropertyRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/RemoveGasCertUploadFormPagePropertyRegistration.kt
@@ -3,16 +3,15 @@ package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.propertyRe
 import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.controllers.RegisterPropertyController
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BackLink
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Form
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.FormWithRadios
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Heading
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.RemoveGasCertUploadStep
 
-// TODO PDJB-636: Implement Remove Gas Cert Upload page object
 class RemoveGasCertUploadFormPagePropertyRegistration(
     page: Page,
 ) : BasePage(page, "${RegisterPropertyController.PROPERTY_REGISTRATION_ROUTE}/${RemoveGasCertUploadStep.ROUTE_SEGMENT}") {
     val heading = Heading(page.locator("h1"))
-    val form = Form(page)
+    val form = FormWithRadios(page)
     val backLink = BackLink.default(page)
 }
