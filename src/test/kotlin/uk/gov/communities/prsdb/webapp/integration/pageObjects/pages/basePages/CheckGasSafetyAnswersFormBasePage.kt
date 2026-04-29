adc96460d2c727340189d1eab8efee392541954c
JasCon <jasmin.conterio@softwire.com>
2026-04-29 09:57:53 +0100

    PDJB-764: Move summary lists on the check answers page page to their own classes

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/basePages/CheckGasSafetyAnswersFormBasePage.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/basePages/CheckGasSafetyAnswersFormBasePage.kt
@@ -14,20 +14,20 @@ open class CheckGasSafetyAnswersFormBasePage(
     val sectionHeader = SectionHeader(page.locator("main"))
     val form = Form(page)
     val gasSupplySummaryList = GasSupplySummaryList(page)
-    val certSummaryList = CertSummaryList(page)
+    val certSummaryList = GasCertSummaryList(page)
+}
 
-    class GasSupplySummaryList(
-        page: Page,
-    ) : SummaryList(page, 0) {
-        val gasSupplyRow = getRow("Does the property have a gas supply or any gas appliances?")
-        val gasCertRow = getRow("Do you have a gas safety certificate for this property?")
-    }
+class GasSupplySummaryList(
+    page: Page,
+) : SummaryList(page, 0) {
+    val gasSupplyRow = getRow("Does the property have a gas supply or any gas appliances?")
+    val gasCertRow = getRow("Do you have a gas safety certificate for this property?")
+}
 
-    class CertSummaryList(
-        page: Page,
-    ) : SummaryList(page, 1) {
-        val validGasCertRow = getRow("Do you have a valid gas safety certificate for this property?")
-        val issueDateRow = getRow("Issue date")
-        val yourCertificateRow = getRow("Your certificate")
-    }
+class GasCertSummaryList(
+    page: Page,
+) : SummaryList(page, 1) {
+    val validGasCertRow = getRow("Do you have a valid gas safety certificate for this property?")
+    val issueDateRow = getRow("Issue date")
+    val yourCertificateRow = getRow("Your certificate")
 }
