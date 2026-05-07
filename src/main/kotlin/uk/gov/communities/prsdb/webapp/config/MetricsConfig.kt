67a284e5eae5dbec17aac24134ede6d60ebcdec9
Travis Woodward <104013113+Travis-Softwire@users.noreply.github.com>
2026-05-07 16:51:44 +0100

    PDJB-NONE: Restrict CloudWatch metrics to JVM and process diagnostics

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/config/MetricsConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/MetricsConfig.kt
@@ -8,4 +8,14 @@ import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebConfig
 class MetricsConfig {
     @Bean
     fun denyHttpServerRequestMetrics(): MeterFilter = MeterFilter.denyNameStartsWith("http.server.requests")
+
+    @Bean
+    fun allowOnlyDiagnosticMetrics(): MeterFilter =
+        MeterFilter.denyUnless { id ->
+            val name = id.name
+            name.startsWith("jvm.") ||
+                name.startsWith("process.") ||
+                name.startsWith("system.cpu") ||
+                name.startsWith("system.load")
+        }
 }
