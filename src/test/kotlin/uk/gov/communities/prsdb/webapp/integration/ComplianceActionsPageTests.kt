7272a849c43ebbfe7d35a918f524dcad0f1bc35e
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-05-21 12:15:02 +0100

    PDJB-792: Fix test import

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/ComplianceActionsPageTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/ComplianceActionsPageTests.kt
@@ -3,8 +3,8 @@ package uk.gov.communities.prsdb.webapp.integration
 import com.microsoft.playwright.Page
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
+import uk.gov.communities.prsdb.webapp.constants.COMPLIANCE_ACTIONS_PAGE_MAY26_REDESIGN
 import uk.gov.communities.prsdb.webapp.constants.COMPLIANCE_INFO_FRAGMENT
-import uk.gov.communities.prsdb.webapp.constants.USE_COMPLIANCE_ACTIONS_PAGE_REDESIGN
 import uk.gov.communities.prsdb.webapp.integration.IntegrationTestWithImmutableData.NestedIntegrationTestWithImmutableData
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BaseComponent.Companion.assertThat
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.PropertyDetailsPageLandlordView
@@ -18,7 +18,7 @@ class ComplianceActionsPageTests : IntegrationTest() {
         NestedIntegrationTestWithImmutableData("data-mockuser-landlord-with-compliance-actions.sql") {
         @BeforeEach
         fun disableRedesignFlag() {
-            featureFlagManager.disableFeature(USE_COMPLIANCE_ACTIONS_PAGE_REDESIGN)
+            featureFlagManager.disableFeature(COMPLIANCE_ACTIONS_PAGE_MAY26_REDESIGN)
         }
 
         @Test
@@ -84,7 +84,7 @@ class ComplianceActionsPageTests : IntegrationTest() {
         NestedIntegrationTestWithImmutableData("data-mockuser-landlord-with-compliance-actions.sql") {
         @BeforeEach
         fun enableRedesignFlag() {
-            featureFlagManager.enableFeature(USE_COMPLIANCE_ACTIONS_PAGE_REDESIGN)
+            featureFlagManager.enableFeature(COMPLIANCE_ACTIONS_PAGE_MAY26_REDESIGN)
         }
 
         @Test
