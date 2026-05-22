ce15efd696378bc1d20f99042d1be411aa701aa3
Tom Hanmer <tom.hanmer@softwire.com>
2026-05-22 10:57:52 +0100

    PDJB-794: simplify migration

--- src/main/resources/db/migrations/V1_26_0__add_compliance_tracking_fields.sql
+++ src/main/resources/db/migrations/V1_26_0__add_compliance_tracking_fields.sql
@@ -2,12 +2,7 @@ ALTER TABLE property_ownership ADD COLUMN last_occupied_date DATE;
 
 UPDATE property_ownership
 SET last_occupied_date = created_date::date
-WHERE current_num_tenants > 0
-  AND current_num_households > 0
-  AND num_bedrooms IS NOT NULL AND num_bedrooms > 0
-  AND furnished_status IS NOT NULL
-  AND rent_frequency IS NOT NULL
-  AND rent_amount IS NOT NULL;
+WHERE current_num_tenants > 0;
 
 ALTER TABLE property_compliance ADD COLUMN gas_safety_cert_provide_later BOOLEAN;
 ALTER TABLE property_compliance ADD COLUMN electrical_safety_cert_provide_later BOOLEAN;
