f39a4b0d8129474d690dffc4929b95e47c05ffd8
JasCon <jasmin.conterio@softwire.com>
2026-05-28 12:27:17 +0100

    PDJB-260: Update the journey map

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/acceptOrRejectJointLandlordInvitation/AcceptOrRejectJointLandlordInvitationJourneyFactory.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/acceptOrRejectJointLandlordInvitation/AcceptOrRejectJointLandlordInvitationJourneyFactory.kt
@@ -5,15 +5,20 @@ import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFramewo
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
 import uk.gov.communities.prsdb.webapp.controllers.AcceptOrRejectJointLandlordInvitationController.Companion.JOINT_LANDLORD_INVITATION_ACCEPTED_CONFIRMATION_ROUTE
 import uk.gov.communities.prsdb.webapp.controllers.AcceptOrRejectJointLandlordInvitationController.Companion.JOINT_LANDLORD_INVITATION_REJECTED_CONFIRMATION_ROUTE
+import uk.gov.communities.prsdb.webapp.controllers.RegisterLandlordController.Companion.LANDLORD_REGISTRATION_START_PAGE_ROUTE
 import uk.gov.communities.prsdb.webapp.exceptions.PrsdbWebException
 import uk.gov.communities.prsdb.webapp.journeys.AbstractJourneyState
+import uk.gov.communities.prsdb.webapp.journeys.Destination
 import uk.gov.communities.prsdb.webapp.journeys.JourneyState
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStateDelegateProvider
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
 import uk.gov.communities.prsdb.webapp.journeys.StepLifecycleOrchestrator
 import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.AcceptOrRejectStep
+import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.CheckUserRoleStep
+import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.ConfirmYouAreALandlordForThisPropertyStep
 import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.InviteUnavailableStep
 import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.TokenValidationResult
+import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.UserRoleStatus
 import uk.gov.communities.prsdb.webapp.journeys.acceptOrRejectJointLandlordInvitation.steps.ValidateTokenStep
 import uk.gov.communities.prsdb.webapp.journeys.builders.JourneyBuilder.Companion.journey
 import uk.gov.communities.prsdb.webapp.journeys.hasOutcome
@@ -59,11 +64,38 @@ class AcceptOrRejectJointLandlordInvitationJourneyFactory(
                     when (mode) {
                         // TODO PDJB-260 - if they submit yes we will need to check if the user is a registered latndlord and if not they need to register
                         //   The existing landlord registration journey covers most of this but can't be used as-is as we need different exit points here
-                        YesOrNo.YES -> JOINT_LANDLORD_INVITATION_ACCEPTED_CONFIRMATION_ROUTE
+                        YesOrNo.YES -> {
+                            Destination(journey.checkUserRoleStep).toUrlStringOrNull()
+                                ?: throw PrsdbWebException("Url string for nextUrl cannot be null")
+                        }
 
-                        YesOrNo.NO -> JOINT_LANDLORD_INVITATION_REJECTED_CONFIRMATION_ROUTE
+                        YesOrNo.NO -> {
+                            JOINT_LANDLORD_INVITATION_REJECTED_CONFIRMATION_ROUTE
+                        }
                     }
                 }
+            }
+            // TODO PDJB-260 - direct back to here after ll registration journey?
+            step(journey.checkUserRoleStep) {
+                routeSegment(CheckUserRoleStep.ROUTE_SEGMENT)
+                parents { journey.acceptOrRejectStep.hasOutcome(YesOrNo.YES) }
+                nextUrl { mode ->
+                    when (mode) {
+                        UserRoleStatus.USER_NOT_REGISTERED_AS_LANDLORD -> {
+                            LANDLORD_REGISTRATION_START_PAGE_ROUTE
+                        }
+
+                        UserRoleStatus.USER_IS_ALREADY_REGISTERED_AS_LANDLORD -> {
+                            Destination(journey.confirmYouAreALandlordForThisPropertyStep).toUrlStringOrNull()
+                                ?: throw PrsdbWebException("Url string for nextUrl cannot be null")
+                        }
+                    }
+                }
+            }
+            step(journey.confirmYouAreALandlordForThisPropertyStep) {
+                routeSegment(ConfirmYouAreALandlordForThisPropertyStep.ROUTE_SEGMENT)
+                parents { journey.checkUserRoleStep.hasOutcome(UserRoleStatus.USER_IS_ALREADY_REGISTERED_AS_LANDLORD) }
+                nextUrl { JOINT_LANDLORD_INVITATION_ACCEPTED_CONFIRMATION_ROUTE }
             }
             step(journey.inviteUnavailableStep) {
                 routeSegment(InviteUnavailableStep.ROUTE_SEGMENT)
@@ -79,9 +111,11 @@ class AcceptOrRejectJointLandlordInvitationJourneyFactory(
 
 @JourneyFrameworkComponent
 class AcceptOrRejectJointLandlordInvitationJourney(
-    val validateTokenStep: ValidateTokenStep,
-    val acceptOrRejectStep: AcceptOrRejectStep,
-    val inviteUnavailableStep: InviteUnavailableStep,
+    override val validateTokenStep: ValidateTokenStep,
+    override val acceptOrRejectStep: AcceptOrRejectStep,
+    override val checkUserRoleStep: CheckUserRoleStep,
+    override val confirmYouAreALandlordForThisPropertyStep: ConfirmYouAreALandlordForThisPropertyStep,
+    override val inviteUnavailableStep: InviteUnavailableStep,
     journeyStateService: JourneyStateService,
 ) : AbstractJourneyState(journeyStateService),
     AcceptOrRejectJointLandlordInvitationJourneyState {
@@ -99,4 +133,9 @@ class AcceptOrRejectJointLandlordInvitationJourney(
 
 interface AcceptOrRejectJointLandlordInvitationJourneyState : JourneyState {
     val invitationToken: String
+    val validateTokenStep: ValidateTokenStep
+    val acceptOrRejectStep: AcceptOrRejectStep
+    val checkUserRoleStep: CheckUserRoleStep
+    val confirmYouAreALandlordForThisPropertyStep: ConfirmYouAreALandlordForThisPropertyStep
+    val inviteUnavailableStep: InviteUnavailableStep
 }
