0bc31cb3b14e1f83c9833a5fcd9ac58da03cf284
Tom Hanmer <tom.hanmer@softwire.com>
2026-05-28 11:53:24 +0100

    PDJB-794: refactor notification banner to use statuses

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/summaryModels/propertyComplianceViewModels/NotificationBannerViewModelServiceRedesign.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/summaryModels/propertyComplianceViewModels/NotificationBannerViewModelServiceRedesign.kt
@@ -2,26 +2,28 @@ package uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.property
 
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
 import uk.gov.communities.prsdb.webapp.constants.COMPLIANCE_INFO_FRAGMENT
+import uk.gov.communities.prsdb.webapp.constants.enums.ComplianceCertStatus
 import uk.gov.communities.prsdb.webapp.database.entity.PropertyCompliance
+import uk.gov.communities.prsdb.webapp.models.dataModels.ComplianceStatusDataModel
 
 @PrsdbWebService("notificationBannerViewModelServiceRedesign")
 class NotificationBannerViewModelServiceRedesign : NotificationBannerViewModelService {
     override fun getNotificationMessageKeys(
         propertyCompliance: PropertyCompliance,
     ): List<PropertyComplianceViewModel.PropertyComplianceNotificationMessage> {
-        val isGasExpired = propertyCompliance.isGasSafetyCertExpired == true
-        val isElectricalExpired = propertyCompliance.isElectricalSafetyExpired == true
-        val isEpcExpired = propertyCompliance.isEpcExpired == true
+        val statusModel = ComplianceStatusDataModel.fromPropertyCompliance(propertyCompliance)
+
+        val isGasExpired = statusModel.gasSafetyStatus == ComplianceCertStatus.EXPIRED
+        val isElectricalExpired = statusModel.eicrStatus == ComplianceCertStatus.EXPIRED
+        val isEpcExpired = statusModel.epcStatus == ComplianceCertStatus.EXPIRED
         val displayAnyExpired = isGasExpired || isElectricalExpired || isEpcExpired
         val expiredCerts = listOf(isGasExpired, isElectricalExpired, isEpcExpired).count { it }
 
-        val isOccupied = propertyCompliance.propertyOwnership.isOccupied
-        val displayIsGasMissing =
-            isOccupied && (propertyCompliance.isGasSafetyCertMissing || propertyCompliance.gasSafetyCertProvideLater == true)
-        val displayIsElectricalMissing =
-            isOccupied && (propertyCompliance.isElectricalSafetyMissing || propertyCompliance.electricalSafetyCertProvideLater == true)
-        val displayIsEpcMissing =
-            isOccupied && (propertyCompliance.isEpcMissing || propertyCompliance.epcProvideLater == true)
+        val missingStatuses = listOf(ComplianceCertStatus.NOT_ADDED, ComplianceCertStatus.PROVIDE_LATER)
+        val isOccupied = statusModel.isOccupied
+        val displayIsGasMissing = isOccupied && statusModel.gasSafetyStatus in missingStatuses
+        val displayIsElectricalMissing = isOccupied && statusModel.eicrStatus in missingStatuses
+        val displayIsEpcMissing = isOccupied && statusModel.epcStatus in missingStatuses
 
         val displayAnyMissing = displayIsGasMissing || displayIsElectricalMissing || displayIsEpcMissing
 
@@ -50,16 +52,13 @@ class NotificationBannerViewModelServiceRedesign : NotificationBannerViewModelSe
         )
     }
 
-    override fun getIsAllValid(propertyCompliance: PropertyCompliance): Boolean =
-        !propertyCompliance.isGasSafetyCertMissing &&
-            !propertyCompliance.isElectricalSafetyMissing &&
-            !propertyCompliance.isEpcMissing &&
-            propertyCompliance.gasSafetyCertProvideLater != true &&
-            propertyCompliance.electricalSafetyCertProvideLater != true &&
-            propertyCompliance.epcProvideLater != true &&
-            propertyCompliance.isGasSafetyCertExpired != true &&
-            propertyCompliance.isElectricalSafetyExpired != true &&
-            propertyCompliance.isEpcExpired != true
+    override fun getIsAllValid(propertyCompliance: PropertyCompliance): Boolean {
+        val statusModel = ComplianceStatusDataModel.fromPropertyCompliance(propertyCompliance)
+        val validStatuses = listOf(ComplianceCertStatus.ADDED, ComplianceCertStatus.NOT_REQUIRED)
+        return statusModel.gasSafetyStatus in validStatuses &&
+            statusModel.eicrStatus in validStatuses &&
+            statusModel.epcStatus in validStatuses
+    }
 
     companion object {
         private const val NOTIFICATION_KEY_PREFIX = "propertyDetails.complianceInformation.notificationBanner"
