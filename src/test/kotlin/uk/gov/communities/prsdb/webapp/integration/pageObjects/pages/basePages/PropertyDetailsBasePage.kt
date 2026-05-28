43891a01ef2c95e2353f747f039db14656428f1e
Tom Hanmer <tom.hanmer@softwire.com>
2026-05-28 17:18:41 +0100

    PDJB-794: fix tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/basePages/PropertyDetailsBasePage.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/basePages/PropertyDetailsBasePage.kt
@@ -1,5 +1,6 @@
 package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages
 
+import com.microsoft.playwright.Locator
 import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BackLink
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.SummaryCard
@@ -15,7 +16,7 @@ abstract class PropertyDetailsBasePage(
     val propertyComplianceSummaryList = PropertyComplianceSummaryList(page)
     val gasSafetyCard = GasSafetySummaryCard(page, "Gas safety certificate")
     val electricalSafetyCard = ElectricalSafetySummaryCard(page, "Electrical safety certificate")
-    val epcCard = SummaryCard(page, "Energy performance certificate (EPC)")
+    val epcCard = EpcSummaryCard(page, "Energy performance certificate (EPC)")
     val landlordSummaryList = LandlordSummaryList(page)
 
     val backLink = BackLink.default(page)
@@ -66,5 +67,24 @@ abstract class PropertyDetailsBasePage(
         val fireSafetyRow = getRow("Fire safety responsibilities")
         val propertySafetyRow = getRow("Health and safety in rental properties")
         val responsibilityToTenantsRow = getRow("Your responsibilities to your tenants")
+    }
+
+    class EpcSummaryCard(
+        page: Page,
+        title: String,
+    ) : SummaryCard(page, title) {
+        override val summaryList = EpcSummaryList(locator)
+    }
+
+    class EpcSummaryList(
+        locator: Locator,
+    ) : SummaryList(locator, index = 0) {
+        val hasEpcRow = getRow("Do you have an EPC")
+        val isEpcRequiredRow = getRow("Is an EPC required")
+        val epcExemptionRow = getRow("Why does this property not need an EPC")
+        val certificateStatusRow = getRow("Certificate status")
+        val energyRatingRow = getRow("Energy rating")
+        val expiryDateRow = getRow("Expiry date")
+        val certificateNumberRow = getRow("Certificate number")
     }
 }
