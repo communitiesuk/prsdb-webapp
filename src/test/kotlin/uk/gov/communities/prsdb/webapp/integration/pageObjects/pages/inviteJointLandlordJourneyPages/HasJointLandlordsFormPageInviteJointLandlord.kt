0b1457cfbd45ab11c12cfc6d63b9320d0d14cc39
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-06-11 15:32:42 +0100

    PDJB-306: Add integration tests

---
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/inviteJointLandlordJourneyPages/HasJointLandlordsFormPageInviteJointLandlord.kt
@@ -0,0 +1,15 @@
+package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.inviteJointLandlordJourneyPages
+
+import com.microsoft.playwright.Page
+import uk.gov.communities.prsdb.webapp.controllers.InviteJointLandlordController
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.HasJointLandlordsFormBasePage
+import uk.gov.communities.prsdb.webapp.journeys.shared.inviteJointLandlord.HasJointLandlordsStep
+
+class HasJointLandlordsFormPageInviteJointLandlord(
+    page: Page,
+    urlArguments: Map<String, String>,
+) : HasJointLandlordsFormBasePage(
+        page,
+        "${InviteJointLandlordController.getInviteJointLandlordRoute(urlArguments["propertyOwnershipId"]!!.toLong())}/" +
+            HasJointLandlordsStep.ROUTE_SEGMENT,
+    )
