d2674d9526c9b95144800dfbb1f2cba5a46b5c30
Samuel Young <samuel.young@softwire.com>
2026-06-17 16:51:31 +0100

    PDJB-268/PDJB-269: Move the transaction away from exitProcess (#1461)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/application/IncompletePropertiesReminderTaskApplicationRunner.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/application/IncompletePropertiesReminderTaskApplicationRunner.kt
@@ -1,5 +1,6 @@
 package uk.gov.communities.prsdb.webapp.application
 
+import jakarta.transaction.Transactional
 import kotlinx.datetime.daysUntil
 import kotlinx.datetime.toKotlinLocalDate
 import org.springframework.boot.ApplicationArguments
@@ -7,6 +8,7 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
 import uk.gov.communities.prsdb.webapp.annotations.taskAnnotations.PrsdbScheduledTask
+import uk.gov.communities.prsdb.webapp.annotations.taskAnnotations.PrsdbTaskService
 import uk.gov.communities.prsdb.webapp.constants.INCOMPLETE_PROPERTY_AGE_WHEN_REMINDER_EMAIL_DUE_IN_DAYS
 import uk.gov.communities.prsdb.webapp.exceptions.PersistentEmailSendException
 import uk.gov.communities.prsdb.webapp.exceptions.TrackEmailSentException
@@ -24,15 +26,12 @@ import kotlin.system.exitProcess
 @PrsdbScheduledTask("incomplete-property-reminder-scheduled-task")
 class IncompletePropertiesReminderTaskApplicationRunner(
     private val context: ApplicationContext,
-    private val emailSender: EmailNotificationService<IncompletePropertyReminderEmail>,
-    private val absoluteUrlProvider: AbsoluteUrlProvider,
-    private val incompletePropertiesService: IncompletePropertiesService,
+    private val taskLogic: IncompletePropertiesReminderTaskLogic,
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         println("Executing incomplete properties reminder scheduled task")
 
-        // Separating into its own method to allow this to be tested without "exitProcess" being called
-        incompletePropertiesReminderTaskLogic()
+        taskLogic.sendIncompletePropertyReminders()
 
         val code =
             SpringApplication.exit(context, { 0 }).also {
@@ -40,8 +39,16 @@ class IncompletePropertiesReminderTaskApplicationRunner(
             }
         exitProcess(code)
     }
+}
 
-    private fun incompletePropertiesReminderTaskLogic() {
+@PrsdbTaskService
+class IncompletePropertiesReminderTaskLogic(
+    private val emailSender: EmailNotificationService<IncompletePropertyReminderEmail>,
+    private val absoluteUrlProvider: AbsoluteUrlProvider,
+    private val incompletePropertiesService: IncompletePropertiesService,
+) {
+    @Transactional
+    fun sendIncompletePropertyReminders() {
         val prsdUrl = absoluteUrlProvider.buildLandlordDashboardUri().toString()
         val cutoffDate =
             DateTimeHelper.getJavaInstantFromLocalDate(
@@ -99,9 +106,5 @@ class IncompletePropertiesReminderTaskApplicationRunner(
         println(statusMessage)
         println("Exception message: ${ex.message}")
         println("Stack trace: ${ex.stackTraceToString()}")
-    }
-
-    companion object {
-        const val INCOMPLETE_PROPERTY_REMINDER_TASK_METHOD_NAME = "incompletePropertiesReminderTaskLogic"
     }
 }
