02e57d6a5685c9aa9e52638b07b481b7d4455cdb
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-07-13 10:34:09 +0100

    PDJB-1284: Remove TODO for integration test (it already works)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordRegistrationJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordRegistrationJourneyTests.kt
@@ -339,7 +339,6 @@ class LandlordRegistrationJourneyTests : IntegrationTestWithMutableData("data-mo
         val leadTrusteeSelectAddressPage = assertPageIs(page, LeadTrusteeSelectAddressFormPageLandlordRegistration::class)
         leadTrusteeSelectAddressPage.selectAddressAndSubmit("1 PRSDB Square, EG1 2AA")
 
-        // TODO: PDJB-1284 - Submit real governing body details once the step is implemented
         val orgGovBodyDetailsPage = assertPageIs(page, OrgGovBodyDetailsFormPageLandlordRegistration::class)
         orgGovBodyDetailsPage.submitHasDetails()
 
