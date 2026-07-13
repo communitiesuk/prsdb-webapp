0f555b4630649890d95b3552562057ba506db8b8
Tom Hanmer <43523985+TomHanmer29@users.noreply.github.com>
2026-07-13 17:26:06 +0100

    PDJB-941: remove headers from mini cya pages for compliance (#1578)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyRegistrationJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyRegistrationJourneyTests.kt
@@ -1519,6 +1519,7 @@ class PropertyRegistrationJourneyTests : IntegrationTestWithMutableData("data-lo
         assertThat(hasGasSupplyPage.heading).containsText("Does the property have a gas supply or any gas appliances?")
         hasGasSupplyPage.submitHasNoGasSupply()
         val checkGasSafetyAnswersPage = assertPageIs(page, CheckGasSafetyAnswersFormPagePropertyRegistration::class)
+        assertThat(checkGasSafetyAnswersPage.sectionHeader).isHidden()
         checkGasSafetyAnswersPage.form.submit()
 
         taskListPage = assertPageIs(page, TaskListPagePropertyRegistration::class)
@@ -1528,6 +1529,7 @@ class PropertyRegistrationJourneyTests : IntegrationTestWithMutableData("data-lo
         val provideElectricalCertLaterPage = assertPageIs(page, ProvideElectricalCertLaterFormPagePropertyRegistration::class)
         provideElectricalCertLaterPage.form.submit()
         val checkElectricalSafetyAnswersPage = assertPageIs(page, CheckElectricalSafetyAnswersFormPagePropertyRegistration::class)
+        assertThat(checkElectricalSafetyAnswersPage.sectionHeader).isHidden()
 
         whenever(epcRegisterClient.getByUprn(uprnForSelectedAddress)).thenReturn(MockEpcData.epcRegisterClientEpcNotFoundResponse)
         checkElectricalSafetyAnswersPage.form.submit()
@@ -1538,6 +1540,7 @@ class PropertyRegistrationJourneyTests : IntegrationTestWithMutableData("data-lo
         val provideEpcLaterPage = assertPageIs(page, ProvideEpcLaterFormPagePropertyRegistration::class)
         provideEpcLaterPage.form.submit()
         val checkEpcAnswersPage = assertPageIs(page, CheckEpcAnswersFormPagePropertyRegistration::class)
+        assertThat(checkEpcAnswersPage.sectionHeader).isHidden()
         checkEpcAnswersPage.form.submit()
 
         taskListPage = assertPageIs(page, TaskListPagePropertyRegistration::class)
