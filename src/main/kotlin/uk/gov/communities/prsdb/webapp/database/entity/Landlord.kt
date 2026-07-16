247aa221414e36514490d528d583d3ed2780ae7a
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-07-16 09:26:19 +0100

    PDJB-NONE: add a landlord type discriminator

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/Landlord.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/Landlord.kt
@@ -11,12 +11,17 @@ import jakarta.persistence.InheritanceType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
+import jakarta.persistence.Transient
+import uk.gov.communities.prsdb.webapp.constants.enums.LandlordType
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "landlord_type", discriminatorType = DiscriminatorType.INTEGER)
 // Hibernate creates subclass proxies for this inheritance root, so it and its persistent getters must remain open.
 abstract class Landlord : ModifiableAuditableEntity() {
+    @get:Transient
+    abstract val landlordType: LandlordType
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     open val id: Long = 0
