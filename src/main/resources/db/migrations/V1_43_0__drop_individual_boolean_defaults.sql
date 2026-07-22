79f7586bcc11010c24cfd5a9c288bfbaee39916e
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-07-22 14:21:48 +0100

    PDJB-1180: Drop default for IL fields

---
+++ src/main/resources/db/migrations/V1_43_0__drop_individual_boolean_defaults.sql
@@ -0,0 +1,4 @@
+ALTER TABLE landlord ALTER COLUMN individual_is_active DROP DEFAULT;
+ALTER TABLE landlord ALTER COLUMN individual_is_verified DROP DEFAULT;
+ALTER TABLE landlord ALTER COLUMN individual_has_accepted_privacy_notice DROP DEFAULT;
+ALTER TABLE landlord ALTER COLUMN individual_has_responded_to_feedback DROP DEFAULT;
