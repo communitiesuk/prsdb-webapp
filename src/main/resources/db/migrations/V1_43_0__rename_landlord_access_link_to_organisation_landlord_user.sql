fe89b0eba194cef26895aaad6952bc106536eecc
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-07-24 10:21:28 +0100

    PDJB-1180: Fix constraint name too long

--- src/main/resources/db/migrations/V1_43_0__rename_landlord_access_link_to_organisation_landlord_user.sql
+++ src/main/resources/db/migrations/V1_43_0__rename_landlord_access_link_to_organisation_landlord_user.sql
@@ -6,6 +6,6 @@ ALTER TABLE organisation_landlord_user RENAME COLUMN landlord_id TO organisation
 ALTER INDEX idx_landlord_access_link_subject RENAME TO idx_organisation_landlord_user_subject;
 ALTER INDEX idx_landlord_access_link_landlord RENAME TO idx_organisation_landlord_user_organisation_landlord;
 
--- Rename the unique constraint
+-- Rename the unique constraint, organisation -> org in new name else it'd be truncated
 ALTER TABLE organisation_landlord_user RENAME CONSTRAINT landlord_access_link_landlord_id_subject_identifier_key
-    TO organisation_landlord_user_organisation_landlord_id_subject_identifier_key;
+    TO org_landlord_user_org_landlord_id_subject_identifier_key;
