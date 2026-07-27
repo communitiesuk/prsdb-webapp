3c35f6dc52339b5bf2dcb4dff80289ed980aab74
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-07-27 10:03:25 +0100

    PDJB-1275: Remove final TODOs for ILR

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/IndividualLandlordRepository.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/repository/IndividualLandlordRepository.kt
@@ -4,13 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.communities.prsdb.webapp.database.entity.IndividualLandlord
 import java.time.Instant
 
-// TODO: PDJB-1275: Update checks for landlord users to account for multiple users representing a landlord
 // The underscore tells JPA to access fields relating to the referenced table
 @Suppress("ktlint:standard:function-naming")
 interface IndividualLandlordRepository :
     JpaRepository<IndividualLandlord?, Long?>,
     LandlordSearchRepository {
-    // TODO: PDJB-1275: Update checks for landlord users to account for multiple users representing a landlord
     fun findByBaseUser_Id(subjectId: String): IndividualLandlord?
 
     fun deleteByBaseUser_Id(subjectId: String)
