9083050120199fe9f40f67c7544b7e46823bff13
Alexander Read <alexander.read@communities.gov.uk>
2026-07-27 16:44:30 +0100

    PDJB-896: Migrate UpdateLandlordEmailController to JourneyStepDispatcher

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/UpdateLandlordEmailController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/UpdateLandlordEmailController.kt
@@ -1,21 +1,19 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
-import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.ModelAndView
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbController
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_DETAILS_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.UpdateLandlordEmailController.Companion.UPDATE_EMAIL_ROUTE
 import uk.gov.communities.prsdb.webapp.journeys.FormData
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
-import uk.gov.communities.prsdb.webapp.journeys.NoSuchJourneyException
+import uk.gov.communities.prsdb.webapp.journeys.JourneyStepDispatcher
+import uk.gov.communities.prsdb.webapp.journeys.StepLifecycleOrchestrator
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.email.UpdateEmailJourneyFactory
 import java.security.Principal
 
@@ -25,36 +23,30 @@ import java.security.Principal
 class UpdateLandlordEmailController(
     private val journeyFactory: UpdateEmailJourneyFactory,
 ) {
-    @GetMapping("{stepName}")
+    @GetMapping("/{*stepPath}")
     fun getUpdateStep(
         principal: Principal,
-        @PathVariable("stepName") stepName: String,
-    ): ModelAndView =
-        try {
-            val journeyMap = journeyFactory.createJourneySteps()
-            journeyMap[stepName]?.getStepModelAndView()
-                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Step not found")
-        } catch (_: NoSuchJourneyException) {
-            val journeyId = journeyFactory.initializeJourneyState(principal)
-            val redirectUrl = JourneyStateService.urlWithJourneyState(stepName, journeyId)
-            ModelAndView("redirect:$redirectUrl")
-        }
+        @PathVariable stepPath: String,
+    ): ModelAndView = dispatchJourneyStep(stepPath, principal) { getStepModelAndView() }
 
-    @PostMapping("{stepName}")
+    @PostMapping("/{*stepPath}")
     fun postUpdateStep(
         principal: Principal,
-        @PathVariable("stepName") stepName: String,
+        @PathVariable stepPath: String,
         @RequestParam formData: FormData,
+    ): ModelAndView = dispatchJourneyStep(stepPath, principal) { postStepModelAndView(formData) }
+
+    private fun dispatchJourneyStep(
+        stepPath: String,
+        principal: Principal,
+        dispatch: StepLifecycleOrchestrator.() -> ModelAndView,
     ): ModelAndView =
-        try {
-            val journeyMap = journeyFactory.createJourneySteps()
-            journeyMap[stepName]?.postStepModelAndView(formData)
-                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Step not found")
-        } catch (_: NoSuchJourneyException) {
-            val journeyId = journeyFactory.initializeJourneyState(principal)
-            val redirectUrl = JourneyStateService.urlWithJourneyState(stepName, journeyId)
-            ModelAndView("redirect:$redirectUrl")
-        }
+        JourneyStepDispatcher.handleInitialisableRequest(
+            rawStepPath = stepPath,
+            createRoutingMap = { journeyFactory.createJourneySteps() },
+            initialiseJourney = { journeyFactory.initializeJourneyState(principal) },
+            dispatch = dispatch,
+        )
 
     companion object {
         const val UPDATE_EMAIL_ROUTE = "/$LANDLORD_PATH_SEGMENT/$LANDLORD_DETAILS_PATH_SEGMENT/update-email"
