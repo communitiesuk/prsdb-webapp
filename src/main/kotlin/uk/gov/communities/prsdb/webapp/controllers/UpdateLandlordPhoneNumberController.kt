8a8f07f471ced46b057991bb230e8d3379a5e5a4
Alexander Read <alexander.read@communities.gov.uk>
2026-07-27 16:50:59 +0100

    PDJB-896: Migrate UpdateLandlordPhoneNumberController to JourneyStepDispatcher

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/UpdateLandlordPhoneNumberController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/UpdateLandlordPhoneNumberController.kt
@@ -1,21 +1,19 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
-import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.ModelAndView
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbController
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_DETAILS_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.UpdateLandlordPhoneNumberController.Companion.UPDATE_PHONE_NUMBER_ROUTE
 import uk.gov.communities.prsdb.webapp.journeys.FormData
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
-import uk.gov.communities.prsdb.webapp.journeys.NoSuchJourneyException
+import uk.gov.communities.prsdb.webapp.journeys.JourneyStepDispatcher
+import uk.gov.communities.prsdb.webapp.journeys.StepLifecycleOrchestrator
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.phoneNumber.UpdatePhoneNumberJourneyFactory
 import java.security.Principal
 
@@ -25,36 +23,30 @@ import java.security.Principal
 class UpdateLandlordPhoneNumberController(
     private val updatePhoneNumberJourneyFactory: UpdatePhoneNumberJourneyFactory,
 ) {
-    @GetMapping("/{stepName}")
+    @GetMapping("/{*stepPath}")
     fun getJourneyStep(
-        @PathVariable stepName: String,
+        @PathVariable stepPath: String,
         principal: Principal,
-    ): ModelAndView =
-        try {
-            val journeySteps = updatePhoneNumberJourneyFactory.createJourneySteps()
-            journeySteps[stepName]?.getStepModelAndView()
-                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Step not found")
-        } catch (_: NoSuchJourneyException) {
-            val journeyId = updatePhoneNumberJourneyFactory.initializeJourneyState(principal)
-            val redirectUrl = JourneyStateService.urlWithJourneyState(stepName, journeyId)
-            ModelAndView("redirect:$redirectUrl")
-        }
+    ): ModelAndView = dispatchJourneyStep(stepPath, principal) { getStepModelAndView() }
 
-    @PostMapping("/{stepName}")
+    @PostMapping("/{*stepPath}")
     fun postJourneyStep(
-        @PathVariable stepName: String,
+        @PathVariable stepPath: String,
         @RequestParam formData: FormData,
         principal: Principal,
+    ): ModelAndView = dispatchJourneyStep(stepPath, principal) { postStepModelAndView(formData) }
+
+    private fun dispatchJourneyStep(
+        stepPath: String,
+        principal: Principal,
+        dispatch: StepLifecycleOrchestrator.() -> ModelAndView,
     ): ModelAndView =
-        try {
-            val journeySteps = updatePhoneNumberJourneyFactory.createJourneySteps()
-            journeySteps[stepName]?.postStepModelAndView(formData)
-                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Step not found")
-        } catch (_: NoSuchJourneyException) {
-            val journeyId = updatePhoneNumberJourneyFactory.initializeJourneyState(principal)
-            val redirectUrl = JourneyStateService.urlWithJourneyState(stepName, journeyId)
-            ModelAndView("redirect:$redirectUrl")
-        }
+        JourneyStepDispatcher.handleInitialisableRequest(
+            rawStepPath = stepPath,
+            createRoutingMap = { updatePhoneNumberJourneyFactory.createJourneySteps() },
+            initialiseJourney = { updatePhoneNumberJourneyFactory.initializeJourneyState(principal) },
+            dispatch = dispatch,
+        )
 
     companion object {
         const val UPDATE_PHONE_NUMBER_ROUTE = "/$LANDLORD_PATH_SEGMENT/$LANDLORD_DETAILS_PATH_SEGMENT/update-phone-number"
