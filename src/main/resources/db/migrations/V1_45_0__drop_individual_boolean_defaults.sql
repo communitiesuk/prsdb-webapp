8d4713c59913673a65cedbd73c182c367e3b52c5
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-07-28 15:45:23 +0100

    PDJB-1180: Fix migrations

--- src/main/resources/db/migrations/V1_45_0__drop_individual_boolean_defaults.sql
+++ src/main/resources/db/migrations/V1_45_0__drop_individual_boolean_defaults.sql
@@ -1,4 +1,3 @@
 ALTER TABLE landlord ALTER COLUMN individual_is_active DROP DEFAULT;
 ALTER TABLE landlord ALTER COLUMN individual_is_verified DROP DEFAULT;
 ALTER TABLE landlord ALTER COLUMN individual_has_accepted_privacy_notice DROP DEFAULT;
-ALTER TABLE landlord ALTER COLUMN individual_has_responded_to_feedback DROP DEFAULT;
