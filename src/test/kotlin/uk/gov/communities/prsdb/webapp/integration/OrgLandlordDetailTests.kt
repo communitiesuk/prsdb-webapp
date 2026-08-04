9fe6ba8fadee1b8506b784077483d7c53191b8aa
Tom Hanmer <tom.hanmer@softwire.com>
2026-08-04 12:30:48 +0100

    PDJB-1251: remove uneccesary org ll

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/OrgLandlordDetailTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/OrgLandlordDetailTests.kt
@@ -7,12 +7,13 @@ import uk.gov.communities.prsdb.webapp.constants.REGISTERED_PROPERTIES_FRAGMENT
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BaseComponent.Companion.assertThat
 import kotlin.test.assertEquals
 
-class OrgLandlordDetailTests : IntegrationTestWithImmutableData("data-mockuser-organisation-landlord.sql") {
+@WithOrgLandlordProfile
+class OrgLandlordDetailTests : IntegrationTestWithImmutableData("data-local.sql") {
     @Test
     fun `the org landlord details page loads with the organisation name, details tab selected and a delete organisation link`(page: Page) {
         val detailsPage = navigator.goToOrgLandlordDetails()
 
-        assertThat(page.locator("h1")).containsText("Keystone Living Group")
+        assertThat(page.locator("#main-content h1")).containsText("Local Organisation Landlord")
         assertThat(detailsPage.deleteOrganisationLink).isVisible()
         assertEquals("organisation-details", detailsPage.tabs.activeTabPanelId)
     }
@@ -21,11 +22,11 @@ class OrgLandlordDetailTests : IntegrationTestWithImmutableData("data-mockuser-o
     fun `the organisation details tab shows the organisation's details`(page: Page) {
         navigator.goToOrgLandlordDetails()
 
-        assertThat(page.getByText("3rd Floor, 88 Kingsway Square, London, ZX1 4QP")).isVisible()
-        assertThat(page.getByText("hello@keystoneliving.co.uk")).isVisible()
-        assertThat(page.getByText("020 7123 4567")).isVisible()
+        assertThat(page.getByText("5 Mythical Place")).isVisible()
+        assertThat(page.getByText("local-org-landlord@example.com")).isVisible()
+        assertThat(page.getByText("07111111111")).isVisible()
         assertThat(page.getByText("Companies House number")).isVisible()
-        assertThat(page.getByText("01234567")).isVisible()
+        assertThat(page.getByText("12345678")).isVisible()
     }
 
     @Test
@@ -35,9 +36,9 @@ class OrgLandlordDetailTests : IntegrationTestWithImmutableData("data-mockuser-o
         detailsPage.tabs.goToOrganisationContacts()
 
         assertThat(page.getByText("Main contact")).isVisible()
-        assertThat(page.getByText("Jane Doe")).isVisible()
-        assertThat(page.getByText("jane.doe@keystoneliving.co.uk")).isVisible()
-        assertThat(page.getByText("020 7123 4568")).isVisible()
+        assertThat(page.getByText("Local Main Contact")).isVisible()
+        assertThat(page.getByText("local-main-contact@example.com")).isVisible()
+        assertThat(page.getByText("07111111113")).isVisible()
     }
 
     @Test
