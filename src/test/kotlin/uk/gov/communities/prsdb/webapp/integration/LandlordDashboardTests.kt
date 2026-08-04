ddd132f08eebea61419b980b3ef59d89ca77f9a4
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-08-04 16:28:49 +0100

    PDJB-1278: add additional tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDashboardTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDashboardTests.kt
@@ -178,5 +178,6 @@ class OrgLandlordDashboardTests : IntegrationTestWithImmutableData("data-local.s
         val dashboard = navigator.goToLandlordDashboard()
         assertThat(dashboard.dashboardBannerHeading).containsText("Local Organisation Landlord")
         assertThat(dashboard.dashboardBannerSubHeading).containsText("Landlord registration number")
+        assertThat(dashboard.dashboardBannerSubHeading).containsText("L-CKG4-JFNZ")
     }
 }
