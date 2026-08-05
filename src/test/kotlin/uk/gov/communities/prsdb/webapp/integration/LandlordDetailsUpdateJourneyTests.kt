cb8ec78530645f1369580db523d3bb65d2131c37
Danielle Dias <danielle.dias@madetech.com>
2026-08-05 11:57:20 +0100

    PDJB-1234 - Update tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDetailsUpdateJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDetailsUpdateJourneyTests.kt
@@ -163,45 +163,5 @@ class LandlordDetailsUpdateJourneyTests : IntegrationTestWithMutableData("data-l
         }
     }
 
-    @Nested
-    inner class OrganisationNameUpdates : NestedIntegrationTestWithMutableData(
-        listOf("data-mockuser-not-landlord.sql", "data-mockuser-org-landlord.sql"),
-    ) {
-        @BeforeEach
-        fun setup() {
-            featureFlagManager.enable(ORGANISATION_LANDLORD_REGISTRATION)
-        }
-
-        @Test
-        fun `An organisation landlord can update organisation name from landlord details and return to details page`(page: Page) {
-            val orgLandlordDetailsPage = navigator.goToOrgLandlordDetails()
-            orgLandlordDetailsPage.clickOrganisationNameChangeLinkAndWait()
-
-            val updateOrgNamePage = assertPageIs(page, OrgNameFormPageUpdateLandlordDetails::class)
-            updateOrgNamePage.submitName("Updated Organisation Name")
-
-            val updatedDetailsPage = assertPageIs(page, OrgLandlordDetailsPage::class)
-            assertThat(updatedDetailsPage.mainContent).containsText("Updated Organisation Name")
-        }
-
-        @Test
-        fun `Organisation name change link opens the organisation name update page`(page: Page) {
-            val orgLandlordDetailsPage = navigator.goToOrgLandlordDetails()
-            orgLandlordDetailsPage.clickOrganisationNameChangeLinkAndWait()
-
-            assertPageIs(page, OrgNameFormPageUpdateLandlordDetails::class)
-        }
-
-        @Test
-        fun `Submitting an empty organisation name on update shows a validation error`(page: Page) {
-            val orgLandlordDetailsPage = navigator.goToOrgLandlordDetails()
-            orgLandlordDetailsPage.clickOrganisationNameChangeLinkAndWait()
-
-            val updateOrgNamePage = assertPageIs(page, OrgNameFormPageUpdateLandlordDetails::class)
-            updateOrgNamePage.submitName("")
-            assertThat(updateOrgNamePage.form.getErrorMessage()).containsText("Enter an organisation name")
-        }
-    }
-
     private fun formatDateOfBirth(date: LocalDate): String = "${date.dayOfMonth} ${date.month} ${date.year}"
 }
