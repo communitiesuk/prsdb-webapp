b7c77beee4a940d1fc0358395512c2fd8cc51ab0
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-08-10 10:47:10 +0100

    PDJB-1237: add reg CYA page tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/updateOrganisationTypeJourneyPages/OrgTypeTrustInterruptionPageUpdateOrganisationType.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/updateOrganisationTypeJourneyPages/OrgTypeTrustInterruptionPageUpdateOrganisationType.kt
@@ -2,14 +2,9 @@ package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateOrga
 
 import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.controllers.UpdateOrganisationTypeController.Companion.UPDATE_ORG_TYPE_ROUTE
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.PostForm
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.InterruptionPage
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.organisationType.OrgTypeTrustInterruptionStep
 
 class OrgTypeTrustInterruptionPageUpdateOrganisationType(
     page: Page,
-) : BasePage(page, "$UPDATE_ORG_TYPE_ROUTE/${OrgTypeTrustInterruptionStep.ROUTE_SEGMENT}") {
-    val form = PostForm(page)
-
-    fun submit() = form.submit()
-}
+) : InterruptionPage(page, "$UPDATE_ORG_TYPE_ROUTE/${OrgTypeTrustInterruptionStep.ROUTE_SEGMENT}")
