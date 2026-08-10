e705fdf3713f88753734f99bfd34402fe2f14016
Tom Hanmer <tom.hanmer@softwire.com>
2026-08-10 17:29:58 +0100

    PDJB-1238: fix tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/summaryModels/OrgLandlordViewModelTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/summaryModels/OrgLandlordViewModelTests.kt
@@ -164,7 +164,12 @@ class OrgLandlordViewModelTests {
                 charityRegisteredWith = CharityRegulator.ENGLAND_AND_WALES,
                 charityNumber = "0123456",
             )
-        val changeableHeadings = listOf("landlordDetails.org.name", "landlordDetails.org.organisationType")
+        val changeableHeadings =
+            listOf(
+                "landlordDetails.org.name",
+                "landlordDetails.org.organisationType",
+                "landlordDetails.org.registeredWithCompaniesHouse",
+            )
 
         val viewModel = OrgLandlordViewModel(landlord)
 
