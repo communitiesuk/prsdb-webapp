f4fab40af92dde3b9917e955e2ac8461d26ca4b8
Tom Hanmer <tom.hanmer@softwire.com>
2026-08-11 16:37:26 +0100

    PDJB-1238: add new test case

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/UpdateCompaniesHouseJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/UpdateCompaniesHouseJourneyTests.kt
@@ -5,16 +5,26 @@ import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.communities.prsdb.webapp.constants.ORGANISATION_LANDLORD_REGISTRATION
+import uk.gov.communities.prsdb.webapp.database.repository.OrganisationLandlordRepository
+import uk.gov.communities.prsdb.webapp.database.repository.OrganisationalLandlordUserRepository
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.OrgLandlordDetailsPage
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage.Companion.assertPageIs
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateCompaniesHousePages.CompaniesHouseUpdateCheckAnswersPage
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateCompaniesHousePages.CompaniesHouseUpdateInterruptionPage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateCompaniesHousePages.OrgCompanyNumberFormPageUpdateCompaniesHouse
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateCompaniesHousePages.OrgIsRegisteredCompanyFormPageUpdateCompaniesHouse
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgGovBodyWhoToProvideStep
 
 @WithOrgLandlordProfile
 class UpdateCompaniesHouseJourneyTests : IntegrationTestWithMutableData("data-local.sql") {
+    @Autowired
+    private lateinit var organisationLandlordRepository: OrganisationLandlordRepository
+
+    @Autowired
+    private lateinit var organisationalLandlordUserRepository: OrganisationalLandlordUserRepository
+
     @BeforeEach
     fun setup() {
         featureFlagManager.enable(ORGANISATION_LANDLORD_REGISTRATION)
@@ -58,5 +68,34 @@ class UpdateCompaniesHouseJourneyTests : IntegrationTestWithMutableData("data-lo
         interruptionPage.submit()
 
         assertTrue(page.url().contains(OrgGovBodyWhoToProvideStep.ROUTE_SEGMENT))
+    }
+
+    @Test
+    fun `Changing the registration answer to yes shows the interruption then asks for the company number`(page: Page) {
+        val landlord =
+            organisationalLandlordUserRepository
+                .findByBaseUser_Id("urn:fdc:gov.uk:2022:ORG01")
+                .single()
+                .organisationalLandlord
+        landlord.companyNumber = null
+        organisationLandlordRepository.save(landlord)
+
+        val orgLandlordDetailsPage = navigator.goToOrgLandlordDetails()
+        orgLandlordDetailsPage.clickCompaniesHouseChangeLinkAndWait()
+
+        val isRegisteredCompanyPage = assertPageIs(page, OrgIsRegisteredCompanyFormPageUpdateCompaniesHouse::class)
+        isRegisteredCompanyPage.submitYes()
+
+        val interruptionPage = assertPageIs(page, CompaniesHouseUpdateInterruptionPage::class)
+        interruptionPage.submit()
+
+        val companyNumberPage = assertPageIs(page, OrgCompanyNumberFormPageUpdateCompaniesHouse::class)
+        companyNumberPage.submitCompanyNumber("87654321")
+
+        val checkAnswersPage = assertPageIs(page, CompaniesHouseUpdateCheckAnswersPage::class)
+        checkAnswersPage.confirmAndSubmit()
+
+        val updatedDetailsPage = assertPageIs(page, OrgLandlordDetailsPage::class)
+        assertThat(updatedDetailsPage.mainContent).containsText("87654321")
     }
 }
