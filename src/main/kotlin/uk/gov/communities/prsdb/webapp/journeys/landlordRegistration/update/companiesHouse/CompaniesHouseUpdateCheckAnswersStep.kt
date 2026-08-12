b812e1b41419a820017217145189c429c28d78d3
Tom Hanmer <tom.hanmer@softwire.com>
2026-08-12 17:18:49 +0100

    PDJB-1464: basic cya page

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/companiesHouse/CompaniesHouseUpdateCheckAnswersStep.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/companiesHouse/CompaniesHouseUpdateCheckAnswersStep.kt
@@ -1,33 +1,150 @@
 package uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.companiesHouse
 
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
-import uk.gov.communities.prsdb.webapp.journeys.AbstractRequestableStepConfig
-import uk.gov.communities.prsdb.webapp.journeys.JourneyState
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStep.RequestableStep
-import uk.gov.communities.prsdb.webapp.journeys.shared.Complete
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.NoInputFormModel
+import uk.gov.communities.prsdb.webapp.constants.enums.GoverningBodyMemberType
+import uk.gov.communities.prsdb.webapp.exceptions.NotNullFormModelValueIsNullException.Companion.notNullValue
+import uk.gov.communities.prsdb.webapp.journeys.Destination
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStepConfig
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgCompanyNumberFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgIsRegisteredCompanyFormModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryCardActionViewModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryCardViewModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryListRowViewModel
+import uk.gov.communities.prsdb.webapp.services.LandlordService
 
-// TODO PDJB-1464: replace this placeholder with the real check-your-answers page (summary rows, change links, email).
 @JourneyFrameworkComponent
-class CompaniesHouseUpdateCheckAnswersStepConfig :
-    AbstractRequestableStepConfig<Complete, NoInputFormModel, JourneyState>() {
-    override val formModelClass = NoInputFormModel::class
+class CompaniesHouseUpdateCheckAnswersStepConfig(
+    private val landlordService: LandlordService,
+) : AbstractCheckYourAnswersStepConfig<UpdateCompaniesHouseJourneyState>() {
+    override fun chooseTemplate(state: UpdateCompaniesHouseJourneyState) = "forms/companiesHouseUpdateCheckAnswersForm"
 
-    override fun getStepSpecificContent(state: JourneyState) =
-        mapOf(
+    override fun getStepSpecificContent(state: UpdateCompaniesHouseJourneyState): Map<String, Any?> {
+        val registeredWithCompaniesHouse = isRegisteredWithCompaniesHouse(state)
+        return mapOf(
             "title" to "landlordDetails.update.title",
-            "todoComment" to "TODO PDJB-1464: Companies House update check your answers",
+            "showWarning" to true,
+            "submitButtonText" to "forms.buttons.confirmAndSubmitUpdate",
+            "companyVariant" to registeredWithCompaniesHouse,
+            "summaryListData" to getCompanyDetailsRows(state, registeredWithCompaniesHouse),
+            "governingBodyMemberCards" to
+                if (registeredWithCompaniesHouse) emptyList() else getGovBodyMemberCards(state),
         )
+    }
 
-    override fun chooseTemplate(state: JourneyState) = "forms/todo"
+    override fun afterStepDataIsAdded(state: UpdateCompaniesHouseJourneyState) {
+        if (isRegisteredWithCompaniesHouse(state)) {
+            val companyNumber =
+                state.updateCompaniesHouseTask.orgCompanyNumberStep.formModel
+                    .notNullValue(OrgCompanyNumberFormModel::companyNumber)
+            landlordService.updateOrganisationalLandlordToRegisteredCompany(companyNumber)
+        } else {
+            landlordService.updateOrganisationalLandlordToNonRegisteredCompany(
+                state.updateCompaniesHouseTask.governingBodyMembersMap?.values?.toList().orEmpty(),
+            )
+        }
+    }
 
-    override fun mode(state: JourneyState) = getFormModelFromStateOrNull(state)?.let { Complete.COMPLETE }
+    private fun isRegisteredWithCompaniesHouse(state: UpdateCompaniesHouseJourneyState): Boolean =
+        state.updateCompaniesHouseTask.orgIsRegisteredCompanyStep.formModel
+            .notNullValue(OrgIsRegisteredCompanyFormModel::companiesHouse)
+
+    private fun getCompanyDetailsRows(
+        state: UpdateCompaniesHouseJourneyState,
+        registeredWithCompaniesHouse: Boolean,
+    ): List<SummaryListRowViewModel> {
+        val task = state.updateCompaniesHouseTask
+        return buildList {
+            add(
+                SummaryListRowViewModel.forCheckYourAnswersPage(
+                    "landlordDetails.org.registeredWithCompaniesHouse",
+                    registeredWithCompaniesHouse,
+                    Destination.VisitableStep(
+                        task.orgIsRegisteredCompanyStep,
+                        state.getCyaJourneyId(task.orgIsRegisteredCompanyStep),
+                    ),
+                ),
+            )
+            if (registeredWithCompaniesHouse) {
+                add(
+                    SummaryListRowViewModel.forCheckYourAnswersPage(
+                        "landlordDetails.org.companyNumber",
+                        task.orgCompanyNumberStep.formModel.notNullValue(OrgCompanyNumberFormModel::companyNumber),
+                        Destination.VisitableStep(
+                            task.orgCompanyNumberStep,
+                            state.getCyaJourneyId(task.orgCompanyNumberStep),
+                        ),
+                    ),
+                )
+            }
+        }
+    }
+
+    private fun getGovBodyMemberCards(state: UpdateCompaniesHouseJourneyState): List<SummaryCardViewModel> {
+        val task = state.updateCompaniesHouseTask
+        val members = task.governingBodyMembersMap ?: emptyMap()
+        return members
+            .toList()
+            .sortedBy { it.first }
+            .mapIndexed { displayIndex, (_, member) ->
+                SummaryCardViewModel(
+                    title = memberCardTitleKey(member.type),
+                    cardNumber = (displayIndex + 1).toString(),
+                    summaryList =
+                        listOf(
+                            SummaryListRowViewModel.forCheckYourAnswersPage(
+                                "landlordDetails.org.governingBody.role",
+                                memberRoleKey(member.type),
+                                Destination.Nowhere(),
+                            ),
+                            SummaryListRowViewModel.forCheckYourAnswersPage(
+                                "landlordDetails.org.governingBody.memberName",
+                                member.name,
+                                Destination.Nowhere(),
+                            ),
+                            SummaryListRowViewModel.forCheckYourAnswersPage(
+                                "landlordDetails.org.governingBody.memberDateOfBirth",
+                                member.dateOfBirth,
+                                Destination.Nowhere(),
+                            ),
+                            SummaryListRowViewModel.forCheckYourAnswersPage(
+                                "landlordDetails.org.governingBody.memberAddress",
+                                member.address.toMultiLineAddress().split("\n"),
+                                Destination.Nowhere(),
+                            ),
+                        ),
+                    actions =
+                        SummaryCardActionViewModel.changeAction(
+                            Destination.VisitableStep(
+                                task.orgGovBodyMembersTask.orgGovBodyMemberListStep,
+                                state.getCyaJourneyId(task.orgGovBodyMembersTask.orgGovBodyMemberListStep),
+                            ),
+                        ),
+                )
+            }
+    }
+
+    private fun memberCardTitleKey(type: GoverningBodyMemberType) =
+        when (type) {
+            GoverningBodyMemberType.DIRECTOR -> "landlordDetails.org.governingBody.memberCardTitle.director"
+            GoverningBodyMemberType.TRUSTEE -> "landlordDetails.org.governingBody.memberCardTitle.trustee"
+            GoverningBodyMemberType.PARTNER -> "landlordDetails.org.governingBody.memberCardTitle.partner"
+            GoverningBodyMemberType.OTHER -> "landlordDetails.org.governingBody.memberCardTitle.other"
+        }
+
+    private fun memberRoleKey(type: GoverningBodyMemberType) =
+        when (type) {
+            GoverningBodyMemberType.DIRECTOR -> "landlordDetails.org.governingBody.type.director"
+            GoverningBodyMemberType.TRUSTEE -> "landlordDetails.org.governingBody.type.trustee"
+            GoverningBodyMemberType.PARTNER -> "landlordDetails.org.governingBody.type.partner"
+            GoverningBodyMemberType.OTHER -> "landlordDetails.org.governingBody.type.other"
+        }
 }
 
 @JourneyFrameworkComponent
 final class CompaniesHouseUpdateCheckAnswersStep(
     stepConfig: CompaniesHouseUpdateCheckAnswersStepConfig,
-) : RequestableStep<Complete, NoInputFormModel, JourneyState>(stepConfig) {
+) : AbstractCheckYourAnswersStep<UpdateCompaniesHouseJourneyState>(stepConfig) {
     companion object {
         const val ROUTE_SEGMENT = "check-answers"
     }
--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/companiesHouse/UpdateCompaniesHouseJourneyFactory.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/companiesHouse/UpdateCompaniesHouseJourneyFactory.kt
@@ -1,16 +1,24 @@
 package uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.companiesHouse
 
+import kotlinx.datetime.Instant
 import org.springframework.beans.factory.ObjectFactory
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
 import uk.gov.communities.prsdb.webapp.controllers.LandlordDetailsController.Companion.LANDLORD_DETAILS_FOR_LANDLORD_ROUTE
 import uk.gov.communities.prsdb.webapp.journeys.AbstractJourneyState
 import uk.gov.communities.prsdb.webapp.journeys.Destination
-import uk.gov.communities.prsdb.webapp.journeys.JourneyState
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
 import uk.gov.communities.prsdb.webapp.journeys.StepLifecycleOrchestrator
 import uk.gov.communities.prsdb.webapp.journeys.builders.JourneyBuilder.Companion.journey
 import uk.gov.communities.prsdb.webapp.journeys.isComplete
+import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.states.OrgGovBodyMembersDependencies
+import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgCompanyNumberStep
+import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgGovBodyMemberListStep
+import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgIsRegisteredCompanyStep
+import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.FinishCyaJourneyStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.states.CheckYourAnswersJourneyState
+import uk.gov.communities.prsdb.webapp.journeys.shared.states.CheckYourAnswersJourneyState.Companion.checkAnswerStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.states.CheckYourAnswersJourneyState.Companion.checkAnswerTask
 import java.security.Principal
 
 @PrsdbWebService
@@ -20,26 +28,60 @@ class UpdateCompaniesHouseJourneyFactory(
     fun createJourneySteps(): Map<String, StepLifecycleOrchestrator> {
         val state = stateFactory.getObject()
 
-        return journey(state) {
+        return if (state.checkingAnswersFor == null) {
+            mainJourneyMap(state)
+        } else {
+            checkYourAnswersJourneyMap(state)
+        }
+    }
+
+    private fun mainJourneyMap(state: UpdateCompaniesHouseJourney): Map<String, StepLifecycleOrchestrator> =
+        journey(state) {
             unreachableStepUrl { LANDLORD_DETAILS_FOR_LANDLORD_ROUTE }
             configure { withAdditionalContentProperty { "title" to "landlordDetails.update.title" } }
 
             task(journey.updateCompaniesHouseTask) {
                 initialStep()
                 backDestination { Destination.ExternalUrl(LANDLORD_DETAILS_FOR_LANDLORD_ROUTE) }
-                nextStep { journey.checkAnswersStep }
+                nextStep { journey.cyaStep }
             }
-            step(journey.checkAnswersStep) {
+            step(journey.cyaStep) {
                 routeSegment(CompaniesHouseUpdateCheckAnswersStep.ROUTE_SEGMENT)
                 parents { journey.updateCompaniesHouseTask.isComplete() }
-                nextStep { journey.completeCompaniesHouseUpdateStep }
-            }
-            step(journey.completeCompaniesHouseUpdateStep) {
-                parents { journey.checkAnswersStep.isComplete() }
                 nextUrl { LANDLORD_DETAILS_FOR_LANDLORD_ROUTE }
             }
         }
-    }
+
+    private fun checkYourAnswersJourneyMap(state: UpdateCompaniesHouseJourney): Map<String, StepLifecycleOrchestrator> =
+        journey(state) {
+            configure { withAdditionalContentProperty { "title" to "landlordDetails.update.title" } }
+            configureFirst { backDestination { journey.returnToCyaPageDestination } }
+            unreachableStepDestination { journey.returnToCyaPageDestination }
+
+            when (state.checkingAnswersFor) {
+                OrgIsRegisteredCompanyStep.ROUTE_SEGMENT -> {
+                    checkAnswerTask(journey.updateCompaniesHouseTask)
+                }
+
+                OrgCompanyNumberStep.ROUTE_SEGMENT -> {
+                    checkAnswerStep(journey.updateCompaniesHouseTask.orgCompanyNumberStep, OrgCompanyNumberStep.ROUTE_SEGMENT)
+                }
+
+                OrgGovBodyMemberListStep.ROUTE_SEGMENT -> {
+                    checkAnswerTask(
+                        journey.updateCompaniesHouseTask.orgGovBodyMembersTask,
+                        { OrgGovBodyMembersDependencies(listState = journey.updateCompaniesHouseTask) },
+                    )
+                    configureStep(journey.updateCompaniesHouseTask.orgGovBodyMembersTask.orgGovBodyMemberListStep) {
+                        backDestination { journey.returnToCyaPageDestination }
+                    }
+                }
+            }
+            step(journey.finishCyaStep) {
+                initialStep()
+                nextDestination { Destination.Nowhere() }
+            }
+        }
 
     fun initializeJourneyState(user: Principal): String = stateFactory.getObject().initializeOrRestoreState(user)
 }
@@ -47,12 +89,18 @@ class UpdateCompaniesHouseJourneyFactory(
 @JourneyFrameworkComponent
 class UpdateCompaniesHouseJourney(
     override val updateCompaniesHouseTask: UpdateCompaniesHouseTask,
-    override val checkAnswersStep: CompaniesHouseUpdateCheckAnswersStep,
-    override val completeCompaniesHouseUpdateStep: CompleteCompaniesHouseUpdateStep,
+    override val cyaStep: CompaniesHouseUpdateCheckAnswersStep,
+    override val finishCyaStep: FinishCyaJourneyStep,
+    override val stateFactory: ObjectFactory<UpdateCompaniesHouseJourneyState>,
     journeyStateService: JourneyStateService,
     private val journeyName: String = "companies-house",
 ) : AbstractJourneyState(journeyStateService),
     UpdateCompaniesHouseJourneyState {
+    override var cyaJourneys: Map<String, String> = mapOf()
+    override var checkingAnswersFor: String? by delegateProvider.nullableDelegate("checkingAnswersFor")
+    override var originalJourneyUpdated: Instant? by delegateProvider.nullableDelegate("originalJourneyUpdated")
+    override var cyaUrlPath: String? by delegateProvider.nullableDelegate("cyaRouteSegment")
+
     override fun generateJourneyId(seed: Any?): String {
         val user: Principal? = seed as? Principal
         return super<AbstractJourneyState>.generateJourneyId(
@@ -61,8 +109,8 @@ class UpdateCompaniesHouseJourney(
     }
 }
 
-interface UpdateCompaniesHouseJourneyState : JourneyState {
+interface UpdateCompaniesHouseJourneyState : CheckYourAnswersJourneyState {
     val updateCompaniesHouseTask: UpdateCompaniesHouseTask
-    val checkAnswersStep: CompaniesHouseUpdateCheckAnswersStep
-    val completeCompaniesHouseUpdateStep: CompleteCompaniesHouseUpdateStep
+    override val cyaStep: CompaniesHouseUpdateCheckAnswersStep
+    override val finishCyaStep: FinishCyaJourneyStep
 }
