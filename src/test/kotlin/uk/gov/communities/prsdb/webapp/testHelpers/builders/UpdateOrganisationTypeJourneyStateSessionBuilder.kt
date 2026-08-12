72ad609f11fa533c6972f79d92ae22454a3d5ba3
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-08-12 16:31:07 +0100

    PDJB-1466: share org type session building methods

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/UpdateOrganisationTypeJourneyStateSessionBuilder.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/testHelpers/builders/UpdateOrganisationTypeJourneyStateSessionBuilder.kt
@@ -1,17 +1,10 @@
 package uk.gov.communities.prsdb.webapp.testHelpers.builders
 
 import uk.gov.communities.prsdb.webapp.constants.enums.OrgType
-import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgTypeStep
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgTypeFormModel
 
 class UpdateOrganisationTypeJourneyStateSessionBuilder :
-    JourneyStateSessionBuilder<UpdateOrganisationTypeJourneyStateSessionBuilder>() {
-    fun withOrgType(orgTypes: List<OrgType>): UpdateOrganisationTypeJourneyStateSessionBuilder {
-        val formModel = OrgTypeFormModel().apply { this.orgTypes = orgTypes.map { it.name }.toMutableList() }
-        withSubmittedValue(OrgTypeStep.ROUTE_SEGMENT, formModel)
-        return self()
-    }
-
+    JourneyStateSessionBuilder<UpdateOrganisationTypeJourneyStateSessionBuilder>(),
+    OrgTypeStateBuilder<UpdateOrganisationTypeJourneyStateSessionBuilder> {
     companion object {
         fun beforeTrustInterruption(orgTypes: List<OrgType>) = UpdateOrganisationTypeJourneyStateSessionBuilder().withOrgType(orgTypes)
     }
