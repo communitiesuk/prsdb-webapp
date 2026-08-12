651333e8124ce6d76e52435f6fdaa7a202873742
Bill Haigh <Bill.Haigh@softwire.com>
2026-08-12 15:10:57 +0100

    PDJB-1239: Share charity page objects between the charity journeys
    The is-registered-charity, charity-registered-with and charity-number pages are
    identical across the landlord registration, joint landlord invitation and update
    landlord details journeys apart from their URL, so move them to abstract base
    pages taking a url segment, following the existing OrgNameFormBasePage and
    OrgTypeFormPage precedent.

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/updateLandlordDetailsPages/OrgCharityNumberEnglandAndWalesFormPageUpdateLandlordDetails.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/updateLandlordDetailsPages/OrgCharityNumberEnglandAndWalesFormPageUpdateLandlordDetails.kt
@@ -2,24 +2,9 @@ package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateLand
 
 import com.microsoft.playwright.Page
 import uk.gov.communities.prsdb.webapp.controllers.UpdateOrganisationLandlordCharityController.Companion.UPDATE_ORG_CHARITY_ROUTE
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.PostForm
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.TextInput
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.OrgCharityNumberFormBasePage
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgCharityNumberEnglandAndWalesStep
 
 class OrgCharityNumberEnglandAndWalesFormPageUpdateLandlordDetails(
     page: Page,
-) : BasePage(page, "$UPDATE_ORG_CHARITY_ROUTE/${OrgCharityNumberEnglandAndWalesStep.ROUTE_SEGMENT}") {
-    val form = CharityNumberForm(page)
-
-    fun submitCharityNumber(charityNumber: String) {
-        form.charityNumberInput.fill(charityNumber)
-        form.submit()
-    }
-
-    class CharityNumberForm(
-        page: Page,
-    ) : PostForm(page) {
-        val charityNumberInput = TextInput.textByFieldName(locator, "charityNumber")
-    }
-}
+) : OrgCharityNumberFormBasePage(page, "$UPDATE_ORG_CHARITY_ROUTE/${OrgCharityNumberEnglandAndWalesStep.ROUTE_SEGMENT}")
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/updateLandlordDetailsPages/OrgCharityRegisteredWithFormPageUpdateLandlordDetails.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/updateLandlordDetailsPages/OrgCharityRegisteredWithFormPageUpdateLandlordDetails.kt
@@ -1,26 +1,10 @@
 package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateLandlordDetailsPages
 
 import com.microsoft.playwright.Page
-import uk.gov.communities.prsdb.webapp.constants.enums.CharityRegulator
 import uk.gov.communities.prsdb.webapp.controllers.UpdateOrganisationLandlordCharityController.Companion.UPDATE_ORG_CHARITY_ROUTE
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.PostForm
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Radios
-import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.OrgCharityRegisteredWithFormBasePage
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.OrgCharityRegisteredWithStep
 
 class OrgCharityRegisteredWithFormPageUpdateLandlordDetails(
     page: Page,
-) : BasePage(page, "$UPDATE_ORG_CHARITY_ROUTE/${OrgCharityRegisteredWithStep.ROUTE_SEGMENT}") {
-    val form = CharityRegisteredWithForm(page)
-
-    fun submitCharityRegisteredWith(charityRegulator: CharityRegulator) {
-        form.charityRegisteredWithRadios.selectValue(charityRegulator.name)
-        form.submit()
-    }
-
-    class CharityRegisteredWithForm(
-        page: Page,
-    ) : PostForm(page) {
-        val charityRegisteredWithRadios = Radios(locator)
-    }
-}
+) : OrgCharityRegisteredWithFormBasePage(page, "$UPDATE_ORG_CHARITY_ROUTE/${OrgCharityRegisteredWithStep.ROUTE_SEGMENT}")
