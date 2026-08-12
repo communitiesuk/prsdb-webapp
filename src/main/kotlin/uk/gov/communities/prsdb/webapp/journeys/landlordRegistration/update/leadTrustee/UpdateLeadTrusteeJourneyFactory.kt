1c76c0f204921db62a2b655b54dcd622bf2ceb06
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-08-12 15:10:32 +0100

    PDJB-1470: add journey & template

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/leadTrustee/UpdateLeadTrusteeJourneyFactory.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/leadTrustee/UpdateLeadTrusteeJourneyFactory.kt
@@ -1,11 +1,12 @@
 package uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.leadTrustee
 
+import kotlinx.datetime.Instant
 import org.springframework.beans.factory.ObjectFactory
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
 import uk.gov.communities.prsdb.webapp.controllers.LandlordDetailsController.Companion.LANDLORD_DETAILS_FOR_LANDLORD_ROUTE
 import uk.gov.communities.prsdb.webapp.journeys.AbstractJourneyState
-import uk.gov.communities.prsdb.webapp.journeys.JourneyState
+import uk.gov.communities.prsdb.webapp.journeys.Destination
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStateService
 import uk.gov.communities.prsdb.webapp.journeys.StepLifecycleOrchestrator
 import uk.gov.communities.prsdb.webapp.journeys.builders.JourneyBuilder.Companion.journey
@@ -16,6 +17,11 @@ import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.LeadTrusteeNameStep
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.stepConfig.LeadTrusteePhoneStep
 import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.tasks.LeadTrusteeTask
+import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.FinishCyaJourneyStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.states.CheckYourAnswersJourneyState
+import uk.gov.communities.prsdb.webapp.journeys.shared.states.CheckYourAnswersJourneyState.Companion.checkAnswerStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.states.CheckYourAnswersJourneyState.Companion.checkAnswerTask
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.LookupAddressStep
 import uk.gov.communities.prsdb.webapp.journeys.shared.tasks.TrusteeAddressTask
 import java.security.Principal
 
@@ -26,7 +32,15 @@ class UpdateLeadTrusteeJourneyFactory(
     fun createJourneySteps(): Map<String, StepLifecycleOrchestrator> {
         val state = stateFactory.getObject()
 
-        return journey(state) {
+        return if (state.checkingAnswersFor == null) {
+            mainJourneyMap(state)
+        } else {
+            checkYourAnswersJourneyMap(state)
+        }
+    }
+
+    private fun mainJourneyMap(state: UpdateLeadTrusteeJourney): Map<String, StepLifecycleOrchestrator> =
+        journey(state) {
             unreachableStepUrl { LANDLORD_DETAILS_FOR_LANDLORD_ROUTE }
             task(journey.leadTrusteeTask) {
                 initialStep()
@@ -46,7 +60,24 @@ class UpdateLeadTrusteeJourneyFactory(
                 nextUrl { LANDLORD_DETAILS_FOR_LANDLORD_ROUTE + "#organisation-contacts" }
             }
         }
-    }
+
+    private fun checkYourAnswersJourneyMap(state: UpdateLeadTrusteeJourney): Map<String, StepLifecycleOrchestrator> =
+        journey(state) {
+            configureFirst { backDestination { journey.returnToCyaPageDestination } }
+            unreachableStepUrl { LANDLORD_DETAILS_FOR_LANDLORD_ROUTE }
+            configure {
+                withAdditionalContentProperty { "title" to "landlordDetails.update.title" }
+            }
+            checkAnswerStep(journey.leadTrusteeTask.leadTrusteeNameStep, LeadTrusteeNameStep.ROUTE_SEGMENT)
+            checkAnswerStep(journey.leadTrusteeTask.leadTrusteeDobStep, LeadTrusteeDobStep.ROUTE_SEGMENT)
+            checkAnswerStep(journey.leadTrusteeTask.leadTrusteeEmailStep, LeadTrusteeEmailStep.ROUTE_SEGMENT)
+            checkAnswerStep(journey.leadTrusteeTask.leadTrusteePhoneStep, LeadTrusteePhoneStep.ROUTE_SEGMENT)
+            checkAnswerTask(journey.leadTrusteeTask.trusteeAddressTask, LookupAddressStep.ROUTE_SEGMENT)
+            step(journey.finishCyaStep) {
+                parents { journey.leadTrusteeTask.trusteeAddressTask.isComplete() }
+                nextDestination { Destination.Nowhere() }
+            }
+        }
 
     fun initializeJourneyState(user: Principal): String = stateFactory.getObject().initializeOrRestoreState(user)
 }
@@ -61,10 +92,17 @@ class UpdateLeadTrusteeJourney(
     override val leadTrusteeTask: LeadTrusteeTask,
     override val cyaStep: UpdateLeadTrusteeCyaStep,
     override val completeLeadTrusteeUpdateStep: CompleteLeadTrusteeUpdateStep,
+    override val finishCyaStep: FinishCyaJourneyStep,
+    override val stateFactory: ObjectFactory<UpdateLeadTrusteeJourneyState>,
     journeyStateService: JourneyStateService,
     private val journeyName: String = "lead-trustee",
 ) : AbstractJourneyState(journeyStateService),
     UpdateLeadTrusteeJourneyState {
+    override var cyaJourneys: Map<String, String> = mapOf()
+    override var checkingAnswersFor: String? by delegateProvider.nullableDelegate("checkingAnswersFor")
+    override var cyaUrlPath: String? by delegateProvider.nullableDelegate("cyaRouteSegment")
+    override var originalJourneyUpdated: Instant? by delegateProvider.nullableDelegate("originalJourneyUpdated")
+
     override fun generateJourneyId(seed: Any?): String {
         val user: Principal? = seed as? Principal
 
@@ -76,8 +114,8 @@ class UpdateLeadTrusteeJourney(
 
 interface UpdateLeadTrusteeJourneyState :
     LeadTrusteeState,
-    JourneyState {
+    CheckYourAnswersJourneyState {
     val leadTrusteeTask: LeadTrusteeTask
-    val cyaStep: UpdateLeadTrusteeCyaStep
+    override val cyaStep: UpdateLeadTrusteeCyaStep
     val completeLeadTrusteeUpdateStep: CompleteLeadTrusteeUpdateStep
 }
