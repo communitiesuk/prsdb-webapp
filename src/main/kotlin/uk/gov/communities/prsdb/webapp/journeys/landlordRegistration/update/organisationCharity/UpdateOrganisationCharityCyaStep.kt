f80d734ad2bdec660248897dda0bd70725be46ee
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-08-14 17:58:11 +0100

    PDJB-1463: add charity CYA page

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/organisationCharity/UpdateOrganisationCharityCyaStep.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/organisationCharity/UpdateOrganisationCharityCyaStep.kt
@@ -1,28 +1,177 @@
 package uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.organisationCharity
 
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
-import uk.gov.communities.prsdb.webapp.journeys.AbstractRequestableStepConfig
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStep.RequestableStep
-import uk.gov.communities.prsdb.webapp.journeys.shared.Complete
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.NoInputFormModel
+import uk.gov.communities.prsdb.webapp.constants.enums.CharityRegulator
+import uk.gov.communities.prsdb.webapp.exceptions.NotNullFormModelValueIsNullException.Companion.notNullValue
+import uk.gov.communities.prsdb.webapp.exceptions.PrsdbWebException
+import uk.gov.communities.prsdb.webapp.journeys.Destination
+import uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.tasks.OrgCharityTask
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStepConfig
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.CharityRegisteredWithFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgCharityNumberEnglandAndWalesFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgCharityNumberNorthernIrelandFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgCharityNumberScotlandFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.OrgIsRegisteredCharityFormModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.SectionHeaderViewModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryListRowViewModel
+import uk.gov.communities.prsdb.webapp.services.LandlordService
 
 @JourneyFrameworkComponent
-class UpdateOrganisationCharityCyaStepConfig :
-    AbstractRequestableStepConfig<Complete, NoInputFormModel, UpdateOrganisationCharityJourneyState>() {
-    override val formModelClass = NoInputFormModel::class
+class UpdateOrganisationCharityCyaStepConfig(
+    private val landlordService: LandlordService,
+) : AbstractCheckYourAnswersStepConfig<UpdateOrganisationCharityJourneyState>() {
+    override fun getStepSpecificContent(state: UpdateOrganisationCharityJourneyState): Map<String, Any?> =
+        mapOf(
+            "title" to "landlordDetails.update.title",
+            "sectionHeaderInfo" to SectionHeaderViewModel("landlordDetails.title", 0, 0, useNumbering = false),
+            "showWarning" to true,
+            "submitButtonText" to "forms.buttons.confirmAndSubmitUpdate",
+            "summaryName" to "forms.update.checkCharity.summaryName",
+            "summaryListData" to buildSummaryRows(state),
+        )
 
-    override fun getStepSpecificContent(state: UpdateOrganisationCharityJourneyState) =
-        mapOf("todoComment" to "TODO: PDJB-1463 - Organisation charity check your answers page")
+    override fun afterStepDataIsAdded(state: UpdateOrganisationCharityJourneyState) {
+        val task = state.charityTask
 
-    override fun chooseTemplate(state: UpdateOrganisationCharityJourneyState) = "forms/todo"
+        if (!task.orgIsRegisteredCharityStep.formModel.notNullValue(OrgIsRegisteredCharityFormModel::charity)) {
+            landlordService.updateOrganisationLandlordAsNotARegisteredCharity()
+            return
+        }
 
-    override fun mode(state: UpdateOrganisationCharityJourneyState) = getFormModelFromStateOrNull(state)?.let { Complete.COMPLETE }
+        val charityRegisteredWith = task.getCharityRegisteredWith()
+        val charityNumber = task.getCharityNumber(charityRegisteredWith)
+
+        if (charityNumber == null) {
+            landlordService.updateOrganisationLandlordAsRegisteredCharityWithNoRegulator()
+        } else {
+            landlordService.updateOrganisationLandlordCharityRegistration(charityRegisteredWith, charityNumber)
+        }
+    }
+
+    override fun resolveNextDestination(
+        state: UpdateOrganisationCharityJourneyState,
+        defaultDestination: Destination,
+    ): Destination {
+        state.deleteJourney()
+        return defaultDestination
+    }
+
+    private fun buildSummaryRows(state: UpdateOrganisationCharityJourneyState): List<SummaryListRowViewModel> =
+        buildList {
+            val task = state.charityTask
+
+            val isRegisteredCharity =
+                task.orgIsRegisteredCharityStep.formModel.notNullValue(OrgIsRegisteredCharityFormModel::charity)
+            add(
+                SummaryListRowViewModel.forCheckYourAnswersPage(
+                    "landlordDetails.org.registeredCharity",
+                    isRegisteredCharity,
+                    Destination.VisitableStep(
+                        task.orgIsRegisteredCharityStep,
+                        state.getCyaJourneyId(task.orgIsRegisteredCharityStep),
+                    ),
+                ),
+            )
+
+            if (isRegisteredCharity) {
+                val charityRegulator = task.orgCharityRegisteredWithStep.formModel.charityRegisteredWith
+                if (charityRegulator != null) {
+                    add(
+                        SummaryListRowViewModel.forCheckYourAnswersPage(
+                            "landlordDetails.org.charityCommission",
+                            charityRegulator,
+                            Destination.VisitableStep(
+                                task.orgCharityRegisteredWithStep,
+                                state.getCyaJourneyId(task.orgCharityRegisteredWithStep),
+                            ),
+                        ),
+                    )
+
+                    if (charityRegulator != CharityRegulator.NONE) {
+                        add(charityNumberRow(state, task, charityRegulator))
+                    }
+                }
+            }
+        }
+
+    private fun charityNumberRow(
+        state: UpdateOrganisationCharityJourneyState,
+        task: OrgCharityTask,
+        regulator: CharityRegulator,
+    ): SummaryListRowViewModel {
+        val headingKey = "landlordDetails.org.charityNumber"
+        return when (regulator) {
+            CharityRegulator.ENGLAND_AND_WALES -> {
+                SummaryListRowViewModel.forCheckYourAnswersPage(
+                    headingKey,
+                    task.orgCharityNumberEnglandAndWalesStep.formModel.notNullValue(
+                        OrgCharityNumberEnglandAndWalesFormModel::charityNumber,
+                    ),
+                    Destination.VisitableStep(
+                        task.orgCharityNumberEnglandAndWalesStep,
+                        state.getCyaJourneyId(task.orgCharityNumberEnglandAndWalesStep),
+                    ),
+                )
+            }
+
+            CharityRegulator.NORTHERN_IRELAND -> {
+                SummaryListRowViewModel.forCheckYourAnswersPage(
+                    headingKey,
+                    task.orgCharityNumberNorthernIrelandStep.formModel.notNullValue(
+                        OrgCharityNumberNorthernIrelandFormModel::charityNumber,
+                    ),
+                    Destination.VisitableStep(
+                        task.orgCharityNumberNorthernIrelandStep,
+                        state.getCyaJourneyId(task.orgCharityNumberNorthernIrelandStep),
+                    ),
+                )
+            }
+
+            CharityRegulator.SCOTLAND -> {
+                SummaryListRowViewModel.forCheckYourAnswersPage(
+                    headingKey,
+                    task.orgCharityNumberScotlandStep.formModel.notNullValue(OrgCharityNumberScotlandFormModel::charityNumber),
+                    Destination.VisitableStep(
+                        task.orgCharityNumberScotlandStep,
+                        state.getCyaJourneyId(task.orgCharityNumberScotlandStep),
+                    ),
+                )
+            }
+
+            CharityRegulator.NONE -> {
+                throw PrsdbWebException("charityNumberRow should only be called for a regulator that issues a charity number")
+            }
+        }
+    }
+
+    private fun OrgCharityTask.getCharityRegisteredWith() =
+        orgCharityRegisteredWithStep.formModel.notNullValue(CharityRegisteredWithFormModel::charityRegisteredWith)
+
+    private fun OrgCharityTask.getCharityNumber(charityRegisteredWith: CharityRegulator) =
+        when (charityRegisteredWith) {
+            CharityRegulator.ENGLAND_AND_WALES -> {
+                orgCharityNumberEnglandAndWalesStep.formModel.notNullValue(OrgCharityNumberEnglandAndWalesFormModel::charityNumber)
+            }
+
+            CharityRegulator.NORTHERN_IRELAND -> {
+                orgCharityNumberNorthernIrelandStep.formModel.notNullValue(OrgCharityNumberNorthernIrelandFormModel::charityNumber)
+            }
+
+            CharityRegulator.SCOTLAND -> {
+                orgCharityNumberScotlandStep.formModel.notNullValue(OrgCharityNumberScotlandFormModel::charityNumber)
+            }
+
+            CharityRegulator.NONE -> {
+                null
+            }
+        }
 }
 
 @JourneyFrameworkComponent
 final class UpdateOrganisationCharityCyaStep(
     stepConfig: UpdateOrganisationCharityCyaStepConfig,
-) : RequestableStep<Complete, NoInputFormModel, UpdateOrganisationCharityJourneyState>(stepConfig) {
+) : AbstractCheckYourAnswersStep<UpdateOrganisationCharityJourneyState>(stepConfig) {
     companion object {
         const val ROUTE_SEGMENT = "organisation-charity-check-your-answers"
     }
