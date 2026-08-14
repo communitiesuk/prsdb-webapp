8a6d05a00028371769cf653d3b25ed81dd9cd938
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-08-14 15:56:33 +0100

    PDJB-1444: revert changes to Address.kt

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/Address.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/database/entity/Address.kt
@@ -78,7 +78,25 @@ class Address() : ModifiableAuditableEntity() {
 
     fun getSelectedAddress(): String = if (uprn == null) MANUAL_ADDRESS_CHOSEN else singleLineAddress
 
-    fun toMultiLineAddress(): String = AddressDataModel.fromAddress(this).toMultiLineAddress()
+    fun toMultiLineAddress(): String =
+        if (hasAddressComponents()) {
+            buildMultiLineAddressFromComponents()
+        } else {
+            singleLineAddress.replace(", ", "\n")
+        }
+
+    private fun hasAddressComponents(): Boolean = streetName != null || buildingName != null || buildingNumber != null
+
+    private fun buildMultiLineAddressFromComponents(): String =
+        listOfNotNull(
+            organisation,
+            subBuilding,
+            listOfNotNull(buildingNumber, streetName).joinToString(" ").ifBlank { null },
+            buildingName,
+            locality,
+            townName,
+            postcode,
+        ).joinToString("\n")
 
     companion object {
         const val SINGLE_LINE_ADDRESS_LENGTH = 1000
