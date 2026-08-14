a87f4a8dc52f1279729b9688130490821a55e16d
Samuel Young <samuel.young@softwire.com>
2026-08-14 16:36:48 +0000

    PDJB-1470: Org update lead trustee cya (#1740)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/leadTrustee/UpdateLeadTrusteeCyaStep.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/landlordRegistration/update/leadTrustee/UpdateLeadTrusteeCyaStep.kt
@@ -1,27 +1,91 @@
 package uk.gov.communities.prsdb.webapp.journeys.landlordRegistration.update.leadTrustee
 
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
-import uk.gov.communities.prsdb.webapp.journeys.AbstractRequestableStepConfig
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStep.RequestableStep
-import uk.gov.communities.prsdb.webapp.journeys.shared.Complete
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.NoInputFormModel
+import uk.gov.communities.prsdb.webapp.exceptions.NotNullFormModelValueIsNullException.Companion.notNullValue
+import uk.gov.communities.prsdb.webapp.journeys.Destination
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStep
+import uk.gov.communities.prsdb.webapp.journeys.shared.stepConfig.AbstractCheckYourAnswersStepConfig
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.LeadTrusteeEmailFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.LeadTrusteeNameFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.LeadTrusteePhoneFormModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryCardViewModel
+import uk.gov.communities.prsdb.webapp.models.viewModels.summaryModels.SummaryListRowViewModel
 
 @JourneyFrameworkComponent
-class UpdateLeadTrusteeCyaStepConfig : AbstractRequestableStepConfig<Complete, NoInputFormModel, UpdateLeadTrusteeJourneyState>() {
-    override val formModelClass = NoInputFormModel::class
+class UpdateLeadTrusteeCyaStepConfig : AbstractCheckYourAnswersStepConfig<UpdateLeadTrusteeJourneyState>() {
+    override fun chooseTemplate(state: UpdateLeadTrusteeJourneyState) = "forms/checkLeadTrusteeAnswersForm"
 
-    override fun getStepSpecificContent(state: UpdateLeadTrusteeJourneyState) =
-        mapOf("todoComment" to "TODO: PDJB-1470 - Lead trustee check your answers page")
+    override fun getStepSpecificContent(state: UpdateLeadTrusteeJourneyState): Map<String, Any?> =
+        mapOf(
+            "title" to "landlordDetails.update.title",
+            "showWarning" to true,
+            "submitButtonText" to "forms.buttons.confirmAndSubmitUpdate",
+            "leadTrusteeCard" to getLeadTrusteeCard(state),
+        )
 
-    override fun chooseTemplate(state: UpdateLeadTrusteeJourneyState) = "forms/todo"
+    override fun resolveNextDestination(
+        state: UpdateLeadTrusteeJourneyState,
+        defaultDestination: Destination,
+    ): Destination = defaultDestination
 
-    override fun mode(state: UpdateLeadTrusteeJourneyState) = getFormModelFromStateOrNull(state)?.let { Complete.COMPLETE }
+    private fun getLeadTrusteeCard(state: UpdateLeadTrusteeJourneyState): SummaryCardViewModel {
+        val leadTrusteeTask = state.leadTrusteeTask
+        return SummaryCardViewModel(
+            title = "landlordDetails.org.leadTrusteeHeading",
+            summaryList =
+                listOf(
+                    SummaryListRowViewModel.forCheckYourAnswersPage(
+                        "landlordDetails.org.leadTrusteeName",
+                        leadTrusteeTask.leadTrusteeNameStep.formModel.notNullValue(LeadTrusteeNameFormModel::name),
+                        Destination.VisitableStep(
+                            leadTrusteeTask.leadTrusteeNameStep,
+                            state.getCyaJourneyId(leadTrusteeTask.leadTrusteeNameStep),
+                        ),
+                    ),
+                    SummaryListRowViewModel.forCheckYourAnswersPage(
+                        "landlordDetails.org.leadTrusteeDateOfBirth",
+                        leadTrusteeTask.leadTrusteeDobStep.formModel.toLocalDateOrNull(),
+                        Destination.VisitableStep(
+                            leadTrusteeTask.leadTrusteeDobStep,
+                            state.getCyaJourneyId(leadTrusteeTask.leadTrusteeDobStep),
+                        ),
+                    ),
+                    SummaryListRowViewModel.forCheckYourAnswersPage(
+                        "landlordDetails.org.leadTrusteeEmail",
+                        leadTrusteeTask.leadTrusteeEmailStep.formModel.notNullValue(LeadTrusteeEmailFormModel::emailAddress),
+                        Destination.VisitableStep(
+                            leadTrusteeTask.leadTrusteeEmailStep,
+                            state.getCyaJourneyId(leadTrusteeTask.leadTrusteeEmailStep),
+                        ),
+                    ),
+                    SummaryListRowViewModel.forCheckYourAnswersPage(
+                        "landlordDetails.org.leadTrusteePhone",
+                        leadTrusteeTask.leadTrusteePhoneStep.formModel.notNullValue(LeadTrusteePhoneFormModel::phoneNumber),
+                        Destination.VisitableStep(
+                            leadTrusteeTask.leadTrusteePhoneStep,
+                            state.getCyaJourneyId(leadTrusteeTask.leadTrusteePhoneStep),
+                        ),
+                    ),
+                    SummaryListRowViewModel.forCheckYourAnswersPage(
+                        "landlordDetails.org.leadTrusteeAddress",
+                        leadTrusteeTask.trusteeAddressTask
+                            .getAddress()
+                            .toMultiLineAddress()
+                            .split("\n"),
+                        Destination.VisitableStep(
+                            leadTrusteeTask.trusteeAddressTask.lookupAddressStep,
+                            state.getCyaJourneyId(leadTrusteeTask.trusteeAddressTask.lookupAddressStep),
+                        ),
+                    ),
+                ),
+        )
+    }
 }
 
 @JourneyFrameworkComponent
 final class UpdateLeadTrusteeCyaStep(
     stepConfig: UpdateLeadTrusteeCyaStepConfig,
-) : RequestableStep<Complete, NoInputFormModel, UpdateLeadTrusteeJourneyState>(stepConfig) {
+) : AbstractCheckYourAnswersStep<UpdateLeadTrusteeJourneyState>(stepConfig) {
     companion object {
         const val ROUTE_SEGMENT = "lead-trustee-check-your-answers"
     }
