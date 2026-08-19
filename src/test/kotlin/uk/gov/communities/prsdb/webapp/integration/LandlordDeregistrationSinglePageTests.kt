9203e05917ad6c1a35d9969e08aac6d12bf2a4da
Danielle Dias <danielle.dias@madetech.com>
2026-08-19 09:34:17 +0100

    PDJB-1249 - Update tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDeregistrationSinglePageTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LandlordDeregistrationSinglePageTests.kt
@@ -1,6 +1,7 @@
 package uk.gov.communities.prsdb.webapp.integration
 
 import com.microsoft.playwright.Page
+import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat as playwrightAssertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.communities.prsdb.webapp.integration.IntegrationTestWithImmutableData.NestedIntegrationTestWithImmutableData
@@ -31,6 +32,13 @@ class LandlordDeregistrationSinglePageTests : IntegrationTest() {
     @Nested
     inner class LandlordWithoutProperties : NestedIntegrationTestWithImmutableData("data-unverified-landlord.sql") {
         @Test
+        fun `User is returned to the landlord details page if they submit No`(page: Page) {
+            val areYouSurePage = navigator.goToLandlordDeregistrationAreYouSurePage()
+            areYouSurePage.submitDoesNotWantToProceed()
+            BasePage.assertPageIs(page, LandlordDetailsPage::class)
+        }
+
+        @Test
         fun `User is returned to the landlord details page if they click the cancel link`(page: Page) {
             val areYouSurePage = navigator.goToLandlordDeregistrationAreYouSurePage()
             areYouSurePage.noPropertiesCancelLink.clickAndWait()
@@ -42,6 +50,14 @@ class LandlordDeregistrationSinglePageTests : IntegrationTest() {
             val areYouSurePage = navigator.goToLandlordDeregistrationAreYouSurePage()
             areYouSurePage.backLink.clickAndWait()
             BasePage.assertPageIs(page, LandlordDetailsPage::class)
+        }
+
+        @Test
+        fun `Submitting with no option selected returns an error`() {
+            val areYouSurePage = navigator.goToLandlordDeregistrationAreYouSurePage()
+            areYouSurePage.form.submit()
+            playwrightAssertThat(areYouSurePage.form.getErrorMessage("wantsToProceed"))
+                .containsText("Select whether you want to delete your account")
         }
 
         @Test
