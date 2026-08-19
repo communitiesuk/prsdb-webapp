c85aaab501b29e422d34b05b29e2e0a4c9c9bde1
Samuel Young <samuel.young@softwire.com>
2026-08-19 14:47:37 +0000

    PDJB-1409: Letting agent can provide details (#1776)

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/delegateToLettingAgent/stepConfig/AllowLettingAgentStepConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/delegateToLettingAgent/stepConfig/AllowLettingAgentStepConfig.kt
@@ -2,32 +2,37 @@ package uk.gov.communities.prsdb.webapp.journeys.delegateToLettingAgent.stepConf
 
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.JourneyFrameworkComponent
 import uk.gov.communities.prsdb.webapp.journeys.AbstractRequestableStepConfig
+import uk.gov.communities.prsdb.webapp.journeys.FormData
 import uk.gov.communities.prsdb.webapp.journeys.JourneyStep.RequestableStep
 import uk.gov.communities.prsdb.webapp.journeys.delegateToLettingAgent.DelegateToLettingAgentJourneyState
 import uk.gov.communities.prsdb.webapp.journeys.shared.Complete
-import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.NoInputFormModel
+import uk.gov.communities.prsdb.webapp.models.requestModels.formModels.AllowLettingAgentEmailFormModel
+import uk.gov.communities.prsdb.webapp.services.UserToLandlordService
 
 @JourneyFrameworkComponent("delegateToLettingAgentAllowLettingAgentStepConfig")
-class AllowLettingAgentStepConfig :
-    AbstractRequestableStepConfig<Complete, NoInputFormModel, DelegateToLettingAgentJourneyState>() {
-    override val formModelClass = NoInputFormModel::class
+class AllowLettingAgentStepConfig(
+    private val userToLandlordService: UserToLandlordService,
+) : AbstractRequestableStepConfig<Complete, AllowLettingAgentEmailFormModel, DelegateToLettingAgentJourneyState>() {
+    override val formModelClass = AllowLettingAgentEmailFormModel::class
 
-    override fun getStepSpecificContent(state: DelegateToLettingAgentJourneyState) =
-        mapOf(
-            "todoComment" to
-                "TODO: PDJB-1409 - Allow your letting agent or property manager to provide details page " +
-                "(the real page's submit button must use the transactionSubmitButton fragment for metrics)",
-        )
+    override fun getStepSpecificContent(state: DelegateToLettingAgentJourneyState) = emptyMap<String, Any?>()
 
-    override fun chooseTemplate(state: DelegateToLettingAgentJourneyState) = "forms/todo"
+    override fun chooseTemplate(state: DelegateToLettingAgentJourneyState) = "forms/allowLettingAgentForm"
 
     override fun mode(state: DelegateToLettingAgentJourneyState): Complete? = getFormModelFromStateOrNull(state)?.let { Complete.COMPLETE }
+
+    override fun enrichSubmittedDataBeforeValidation(
+        state: DelegateToLettingAgentJourneyState,
+        formData: FormData,
+    ): FormData =
+        super.enrichSubmittedDataBeforeValidation(state, formData) +
+            (AllowLettingAgentEmailFormModel::landlordEmail.name to userToLandlordService.getCurrentLandlordForUser().email)
 }
 
 @JourneyFrameworkComponent("delegateToLettingAgentAllowLettingAgentStep")
 final class AllowLettingAgentStep(
     stepConfig: AllowLettingAgentStepConfig,
-) : RequestableStep<Complete, NoInputFormModel, DelegateToLettingAgentJourneyState>(stepConfig) {
+) : RequestableStep<Complete, AllowLettingAgentEmailFormModel, DelegateToLettingAgentJourneyState>(stepConfig) {
     companion object {
         const val ROUTE_SEGMENT = "allow-letting-agent"
     }
