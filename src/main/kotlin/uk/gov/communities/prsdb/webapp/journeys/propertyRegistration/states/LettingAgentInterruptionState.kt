4b44c352f96d8eb97458e34b1e983777bb3fdbe2
Bill Haigh <bill.haigh@softwire.com>
2026-08-24 15:00:40 +0100

    PDJB-1417: Remove redundant comment from LettingAgentInterruptionState

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/states/LettingAgentInterruptionState.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/states/LettingAgentInterruptionState.kt
@@ -6,10 +6,5 @@ interface LettingAgentInterruptionState : JourneyState {
     val wasOccupied: Boolean
     val isDelegatedToLettingAgent: Boolean
 
-    /**
-     * The letting agent interruption is only shown when the property was occupied at the start of the journey and is
-     * delegated to a letting agent - otherwise the "you've changed this property to being unoccupied" copy would not
-     * be true.
-     */
     val showsLettingAgentInterruption: Boolean get() = wasOccupied && isDelegatedToLettingAgent
 }
