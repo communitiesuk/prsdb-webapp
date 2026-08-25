bef0295b7e995b4e4804348c5a5d55aed6f8f748
Travis Woodward <Travis.Woodward@softwire.com>
2026-08-25 16:02:59 +0100

    PDJB-924: Preload and cache GDS Transport fonts to prevent layout shift

---
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/FontCacheConfig.kt
@@ -0,0 +1,18 @@
+package uk.gov.communities.prsdb.webapp.config
+
+import org.springframework.http.CacheControl
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebConfiguration
+import uk.gov.communities.prsdb.webapp.constants.ASSETS_PATH_SEGMENT
+import java.time.Duration
+
+@PrsdbWebConfiguration
+class FontCacheConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry
+            .addResourceHandler("/$ASSETS_PATH_SEGMENT/fonts/**")
+            .addResourceLocations("classpath:/static/$ASSETS_PATH_SEGMENT/fonts/")
+            .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)).cachePublic().immutable())
+    }
+}
