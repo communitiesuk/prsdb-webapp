cf36bb02e55592c398aa2e106d6817ba87162877
Tom Hanmer <tom.hanmer@softwire.com>
2026-09-01 15:31:42 +0100

    PDJB-1658: add has password step

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/config/security/LettingAgentSecurityConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/config/security/LettingAgentSecurityConfig.kt
@@ -17,7 +17,6 @@ import uk.gov.communities.prsdb.webapp.constants.LETTING_AGENT_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.LettingAgentInvitationController.Companion.LETTING_AGENT_INVITATION_ROUTE
 import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.ConfirmationStep
 import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.EnterPasswordStep
-import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.HasPasswordStep
 import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.SetPasswordStep
 import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.StartStep
 import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.StoreAccessStep
@@ -42,10 +41,6 @@ class LettingAgentSecurityConfig {
                     .requestMatchers(
                         // TODO: PDJB-1660: Remove when validate token step becomes an internal step
                         "$LETTING_AGENT_INVITATION_ROUTE/${ValidateTokenStep.ROUTE_SEGMENT}",
-                    ).anonymous()
-                    .requestMatchers(
-                        // TODO: PDJB-1658: Remove when check password set step becomes an internal step
-                        "$LETTING_AGENT_INVITATION_ROUTE/${HasPasswordStep.ROUTE_SEGMENT}",
                     ).anonymous()
                     .requestMatchers(
                         "$LETTING_AGENT_INVITATION_ROUTE/${SetPasswordStep.ROUTE_SEGMENT}",
