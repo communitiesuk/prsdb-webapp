8dc8842316e763f11c15f606348cc140cd259dbc
Danielle Dias <danielle.dias@madetech.com>
2026-09-03 15:59:21 +0100

    PDJB-1601: Restructured CYA displays property address on multiple lines

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/steps/PropertyRegistrationCyaStepConfig.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/steps/PropertyRegistrationCyaStepConfig.kt
@@ -287,12 +287,20 @@ class PropertyRegistrationCyaStepConfig(
         )
 
     private fun getPropertyDetailsSummaryList(state: PropertyRegistrationJourneyState) =
-        getAddressRows(state, "forms.checkPropertyAnswers.propertyDetails.address") +
+        getAddressRows(
+            state,
+            "forms.checkPropertyAnswers.propertyDetails.address",
+            multiLineAddress = false,
+        ) +
             getPropertyTypeRow(state) +
             getOwnershipTypeRow(state, "forms.checkPropertyAnswers.propertyDetails.ownership")
 
     private fun getRestructuredPropertyDetailsSummaryList(state: PropertyRegistrationJourneyState) =
-        getAddressRows(state, "propertyDetails.propertyRecord.propertyDetails.address") +
+        getAddressRows(
+            state,
+            "propertyDetails.propertyRecord.propertyDetails.address",
+            multiLineAddress = true,
+        ) +
             getPropertyTypeRow(state) +
             getBedroomsRow(state)
 
@@ -306,11 +314,12 @@ class PropertyRegistrationCyaStepConfig(
     private fun getAddressRows(
         state: PropertyRegistrationJourneyState,
         addressHeadingKey: String,
+        multiLineAddress: Boolean,
     ) = state.propertyDetailsTask.addressTask.getAddress().let { address ->
         listOf(
             SummaryListRowViewModel.forCheckYourAnswersPage(
                 addressHeadingKey,
-                address.singleLineAddress,
+                if (multiLineAddress) address.toMultiLineAddress().split("\n") else address.singleLineAddress,
                 Destination.VisitableStep(
                     state.propertyDetailsTask.addressTask.lookupAddressStep,
                     state.getCyaJourneyId(state.propertyDetailsTask.addressTask.lookupAddressStep),
