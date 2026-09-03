9ed8b776e08eee19f347149e96c5b4951c4a2497
JasCon <jasmin.conterio@softwire.com>
2026-09-03 15:34:37 +0100

    PDJB-1570: Remove unnecessary comment

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LettingAgentPropertyDetailsController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LettingAgentPropertyDetailsController.kt
@@ -73,8 +73,7 @@ class LettingAgentPropertyDetailsController(
                 propertyOwnershipId = propertyOwnershipId,
             ),
         )
-        // TODO PDJB-1567: This page currently has no back link, as it is the closest thing a letting agent
-        //  has to a dashboard. A back link (e.g. to the letting-agent confirmation page) may be added here later.
+
         return LETTING_AGENT_PROPERTY_DETAILS_VIEW
     }
 
