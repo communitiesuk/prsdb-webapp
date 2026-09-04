dffbd916b187681695ca279d750ea05e5d94d3a1
Bill Haigh <bill.haigh@softwire.com>
2026-09-04 12:04:17 +0000

    PDJB-1568: Add enter password page for letting agent property delegation (#1844)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentInvitationJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentInvitationJourneyTests.kt
@@ -2,10 +2,7 @@ package uk.gov.communities.prsdb.webapp.integration
 
 import com.microsoft.playwright.Page
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.crypto.password.PasswordEncoder
 import uk.gov.communities.prsdb.webapp.constants.DELEGATE_TO_LETTING_AGENT
-import uk.gov.communities.prsdb.webapp.database.repository.LettingAgentAccessRepository
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage.Companion.assertPageIs
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentInvitationJourneyPages.EnterPasswordPage
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentInvitationJourneyPages.PasswordCreationConfirmationPage
@@ -14,14 +11,11 @@ import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgen
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentInvitationJourneyPages.ValidateTokenPage
 
 class LettingAgentInvitationJourneyTests : IntegrationTestWithMutableData("data-local.sql") {
-    @Autowired
-    private lateinit var lettingAgentAccessRepository: LettingAgentAccessRepository
-
-    @Autowired
-    private lateinit var passwordEncoder: PasswordEncoder
-
     private val tokenWithoutPassword = "3334abcd-5678-abcd-1234-567abcd1111a"
+
     private val tokenWithPassword = "3334abcd-5678-abcd-1234-567abcd2222b"
+
+    private val seededPassword = "Password123!" // pragma: allowlist secret
 
     @Test
     fun `user who does not have a password can walk the set password journey`(page: Page) {
@@ -32,7 +26,7 @@ class LettingAgentInvitationJourneyTests : IntegrationTestWithMutableData("data-
         assertPageIs(page, ValidateTokenPage::class)
         validateTokenPage.form.submit()
 
-        val rawPassword = "password1"
+        val rawPassword = "password1" // pragma: allowlist secret
         val setPasswordPage = assertPageIs(page, SetPasswordPage::class)
         setPasswordPage.submitPasswords(rawPassword, rawPassword)
 
@@ -56,9 +50,8 @@ class LettingAgentInvitationJourneyTests : IntegrationTestWithMutableData("data-
         assertPageIs(page, ValidateTokenPage::class)
         validateTokenPage.form.submit()
 
-        // TODO PDJB-1568: Update when enter password page is implemented
         val enterPasswordPage = assertPageIs(page, EnterPasswordPage::class)
-        enterPasswordPage.form.submit()
+        enterPasswordPage.submitPassword(seededPassword)
 
         // TODO PDJB-1659: Remove this step from the journey test
         val storeAccessPage = assertPageIs(page, StoreAccessPage::class)
