9e9a640c469f0902cbd8fc80bffdd46cfd0417ff
Tom Hanmer <tom.hanmer@softwire.com>
2026-09-07 16:51:12 +0100

    PDJB-1574: remove superfluous TODO

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/summaryModels/LettingAgentPropertyDetailsViewModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/viewModels/summaryModels/LettingAgentPropertyDetailsViewModel.kt
@@ -34,10 +34,6 @@ class LettingAgentPropertyDetailsViewModel(
     val provideDetailsInsetText: String =
         getProvideLaterDeadlineText("propertyDetails.lettingAgentView.provideDetailsInset")
 
-    // TODO PDJB-1571, PDJB-1572, PDJB-1573, PDJB-1575, PDJB-1576: letting agents will get change links on
-    //  these rows (pointing at letting-agent update journeys) once those journeys are built. The shared section
-    //  builders show a letting-agent change link on a row only once rowWithViewTypeSpecificChangeLink is given a lettingAgentActionLink
-    //  for it; until then the rows render without links (see PropertyDetailsViewModelBase).
     val licensingSection: List<SummaryListRowViewModel> = buildLicensingSection()
 
     val tenancySection: List<SummaryListRowViewModel> = buildTenancySection()
