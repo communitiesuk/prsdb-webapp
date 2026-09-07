deb238754e97d8f7a154541f312a281d396b93c8
samyou-softwire <108681823+samyou-softwire@users.noreply.github.com>
2026-09-07 10:57:55 +0100

    PDJB-1574: add other user controller tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/LettingAgentUpdateRentIncludesBillsControllerTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/controllers/LettingAgentUpdateRentIncludesBillsControllerTests.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.get
@@ -126,6 +127,31 @@ class LettingAgentUpdateRentIncludesBillsControllerTests(
 
         mvc.get(updateStepRoute).andExpect {
             status { isNotFound() }
+        }
+    }
+
+    @Test
+    @WithMockUser(roles = ["LANDLORD"], username = "landlord-user")
+    fun `getUpdateStep returns 403 when a landlord is logged in`() {
+        mvc.get(updateStepRoute).andExpect {
+            status { isForbidden() }
+        }
+    }
+
+    @Test
+    @WithMockUser(username = "letting-agent-user")
+    fun `getUpdateStep returns 403 when a letting agent with access to a different property is logged in`() {
+        // TODO: PDJB-1683: Ensure this test works & checks the accesses in the session correctly
+        mvc.get(updateStepRoute).andExpect {
+            status { isForbidden() }
+        }
+    }
+
+    @Test
+    @WithMockUser(roles = ["LOCAL_COUNCIL_USER"], username = "council-user")
+    fun `getUpdateStep returns 403 when a local council user is logged in`() {
+        mvc.get(updateStepRoute).andExpect {
+            status { isForbidden() }
         }
     }
 
