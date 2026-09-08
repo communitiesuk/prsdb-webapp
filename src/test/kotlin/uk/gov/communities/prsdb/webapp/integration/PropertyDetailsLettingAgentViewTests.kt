f938ad999af0abcf9a99ef4645fc4d5447b6cade
Jasmin Conterio <jasmin.conterio@communities.gov.uk>
2026-09-08 08:35:37 +0000

    PDJB-1570: Show gas provide-later on delegated property details (#1863)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyDetailsLettingAgentViewTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyDetailsLettingAgentViewTests.kt
@@ -41,6 +41,20 @@ class PropertyDetailsLettingAgentViewTests : IntegrationTestWithImmutableData("d
     }
 
     @Test
+    fun `when all details are delegated every outstanding field shows provide-later text`(page: Page) {
+        val detailsPage = navigator.goToPropertyDetailsLettingAgentView(allDetailsDelegatedToken)
+
+        assertThat(detailsPage.provideDetailsInset).containsText("Provide all details")
+        assertThat(detailsPage.summaryList.licensingRow.value).containsText("Provide this later")
+        assertThat(detailsPage.summaryList.tenancyRow.value).containsText("Provide this later")
+        // TODO PDJB-1665: the "Does the property have a gas supply..." row should be removed for gas "provide this
+        //  later"; update/remove the hasGasSupply placeholder assertions here when that row is hidden.
+        assertThat(detailsPage.gasSafetyCard.summaryList.hasCertRow.value).containsText("Provide this later")
+        assertThat(detailsPage.electricalSafetyCard).containsText("Provide this later")
+        assertThat(detailsPage.epcCard).containsText("Provide this later")
+    }
+
+    @Test
     fun `when only licensing and tenancy are outstanding the inset is shown and compliance is valid`(page: Page) {
         val detailsPage = navigator.goToPropertyDetailsLettingAgentView(licensingAndTenancyOutstandingToken)
 
