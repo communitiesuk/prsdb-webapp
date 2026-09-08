95c73ad4eeadad3d08a31c3ebaf60e263a6d92a6
Tom Hanmer <tom.hanmer@softwire.com>
2026-09-08 11:42:03 +0100

    PDJB-1573: refactor controllers to commonise the bulk of them and fix TODO

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LettingAgentUpdateTenancyDetailsController.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/controllers/LettingAgentUpdateTenancyDetailsController.kt
@@ -1,23 +1,12 @@
 package uk.gov.communities.prsdb.webapp.controllers
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.server.ResponseStatusException
-import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.util.UriTemplate
-import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.AvailableWhenFeatureEnabled
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbController
-import uk.gov.communities.prsdb.webapp.constants.DELEGATE_TO_LETTING_AGENT
 import uk.gov.communities.prsdb.webapp.constants.LANDLORD_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.LETTING_AGENT_PATH_SEGMENT
 import uk.gov.communities.prsdb.webapp.constants.PROPERTY_DETAILS_SEGMENT
 import uk.gov.communities.prsdb.webapp.controllers.LettingAgentUpdateTenancyDetailsController.Companion.LETTING_AGENT_UPDATE_TENANCY_DETAILS_ROUTE
-import uk.gov.communities.prsdb.webapp.journeys.FormData
-import uk.gov.communities.prsdb.webapp.journeys.JourneyStepDispatcher
 import uk.gov.communities.prsdb.webapp.journeys.StepLifecycleOrchestrator
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.update.tenancyDetails.UpdateTenancyDetailsJourneyFactory
 import uk.gov.communities.prsdb.webapp.services.LettingAgentAccessService
@@ -28,44 +17,15 @@ import java.util.UUID
 @RequestMapping(LETTING_AGENT_UPDATE_TENANCY_DETAILS_ROUTE)
 class LettingAgentUpdateTenancyDetailsController(
     private val journeyFactory: UpdateTenancyDetailsJourneyFactory,
-    private val lettingAgentAccessService: LettingAgentAccessService,
-    private val propertyOwnershipService: PropertyOwnershipService,
-) {
-    @AvailableWhenFeatureEnabled(DELEGATE_TO_LETTING_AGENT)
-    @GetMapping("/{*stepPath}")
-    fun getUpdateStep(
-        @PathVariable token: UUID,
-        @PathVariable stepPath: String,
-    ): ModelAndView = dispatchJourneyStep(stepPath, token) { getStepModelAndView() }
+    lettingAgentAccessService: LettingAgentAccessService,
+    propertyOwnershipService: PropertyOwnershipService,
+) : AbstractLettingAgentUpdateController(lettingAgentAccessService, propertyOwnershipService) {
+    override fun createJourneySteps(
+        propertyOwnershipId: Long,
+        returnUrl: String,
+    ): Map<String, StepLifecycleOrchestrator> = journeyFactory.createJourneySteps(propertyOwnershipId, returnUrl)
 
-    @AvailableWhenFeatureEnabled(DELEGATE_TO_LETTING_AGENT)
-    @PostMapping("/{*stepPath}")
-    fun postUpdateStep(
-        @PathVariable token: UUID,
-        @PathVariable stepPath: String,
-        @RequestParam formData: FormData,
-    ): ModelAndView = dispatchJourneyStep(stepPath, token) { postStepModelAndView(formData) }
-
-    private fun dispatchJourneyStep(
-        stepPath: String,
-        token: UUID,
-        dispatch: StepLifecycleOrchestrator.() -> ModelAndView,
-    ): ModelAndView {
-        val propertyOwnershipId =
-            lettingAgentAccessService.getInvitationByTokenOrNull(token)?.propertyOwnership?.id
-                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "No letting agent access found for token $token")
-
-        propertyOwnershipService.throwIfCurrentUserNotAuthorizedToEdit(propertyOwnershipId)
-
-        val returnUrl = LettingAgentPropertyDetailsController.getLettingAgentPropertyDetailsPath(token)
-
-        return JourneyStepDispatcher.handleInitialisableRequest(
-            rawStepPath = stepPath,
-            createRoutingMap = { journeyFactory.createJourneySteps(propertyOwnershipId, returnUrl) },
-            initialiseJourney = { journeyFactory.initialiseJourneyState(token) },
-            dispatch = dispatch,
-        )
-    }
+    override fun initialiseJourneyState(token: UUID): String = journeyFactory.initialiseJourneyState(token)
 
     companion object {
         const val LETTING_AGENT_UPDATE_TENANCY_DETAILS_ROUTE =
