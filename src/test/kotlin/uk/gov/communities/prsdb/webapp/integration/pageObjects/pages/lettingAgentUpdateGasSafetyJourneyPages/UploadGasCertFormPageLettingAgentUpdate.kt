430f621f7e1c8c4a9a22012565c1825ecde69afb
Ben Johnson <157490728+bnjn-mt@users.noreply.github.com>
2026-09-11 11:51:50 +0000

    PDJB-1577: LetA gas safety update journey (#1875)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/lettingAgentUpdateGasSafetyJourneyPages/UploadGasCertFormPageLettingAgentUpdate.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/lettingAgentUpdateGasSafetyJourneyPages/UploadGasCertFormPageLettingAgentUpdate.kt
@@ -1,16 +1,16 @@
-package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.updateGasSafetyJourneyPages
+package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentUpdateGasSafetyJourneyPages
 
 import com.microsoft.playwright.Page
-import uk.gov.communities.prsdb.webapp.controllers.LandlordUpdateGasSafetyController
+import uk.gov.communities.prsdb.webapp.controllers.LettingAgentUpdateGasSafetyController
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.UploadCertFormBasePage
 import uk.gov.communities.prsdb.webapp.journeys.propertyRegistration.steps.UploadGasCertStep
+import java.util.UUID
 
-class UploadGasCertFormPageUpdateGasSafety(
+class UploadGasCertFormPageLettingAgentUpdate(
     page: Page,
     urlArguments: Map<String, String>,
 ) : UploadCertFormBasePage(
         page,
-        LandlordUpdateGasSafetyController.UPDATE_GAS_SAFETY_ROUTE
-            .replace("{propertyOwnershipId}", urlArguments["propertyOwnershipId"]!!) +
+        LettingAgentUpdateGasSafetyController.getUpdateGasSafetyRoute(UUID.fromString(urlArguments["token"]!!)) +
             "/${UploadGasCertStep.ROUTE_SEGMENT}",
     )
