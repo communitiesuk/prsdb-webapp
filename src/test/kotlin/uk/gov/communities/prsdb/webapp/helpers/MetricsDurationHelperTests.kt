0b4f25907583a173ad9e44b1b6980423a7a8e178
Bill Haigh <bill.haigh@softwire.com>
2026-09-14 16:54:05 +0100

    PDJB-NONE: Intentionally fail a unit test for QA

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/helpers/MetricsDurationHelperTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/helpers/MetricsDurationHelperTests.kt
@@ -7,6 +7,7 @@ import java.time.Duration
 import java.util.Locale
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class MetricsDurationHelperTests {
     // Echoes back "<amount> <unit>" so assertions can verify which key (singular vs plural) and
@@ -42,6 +43,7 @@ class MetricsDurationHelperTests {
         val duration = Duration.ofDays(1).plusHours(6).plusMinutes(22)
 
         assertEquals("1 day, 6 hours, 22 minutes", formatDuration(duration, messageSource))
+        fail("Intentional failure for QA: remove after verifying test failure reporting.")
     }
 
     @Test
