90d91d8fe9ad12740b75261e82f7ceaa54d8e8cd
bnjn-mt <benjamin.johnson@madetech.com>
2026-09-14 17:13:00 +0100

    PDJB-1719: Fix missing hasCertIsValidForAction method causing gas cert form validation failures

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/HasGasCertFormModel.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/models/requestModels/formModels/HasGasCertFormModel.kt
@@ -1,5 +1,6 @@
 package uk.gov.communities.prsdb.webapp.models.requestModels.formModels
 
+import uk.gov.communities.prsdb.webapp.constants.PROVIDE_THIS_LATER_BUTTON_ACTION_NAME
 import uk.gov.communities.prsdb.webapp.validation.ConstraintDescriptor
 import uk.gov.communities.prsdb.webapp.validation.DelegatedPropertyConstraintValidator
 import uk.gov.communities.prsdb.webapp.validation.IsValidPrioritised
@@ -19,4 +20,6 @@ class HasGasCertFormModel : FormModel {
     var hasCert: Boolean? = null
 
     var action: String? = null
+
+    fun hasCertIsValidForAction(): Boolean = action == PROVIDE_THIS_LATER_BUTTON_ACTION_NAME || hasCert != null
 }
