5e02d4f2c57c7a1668cd69b09e0304505a3c6f3e
Bill Haigh <bill.haigh@softwire.com>
2026-09-14 16:45:47 +0100

    PDJB-NONE: Restore compliance update CYA button labels

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentUpdateEpcJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentUpdateEpcJourneyTests.kt
@@ -118,7 +118,7 @@ class LettingAgentUpdateEpcJourneyTests : IntegrationTestWithMutableData("data-l
 
     private fun saveAnswers(page: Page) {
         val checkAnswersPage = assertPageIs(page, CheckEpcAnswersFormPageLettingAgentUpdateEpc::class, urlArguments)
-        assertThatComponent(checkAnswersPage.form.submitButton).hasText("Continue")
+        assertThatComponent(checkAnswersPage.form.submitButton).hasText("Save and continue")
         checkAnswersPage.form.submit()
     }
 
