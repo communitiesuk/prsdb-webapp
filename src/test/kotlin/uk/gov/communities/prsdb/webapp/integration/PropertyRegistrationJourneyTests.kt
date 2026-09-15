82dae49a588dc8b37c10af66ddd8da857b142795
Tom Hanmer <tom.hanmer@softwire.com>
2026-09-15 15:52:17 +0100

    PDJB-392: fix test

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyRegistrationJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyRegistrationJourneyTests.kt
@@ -2065,8 +2065,21 @@ class PropertyRegistrationJourneyTests : IntegrationTestWithMutableData("data-lo
 
         @Test
         fun `registering a property sets the registering landlord's anniversary to the registration date when it is null`(page: Page) {
-            val checkAnswersPage = navigator.skipToPropertyRegistrationCheckAnswersPageOccupied()
+            val taskListPage =
+                navigator.goToRestructuredPropertyRegistrationTaskList(
+                    PropertyStateSessionBuilder
+                        .beforePropertyRegistrationCheckAnswersOccupied()
+                        .withBedrooms(),
+                )
+            taskListPage.clickSubmitYourRegistrationTaskWithName("Check and submit your answers")
+            val checkAnswersPage = assertPageIs(page, CheckAnswersPagePropertyRegistration::class)
             checkAnswersPage.confirm()
+
+            val confirmMissingCompliancePage =
+                assertPageIs(page, ConfirmMissingComplianceFormPagePropertyRegistration::class)
+            confirmMissingCompliancePage.form.radios.selectValue("true")
+            confirmMissingCompliancePage.form.submit()
+
             assertPageIs(page, ConfirmationPagePropertyRegistration::class)
 
             val propertyOwnershipCaptor = captor<PropertyOwnership>()
@@ -2075,10 +2088,16 @@ class PropertyRegistrationJourneyTests : IntegrationTestWithMutableData("data-lo
                 propertyOwnershipRepository.findByRegistrationNumber_Number(propertyOwnershipCaptor.value.registrationNumber.number)
                     ?: error("Property ownership was not saved")
 
-            val registeringLandlord = savedPropertyOwnership.landlords.single()
+            val anniversary =
+                jdbcTemplate.queryForMap(
+                    "SELECT l.anniversary_day, l.anniversary_month FROM landlord l" +
+                        " JOIN ownership_link ol ON ol.landlord_id = l.id" +
+                        " WHERE ol.landlordship_id = ?",
+                    savedPropertyOwnership.id,
+                )
             val registrationDate = DateTimeHelper().getCurrentDateInUK()
-            assertEquals(registrationDate.dayOfMonth, registeringLandlord.anniversaryDay)
-            assertEquals(registrationDate.monthNumber, registeringLandlord.anniversaryMonth)
+            assertEquals(registrationDate.dayOfMonth, anniversary["anniversary_day"])
+            assertEquals(registrationDate.monthNumber, anniversary["anniversary_month"])
         }
 
         // TODO PDJB-1022: Remove this nested class when the DELEGATE_TO_LETTING_AGENT feature flag is removed
