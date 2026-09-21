e28127ef0871523aa46357dcaf812ca0a40390ea
Danielle Dias <danielle.dias@madetech.com>
2026-09-21 17:38:00 +0100

    PDJB-1590: bind delegated task dependencies in CYA branch

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/PropertyRegistrationJourneyFactory.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/PropertyRegistrationJourneyFactory.kt
@@ -146,7 +146,7 @@ class PropertyRegistrationJourneyFactory(
 
                 LettingAgentEmailStep.ROUTE_SEGMENT -> {
                     if (featureFlagManager.checkFeature(DELEGATE_TO_LETTING_AGENT)) {
-                        fromTask(journey.whoProvidesDetailsTask) {
+                        fromTask(journey.whoProvidesDetailsTask, journey) {
                             checkAnswerStep(task.lettingAgentEmailStep, LettingAgentEmailStep.ROUTE_SEGMENT)
                         }
                     } else {
