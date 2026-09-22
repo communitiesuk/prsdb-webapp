4975d5e8749b6e0c4a4f70c160ac8f49a2320cd5
JasCon <jasmin.conterio@softwire.com>
2026-09-22 14:52:56 +0100

    PDJB-1602: Add missing-details section to property registration confirmation email

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/services/PropertyRegistrationService.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/services/PropertyRegistrationService.kt
@@ -3,6 +3,8 @@ package uk.gov.communities.prsdb.webapp.services
 import jakarta.persistence.EntityExistsException
 import jakarta.transaction.Transactional
 import uk.gov.communities.prsdb.webapp.annotations.webAnnotations.PrsdbWebService
+import uk.gov.communities.prsdb.webapp.config.managers.FeatureFlagManager
+import uk.gov.communities.prsdb.webapp.constants.PROPERTY_REGISTRATION_PHASE_TWO
 import uk.gov.communities.prsdb.webapp.constants.PROVIDE_LATER_DEADLINE_DAYS
 import uk.gov.communities.prsdb.webapp.constants.enums.CertificateType
 import uk.gov.communities.prsdb.webapp.constants.enums.EpcExemptionReason
@@ -38,6 +40,7 @@ class PropertyRegistrationService(
     private val propertyComplianceService: PropertyComplianceService,
     private val lettingAgentAccessService: LettingAgentAccessService,
     private val delegateToLettingAgentEmailService: DelegateToLettingAgentEmailService,
+    private val featureFlagManager: FeatureFlagManager,
 ) {
     @Transactional
     fun registerProperty(
@@ -143,7 +146,18 @@ class PropertyRegistrationService(
 
         confirmationService.setLastPrnRegisteredThisSession(propertyOwnership.registrationNumber.number)
 
-        sendConfirmationEmails(landlord, propertyOwnership, addressModel, jointLandlordEmails, isDelegatedToLettingAgent)
+        sendConfirmationEmails(
+            landlord,
+            propertyOwnership,
+            addressModel,
+            jointLandlordEmails,
+            isDelegatedToLettingAgent,
+            licenseProvideLater = licenseProvideLater,
+            gasSafetyCertProvideLater = gasSafetyCertProvideLater ?: false,
+            electricalSafetyCertProvideLater = electricalSafetyCertProvideLater ?: false,
+            epcProvideLater = epcProvideLater ?: false,
+            tenancyProvideLater = tenancyProvideLater ?: false,
+        )
     }
 
     private fun createPropertyOwnershipAndRelatedEntities(
@@ -210,6 +224,11 @@ class PropertyRegistrationService(
         addressModel: AddressDataModel,
         jointLandlordEmails: List<String>?,
         isDelegatedToLettingAgent: Boolean,
+        licenseProvideLater: Boolean,
+        gasSafetyCertProvideLater: Boolean,
+        electricalSafetyCertProvideLater: Boolean,
+        epcProvideLater: Boolean,
+        tenancyProvideLater: Boolean,
     ) {
         // TODO: PDJB-1274: Update emails to account for org landlord (check which org email address to use, currently registrant)
         confirmationEmailSender.sendEmail(
@@ -223,6 +242,12 @@ class PropertyRegistrationService(
                 propertyOwnership.isOccupied,
                 jointLandlordEmails,
                 isDelegatedToLettingAgent,
+                isPdjb939PhaseTwoEnabled = featureFlagManager.checkFeature(PROPERTY_REGISTRATION_PHASE_TWO),
+                licenseProvideLater = licenseProvideLater,
+                gasSafetyCertProvideLater = gasSafetyCertProvideLater,
+                electricalSafetyCertProvideLater = electricalSafetyCertProvideLater,
+                epcProvideLater = epcProvideLater,
+                tenancyProvideLater = tenancyProvideLater,
             ),
         )
 
--- src/main/resources/emails/PropertyRegistrationConfirmation.md
+++ src/main/resources/emails/PropertyRegistrationConfirmation.md
@@ -20,6 +20,14 @@ Make a note of the Property Registration Number.
 
 ((hasJointLandlords??These invitations will expire in 28 days.))
 
+((hasMissingDetails??## Providing missing details))
+
+((hasMissingDetails??You still need to provide the:))
+
+((missingDetailsList))
+
+((hasMissingDetails??To keep your property registered, provide these details within 28 days.))
+
 ((hasDelegatedToLettingAgent??## You sent a request to your letting agent or property manager))
 
 ((hasDelegatedToLettingAgent??You’ve asked your letting agent or property manager to provide the:))
