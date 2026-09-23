cf5dae5458e7703753743a6afbbeaf1f395463e0
Danielle Dias <danielle.dias@madetech.com>
2026-09-23 11:12:56 +0100

    PDJB-1590: revert change

--- src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/checkAnswersChangeJourneys/WhoProvidesChangeCyaJourney.kt
+++ src/main/kotlin/uk/gov/communities/prsdb/webapp/journeys/propertyRegistration/checkAnswersChangeJourneys/WhoProvidesChangeCyaJourney.kt
@@ -18,11 +18,6 @@ fun <T : PropertyRegistrationJourneyState> JourneyBuilder<T>.whoProvidesChangeCy
             routeSegment(WhoProvidesRentalDetailsStep.ROUTE_SEGMENT)
             nextStep { journey.whoProvidesUpdateRoutingStep }
         }
-        step(task.lettingAgentEmailStep) {
-            routeSegment(LettingAgentEmailStep.ROUTE_SEGMENT)
-            parents { journey.confirmChangeToLettingAgentStep.isComplete() }
-            nextStep { journey.finishCyaStep }
-        }
     }
     step<WhoProvidesUpdateRouteMode, WhoProvidesUpdateRoutingStepConfig>(journey.whoProvidesUpdateRoutingStep) {
         stepSpecificInitialisation {
@@ -43,5 +38,12 @@ fun <T : PropertyRegistrationJourneyState> JourneyBuilder<T>.whoProvidesChangeCy
             journey.whoProvidesUpdateRoutingStep.hasOutcome(WhoProvidesUpdateRouteMode.CHANGED_TO_LETTING_AGENT)
         }
         nextStep { journey.whoProvidesDetailsTask.lettingAgentEmailStep }
+    }
+    fromTask(journey.whoProvidesDetailsTask) {
+        step(task.lettingAgentEmailStep) {
+            routeSegment(LettingAgentEmailStep.ROUTE_SEGMENT)
+            parents { journey.confirmChangeToLettingAgentStep.isComplete() }
+            nextStep { journey.finishCyaStep }
+        }
     }
 }
