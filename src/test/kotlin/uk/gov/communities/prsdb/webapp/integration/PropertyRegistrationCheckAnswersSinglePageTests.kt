3f109186f82941a98354134b72e4e5062289ecd1
Samuel Young <samuel.young@softwire.com>
2026-09-24 16:21:16 +0000

    PDJB-1505: Fix headers on prop reg CYA (#1944)

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyRegistrationCheckAnswersSinglePageTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/PropertyRegistrationCheckAnswersSinglePageTests.kt
@@ -179,8 +179,7 @@ class PropertyRegistrationCheckAnswersSinglePageTests : IntegrationTestWithImmut
                         .withSubmittedValue(
                             LettingAgentEmailStep.ROUTE_SEGMENT,
                             AllowLettingAgentEmailFormModel().apply { emailAddress = "letting.agent@example.com" },
-                        )
-                        .withBedrooms(),
+                        ).withBedrooms(),
                 )
             taskListPage.clickSubmitYourRegistrationTaskWithName("Submit and pay")
             val checkAnswersPage = assertPageIs(page, CheckAnswersPagePropertyRegistration::class)
@@ -201,12 +200,20 @@ class PropertyRegistrationCheckAnswersSinglePageTests : IntegrationTestWithImmut
             BaseComponent.assertThat(checkAnswersPage.lettingAgentDelegationSubheading).isVisible()
             BaseComponent.assertThat(checkAnswersPage.lettingAgentDelegationBodyText).isVisible()
             assertThat(checkAnswersPage.summaryList.whoProvidesRentalDetailsRow.value).containsText("My letting agent or property manager")
-            BaseComponent.assertThat(checkAnswersPage.summaryList.whoProvidesRentalDetailsRow.actions.getActionLink("Change")).isVisible()
+            BaseComponent
+                .assertThat(
+                    checkAnswersPage.summaryList.whoProvidesRentalDetailsRow.actions
+                        .getActionLink("Change"),
+                ).isVisible()
             assertThat(
                 checkAnswersPage.summaryList.lettingAgentEmailRow.key,
             ).containsText("Letting agent or property manager’s email address")
             assertThat(checkAnswersPage.summaryList.lettingAgentEmailRow.value).containsText("letting.agent@example.com")
-            BaseComponent.assertThat(checkAnswersPage.summaryList.lettingAgentEmailRow.actions.getActionLink("Change")).isVisible()
+            BaseComponent
+                .assertThat(
+                    checkAnswersPage.summaryList.lettingAgentEmailRow.actions
+                        .getActionLink("Change"),
+                ).isVisible()
             checkAnswersPage.summaryList.lettingAgentEmailRow.clickFirstActionLinkAndWait()
             val emailPage = assertPageIs(page, LettingAgentEmailPagePropertyRegistration::class)
 
@@ -285,6 +292,14 @@ class PropertyRegistrationCheckAnswersSinglePageTests : IntegrationTestWithImmut
             BaseComponent.assertThat(checkAnswersPage.restructuredHeading).containsText("Check your answers for:")
             BaseComponent.assertThat(checkAnswersPage.aboutYourPropertyHeading).isVisible()
             BaseComponent.assertThat(checkAnswersPage.propertyDetailsHeading).isVisible()
+        }
+
+        @Test
+        fun `CYA page renders EPC summary card title as h4 to preserve heading hierarchy`(page: Page) {
+            val checkAnswersPage = navigator.skipToPropertyRegistrationCheckAnswersPageEpcExpiredInDateAtTenancyStart()
+
+            BaseComponent.assertThat(checkAnswersPage.epcCard.title).isVisible()
+            assertThat(page.locator("main h4.govuk-summary-card__title")).hasCount(1)
         }
 
         @Test
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/CheckAnswersPagePropertyRegistration.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/propertyRegistrationJourneyPages/CheckAnswersPagePropertyRegistration.kt
@@ -7,6 +7,7 @@ import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.FormWi
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Heading
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Paragraph
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.PostForm
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.SummaryCard
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.SummaryList
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.TicketPanel
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Warning
@@ -110,6 +111,8 @@ class CheckAnswersPagePropertyRegistration(
 
     val epcHeading =
         Heading(page.locator("h3", Page.LocatorOptions().setHasText("Energy performance certificate (EPC)")))
+
+    val epcCard = SummaryCard(page, "Your EPC", headingLevel = "h4")
 
     class CheckAnswersPropertyRegistrationSummaryList(
         page: Page,
